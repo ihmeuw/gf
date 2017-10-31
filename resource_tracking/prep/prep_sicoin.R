ec65d04854388f2f9403253543dad7244db8aa8d
Irena Chen <irenac2@ihme.washington.edu>
2017-10-31 12:29:47 -0700

    generalize code to every c-coin file

--- resource_tracking/prep/prep_sicoin.R
+++ resource_tracking/prep/prep_sicoin.R
@@ -1,8 +1,8 @@
 # ----------------------------------------------
-# David Phillips
+# Irena Chen
 #
-# 1/25/2017
-# Template for a typical self-contained analysis
+# 10/31/2017
+# Template for prepping C-COIN budget data 
 # ----------------------------------------------
 
 
@@ -20,10 +20,10 @@ library(readxl)
 # Files and directories
 
 # data directory
-dir <- 'J:/Project/Evaluation/GF/resource_tracking/gtm/ghe_s'
+dir <- 'C:/Users/irenac2/Documents/'
 
 # input file
-inFile <- paste0(dir, 'data_file.csv')
+inFile <- paste0(dir, '2013 MALARIA PRESUPUESTO POR ORGANISMO (departamento municipio).xls')
 
 # output files
 modelOutputFile <- paste0(dir, 'output.rdata')
@@ -34,31 +34,32 @@ graphFile <- paste0(dir, 'graphs.pdf')
 # ----------------------------------------------
 # Load/prep data
 
-# load
-gf_data <- read_excel("C:/Users/irenac2/Documents/2013 MALARIA PRESUPUESTO POR ORGANISMO (departamento municipio).xls",
-                         range='B247:AV302'
-)
+# load - figure out how to load 
+gf_data <- read_excel(inFile)
+
+## variable for the year 
+budget_year <- gf_data$X__8[13]
+
 ## remove empty columns 
 gf_data<- Filter(function(x)!all(is.na(x)), gf_data)
+
+
+##pull data between these two indices 
+gf_data <- gf_data[c(which(gf_data$X__10 %in% "FONDO MUNDIAL"):which(gf_data$X__6 %in% "0425  FONDO MUNDIAL")),]
 
 # remove rows with "TOTAL" (they are redundant from looking at the original file),
 gf_subset <- data.table(gf_data[ grep("TOTAL", gf_data$X__3, invert = TRUE) , ])
 
 # remove rows where X__9 has a value (they are redundant),
-gf_subset <- na.omit(gf_subset, cols="X__9")
+gf_subset <- na.omit(gf_subset, cols="X__10")
 # subset observations
 
 ## get region + budgeted expenses 
-
-budget_dataset <- gf_subset[, c("X__9", "X__14"), with=FALSE]
-names(budget_dataset) <- c("loc_id", "budgeted_amount")
-# format variables
+budget_dataset <- gf_subset[, c("X__9", "X__24"), with=FALSE]
+names(budget_dataset) <- c("loc_id", "devengado")
 
 
-# reshape data
 
-
-# make new variables
 
 # ----------------------------------------------
 
