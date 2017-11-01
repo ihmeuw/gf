1c3b073f526ba121955cc5aed46967b6ae0eeafc
Irena Chen <irenac2@ihme.washington.edu>
2017-11-01 16:00:01 -0700

    add master prep code for sicoin data

---
+++ resource_tracking/prep/master_prep_sicoin.R
@@ -0,0 +1,28 @@
+# ----------------------------------------------
+# Irena Chen
+#
+# 11/1/2017
+# Master prep code that runs all other functions
+# The current workign directory should be the same as this code
+# ----------------------------------------------
+
+
+# load the prep function
+source('./prep_sicoin.r')
+
+
+# load csv
+
+
+for()
+
+  tmpData <- prepSicoin()
+  
+  rbind
+
+}
+# data directory
+dir <- 'C:/Users/irenac2/Documents/'
+
+# input file
+inFile <- paste0(dir, '2013 MALARIA PRESUPUESTO POR ORGANISMO (departamento municipio).xls')
