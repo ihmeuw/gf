4903b5c5d8b9a7ae077736daad05294a1a3f960f
Irena Chen <irenac2@ihme.washington.edu>
2017-11-02 09:21:06 -0700

    define file names and years for all c_coin type budgets

--- resource_tracking/prep/master_prep_sicoin.R
+++ resource_tracking/prep/master_prep_sicoin.R
@@ -3,24 +3,44 @@
 #
 # 11/1/2017
 # Master prep code that runs all other functions
-# The current workign directory should be the same as this code
+# The current working directory should be the same as this code
 # ----------------------------------------------
+#define variables: 
 
+dir <- 'J:/Project/Evaluation/GF/resource_tracking/gtm/ghe_s/'
+period <-365
+cost_category <- "All"
 
+# ----------------------------------------------
 # load the prep function
-source('./prep_sicoin.r')
 
 
 # load csv
+file_list <- read.csv("C:/Users/irenac2/repos/gf/resource_tracking/prep/file_format_list.csv")
+
+source('./prep_sicoin.r')
+
+file_names <- file_list$filename[file_list$format=="c_coin"]
+file_years <- file_list$year[file_list$format=="c_coin"]
+
+inFile <- '2013 MALARIA PRESUPUESTO POR ORGANISMO (departamento municipio).xls'
 
 
-for()
+resource_database <- setNames(data.frame(matrix(ncol = 8, nrow = 0)), 
+                          c("source", "start_date", 
+                            "period","loc_id", "cost", 
+                            "budget","disbursement", "expenditures"))
 
-  tmpData <- prepSicoin()
+## loop over all of the files 
+for(i in 1:length(file_names))
+
+  tmpData <- prepSicoin(dir, file_names[i], file_years[i], period, cost_category)
   
-  rbind
+  resource_database = rbind(resource_database, tmpData)
 
 }
+
+
 # data directory
 dir <- 'C:/Users/irenac2/Documents/'
 
