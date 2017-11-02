b8124c5c865fa93e2e9ea916e927def6192d1f52
Irena Chen <irenac2@ihme.washington.edu>
2017-11-02 14:46:57 -0700

    clean up code to remove unnecessary comments and code parts

--- resource_tracking/prep/master_prep_sicoin.R
+++ resource_tracking/prep/master_prep_sicoin.R
@@ -31,13 +31,6 @@ source('./prep_sicoin.r')
 file_names <- as.character(file_list$filename[file_list$format=="c_coin"])
 file_years <- file_list$year[file_list$format=="c_coin"]
 
-
-# inFile <- '2013 MALARIA PRESUPUESTO POR ORGANISMO (departamento municipio)'
-
-# resource_database <- setNames(data.frame(matrix(ncol = 8, nrow = 0)), 
-  #                        c("loc_id","budget","disbursement", "source", 
- #                           "start_date","period", "cost", "expenditures"))
-
 ## loop over all of the files 
 for(i in 1:length(file_names)){
   tmpData <- prepSicoin(dir, file_names[i], file_years[i], period, cost_category, source)
@@ -47,13 +40,5 @@ for(i in 1:length(file_names)){
   }
   if(i>1){
     resource_database = rbind(resource_database, tmpData, use.names=TRUE)
-    }
-  ## trouble shoot this area b/c of error issues w/out
+  }
 }
-
-
-# data directory
-dir <- 'C:/Users/irenac2/Documents/'
-
-# input file
-inFile <- paste0(dir, '2013 MALARIA PRESUPUESTO POR ORGANISMO (departamento municipio).xls')
