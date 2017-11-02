494ddcd1cffc15e2d5425dc01d42584001df5f05
Irena Chen <irenac2@ihme.washington.edu>
2017-11-02 09:51:30 -0700

    test the master function for bugs

--- resource_tracking/prep/master_prep_sicoin.R
+++ resource_tracking/prep/master_prep_sicoin.R
@@ -5,39 +5,44 @@
 # Master prep code that runs all other functions
 # The current working directory should be the same as this code
 # ----------------------------------------------
+
+# Set up R
+library(data.table)
+library(reshape2)
+library(stringr)
+library(readxl)
+library(rlang)
+
+# ----------------------------------------------
 #define variables: 
 
 dir <- 'J:/Project/Evaluation/GF/resource_tracking/gtm/ghe_s/'
 period <-365
 cost_category <- "All"
+source <- "gf"
 
 # ----------------------------------------------
-# load the prep function
-
 
 # load csv
 file_list <- read.csv("C:/Users/irenac2/repos/gf/resource_tracking/prep/file_format_list.csv")
 
 source('./prep_sicoin.r')
 
-file_names <- file_list$filename[file_list$format=="c_coin"]
+file_names <- as.character(file_list$filename[file_list$format=="c_coin"])
 file_years <- file_list$year[file_list$format=="c_coin"]
 
-inFile <- '2013 MALARIA PRESUPUESTO POR ORGANISMO (departamento municipio).xls'
 
+# inFile <- '2013 MALARIA PRESUPUESTO POR ORGANISMO (departamento municipio)'
 
 resource_database <- setNames(data.frame(matrix(ncol = 8, nrow = 0)), 
-                          c("source", "start_date", 
-                            "period","loc_id", "cost", 
-                            "budget","disbursement", "expenditures"))
+                          c("loc_id","budget","disbursement", "source", 
+                            "start_date","period", "cost", "expenditures"))
 
 ## loop over all of the files 
-for(i in 1:length(file_names))
-
-  tmpData <- prepSicoin(dir, file_names[i], file_years[i], period, cost_category)
-  
-  resource_database = rbind(resource_database, tmpData)
-
+for(i in 1:length(file_names)) {
+  tmpData <- prepSicoin(dir, file_names[i], file_years[i], period, cost_category, source)
+  resource_database = rbind(resource_database, tmpData, use.names=TRUE, fill=TRUE)
+  ## trouble shoot this area b/c of error issues w/out fill=TRUE
 }
 
 
