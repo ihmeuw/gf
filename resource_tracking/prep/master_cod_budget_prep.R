db23ea0f4b177c770febf91c34ada3d6bbeeb566
Irena Chen <irenac2@ihme.washington.edu>
2017-11-08 09:54:04 -0800

    test master code on prepping fr format data

---
+++ resource_tracking/prep/master_cod_budget_prep.R
@@ -0,0 +1,53 @@
+# ----------------------------------------------
+# Irena Chen
+#
+# 11/8/2017
+# Template for prepping GF COD budget data 
+# Inputs:
+# inFile - name of the file to be prepped
+# Outputs:
+# budget_dataset - prepped data.table object
+# ----------------------------------------------
+library(lubridate)
+library(data.table)
+library(readxl)
+library(stats)
+library(stringr)
+library(rlang)
+library(zoo)
+# ----------------------------------------------
+
+## Notes: running this will throw a warning: 
+#Warning messages:
+#1: In `[.data.table`(ghe_data, , `:=`((drop.cols), NULL)) :
+# length(LHS)==0; no columns to delete or assign RHS to.
+
+#But this shouldn't affect the final output. 
+
+
+# ----------------------------------------------
+
+
+dir <- 'J:/Project/Evaluation/GF/resource_tracking/cod/gf/'
+file_list <- read.csv("C:/Users/irenac2/repos/gf/resource_tracking/prep/pdf_list_cod_budgets.csv")
+
+for(i in 1:length(file_list$file_name)){
+  if(file_list$type[i]=="fr"){
+    tmpData <- prep_cod_fr_budget(dir, as.character(file_list$file_name[i]), as.character(file_list$sheet[i]), ymd(file_list$start_date[i]), file_list$qtr_number[i], file_list$disease[i], file_list$loc_id[i], file_list$period[i])
+  # } else if (file_list$format[i]=="gf_budget_cost"){
+    # tmpData <- prep_module_budget(dir, file_list$filename[i], file_list$extension[i], as.character(file_list$sheet[i]), ymd(file_list$start_date[i]), file_list$qtr_number[i])
+  }
+  ## replace the "Q1" category with the associated dates that the quarters map to   
+  # tmpData1 <- map_quarters(tmpData, ymd(file_list$start_date[i]),file_list$qtr_number[i], file_list$loc_id[i], file_list$period[i],file_list$disease[i], file_list$source[i])
+  if(i==1){
+    resource_database = tmpData
+  } else {
+    resource_database = rbind(resource_database, tmpData, use.names=TRUE)
+  }
+}
+
+resource_database$budget <- as.numeric(resource_database$budget)
+
+## since we only have budget data, include exp and disbursed as 0:  
+resource_database$expenditures <- 0 
+resource_database$disbursed <- 0 
