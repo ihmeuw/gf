fac05e5dac4db776bd1b9e9986575723c7ab8ff3
Irena Chen <irenac2@ihme.washington.edu>
2017-11-08 09:33:28 -0800

    add cod budget code

--- resource_tracking/prep/prep_cod_gf_fr_budgets.R
+++ resource_tracking/prep/prep_cod_gf_fr_budgets.R
@@ -1,5 +1,5 @@
 
-prep_module_budget = function(dir, inFile, extension, sheet_name, start_date, qtr_num) {
+prep_cod_fr_budget = function(dir, inFile, extension, sheet_name, start_date, qtr_num) {
   
   col_names <- rep(0, qtr_num+1)
   
@@ -18,7 +18,7 @@ prep_module_budget = function(dir, inFile, extension, sheet_name, start_date, qt
   colnames(ghe_data)[3] <- "cost_column"
   
   ## this type of budget data should always have 13 cost categories 
-  ghe_data <- ghe_data[c(grep("By Cost", ghe_data$cost_column):(grep(13, ghe_data$X__2))),]
+  ghe_data <- ghe_data[c((grep("CostGrp", ghe_data$X__2)):(grep(13, ghe_data$X__2))),]
   
   ## delete 1st two columns 
   ghe_data <- ghe_data[, -c(1:2)]
@@ -37,12 +37,32 @@ prep_module_budget = function(dir, inFile, extension, sheet_name, start_date, qt
   ghe_data<- Filter(function(x) !all(is.na(x)), ghe_data)
   
   ## rename the columns
-  colnames(ghe_data) <- col_names
+  colnames(ghe_data)[1] <- "category"
   
   ## invert the dataset so that budget expenses and quarters are grouped by category
+  ##library(reshape)
   setDT(ghe_data)
-  melted<- melt(ghe_data,id="category", variable.name = "qtr", value.name="budget")
+  ghe_data<- melt(ghe_data,id="category", variable.name = "qtr", value.name="budget")
   
   
-  return(melted)
+  dates <- rep(start_date, qtr_num) # 
+  for (i in 1:length(dates)){
+    if (i==1){
+      dates[i] <- start_date
+    } else {
+      dates[i] <- dates[i-1]%m+% months(3)
+    }
+  }
+  
+  
+  
+  ##turn the list of dates into a dictionary (but only for quarters!) : 
+  dates <- setNames(dates,unique(ghe_data$qtr))
+  
+  
+  ## now match quarters with start dates 
+  kDT = data.table(qtr = names(dates), value = TRUE, start_date = unname(dates))
+  budget_dataset <-ghe_data[kDT, on=.(qtr), start_date := i.start_date ]
+  
+  return( budget_dataset)
 }
