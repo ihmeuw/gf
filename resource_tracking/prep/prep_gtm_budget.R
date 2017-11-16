77f2bee7501db65b4f82705ee74a73556b21143f
Irena Chen <irenac2@ihme.washington.edu>
2017-11-16 10:56:14 -0800

    fix code to get program activity instead of cost categories

--- resource_tracking/prep/prep_gtm_budget.R
+++ resource_tracking/prep/prep_gtm_budget.R
@@ -24,18 +24,18 @@ prep_gtm_budget = function(dir, inFile, extension, sheet_name, start_date, qtr_n
   ghe_data <- data.table(read_excel(paste0(dir, inFile, extension), sheet=sheet_name))
   
   ## the 1st column isn't always the same, so just call it something: 
+  colnames(ghe_data)[4] <- "program_activity"
   colnames(ghe_data)[1] <- "first_column"
-  
   ## this type of budget data should always have 13 cost categories 
-  ghe_data <- ghe_data[c(grep("Category", ghe_data$X__1):(grep(13, ghe_data$first_column))),]
+  ghe_data <- ghe_data[c(grep("PROGRAM ACTIVITY", ghe_data$first_column):(grep("TOTAL", ghe_data$first_column))),]
   
   # drop first row (it's blank)
-  ghe_data <- ghe_data[-1,]
+  ghe_data <- ghe_data[-c(1:3),]
   
   # drop 1st column (unnecessary)
-  ghe_data[[1]] <- NULL  
+  ghe_data[,1:3] <- NULL  
   
-  ghe_data$X__1[1] <- "cost_category"
+  ghe_data$program_activity[1] <- "cost_category"
   
   
   ## also drop columns containing only NA's
@@ -58,10 +58,12 @@ prep_gtm_budget = function(dir, inFile, extension, sheet_name, start_date, qtr_n
   colnames(ghe_data1) <- col_names
   ## drop the first row now that we renamed the columns 
   ghe_data1 <- ghe_data1[-1,]
-  
+  ghe_data1 <- ghe_data1[!is.na(ghe_data1$cost_category),]
   ## invert the dataset so that budget expenses and quarters are grouped by category
   setDT(ghe_data1)
-  melted<- melt(ghe_data1,id="cost_category", variable.name = "qtr", value.name="budget")
+  
+  
+  melted<- melt(ghe_data1,id="cost_category")
   
   dates <- rep(start_date, qtr_num) # 
   for (i in 1:length(dates)){
