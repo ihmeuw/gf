2752b761cbe7845e832ea2b8c406ec6d8c9d018a
Irena Chen <irenac2@ihme.washington.edu>
2017-11-16 11:20:28 -0800

    change code to get program activity instead of cost category

--- resource_tracking/prep/prep_module_gf_budget.R
+++ resource_tracking/prep/prep_module_gf_budget.R
@@ -20,25 +20,28 @@ prep_module_budget = function(dir, inFile, extension, sheet_name, start_date, qt
     }
   }
   
-
+  
   ghe_data <- data.table(read_excel(paste0(dir, inFile, extension), sheet=sheet_name))
   
   ## this type of budget data should always have 13 cost categories 
-  ghe_data <- ghe_data[c(grep("CostG", ghe_data$X__2):(grep(13, ghe_data$X__2))),]
+  colnames(ghe_data)[3] <- "program_activity"
+  ghe_data <- ghe_data[c(grep("Module", ghe_data$program_activity):(grep("Total", ghe_data$program_activity))),]
   
  ## delete 1st two columns 
   ghe_data <- ghe_data[, -c(1:2)]
   
-  
-  ##we only want the data for each quarter, so remove extraneous values: 
-  toMatch <- c("Year", "RCC", "%", "TOTAL", "Phase", "Implem", "Total")
-  ghe_data <- Filter(function(x) !any(grepl(paste(toMatch, collapse="|"), x)), ghe_data)
-  
-  
   colnames(ghe_data) <- as.character(ghe_data[1,])
+
   ## drop the first row now that we renamed the columns 
   ghe_data <- ghe_data[-1,]
+  ##we only want the data for each quarter, so remove extraneous values: 
+  toMatch <- c("Year", "RCC", "%", "TOTAL", "Phase", "Implem", "Total", "Año")
+  drop.cols <- grep(paste(toMatch, collapse="|"), ignore.case=TRUE, colnames(ghe_data))
+  ghe_data <- ghe_data[, (drop.cols) := NULL]
   
+  
+  ghe_data <- ghe_data[-nrow(ghe_data),]
+
   ## also drop columns containing only NA's
   ghe_data<- Filter(function(x) !all(is.na(x)), ghe_data)
   
@@ -48,7 +51,5 @@ prep_module_budget = function(dir, inFile, extension, sheet_name, start_date, qt
   ## invert the dataset so that budget expenses and quarters are grouped by category
   setDT(ghe_data)
   melted<- melt(ghe_data,id="cost_category", variable.name = "qtr", value.name="budget")
-  
-  
   return(melted)
 }
