ad351e64909a97a86fdb43fcf172b2b2a30172f6
IHME\davidp6 <github403>
2017-11-27 17:13:38 -0800

    Split data prep into separate script

--- outcome_measurement/malaria/map_malaria_indicators.r
+++ outcome_measurement/malaria/map_malaria_indicators.r
@@ -3,6 +3,7 @@
 #
 # 11/17/2017
 # Make simple maps of malaria indicators for PCE countries
+# The working directory should be the root of this repo
 # --------------------------------------------------------
 
 # note: geom_tile has a bug when combined with facet_wrap
@@ -40,82 +41,15 @@ inds = c('itn','antmal','prev')
 j = ifelse(Sys.info()[1]=='Windows', 'J:', '/home/j')
 dir = paste0(j, '/Project/Evaluation/GF/outcome_measurement/multi_country/map/')
 
-# input files
-actDir = paste0(dir, 'Africa Cube Public Data/ACT_use_rasters/rasters/rasters/')
-itnDir = paste0(dir, 'Africa Cube Public Data/ITN_use_year_average_adj_rasters/rasters/')
-irsDir = paste0(dir, 'Africa Cube Public Data/IRS_use_rasters/rasters/')
-antmalDir = paste0(dir, 'NEJM Rasters/spatially_disaggregated_antimalarials/')
-prevDir = paste0(dir, '/Africa Cube Public Data/Prevalence_annual_means_rasters/rasters/')
-actFiles = paste0(actDir, list.files(actDir)[grepl('tif', list.files(actDir))])
-itnFiles = paste0(itnDir, list.files(itnDir)[grepl('tif', list.files(itnDir))])
-irsFiles = paste0(irsDir, list.files(irsDir)[grepl('tif', list.files(irsDir))])
-antmalFiles = paste0(antmalDir, list.files(antmalDir)[!grepl('ovr', list.files(antmalDir))])
-antmalFiles = antmalFiles[!grepl('xml', antmalFiles)]
-prevFiles = paste0(prevDir, list.files(prevDir)[grepl('tif', list.files(prevDir))])
-
-# shapefiles
-shapeFileUGA = paste0(dir, '../../../mapping/uga/uga_dist112_map.shp')
-shapeFileCOD = paste0(dir, '../../../mapping/cod/COD_adm3.shp')
-
 # output files
 graphFile = paste0(dir, '/visualizations/graphs.pdf')
 # ----------------------------------------------
 
 
-# ----------------------------------------------------------
+# ------------------------------------------------------
 # Load/prep data
-
-# load data
-if ('itn' %in% inds) rasterDataitn = raster(itnFiles[grepl(year, itnFiles)])
-if ('act' %in% inds) rasterDataact = raster(actFiles[grepl(year, itnFiles)])
-if ('irs' %in% inds) rasterDatairs = raster(irsFiles[grepl(year, irsFiles)])
-if ('antmal' %in% inds) rasterDataantmal = raster(antmalFiles[grepl(year, antmalFiles)])
-if ('prev' %in% inds) rasterDataprev = raster(prevFiles[grepl(year, prevFiles)])
-
-# load shapefile
-mapUGA = shapefile(shapeFileUGA)
-mapCOD = shapefile(shapeFileCOD)
-
-# simplify shapefiles for speed
-mapUGA = gSimplify(mapUGA, tol=0.01, topologyPreserve=TRUE)
-mapCOD = gSimplify(mapCOD, tol=0.01, topologyPreserve=TRUE)
-
-# format polygons as data.table
-shapeDataUGA = data.table(fortify(mapUGA))
-shapeDataCOD = data.table(fortify(mapCOD))
-
-# format rasters
-for(i in inds) {
-	
-	rasterData = get(paste0('rasterData',i))
-
-	# clip to Uganda/DRC
-	rasterDataUGA = crop(rasterData, extent(mapUGA))
-	rasterDataUGA = mask(rasterDataUGA, mapUGA)
-	rasterDataCOD = crop(rasterData, extent(mapCOD))
-	rasterDataCOD = mask(rasterDataCOD, mapCOD)
-
-	# format rasters as data.table
-	dataUGA = data.table(as.data.frame(rasterDataUGA, xy=TRUE))
-	dataCOD = data.table(as.data.frame(rasterDataCOD, xy=TRUE))
-	setnames(dataUGA, c('x','y','value'))
-	setnames(dataCOD, c('x','y','value'))
-	
-	# crop to 99th percentile so extreme points don't skew map scales
-	q1 = quantile(dataUGA$value, .005, na.rm=TRUE)
-	q99 = quantile(dataUGA$value, .995, na.rm=TRUE)
-	if (q1>0) dataUGA[value<q1, value:=q1]
-	dataUGA[value>q99, value:=q99]
-	q1 = quantile(dataCOD$value, .005, na.rm=TRUE)
-	q99 = quantile(dataCOD$value, .995, na.rm=TRUE)
-	if (q1>0) dataCOD[value<q1, value:=q1]
-	dataCOD[value>q99, value:=q99]
-	
-	# assign
-	assign(paste0('dataUGA',i), dataUGA) 
-	assign(paste0('dataCOD',i), dataCOD) 
-}
-# ----------------------------------------------------------
+source('./outcome_measurement/malaria/load_map_data.r')
+# ------------------------------------------------------
 
 
 # ----------------------------------------------------------------------
