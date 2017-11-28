ea398434ff9632ed3eba1de1c638d856ffa33787
IHME\davidp6 <github403>
2017-11-27 17:13:59 -0800

    Split data prep into separate file

---
+++ outcome_measurement/malaria/load_map_data.r
@@ -0,0 +1,113 @@
+# --------------------------------------------------------
+# David Phillips
+#
+# 11/27/2017
+# Script to load data into memory
+# Originally intended to be called by map_malaria_indicators.r and map_unmet_need.r
+# --------------------------------------------------------
+
+# to do:
+# this should eventually be turned into a more general-purpose function
+# clean up handling of input iso3s
+
+# ----------------------------------------------
+# Files and directories
+
+# data directory
+j = ifelse(Sys.info()[1]=='Windows', 'J:', '/home/j')
+dir = paste0(j, '/Project/Evaluation/GF/outcome_measurement/multi_country/map/')
+
+# input files
+actDir = paste0(dir, 'Africa Cube Public Data/ACT_use_rasters/rasters/rasters/')
+itnDir = paste0(dir, 'Africa Cube Public Data/ITN_use_year_average_adj_rasters/rasters/')
+irsDir = paste0(dir, 'Africa Cube Public Data/IRS_use_rasters/rasters/')
+antmalDir = paste0(dir, 'NEJM Rasters/spatially_disaggregated_antimalarials/')
+popDir = paste0(dir, 'NEJM Rasters/unmasked_population_for_incidence/')
+prevDir = paste0(dir, '/Africa Cube Public Data/Prevalence_annual_means_rasters/rasters/')
+actFiles = paste0(actDir, list.files(actDir)[grepl('tif', list.files(actDir))])
+itnFiles = paste0(itnDir, list.files(itnDir)[grepl('tif', list.files(itnDir))])
+irsFiles = paste0(irsDir, list.files(irsDir)[grepl('tif', list.files(irsDir))])
+antmalFiles = paste0(antmalDir, list.files(antmalDir)[!grepl('ovr', list.files(antmalDir))])
+antmalFiles = antmalFiles[!grepl('xml', antmalFiles)]
+popFiles = paste0(popDir, list.files(popDir)[!grepl('ovr', list.files(popDir))])
+popFiles = popFiles[!grepl('xml', popFiles)]
+prevFiles = paste0(prevDir, list.files(prevDir)[grepl('tif', list.files(prevDir))])
+
+# shapefiles
+shapeFileUGA = paste0(dir, '../../../mapping/uga/uga_dist112_map.shp')
+shapeFileCOD = paste0(dir, '../../../mapping/cod/COD_adm3.shp')
+
+# output files
+graphFile = paste0(dir, '/visualizations/graphs.pdf')
+# ----------------------------------------------
+
+
+# ----------------------------------------------------------
+# Load/prep data
+
+# load data
+if ('itn' %in% inds) rasterDataitn = raster(itnFiles[grepl(year, itnFiles)])
+if ('act' %in% inds) rasterDataact = raster(actFiles[grepl(year, itnFiles)])
+if ('irs' %in% inds) rasterDatairs = raster(irsFiles[grepl(year, irsFiles)])
+if ('antmal' %in% inds) rasterDataantmal = raster(antmalFiles[grepl(year, antmalFiles)])
+if ('pop' %in% inds) rasterDatapop = raster(popFiles[grepl(year, popFiles) & grepl('05-15', popFiles)])
+if ('prev' %in% inds) rasterDataprev = raster(prevFiles[grepl(year, prevFiles)])
+
+# load shapefile
+if ('UGA' %in% iso3s) mapUGA = shapefile(shapeFileUGA)
+if ('COD' %in% iso3s) mapCOD = shapefile(shapeFileCOD)
+
+# simplify shapefiles for speed
+if ('UGA' %in% iso3s) mapUGADatatmp = mapUGA@data
+if ('COD' %in% iso3s) mapCODDatatmp = mapCOD@data
+if ('UGA' %in% iso3s) mapUGA = gSimplify(mapUGA, tol=0.01, topologyPreserve=TRUE)
+if ('COD' %in% iso3s) mapCOD = gSimplify(mapCOD, tol=0.01, topologyPreserve=TRUE)
+if ('UGA' %in% iso3s) mapUGA = as(mapUGA, 'SpatialPolygonsDataFrame')
+if ('UGA' %in% iso3s) mapUGA@data = mapUGADatatmp
+if ('COD' %in% iso3s) mapCOD = as(mapCOD, 'SpatialPolygonsDataFrame')
+if ('COD' %in% iso3s) mapCOD@data = mapCODDatatmp
+
+# format polygons as data.table
+if ('UGA' %in% iso3s) shapeDataUGA = data.table(fortify(mapUGA, region='dist112_name'))
+if ('COD' %in% iso3s) shapeDataCOD = data.table(fortify(mapCOD, region='ID_3'))
+if ('UGA' %in% iso3s) shapeDataUGA = merge(shapeDataUGA, mapUGADatatmp, by.x='id', by.y='ID_3', all.x=TRUE)
+if ('COD' %in% iso3s) shapeDataCOD = merge(shapeDataCOD, mapCODDatatmp, by.x='id', by.y='ID_3', all.x=TRUE)
+
+# format rasters
+for(i in inds) {
+	
+	rasterData = get(paste0('rasterData',i))
+
+	# clip to Uganda/DRC
+	if ('UGA' %in% iso3s) rasterDataUGA = crop(rasterData, extent(mapUGA))
+	if ('UGA' %in% iso3s) rasterDataUGA = mask(rasterDataUGA, mapUGA)
+	if ('COD' %in% iso3s) rasterDataCOD = crop(rasterData, extent(mapCOD))
+	if ('COD' %in% iso3s) rasterDataCOD = mask(rasterDataCOD, mapCOD)
+
+	# format rasters as data.table
+	if ('UGA' %in% iso3s) dataUGA = data.table(as.data.frame(rasterDataUGA, xy=TRUE))
+	if ('COD' %in% iso3s) dataCOD = data.table(as.data.frame(rasterDataCOD, xy=TRUE))
+	if ('UGA' %in% iso3s) setnames(dataUGA, c('x','y','value'))
+	if ('COD' %in% iso3s) setnames(dataCOD, c('x','y','value'))
+	
+	# crop to 99th percentile so extreme points don't skew map scales
+	if (i!='pop') {
+		if ('UGA' %in% iso3s) { 
+			q1 = quantile(dataUGA$value, .005, na.rm=TRUE)
+			q99 = quantile(dataUGA$value, .995, na.rm=TRUE)
+			if (q1>0) dataUGA[value<q1, value:=q1]
+			dataUGA[value>q99, value:=q99]
+		}
+		if ('COD' %in% iso3s) { 
+			q1 = quantile(dataCOD$value, .005, na.rm=TRUE)
+			q99 = quantile(dataCOD$value, .995, na.rm=TRUE)
+			if (q1>0) dataCOD[value<q1, value:=q1]
+			dataCOD[value>q99, value:=q99]
+		}
+	}
+	
+	# assign
+	if ('UGA' %in% iso3s) assign(paste0('dataUGA',i), dataUGA) 
+	if ('COD' %in% iso3s) assign(paste0('dataCOD',i), dataCOD) 
+}
+# ----------------------------------------------------------
