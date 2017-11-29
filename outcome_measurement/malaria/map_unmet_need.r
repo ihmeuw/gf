7f4cd9e0b3eefcf5266347208385cf27365c3d42
IHME\davidp6 <github403>
2017-11-29 10:31:31 -0800

    Move unmet need graphs to first preliminary stage

--- outcome_measurement/malaria/map_unmet_need.r
+++ outcome_measurement/malaria/map_unmet_need.r
@@ -8,6 +8,7 @@
 
 # to do:
 # match up age groups better. currently poulation is 5-15 and prevalence is 2-10. everything else (i think) is all ages
+# why does pixel-aggregated unmet antimalarial align so well with total unmet antimalarial (when itn does not)?
 
 # --------------------
 # Set up R
@@ -45,7 +46,7 @@ j = ifelse(Sys.info()[1]=='Windows', 'J:', '/home/j')
 dir = paste0(j, '/Project/Evaluation/GF/outcome_measurement/multi_country/map/')
 
 # output files
-graphFile = paste0(dir, '/visualizations/graphs.pdf')
+graphFile = paste0(dir, '/visualizations/unmet_need_graphs.pdf')
 # ----------------------------------------------
 
 
@@ -87,8 +88,8 @@ if (n2>0) print(paste('Warning:', n2, '% of pixels have antimalarial coverage bu
 dataCOD[, prev_num:=prev*pop]
 
 # compute number of uncovered prevalent cases
-dataCOD[, itn_num:=itn*pop]
-dataCOD[, antmal_num:=antmal*pop]
+dataCOD[, itn_num:=itn*prev_num]
+dataCOD[, antmal_num:=antmal*prev_num]
 dataCOD[, unmet_itn_num:=(1-itn)*prev_num]
 dataCOD[, unmet_antmal_num:=(1-antmal)*prev_num]
 
@@ -105,26 +106,91 @@ natData[, unmet_itn_rate_fal:=unmet_itn_num_fal/pop]
 natData[, unmet_antmal_rate_fal:=unmet_antmal_num_fal/pop]
 
 # aggregate to health zones and repeat
-# hzValues = extract(rasterFromXYZ(dataCOD), mapCOD)
-# hzSums = data.table(do.call('rbind',lapply(hzValues, colSums, na.rm=TRUE)))
-# hzSums[, unmet_itn_rate:=unmet_itn/pop]
-# hzSums[, unmet_antmal_rate:=unmet_antmal/pop]
-# mapCOD@data = data.frame(mapCOD@data, hzSums[,c('pop','prev_num','unmet_itn','unmet_itn','unmet_itn_rate','unmet_antmal_rate'),with=FALSE])
+hzData = extract(rasterFromXYZ(dataCOD[,c('x','y',cols),with=FALSE]), mapCOD)
+hzData = data.table(do.call('rbind',lapply(hzData, colSums, na.rm=TRUE)))
+hzData[, itn:=itn_num/pop]
+hzData[, unmet_itn_num_fal:=(1-itn)*prev_num] # coverage is among the population, but it makes sense to multiply by prevalence to count unmet. that's the inefficiency we're after...
+hzData[, antmal:=antmal_num/pop]
+hzData[, unmet_antmal_num_fal:=(1-antmal)*prev_num]
+hzData[, unmet_itn_rate:=unmet_itn_num/pop]
+hzData[, unmet_antmal_rate:=unmet_antmal_num/pop]
+hzData[, unmet_itn_rate_fal:=unmet_itn_num_fal/pop]
+hzData[, unmet_antmal_rate_fal:=unmet_antmal_num_fal/pop]
+mapCOD@data = data.frame(mapCOD@data, hzSums[,c('pop','prev_num','unmet_itn','unmet_itn','unmet_itn_rate','unmet_antmal_rate'),with=FALSE])
 # ---------------------------------------------------------------------------------------
 
 
-# ----------------------------------------------------------------------
+# ----------------------------------------
 # Set up to graph
 
+# graph data
+graphData = melt(natData)
+graphData[!grepl('_fal', variable), calc:='National Coverage']
+graphData[grepl('_fal', variable), calc:='Pixel-Level']
+graphData[, variable:=gsub('_fal', '', variable)]
+graphData = graphData[variable %in% c('unmet_itn_rate', 'unmet_antmal_rate')]
+graphData[variable=='unmet_itn_rate', variable:='ITN']
+graphData[variable=='unmet_antmal_rate', variable:='Antimalarial']
+
 # colors
-cols = brewer.pal(6, 'Spectral')
+cols1 = brewer.pal(6, 'Spectral')
+cols2 = brewer.pal(6, 'Paired')
 border = 'grey65'
+
+# limits 
+lims = c(min(hzData$unmet_itn_rate,hzData$unmet_itn_rate_fal), max(hzData$unmet_itn_rate,hzData$unmet_itn_rate_fal))
+# ----------------------------------------
+
+
+# ----------------------------------------------------------------------
+# Graph unmet need
+
+# map of pixel-level unmet need
+p1 = ggplot(dataCOD, aes(y=y, x=x, fill=unmet_itn*100)) + 
+	geom_tile() + 
+	geom_path(data=shapeDataCOD, aes(x=long, y=lat, group=group)
+		, color=border, size=.05, inherit.aes=FALSE) + 
+	scale_fill_gradientn('%', colors=cols1, na.value='white') + 
+	coord_fixed(ratio=1) + 
+	scale_x_continuous('', breaks = NULL) + 
+	scale_y_continuous('', breaks = NULL) + 
+	labs(title='Unmet ITN Need (% of cases)') + 
+	theme_minimal(base_size=16) + 
+	theme(plot.title=element_text(hjust=.5)) 
+	
+# national level
+p2 = ggplot(graphData[calc=='Pixel-Level'], aes(x=variable, y=value, fill=calc)) + 
+	geom_bar(stat='identity', position='dodge', fill=cols2[2]) + 
+	labs(title='Unmet Need', y='Unmet Need (% of cases)', x='') + 
+	theme_bw() + 
+	theme(plot.title=element_text(hjust=.5), plot.subtitle=element_text(hjust=.5))
+
+# national level comparing to ecological
+p3 = ggplot(graphData, aes(x=variable, y=value, fill=calc)) + 
+	geom_bar(stat='identity', position='dodge') + 
+	scale_fill_manual('', values=cols2) + 
+	labs(title='Unmet Need', y='Unmet Need (% of cases)', x='') + 
+	theme_bw() + 
+	theme(plot.title=element_text(hjust=.5), plot.subtitle=element_text(hjust=.5))
+
+# health zone level to show the ecological fallacy
+p4 = ggplot(hzData, aes(y=unmet_itn_rate, x=unmet_itn_rate_fal)) + 
+	geom_point(size=2, alpha=.5) + 
+	geom_abline(slope=1, intercept=0) + 
+	scale_x_continuous(limits=lims) +
+	scale_y_continuous(limits=lims) +
+	labs(title='Unmet ITN Need', subtitle='(% of cases)', y='Calculated Based on Health Zone Totals', x='Calculated at Pixel Level') + 
+	theme_bw(base_size=16) + 
+	theme(plot.title=element_text(hjust=.5), plot.subtitle=element_text(hjust=.5))
 # ----------------------------------------------------------------------
 
 
 # --------------------------------
 # Save graphs
 pdf(graphFile, height=6, width=9)
-
+p1
+p2
+p3
+p4
 dev.off()
 # --------------------------------
