65f8f7adc161db2632f1c83e7e62c04a2b6beabb
IHME\davidp6 <github403>
2017-12-05 21:51:47 -0800

    Udate unmet need analysis to use new load function

--- outcome_measurement/malaria/map_unmet_need.r
+++ outcome_measurement/malaria/map_unmet_need.r
@@ -28,7 +28,7 @@ library(gridExtra)
 # Parameters and settings
 
 # year
-year = 2014
+years = seq(2000,2015)
 
 # indicators
 inds = c('itn','antmal','prev','pop')
@@ -50,55 +50,38 @@ dir = paste0(j, '/Project/Evaluation/GF/outcome_measurement/multi_country/map/')
 
 # output files
 graphFile = paste0(dir, '/visualizations/unmet_need_graphs.pdf')
+
+# prep function
+source('./outcome_measurement/malaria/load_map_data.r')
 # ----------------------------------------------
 
 
-# ------------------------------------------------------
+# ------------------------------------------------
 # Load/prep data
-source('./outcome_measurement/malaria/load_map_data.r')
-# ------------------------------------------------------
+out = loadMapData(iso3s, years, inds, crop=FALSE)
+data = out$data
+map = out$maps$COD
+shapeData = out$shapeData
+# ------------------------------------------------
 
 
 # ---------------------------------------------------------------------------------------
 # Assess unmet need
 
-# compute prevalence numbers
-dataCODprev = dataCODprev[order(x,y)]
-dataCODpop = dataCODpop[order(x,y)]
-dataCODitn = dataCODitn[order(x,y)]
-dataCODantmal = dataCODantmal[order(x,y)]
-if (nrow(dataCODprev)!=nrow(dataCODpop)) stop('Error: cbinding rasters with different lengths is bad!')
-if (nrow(dataCODprev)!=nrow(dataCODitn)) stop('Error: cbinding rasters with different lengths is bad!')
-if (nrow(dataCODprev)!=nrow(dataCODantmal)) stop('Error: cbinding rasters with different lengths is bad!')
-dataCOD = cbind(dataCODprev, dataCODpop$value, dataCODitn$value, dataCODantmal$value)
-setnames(dataCOD, c('x','y','prev','pop','itn','antmal'))
-
-# test for problems
-n1 = round(nrow(dataCOD[!is.na(prev) & is.na(pop)])/nrow(dataCOD)*100,3)
-if (n1>0) print(paste('Warning:', n1, '% of pixels have prevalence but not population'))
-n2 = round(nrow(dataCOD[is.na(prev) & !is.na(pop)])/nrow(dataCOD)*100,3)
-if (n2>0) print(paste('Warning:', n2, '% of pixels have population but not prevalence'))
-n1 = round(nrow(dataCOD[!is.na(prev) & is.na(itn)])/nrow(dataCOD)*100,3)
-if (n1>0) print(paste('Warning:', n1, '% of pixels have prevalence but not ITN'))
-n2 = round(nrow(dataCOD[is.na(prev) & !is.na(itn)])/nrow(dataCOD)*100,3)
-if (n2>0) print(paste('Warning:', n2, '% of pixels have ITN but not prevalence'))
-n1 = round(nrow(dataCOD[!is.na(prev) & is.na(antmal)])/nrow(dataCOD)*100,3)
-if (n1>0) print(paste('Warning:', n1, '% of pixels have prevalence but not antimalarial coverage'))
-n2 = round(nrow(dataCOD[is.na(prev) & !is.na(antmal)])/nrow(dataCOD)*100,3)
-if (n2>0) print(paste('Warning:', n2, '% of pixels have antimalarial coverage but not prevalence'))
-
 # compute prevalence number
-dataCOD[, prev_num:=prev*pop]
+data[, prev_num:=prev*pop]
 
 # compute number of uncovered prevalent cases
-dataCOD[, itn_num:=itn*prev_num]
-dataCOD[, antmal_num:=antmal*prev_num]
-dataCOD[, unmet_itn_num:=(1-itn)*prev_num]
-dataCOD[, unmet_antmal_num:=(1-antmal)*prev_num]
+data[, itn_num:=itn*pop]
+data[, antmal_num:=antmal*prev_num]
+data[, unmet_itn:=(1-itn)*prev]
+data[, unmet_itn_num:=(1-itn)*prev_num]
+data[, unmet_antmal:=(1-antmal)*prev]
+data[, unmet_antmal_num:=(1-antmal)*prev_num]
 
 # aggregate to national unmet need and "ecologically-fallacious" unmet need
 cols = c('prev_num','pop','unmet_itn_num','unmet_antmal_num','itn_num','antmal_num')
-natData = dataCOD[,lapply(.SD, sum, na.rm=TRUE), .SDcols=cols]
+natData = data[,lapply(.SD, sum, na.rm=TRUE), .SDcols=cols, by='year']
 natData[, itn:=itn_num/pop]
 natData[, unmet_itn_num_fal:=(1-itn)*prev_num]
 natData[, antmal:=antmal_num/pop]
@@ -109,7 +92,7 @@ natData[, unmet_itn_rate_fal:=unmet_itn_num_fal/pop]
 natData[, unmet_antmal_rate_fal:=unmet_antmal_num_fal/pop]
 
 # aggregate to health zones and repeat
-hzData = extract(rasterFromXYZ(dataCOD[,c('x','y',cols),with=FALSE]), mapCOD)
+hzData = extract(rasterFromXYZ(data[year==max(year),c('x','y',cols),with=FALSE]), map)
 hzData = data.table(do.call('rbind',lapply(hzData, colSums, na.rm=TRUE)))
 hzData[, itn:=itn_num/pop]
 hzData[, unmet_itn_num_fal:=(1-itn)*prev_num] # coverage is among the population, but it makes sense to multiply by prevalence to count unmet. that's the inefficiency we're after...
@@ -119,7 +102,6 @@ hzData[, unmet_itn_rate:=unmet_itn_num/pop]
 hzData[, unmet_antmal_rate:=unmet_antmal_num/pop]
 hzData[, unmet_itn_rate_fal:=unmet_itn_num_fal/pop]
 hzData[, unmet_antmal_rate_fal:=unmet_antmal_num_fal/pop]
-mapCOD@data = data.frame(mapCOD@data, hzSums[,c('pop','prev_num','unmet_itn','unmet_itn','unmet_itn_rate','unmet_antmal_rate'),with=FALSE])
 # ---------------------------------------------------------------------------------------
 
 
@@ -127,7 +109,7 @@ mapCOD@data = data.frame(mapCOD@data, hzSums[,c('pop','prev_num','unmet_itn','un
 # Set up to graph
 
 # graph data
-graphData = melt(natData)
+graphData = melt(natData, id.vars='year')
 graphData[!grepl('_fal', variable), calc:='National Coverage']
 graphData[grepl('_fal', variable), calc:='Pixel-Level']
 graphData[, variable:=gsub('_fal', '', variable)]
@@ -149,9 +131,9 @@ lims = c(min(hzData$unmet_itn_rate,hzData$unmet_itn_rate_fal), max(hzData$unmet_
 # Graph unmet need
 
 # map of pixel-level unmet need
-p1 = ggplot(dataCOD, aes(y=y, x=x, fill=unmet_itn*100)) + 
+p1 = ggplot(data, aes(y=y, x=x, fill=unmet_itn*100)) + 
 	geom_tile() + 
-	geom_path(data=shapeDataCOD, aes(x=long, y=lat, group=group)
+	geom_path(data=shapeData, aes(x=long, y=lat, group=group)
 		, color=border, size=.05, inherit.aes=FALSE) + 
 	scale_fill_gradientn('%', colors=cols1, na.value='white') + 
 	coord_fixed(ratio=1) + 
@@ -162,14 +144,14 @@ p1 = ggplot(dataCOD, aes(y=y, x=x, fill=unmet_itn*100)) +
 	theme(plot.title=element_text(hjust=.5)) 
 	
 # national level
-p2 = ggplot(graphData[calc=='Pixel-Level'], aes(x=variable, y=value, fill=calc)) + 
+p2 = ggplot(graphData[calc=='Pixel-Level' & year==max(year)], aes(x=variable, y=value, fill=calc)) + 
 	geom_bar(stat='identity', position='dodge', fill=cols2[2]) + 
 	labs(title='Unmet Need', y='Unmet Need (% of cases)', x='') + 
 	theme_bw() + 
 	theme(plot.title=element_text(hjust=.5), plot.subtitle=element_text(hjust=.5))
 
 # national level comparing to ecological
-p3 = ggplot(graphData, aes(x=variable, y=value, fill=calc)) + 
+p3 = ggplot(graphData[year==max(year)], aes(x=variable, y=value, fill=calc)) + 
 	geom_bar(stat='identity', position='dodge') + 
 	scale_fill_manual('', values=cols2) + 
 	labs(title='Unmet Need', y='Unmet Need (% of cases)', x='') + 
