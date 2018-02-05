ea406f9ff5850f55fc97e6b799598c76be3ce413
Irena Chen <irenac2@ihme.washington.edu>
2018-02-05 13:27:30 -0800

    add code to handle yearly budget, but monthly disbursements

---
+++ resource_tracking/prep/gtm_prep/prep_sicoin_monthly_data.R
@@ -0,0 +1,81 @@
+# ----------------------------------------------
+# Irena Chen
+#
+# 11/2/2017
+# Template for prepping SICOIN cost category data 
+# Inputs:
+# inFile - name of the file to be prepped
+# year - which year the dataset corresponds to
+#
+# Outputs:
+# budget_dataset - prepped data.table object
+
+# ----------------------------------------------
+# Set up R
+
+library(data.table)
+library(reshape2)
+library(stringr)
+library(readxl)
+library(rlang)
+library(zoo)
+
+# start function
+prep_monthly_sicoin = function(inFile, start_date, disease, period, source) {
+  # --------------------
+  # Test the inputs
+  if (class(inFile)!='character') stop('Error: inFile argument must be a string!')
+  if (class(year)=='character') stop('Error: year argument must be a number!')
+  # ----------------------------------------------
+  # Files and directories
+  
+  # Load/prep data
+  gf_data <- data.table(read_excel(inFile))
+  
+  # ----------------------------------------------
+  ## remove empty columns 
+ if (source=="gf") {
+    gf_data$X__11 <- na.locf(gf_data$X__11, na.rm=FALSE)
+    gf_data$X__10 <- na.locf(gf_data$X__10, na.rm=FALSE)
+    if(month(start_date)==1){
+      budget_dataset <- gf_data[, c("X__10", "X__11","X__19", "X__25"), with=FALSE]
+      names(budget_dataset) <- c("sda_orig", "loc_id", "budget","disbursement")
+      
+    } else {
+      budget_dataset <- gf_data[, c("X__10", "X__11", "X__25"), with=FALSE]
+      names(budget_dataset) <- c("sda_orig", "loc_id", "disbursement")
+      budget_dataset$budget <- NA
+    }
+    # remove rows where cost_categories are missing values
+    budget_dataset <- na.omit(budget_dataset, cols="loc_id")
+    setkeyv(budget_dataset,c("sda_orig", "loc_id"))
+    budget_dataset <- unique(budget_dataset)
+    ##get rid of extra rows where there are NAs or 0s: 
+    budget_dataset  <- budget_dataset[, list(budget=sum(na.omit(budget)), disbursement=sum(na.omit(disbursement))),
+                                   by=c("sda_orig", "loc_id")]
+  }
+  toMatch <- c("government", "recursos", "resources", "multire")
+  budget_dataset <- budget_dataset[ !grepl(paste(toMatch, collapse="|"), tolower(budget_dataset$loc_id)),]
+  
+  
+  # ----------------------------------------------
+  
+  ## Create other variables 
+  budget_dataset$source <- source
+  budget_dataset$start_date <- start_date
+  budget_dataset$period <- period
+  budget_dataset$expenditure <- 0 ## change this once we figure out where exp data is
+  budget_dataset$disease <- disease
+  # ----------------------------------------------
+  
+  # Enforce variable classes
+  if (!is.numeric(budget_dataset$budget)) budget_dataset[,budget:=as.numeric(budget)]
+  if (!is.numeric(budget_dataset$disbursement)) budget_dataset[,disbursement:=as.numeric(disbursement)]
+  if (!is.numeric(budget_dataset$expenditure)) budget_dataset[,expenditure:=as.numeric(expenditure)]
+  
+  # ----------------------------------------------
+  
+  # return prepped data
+  return(budget_dataset)
+  
+}
