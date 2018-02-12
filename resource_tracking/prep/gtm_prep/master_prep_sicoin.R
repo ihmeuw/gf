b4297afcebad2c8c3bad9503fb38891a73143e9f
Irena Chen <irenac2@ihme.washington.edu>
2018-02-12 09:24:26 -0800

    add code to export the sicoin data

--- resource_tracking/prep/gtm_prep/master_prep_sicoin.R
+++ resource_tracking/prep/gtm_prep/master_prep_sicoin.R
@@ -121,11 +121,7 @@ cleaned_database <- resource_database[!resource_database$sda_orig%in%loc_ids,]
 ##output the data to the correct folder 
 
 
-
-
-
-
-write.csv(resource_database, "prepped_sicoin_data.csv", row.names=FALSE, fileEncoding="UTF-8")
+write.csv(cleaned_database, "J:/Project/Evaluation/GF/resource_tracking/gtm/prepped/prepped_sicoin_data.csv", row.names=FALSE, fileEncoding="UTF-8")
 
 
 
