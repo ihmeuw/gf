a888f0cf5e827322f6f00c4576d02c9e4daf24d0
Irena Chen <irenac2@ihme.washington.edu>
2018-02-23 14:16:46 -0800

    continue making geo maps using shape files for budget and disb

--- resource_tracking/visualizations/gtm_municipality_shape_graphs.R
+++ resource_tracking/visualizations/gtm_municipality_shape_graphs.R
@@ -10,6 +10,7 @@
 
 
 # raster package is most of what you need
+rm(list=ls())
 library(rgeos)
 library(raster)
 library(ggplot2)
@@ -32,11 +33,23 @@ setwd('J:/Project/Evaluation/GF/mapping/gtm/')
 # load the shapefile
 shapeData = shapefile('GTM_munis_only.shp')
 
+## load the admin1 shape file: 
+
+adminData = shapefile('archive/GTM_adm1.shp')
+
+
 ## load the sicoin data:
 
 sicoin_data <- data.table(read.csv("J:/Project/Evaluation/GF/resource_tracking/gtm/prepped/prepped_sicoin_data.csv"
                                    ,fileEncoding="latin1"))
 sicoin_data$year <- year(sicoin_data$start_date)
+
+
+# the sicoin loc_id has leading zeroes, which the shape files don't have
+## this gets rid of it: 
+
+sicoin_data$id <- as.numeric(lapply(sicoin_data$loc_id, function(y) sub('^0+(?=[1-9])', '', y, perl=TRUE)))
+
 
 
 ##let's do HIV first since we have that at the yearly level (may have to work around w/ Malaria data)
@@ -55,10 +68,43 @@ hivData[, cumulative_budget:=cumsum(budget), by=c('source', 'loc_name', 'year')]
 hivData[, cumulative_disbursement:=cumsum(disbursement),by=c('source', 'loc_name', 'year')]
 hivData[, absorption:=cumulative_disbursement/cumulative_budget]
 
+# ----------------------------------------------
 
+## split by source (GF vs GHE)
 gheData <- hivData[source=="ghe"]
+gfData <- hivData[source=="gf"]
+
 
 # ----------------------------------------------
+## do a histogram of GHE budget values: 
+hist(gheData$budget/1000000)
+boxplot(gheData$budget)
+
+
+hist(gheData$budget/1000000, breaks=10, main="Breaks=10")
+
+## get quantiles of the budget/disb data:
+quantile(gheData$absorption, na.rm=TRUE)
+
+
+##let's make buckets of the budget variable: 
+library(Hmisc)
+
+# ----------------------------------------------
+## Malaria data: 
+
+malData <- sicoin_data[disease=="malaria"]
+
+malData <- malData[source=="gf"]
+
+
+
+
+
+
+
+
+  # ----------------------------------------------
 ## get the important info from the shape data files - lat/long coordinates, muni codes, etc.
 
 # shapeData is a spatialpolygonsdataframe
@@ -78,56 +124,42 @@ coordinates$id <- as.numeric(coordinates$id)
 # merge on municipality names
 names = data.table(shapeData@data)
 coord_and_names = merge(coordinates, names, by.x='id', by.y='Codigo', allow.cartesian=TRUE)
+admin_dataset = merge(admin_coords, admin_names, by.x = 'id', by.y='ID_1', allow.cartesian=TRUE)
+
 
 # ----------------------------------------------
-## let's do the government health expenditures first: 
-# the sicoin loc_id has leading zeroes, which the shape files don't have
-## this gets rid of it: 
-gheData$id <- as.numeric(lapply(gheData$loc_id, function(y) sub('^0+(?=[1-9])', '', y, perl=TRUE)))
-
-## do a histogram of GHE budget values: 
-hist(gheData$budget/1000000)
-boxplot(gheData$budget)
-
-
-## get summary statistics of the budget/disb data:
-library(pastecs)
-stat.desc(gheData$budget) 
-quantile(gheData$absorption, na.rm=TRUE)
-quantile(gheData$budget, na.rm=TRUE)
-
-##let's make buckets of the budget variable: 
-library(Hmisc)
-gheData$binned_absorption<- cut(gheData$absorption, breaks=c(0,0.003352666, 0.797546032,  0.991525912, 12.616891166), labels=c("0-0.03", "0.03-0.8","0.8-0.99", "0.99+"))
-
-
-
-# merge on the data (all.x=TRUE so the shapefile data doesn't disappear)
-graphData <- merge(coord_and_names, gheData,by='id', all.x=TRUE, allow.cartesian=TRUE)
-
-graphData <- graphData[!is.na(year)]
-graphData<- graphData[with(graphData, order(year)), ]
-
+##get shape data ready to do yearly graphs: 
+gheData <- gheData[with(gheData, order(year)), ]
 
 # draw the polygons using ggplot2
 
+## I put the merge function inside the graphing function/loop - longer time to process, but it frees up memory in R: 
+colors <- c('#b2e2e2', '#66c2a4', '#238b45', '#006d2c')
+names(colors) <- levels(gheData$binned_budget)
+
 gtm_plots <- list()
 i = 1
-for (k in unique(graphData$year)){
-  plot <- (ggplot(graphData[year==k], aes(x=long, y=lat, group=group, fill=as.numeric(binned_absorption))) + 
-  	geom_path() + 
-  	geom_polygon() +
+for (k in unique(gheData$year)){
+  shapedata <- copy(coord_and_names)
+  subdata <- gheData[year==k]
+  shapedata$year <- k
+  # merge on the data (all.x=TRUE so the shapefile data doesn't disappear)
+  graphdata <- merge(shapedata, subdata,by=c('year','id'), all.x=TRUE, allow.cartesian=TRUE)
+  plot <- (ggplot() + geom_polygon(data=graphdata, aes(x=long, y=lat, group=group, fill=binned_budget)) + 
+     coord_equal() +
+    geom_path() +
+      geom_map(map=admin_dataset, data=admin_dataset, 
+               aes(map_id=id,group=group), color="#FF0000", alpha=0.8) + 
+    # geom_polygon(data=admin_dataset, aes(x=long, y=lat, group=group), color="red", alpha=0) + 
   	theme_void() +
-  	  scale_fill_gradient(low = "#bfefd1", high = "#14aa4b", space = "Lab", na.value="grey60",
-  	                    labels=levels(graphData[year==k]$binned_absorption)
-  	                    , breaks = c(1,2,3,4)
-  	                    ) + 
-    labs(title=paste(k, "HIV Absorption of GHE"), fill='Disbursement/Budget  (Cumulative)'))
+  	 scale_fill_manual(na.value = "grey50", 
+  	                    values = colors) + 
+    labs(title=paste(k, "GHE HIV Resources"), fill='Budget ($$ USD)'))
   gtm_plots[[i]] <- plot
   i=i+1
 }
 
-pdf("H:/rt_data/testing.pdf", height=6, width=9)
+pdf("H:/rt_data/gtm_ghe_hiv_by_muni.pdf", height=6, width=9)
 invisible(lapply(gtm_plots, print))
 dev.off()
 
