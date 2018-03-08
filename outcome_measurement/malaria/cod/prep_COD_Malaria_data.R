50f19da9bfa6dbff4ddf27d64cf503bb32ed1bbf
Audrey Batzel <abatzel@uw.edu>
2018-03-07 16:13:18 -0800

    Load data into R and organize R script template

--- outcome_measurement/malaria/cod/prep_COD_Malaria_data.R
+++ outcome_measurement/malaria/cod/prep_COD_Malaria_data.R
@@ -23,40 +23,50 @@ library(zoo)
 
 
 # ----------------------------------------------
-# Files and directories
+# Overview - Files and Directories
 
 # data directory
-
+# J:\Project\Evaluation\GF\outcome_measurement\cod\National_Malaria_Program\Malaria_Data_2014_to_2016
 
 # input file
+  # file path where the files are stored
+  dir <- "J:/Project/Evaluation/GF/outcome_measurement/cod/National_Malaria_Program/Malaria_Data_2014_to_2016"
+  
+  PNLP_files <- read.csv(paste0(dir,"/","PNLP_file_names.csv"), fileEncoding = "latin1")
+  
+# output files 
+  #(note: output to prepped_data folder within cod folder)
+  # cod_malaria_dataset_(year) - prepped data.table object, one per year (?)
+  # cod_malaria_dataset_master - appended version
+# ----------------------------------------------
 
 
-# output files
+# ----------------------------------------------
+# Load data
+
+cod_mdata_KIN16 <- data.table(read_excel(paste0(dir,"/", PNLP_files$File.Names.[1], '.xls'), sheet= "KIN"))
+cod_mdata_BDD16 <- data.table(read_excel(paste0(dir, "/", PNLP_files$File.Names.[1], '.xls'), sheet= "BDD"))
+cod_mdata_OR16 <- data.table(read_excel(paste0(dir,"/", PNLP_files$File.Names.[1], '.xls'), sheet= "OR"))
+
+    #should I combine first and then clean? or clean data and then combine into one dataset?
 
 # ----------------------------------------------
 
 
 # ----------------------------------------------
-# Load/prep data
-
-# load
-##read the data: 
-gf_data <- data.table(read_excel(paste0(dir, inFile), sheet=as.character(sheet_name)))
+# Set names of columns
 
 
-
-# subset variables
-
-
-# subset observations
+# ----------------------------------------------
 
 
-# format variables
-
-
-# reshape data
-
-
-# make new variables
+# ----------------------------------------------
+# Get rid of rows you don't need (and columns) "subset"
+# Clean values any missing, or format numbers such as 1,000 as 1000 
+    # (make it a number not a string)
+# Reshape data
+# Make this all into a function to replicate it for each sheet within
+    # excel doc
+# Append provinces within years, and then all years together
 
 # ----------------------------------------------
