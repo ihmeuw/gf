edba7dd4b2fc2ebe69e76489d79ac5227eaa0b04
Audrey Batzel <abatzel@uw.edu>
2018-03-08 17:01:47 -0800

    make sure prep_data function works and reformatting

--- outcome_measurement/malaria/cod/prep_COD_Malaria_data_function.R
+++ outcome_measurement/malaria/cod/prep_COD_Malaria_data_function.R
@@ -34,6 +34,7 @@
   
   PNLP_files <- read.csv(paste0(dir,"/","PNLP_file_names.csv"), fileEncoding = "latin1")
   # input files are from 2014, 2015, and 2016 and each contain three sheets for three different provinces
+
 # output files 
   #(note: output to prepped_data folder within cod folder)
   # COD_PNLP_Data_2016 - prepped data.table object, one per year (?)
@@ -44,7 +45,7 @@
 # ----------------------------------------------
 # Load data
   
-  cod_mdata_KIN16 <- data.table(read_excel(paste0(dir,"/", PNLP_files$File.Names.[1], '.xls'), sheet= "KIN"))
+  cod_mdata_OR16 <- data.table(read_excel(paste0(dir,"/", PNLP_files$File.Names.[1], '.xls'), sheet= "KIN"))
   cod_mdata_BDD16 <- data.table(read_excel(paste0(dir, "/", PNLP_files$File.Names.[1], '.xls'), sheet= "BDD"))
   cod_mdata_OR16 <- data.table(read_excel(paste0(dir,"/", PNLP_files$File.Names.[1], '.xls'), sheet= "OR"))
   
@@ -55,102 +56,121 @@
 # ----------------------------------------------  
 # Make this all into a function to replicate it for each sheet
   
-  prep_data <- function()
+  # START OF FUNCTION:  
+  prep_data <- function(year, sheetname){
     
-# ----------------------------------------------
-  
-  
+    # load data sheet
+    dataSheet <- data.table(read_excel(paste0(dir,"/Données_", year, "_PNLP.xls"), sheet= sheetname))
+# ----------------------------------------------   
 # ----------------------------------------------
 # Set names of columns
-
-  colnames(cod_mdata_OR16)[1] <- "Province"
-  colnames(cod_mdata_OR16)[2] <- "DPS"
-  colnames(cod_mdata_OR16)[3] <- "Health_Zone"
-  colnames(cod_mdata_OR16)[4] <- "Donor"
-  colnames(cod_mdata_OR16)[5] <- "Operational_Support_Partner"
-  colnames(cod_mdata_OR16)[6] <- "Population"
-  colnames(cod_mdata_OR16)[7] <- "Trimester"  #should this be quarter?
-  colnames(cod_mdata_OR16)[8] <- "Month"
-  colnames(cod_mdata_OR16)[9] <- "New_cases_malaria_Under5"
-  colnames(cod_mdata_OR16)[10] <- "New_cases_malaria_5andOlder"
-  # not sure what this one is colnames(cod_mdata_OR16)[11] <- " "
-  colnames(cod_mdata_OR16)[12] <- "Hospitalized_cases_Under5"
-  colnames(cod_mdata_OR16)[13] <- "Hospitalized_cases_5andOlder"
-  # not sure what this one is colnames(cod_mdata_OR16)[14] <- " "
-  colnames(cod_mdata_OR16)[15] <- "Simple_Malaria_TreatedAccordingToNationalPolicy_Under5"
-  colnames(cod_mdata_OR16)[16] <- "Simple_Malaria_TreatedAccordingToNationalPolicy_5andOlder"
-  # not sure what this one is colnames(cod_mdata_OR16)[17] <- " "
-  colnames(cod_mdata_OR16)[18] <- "Serious_Malaria_TreatedAccordingToNationalPolicy_Under5"
-  colnames(cod_mdata_OR16)[19] <- "Serious_Malaria_TreatedAccordingToNationalPolicy_5andOlder"
-  # not sure what this one is colnames(cod_mdata_OR16)[20] <- " "
-  colnames(cod_mdata_OR16)[21] <- "Malaria_Deaths_Under5"
-  colnames(cod_mdata_OR16)[22] <- "Malaria_Deaths_5andOlder"
-  # not sure what this one is colnames(cod_mdata_OR16)[23] <- " "
-  colnames(cod_mdata_OR16)[24] <- "ANC_1st"
-  colnames(cod_mdata_OR16)[25] <- "ANC_2nd"
-  colnames(cod_mdata_OR16)[26] <- "ANC_3rd"
-  colnames(cod_mdata_OR16)[27] <- "ANC_4th"
-  colnames(cod_mdata_OR16)[28] <- "SP_1st"  #why was SP within ANC (CPN)orginally? Should it relate to that in some way?
-  colnames(cod_mdata_OR16)[29] <- "SP_2nd"
-  colnames(cod_mdata_OR16)[30] <- "SP_3rd"
-  colnames(cod_mdata_OR16)[31] <- "ITN_received"
-  colnames(cod_mdata_OR16)[32] <- "ITN_distAtANC"
-  colnames(cod_mdata_OR16)[33] <- "ITN_distAtPreschool"
-  # Not sure about this one colnames(cod_mdata_OR16)[34] <- "VAR"
-  colnames(cod_mdata_OR16)[35] <- "ACT_2to11mos"
-  colnames(cod_mdata_OR16)[36] <- "ACT_1to5yrs"
-  colnames(cod_mdata_OR16)[37] <- "ACT_6to13yrs"
-  colnames(cod_mdata_OR16)[38] <- "ACT_14yrsAndOlder"
-  colnames(cod_mdata_OR16)[39] <- "ACT_total"
-  colnames(cod_mdata_OR16)[40] <- "ArtLum_receieved" #or is this requested?
-  colnames(cod_mdata_OR16)[41] <- "ArtLum_used"
-  # translation for goutte epaisse? colnames(cod_mdata_OR16)[42] <- "_completed_Under5""
-  # colnames(cod_mdata_OR16)[43] <- "_completed_5andOlder"
-  # colnames(cod_mdata_OR16)[44] <- "_positive_Under5"
-  # colnames(cod_mdata_OR16)[45] <- "_positive_5andOlder""
-  colnames(cod_mdata_OR16)[46] <- "RDT_completed_Under5"
-  colnames(cod_mdata_OR16)[47] <- "RDT_completed_5andOlder"
-  colnames(cod_mdata_OR16)[48] <- "RDT_positive_Under5"
-  colnames(cod_mdata_OR16)[49] <- "RDT_positive_5andOlder"
-  colnames(cod_mdata_OR16)[50] <- "Num_Reports_Received" # are these columns
-  colnames(cod_mdata_OR16)[51] <- "Num_Repots_Expected"  # needed - they are the same
-  colnames(cod_mdata_OR16)[52] <- "Num_Sanitary_Structures"
-  colnames(cod_mdata_OR16)[53] <- "Sanitary_Structures_NumTransmitted" # translation?
-  colnames(cod_mdata_OR16)[54] <- "Sanitary_Structures_NumTransmittedWithinDeadline" # translation?
+    # faster way to do this : setnames(dataSheet, c('', '', '', ''))
+    
+  colnames(dataSheet)[1] <- "Province"
+  colnames(dataSheet)[2] <- "DPS"
+  colnames(dataSheet)[3] <- "Health_Zone"
+  colnames(dataSheet)[4] <- "Donor"
+  colnames(dataSheet)[5] <- "Operational_Support_Partner"
+  colnames(dataSheet)[6] <- "Population"
+  colnames(dataSheet)[7] <- "Trimester"  #should this be quarter?
+  colnames(dataSheet)[8] <- "Month"
+  colnames(dataSheet)[9] <- "New_cases_malaria_Under5"
+  colnames(dataSheet)[10] <- "New_cases_malaria_5andOlder"
+  # not sure what this one is colnames(dataSheet)[11] <- " "
+  colnames(dataSheet)[12] <- "Hospitalized_cases_Under5"
+  colnames(dataSheet)[13] <- "Hospitalized_cases_5andOlder"
+  # not sure what this one is colnames(dataSheet)[14] <- " "
+  colnames(dataSheet)[15] <- "Simple_Malaria_TreatedAccordingToNationalPolicy_Under5"
+  colnames(dataSheet)[16] <- "Simple_Malaria_TreatedAccordingToNationalPolicy_5andOlder"
+  # not sure what this one is colnames(dataSheet)[17] <- " "
+  colnames(dataSheet)[18] <- "Serious_Malaria_TreatedAccordingToNationalPolicy_Under5"
+  colnames(dataSheet)[19] <- "Serious_Malaria_TreatedAccordingToNationalPolicy_5andOlder"
+  # not sure what this one is colnames(dataSheet)[20] <- " "
+  colnames(dataSheet)[21] <- "Malaria_Deaths_Under5"
+  colnames(dataSheet)[22] <- "Malaria_Deaths_5andOlder"
+  # not sure what this one is colnames(dataSheet)[23] <- " "
+  colnames(dataSheet)[24] <- "ANC_1st"
+  colnames(dataSheet)[25] <- "ANC_2nd"
+  colnames(dataSheet)[26] <- "ANC_3rd"
+  colnames(dataSheet)[27] <- "ANC_4th"
+  colnames(dataSheet)[28] <- "SP_1st"  #why was SP within ANC (CPN)orginally? Should it relate to that in some way?
+  colnames(dataSheet)[29] <- "SP_2nd"
+  colnames(dataSheet)[30] <- "SP_3rd"
+  colnames(dataSheet)[31] <- "ITN_received"
+  colnames(dataSheet)[32] <- "ITN_distAtANC"
+  colnames(dataSheet)[33] <- "ITN_distAtPreschool"
+  # Not sure about this one colnames(dataSheet)[34] <- "VAR"
+  colnames(dataSheet)[35] <- "ACT_2to11mos"
+  colnames(dataSheet)[36] <- "ACT_1to5yrs"
+  colnames(dataSheet)[37] <- "ACT_6to13yrs"
+  colnames(dataSheet)[38] <- "ACT_14yrsAndOlder"
+  colnames(dataSheet)[39] <- "ACT_total"
+  colnames(dataSheet)[40] <- "ArtLum_receieved" #or is this requested?
+  colnames(dataSheet)[41] <- "ArtLum_used"
+  # translation for goutte epaisse? colnames(dataSheet)[42] <- "_completed_Under5""
+  # colnames(dataSheet)[43] <- "_completed_5andOlder"
+  # colnames(dataSheet)[44] <- "_positive_Under5"
+  # colnames(dataSheet)[45] <- "_positive_5andOlder""
+  colnames(dataSheet)[46] <- "RDT_completed_Under5"
+  colnames(dataSheet)[47] <- "RDT_completed_5andOlder"
+  colnames(dataSheet)[48] <- "RDT_positive_Under5"
+  colnames(dataSheet)[49] <- "RDT_positive_5andOlder"
+  colnames(dataSheet)[50] <- "Num_Reports_Received" # are these columns
+  colnames(dataSheet)[51] <- "Num_Repots_Expected"  # needed - they are the same
+  colnames(dataSheet)[52] <- "Num_Sanitary_Structures"
+  colnames(dataSheet)[53] <- "Sanitary_Structures_NumTransmitted" # translation?
+  colnames(dataSheet)[54] <- "Sanitary_Structures_NumTransmittedWithinDeadline" # translation?
         # different names for these? "transmitted"? is that correct?
 # ----------------------------------------------
-
-
 # ----------------------------------------------
 # Get rid of rows you don't need- "subset"
   
   # delete first row if it's first value is "NA" or "PROVINCE" as a way to
     # only delete those unnecessary rows, and not any others.
   
-  if ((is.na(cod_mdata_OR16[1,"Province"])) && (cod_mdata_OR16[2,"Province"] == "PROVINCE")){
-    cod_mdata_OR16 <- cod_mdata_OR16[-c(1, 2),]
+  if ((is.na(dataSheet[1,"Province"])) && (dataSheet[2,"Province"] == "PROVINCE")){
+    dataSheet <- dataSheet[-c(1, 2),]
   }
   # should I also remove the last row of totals?
 # ----------------------------------------------
-
-
 # ----------------------------------------------
 # Export the data - to test
-  COD_PNLP_Data_2016 <- cod_mdata_OR16
-  write.csv(COD_PNLP_Data_2016, "J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/COD_PNLP_Data_2016.csv")
-      #to test, wrote to my own drive: write.csv(cod_mdata_OR16, "C:/Users/abatzel/Documents/PCE/cod_mdata_OR16.csv")
+  
+  # return current data sheet
+  return(dataSheet)
+  
+  #COD_PNLP_Data_2016 <- dataSheet
+  # write.csv(dataSheet, paste0("J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/COD_PNLP_", year, "_", sheetname, ".csv"))
+      #to test, wrote to my own drive: write.csv(dataSheet, "C:/Users/abatzel/Documents/PCE/dataSheet.csv")
 # ----------------------------------------------
-
-
+}  #END OF FUNCTION
+# ----------------------------------------------
+  
+  
+# ----------------------------------------------
+# Use a loop to run prep_data() on each of the three sheets for PNLP 2016 data
+  years <- seq(2014, 2016)
+  sheetnames <- c('KIN', 'BDD', 'OR')
+  i <- 1
+  for(y in years) { 
+    for(s in sheetnames) { 
+        currentSheet <- prep_data(y, s)
+        
+        if (i==1) fullData <- currentSheet
+        if (i>1) fullData <- rbind(fullData, currentSheet)
+        i <- i+1
+      }  
+  }
+  
+  prep_data(2016, 'KIN')
+  prep_data(cod_mdata_BDD16)
+# ----------------------------------------------  
+  
+  
 # ----------------------------------------------
 # Clean values any missing, or format numbers such as 1,000 as 1000 
     # (make it a number not a string)
 
-
-# ----------------------------------------------
-
-
-# ----------------------------------------------
 # Reshape data
 
 # Append provinces within years, and then all years together
