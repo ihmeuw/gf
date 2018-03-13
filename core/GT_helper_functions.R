851494e892152f46d3cdd50938c4c8e548d62d0b
Guillermo <lgar89@gmail.com>
2018-03-13 11:07:18 -0600

    municipalities and departments name to code functions have a cache now

--- core/GT_helper_functions.R
+++ core/GT_helper_functions.R
@@ -21,32 +21,64 @@ if (!exists("munisGT")) {
 deptosGT             = unique(munisGT[, c("DEPTO__", "COD_DEPT__") ])
 vocalesTildes        = c("á"="a", "é"="e", "í"="i", "ó"= "o", "ú"="u")
 deptosGT$lookupDepto = str_replace_all(str_to_lower(deptosGT$DEPTO__), vocalesTildes)
-munisGT$lookupMuni  = str_replace_all(str_to_lower(munisGT$NOMBRE__), vocalesTildes)
+munisGT$lookupMuni   = str_replace_all(str_to_lower(munisGT$NOMBRE__), vocalesTildes)
 
-getMuniCodeByName <- function (nombreMuni, nombreDepto, field = "COD_MUNI__") {
-    if (is.na(nombreMuni)) {
-        warning(paste("Found an NA in municipality input nombreDepto was:", nombreDepto))
+deptoNameToCodeCache  = data.frame(original_name = character(), clean_name = character(), code  = integer(), stringsAsFactors = FALSE)
+muniNameToCodeCache  = data.frame(original_name = character(), clean_name = character(), depto_orig_name = character(), depto_code = integer(), code  = integer(), stringsAsFactors = FALSE)
+
+
+getMuniCodeByName <- function (nombreMuni_, nombreDepto_, field = "COD_MUNI__") {
+    if (is.na(nombreMuni_)) {
+        warning(paste("Found an NA in municipality input nombreDepto was:", nombreDepto_))
         muni = NA
     }
     else {
-        nombreMuni  = str_replace_all(str_to_lower(nombreMuni), vocalesTildes)
-        nombreDepto = str_replace_all( str_to_lower(nombreDepto), vocalesTildes)
-        depto       = deptosGT[which.min(stringdist(nombreDepto, deptosGT$lookupDepto, method = "cosine")),]
-        deptoMunis  = munisGT[munisGT$COD_DEPT__ == depto$COD_DEPT__,]
-        muni        = deptoMunis[which.min(stringdist(nombreMuni, deptoMunis$lookupMuni, method = "cosine")), field]
+        nombreMuni  = str_replace_all(str_to_lower(nombreMuni_), vocalesTildes)
+        nombreDepto = str_replace_all( str_to_lower(nombreDepto_), vocalesTildes)
+        
+        deptoCache = deptoNameToCodeCache[deptoNameToCodeCache$clean_name == nombreDepto, "code"]
+        
+        if (length(deptoCache)==0) {
+            depto   = deptosGT[which.min(stringdist(nombreDepto, deptosGT$lookupDepto, method = "cosine")), "COD_DEPT__"]
+            deptoNameToCodeCache[nrow(deptoNameToCodeCache)+1,] <<- list(original_name = nombreDepto_, clean_name = nombreDepto, code  = depto)
+        }
+        else {
+            depto   = deptoCache[1]
+        }
+        
+        muniCache = muniNameToCodeCache[muniNameToCodeCache$clean_name == nombreMuni & muniNameToCodeCache$depto_code == depto, "code"]
+        
+        if (length(muniCache) == 0) {
+            deptoMunis  = munisGT[munisGT$COD_DEPT__ == depto,]
+            muni        = deptoMunis[which.min(stringdist(nombreMuni, deptoMunis$lookupMuni, method = "cosine")), field]
+            muniNameToCodeCache[nrow(muniNameToCodeCache)+1,] <<- list(original_name = nombreMuni_, clean_name = nombreMuni, depto_orig_name = nombreDepto_, code_depto  = depto, code = muni)
+        }
+        else {
+            muni = muniCache
+        }
+        
     }
     muni
 }
 
-getDeptoCodeByName <- function (nombreDepto) {
-    if (is.na(nombreDepto)) {
+getDeptoCodeByName <- function (nombreDepto_) {
+    if (is.na(nombreDepto_)) {
         warning(paste("Found an NA in department input"))
         depto = NA
     }
     else {
-        nombreDepto = str_replace_all( str_to_lower(nombreDepto), vocalesTildes)
-        depto       = deptosGT[which.min(stringdist(nombreDepto, deptosGT$lookupDepto, method = "cosine")),]
-        depto       = depto$COD_DEPT__
+        nombreDepto = str_replace_all( str_to_lower(nombreDepto_), vocalesTildes)
+        
+        deptoCache = deptoNameToCodeCache[deptoNameToCodeCache$clean_name == nombreDepto, "code"]
+        
+        if (length(deptoCache)==0) {
+            depto       = deptosGT[which.min(stringdist(nombreDepto, deptosGT$lookupDepto, method = "cosine")),]
+            depto       = depto$COD_DEPT__
+            deptoNameToCodeCache[nrow(deptoNameToCodeCache)+1,] <<- list(original_name = nombreDepto_, clean_name = nombreDepto, code  = depto)
+        }
+        else {
+            depto = deptoCache
+        }
     }
     depto
 }
