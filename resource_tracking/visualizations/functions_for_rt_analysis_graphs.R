614e74c460e45e22a1a315c3790b6cda7833d773
raubsaurier <33269530+raubsaurier@users.noreply.github.com>
2018-03-14 15:59:29 -0700

    turn "disease" into a factor

--- resource_tracking/visualizations/functions_for_rt_analysis_graphs.R
+++ resource_tracking/visualizations/functions_for_rt_analysis_graphs.R
@@ -44,6 +44,7 @@ disease_names_for_plots <- function(gos_data){
   graphData[disease=='malaria', disease:='Malaria']
   graphData[disease=='tb', disease:='Tuberculosis']
   graphData[disease=='hss', disease:='RSSH']
+  graphData$disease <- factor(graphData$disease, levels=c("HIV/AIDS", "Malaria", "Tuberculosis", "RSSH"))
   return(graphData)
 }
 
