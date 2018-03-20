2e4d92efc0015b3b4cc0e121e8b3c60a070542c2
Audrey Batzel <abatzel@uw.edu>
2018-03-20 11:00:26 -0700

    create a loop to use makeGraph() on each health zone

--- outcome_measurement/malaria/cod/COD_PNLP_decriptive_analysis.R
+++ outcome_measurement/malaria/cod/COD_PNLP_decriptive_analysis.R
@@ -33,47 +33,70 @@
   # input file:
   # J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/COD_PNLP_Data_Indicators
     # csv files were produced by prep_COD_Malaria_data_function.R
-    df <- fread(paste0(dir,"/","COD_PNLP_Data_Indicators",".csv")) 
+    dt <- fread(paste0(dir,"/","COD_PNLP_Data_Indicators",".csv")) 
     
   # output files:
+    # exports all graphs to this folder:
+    # J:\Project\Evaluation\GF\outcome_measurement\cod\visualizations\PNLP_Data
 # ----------------------------------------------
 
-
-# ----------------------------------------------        
-  # create a big set of graphs where there's one plot for each health zone, showing each indicator over time
-  
-    # plot for one health zone, with each indicator
-     # time on x axis, number of new cases of malaria (indicator) on y axis
-      # time needs to encapsulate month & year
-      # select by health zone and then replicate for each health zone
-
-    
-    qplot(month, newCasesMalaria, data=df, facets = . ~ year, geom = c("point", "smooth"))
-
-    g <- ggplot(df[health_zone=='BAGATA' & subpopulation != "pregnantWomen"], aes(month, newCasesMalaria, color = subpopulation))
-    
-    g + geom_point() + facet_grid(.~year) + theme_bw()
     
 # ----------------------------------------------       
-
-    
-# ----------------------------------------------       
-  # convert months to standard, usable date... translate?
-1
-    # translate french to numeric version of month Janvier=1
-    df[month=='Janvier', month:="01"]
-    df[grepl("Janvier", month), month:="01"]
-    #df[df$month=='Janvier',]$month <- "1"
+#******ADD TO PREP CODE:
+  # translate french to numeric version of month Janvier=1
+        # dt[month=='Janvier', month:="01"]
+        # grepl() to make sure that any that may have trailing white space are also changed
+        dt[grepl("Janvier", month), month:="01"]
+        dt[grepl("Février", month), month:="02"]
+        dt[grepl("Mars", month), month:="03"]
+        dt[grepl("Avril", month), month:="04"]
+        dt[grepl("Mai", month), month:="05"]
+        dt[grepl("Juin", month), month:="06"]
+        dt[grepl("Juillet", month), month:="07"]
+        dt[grepl("Août", month), month:="08"]
+        dt[grepl("Septembre", month), month:="09"]
+        dt[grepl("Octobre", month), month:="10"]
+        dt[grepl("Novembre", month), month:="11"]
+        dt[grepl("Décembre", month), month:="12"]
+      
     # make sure it worked for all instances of month
     
     # make string version of the date
-    df[, stringdate:=paste('01', month, year, sep='/')]
+    dt[, stringdate:=paste('01', month, year, sep='/')]
     
     # combine year and month into one variable
-    df[, date:=as.Date(stringdate, "%d/%m/%Y")]
-        # will make a date format variable
-    
-    # add to prep code: check for warnings / characters
-    df[, newCasesMalaria := as.numeric(newCasesMalaria)]
+    dt[, date:=as.Date(stringdate, "%d/%m/%Y")]
   
+#******ADD TO PREP CODE: change numeric values to numeric type
+    # check for warnings / from stray characters
+    dt[, newCasesMalaria := as.numeric(newCasesMalaria)]
+# ----------------------------------------------   
+    
+    
+# ----------------------------------------------        
+    # function that takes health zone and indicator as arguments
+    # to map each indicator for each health zone over time
+ 
+  makeGraph <- function(hz){
+    g <- ggplot(dt[health_zone==hz & subpopulation != "pregnantWomen"], aes(date, newCasesMalaria, color = subpopulation))
+    
+    g + geom_point() + geom_line() + theme_bw() + ggtitle(hz)
+    
+    jpeg(file = paste0("J:/Project/Evaluation/GF/outcome_measurement/cod/visualizations/PNLP_Data/", hz, ".jpeg"))
+  }
+# ----------------------------------------------  
+    
+    
+# ----------------------------------------------      
+    
+  # make a vector of all health zones to loop through 
+  hz_vector <- dt[["health_zone"]]
+
+    # remove duplicates:
+    hz_vector <- unique(hz_vector)
+  
+  # loop through vector of health zones to make a graph for each
+  for (h in hz_vector){
+    makeGraph(h)
+  }
   
