419b6dd1fdcbde37725f8d96070c23d0b3350b20
Audrey Batzel <abatzel@uw.edu>
2018-03-20 16:05:02 -0700

    import long indicator data in order to use facet_wrap on indicator

--- outcome_measurement/malaria/cod/COD_PNLP_decriptive_analysis.R
+++ outcome_measurement/malaria/cod/COD_PNLP_decriptive_analysis.R
@@ -31,13 +31,13 @@
     dir <- "J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data"
  
   # input file:
-  # J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/COD_PNLP_Data_Indicators
+  # J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/COD_PNLP_Data_Indicators_Long
     # csv files were produced by prep_COD_Malaria_data_function.R
-    dt <- fread(paste0(dir,"/","COD_PNLP_Data_Indicators",".csv")) 
+    dt <- fread(paste0(dir,"/","COD_PNLP_Data_Indicators_Long",".csv")) 
     
   # output files:
-    # exports all graphs to this folder:
-    # J:\Project\Evaluation\GF\outcome_measurement\cod\visualizations\PNLP_Data
+    # exports all graphs to to a pdf document here:
+    # J:/Project/Evaluation/GF/outcome_measurement/cod/visualizations/PNLP_Data/Time Series Graphs.pdf
 # ----------------------------------------------
 
     
@@ -66,29 +66,25 @@
     
     # combine year and month into one variable
     dt[, date:=as.Date(stringdate, "%d/%m/%Y")]
-  
-#******ADD TO PREP CODE: change numeric values to numeric type
-    # check for warnings / from stray characters
-    dt[, newCasesMalaria := as.numeric(newCasesMalaria)]
+
 # ----------------------------------------------   
     
     
 # ----------------------------------------------        
-    # function that takes health zone and indicator as arguments
-    # to map each indicator for each health zone over time
+  # function that takes health zone and indicator as arguments
+  # to map each indicator for each health zone over time
+    
+    # add indicators as facets
  
   makeGraph <- function(hz){
-    g <- ggplot(dt[health_zone==hz & subpopulation != "pregnantWomen"], aes(date, newCasesMalaria, color = subpopulation))
+    g <- ggplot(dt[health_zone==hz & subpopulation != "pregnantWomen"], aes(date, newCasesMalariaMild, color = subpopulation, shape=formula_used, ymin=0))
     
-    g + geom_point() + geom_line() + theme_bw() + ggtitle(hz)
-    
-    jpeg(file = paste0("J:/Project/Evaluation/GF/outcome_measurement/cod/visualizations/PNLP_Data/", hz, ".jpeg"))
+    g + geom_point() + geom_line() + theme_bw() + ggtitle(hz) + scale_shape_manual(values=c(1,16))
   }
 # ----------------------------------------------  
     
     
 # ----------------------------------------------      
-    
   # make a vector of all health zones to loop through 
   hz_vector <- dt[["health_zone"]]
 
@@ -96,7 +92,9 @@
     hz_vector <- unique(hz_vector)
   
   # loop through vector of health zones to make a graph for each
-  for (h in hz_vector){
-    makeGraph(h)
+  pdf("J:/Project/Evaluation/GF/outcome_measurement/cod/visualizations/PNLP_Data/Time Series Graphs.pdf", height=6, width=9)
+  for (h in hz_vector) { 
+    print(makeGraph(h))
   }
-  
+  dev.off()
+# ---------------------------------------------- 
