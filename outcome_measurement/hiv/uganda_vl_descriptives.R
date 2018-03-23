779e96489d1c912f78b1ddb5f547aa821810df5e
Caitlin O'Brien-Carelli <ccarelli@ihme.washington.edu>
2018-03-23 15:14:14 -0700

    Creat outcome graphs by district and export as pdf

--- outcome_measurement/hiv/uganda_vl_descriptives.R
+++ outcome_measurement/hiv/uganda_vl_descriptives.R
@@ -43,23 +43,14 @@ uganda_vl[year==2018, sum(samples_received), by=month]
 # ----------------------------------------------
 # add useful variables and prep the data 
 
-# destring sex
+# rename sex
 uganda_vl[ , .(class(sex)) ]
-
-# rename sex, sex1 so that you can work with the sex variable
-setnames(uganda_vl, "sex", "sex1")
-names(uganda_vl)
-
-# destring sex1 into sex for use
-uganda_vl[sex1 == "m", sex := 0]
-uganda_vl[sex1 == "f", sex := 1]
-uganda_vl[sex1 == "x", sex := NA]
+uganda_vl[sex=='m', sex:='Male']
+uganda_vl[sex=='f', sex:='Female']
+uganda_vl[sex=='x', sex:='Unknown']
 
 uganda_vl[, .(sum(sex, na.rm=T)), by=year] # tells you the # of entries with female filter, not females
 uganda_vl [, .(sum(patients_received)), by=.(year, sex)] # patients received by sex by year
-
-# make date variable
-uganda_vl[, date:=as.Date(paste(year, month, '01', sep='-'), '%Y-%m-%d')]
 
 # -----------
 
@@ -70,6 +61,8 @@ uganda_vl_long <- melt(uganda_vl, id.vars=idVars)
 # make date variable
 uganda_vl_long[, date:=as.Date(paste(year, month, '01', sep='-'), '%Y-%m-%d')]
 
+# add date to uganda_vl
+uganda_vl[, date:=as.Date(paste(year, month, '01', sep='-'), '%Y-%m-%d')]
 
 # ----------------------------------------------
 ## COUNTRY LEVEL SUMMARY STATISTICS AND GRAPHS
@@ -79,15 +72,15 @@ uganda_vl_long[, date:=as.Date(paste(year, month, '01', sep='-'), '%Y-%m-%d')]
 
 uganda_vl[,
           .(total_patients = sum(patients_received), samples = sum(samples_received), 
-            total_results = sum(valid_results), total_suppressed = sum(suppressed),
+            results = sum(valid_results), total_suppressed = sum(suppressed),
             suppression_ratio=100*(sum(suppressed)/sum(valid_results))),
           by=.(month,year)]
 
 # table 1: patients, samples, valid results, suppressed, %suppressed over time
-# excludes March 2018 - change to present month
+# excludes March 2018 - change to present month if after March 2018
 table_1 <- uganda_vl[!(month==3 & year==2018),
                       .(total_patients = sum(patients_received), total_samples = sum(samples_received), 
-                        total_results = sum(valid_results), total_suppressed = sum(suppressed),
+                        results = sum(valid_results), total_suppressed = sum(suppressed),
                         suppression_ratio=100*(sum(suppressed)/sum(valid_results))),
                         by=.(month,year)]
 table_1 <- table_1[order(year, month)]
@@ -108,7 +101,7 @@ ggplot(table_1, aes(x=factor(month), y=total_patients, col=factor(year), group=y
   labs(title = "Patients received by month, year", caption="Source: Uganda VL Dashboard", colour="Year")
 
 # total valid viral load test results by month, year
-ggplot(table_1, aes(x=factor(month), y=total_results, col=factor(year), group=year)) + 
+ggplot(table_1, aes(x=factor(month), y=results, col=factor(year), group=year)) + 
   geom_point(size=2.5, alpha=0.8) + geom_line(alpha=0.4) + theme_bw() +
   xlab("Month") + ylab("Viral load test results") + 
   labs(title = "Viral load test results by month, year", caption="Source: Uganda VL Dashboard", colour="Year")
@@ -124,7 +117,7 @@ ggplot(table_1, aes(x=factor(month), y=total_suppressed, col=factor(year), group
 colors <- c('Not Suppressed'='#de2d26', 'Suppressed'='#fc9272')
 
 # stacked bar showing suppressed/not suppressed of valid test results by year 
-ggplot(table_1, aes(x=factor(year), y=total_results, fill='Not Suppressed')) + 
+ggplot(table_1, aes(x=factor(year), y=results, fill='Not Suppressed')) + 
     geom_bar(stat="identity") + 
     geom_bar(aes(y=total_suppressed, fill='Suppressed'), stat='identity') + 
     scale_fill_manual(name='', values=colors) + theme_bw() +
@@ -153,6 +146,164 @@ ggplot(table_1, aes(x=factor(month), y=suppression_ratio, col = factor(year), gr
 # use a loop to graph data reporting by month, year for each district, facility
 
 uganda_vl[, district_id, by=district_id] # 123 districts
+uganda_vl[,facility_id, by=facility_id] # 2040 facilities
+
+# create a version of uganda_vl_long without rejected samples - only relevant graphs
+# use for checking for missing data
+uvl_long <- uganda_vl_long[variable!="rejected_samples"]
+
+# label the variables for graph titles and put the graphs in an intuitive order
+uvl_long$variable <- factor(uvl_long$variable, 
+       levels=c("samples_received", "dbs_samples", "patients_received", "total_results", "valid_results", "suppressed"), 
+       labels=c("Samples Received","DBS Samples", "Patients","Total Results", "Valid Results", "Suppressed"))
+
+table(uvl_long$variable)
+
+# create a single plot to test
+ggplot(uvl_long[facility_id==2], aes(y=value, x=date, color=sex)) + 
+  geom_point() + 
+  geom_line(alpha=0.5) + 
+  facet_wrap(~variable, scales='free_y') +
+  xlab("Date") + ylab("Count") + theme_bw()
+
+
+# --------------------
+# create plots by district by looping over all districts, export as a PDF
+
+# create a long data set with totals by district
+uvl_dist <- uganda_vl[!(month==3 & year==2018),
+                     .(total_samples = sum(samples_received), total_dbs=sum(dbs_samples), total_patients = sum(patients_received),  
+                       total_results=sum(total_results), valid_results = sum(valid_results), total_suppressed = sum(suppressed)),
+                       by=.(district_id, sex, month, year)]
+
+# reshape indicators long for district data
+idVars <- c("district_id", "sex", "month", "year" )
+uvl_dist <- melt(uvl_dist, id.vars=idVars)
+
+# add date variable
+uvl_dist[, date:=as.Date(paste(year, month, '01', sep='-'), '%Y-%m-%d')]
+
+# label the variables for graph titles and put the graphs in an intuitive order
+uvl_dist$variable <- factor(uvl_dist$variable, 
+                                 levels=c("total_samples", "total_dbs", "total_patients", "total_results", "valid_results", "total_suppressed"), 
+                                 labels=c("Samples Received","DBS Samples", "Patients","Total Results", "Valid Results", "Suppressed"))
+
+# single test graphs
+ggplot(uvl_dist[district_id==11], aes(y=value, x=date, color=sex)) + 
+  geom_point() + 
+  geom_line(alpha=0.5) + 
+  facet_wrap(~variable, scales='free_y') +
+  xlab("Date") + ylab("Count") + theme_bw()
+
+
+# -----------
+# loop over all districts and export as a PDF
+# prints samples received, dbs samples, patients, results, valid results, and suppressed by sex, date for each district
+
+pdf('C:/Users/ccarelli/districts.pdf', height=6, width=9)
+for(i in seq(length(list_of_plots))) { 
+  print(list_of_plots[[i]])
+}
+dev.off()
+
+list_of_plots = NULL
+i=1
+for(f in unique(uvl_dist$district_id)) {
+  # make your graph
+  
+  list_of_plots[[i]] <- ggplot(uvl_dist[district_id==f], aes(y=value, x=date, color=sex)) + 
+    geom_point() + 
+    geom_line(alpha=0.5) + 
+    facet_wrap(~variable, scales='free_y') + xlab("Date") + ylab("Count") + theme_bw()
+  
+  i=i+1
+  
+}
+
+# --------------------
+
+
+
+# --------------------
+# create plots by facility by looping over all facilities, export as a PDF
+
+pdf('C:/Users/ccarelli/graphs.pdf', height=6, width=9)
+for(i in seq(length(list_of_plots))) { 
+  print(list_of_plots[[i]])
+}
+dev.off()
+
+list_of_plots = NULL
+i=1
+for(f in unique(uvl_long$facility_id)) {
+  # make your graph
+  
+  list_of_plots[[i]] <- ggplot(uvl_long[facility_id==f], aes(y=value, x=date, color=sex)) + 
+    geom_point() + 
+    geom_line(alpha=0.5) + 
+    facet_wrap(~variable, scales='free_y') + xlab("Date") + ylab("Count") + theme_bw()
+  
+  i=i+1
+}
+
+
+
+
+
+
+
+list_of_plots = NULL
+i=1
+for(f in unique(uvl_long$district_id)) {
+  # make your graph
+  
+  list_of_plots[[i]] <- ggplot(uvl_long[district_id==f], aes(y=value, x=date, color=factor(sex))) + 
+    geom_point() + 
+    geom_line(alpha=0.5) + 
+    facet_wrap(~variable, scales='free_y') +
+    theme_bw() + xlab("Date") 
+  
+  i=i+1
+}
+
+
+
+
+# create plots by district, looping over every district
+
+
+
+
+#+ labs(title=f) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # check if every district reported in every year, or some began later
 
@@ -213,42 +364,6 @@ ggplot(dist_10, aes(x=factor(year), y=results, col=factor(district_id), group=(d
 
 
 #---
-
-ggplot(uganda_vl_long[facility_id==1101], aes(y=value, x=date, color=sex)) + 
-  geom_point() + 
-  geom_line() + 
-  facet_wrap(~variable, scales='free_y')
-
-
-
-list_of_plots = NULL
-i=1
-for(f in unique(uganda_vl_long$facility_id)) {
-  # make your graph
-  
-  list_of_plots[[i]] <- ggplot(uganda_vl_long[facility_id==f], aes(y=value, x=date, color=sex)) + 
-    geom_point() + 
-    geom_line() + 
-    facet_wrap(~variable, scales='free_y')
-  
-  i=i+1
-}
-
-
-
-pdf('C:/Users/ccarelli/graphs.pdf', height=6, width=9)
-for(i in seq(length(list_of_plots))) { 
-  print(list_of_plots[[i]])
-}
-dev.off()
-
-
-
-
-
-
-
-
 
 
 
@@ -324,145 +439,10 @@ ggplot(uganda_vl, aes(x=patients_received, y=))
 
 
 
-
 # ----------------------------------------------
 # annual tables and plots
 
 # --------------------
-
-# 2014
-
-table_1_2014 <- uganda_vl[ year==2014,
-                           .(total_patients = sum(patients_received), samples = sum(samples_received), 
-                             test_results = sum(valid_results), total_suppressed = sum(suppressed),
-                             suppression_ratio=100*(sum(suppressed)/sum(valid_results))),
-                           by=month]
-
-# scatter of plot % suppressed over time
-ggplot(table_1_2014, aes(x = month, y = suppression_ratio )) +
-  geom_point() + theme_bw()
-
-
-# geom_point(shape = 21, size = 4, alpha=0.6) + theme_bw()
-
-# change merge file to import entire year for ease of interpretation
-# drop march of 2018?
-
-ggplot(plot_1, aes(x=total_patients, fill=factor(female))) + geom_bar(position="dodge")
-
-ggplot(plot_1, aes(x=month, y=total_patients, fill=factor(female))) + geom_bar(stat="identity", position="fill")
-
-ggplot(mtcars, aes(x = factor(cyl), fill = factor(am))) + geom_bar()
-
-# used stacked bar for tb status
-
-
-
-# --------------------
-
-table_1_2015 <- uganda_vl[ year==2015,
-                           .(total_patients = sum(patients_received), samples = sum(samples_received), 
-                             test_results = sum(valid_results), total_suppressed = sum(suppressed),
-                             suppression_ratio=100*(sum(suppressed)/sum(valid_results))),
-                           by=month]
-
-# scatter of plot % suppressed over time
-ggplot(table_1_2015, aes(x = month, y = suppression_ratio )) +
-  geom_point() + theme_bw()
-
-
-
-table_2015 <- uganda_vl[year==2015 ,
-                        .(total_patients = sum(patients_received), samples = sum(samples_received), 
-                          test_results = sum(valid_results), total_suppressed = sum(suppressed),
-                          suppression_ratio=100*(sum(suppressed)/sum(valid_results))),
-                        by=.(month,year)]
-table_2015 <- table_2015[order(month)]
-
-ggplot(table_2015, aes(x=month, y=total_suppressed, col = factor(year), group=year)) + 
-  geom_point() + geom_line() + theme_bw() 
-
-ggplot(table_2015, aes(x=month, y=test_results)) + 
-  geom_point() + geom_line() + theme_bw() 
-
-# --------------------
-
-table_1_2016 <- uganda_vl[ year==2016,
-                           .(total_patients = sum(patients_received), samples = sum(samples_received), 
-                             test_results = sum(valid_results), total_suppressed = sum(suppressed),
-                             suppression_ratio=100*(sum(suppressed)/sum(valid_results))),
-                           by=month]
-
-# scatter of plot % suppressed over time
-ggplot(table_1_2016, aes(x = month, y = suppression_ratio )) +
-  geom_point() + theme_bw()
-
-
-# --------------------
-
-table_1_2017 <- uganda_vl[ year==2017,
-                           .(total_patients = sum(patients_received), samples = sum(samples_received), 
-                             test_results = sum(valid_results), total_suppressed = sum(suppressed),
-                             suppression_ratio=100*(sum(suppressed)/sum(valid_results))),
-                           by=month]
-
-# scatter of plot % suppressed over time
-ggplot(table_1_2017, aes(x = month, y = suppression_ratio, color="clarity" )) +
-  geom_point() + theme_bw()
-
-#patients
-ggplot(table_1_2017, aes(x = month, y = total_patients, color="clarity" )) +
-  geom_point() + theme_bw()
-
-ggplot(table_1_2017, aes(x = month, y = samples, color="clarity" )) +
-  geom_point() + theme_bw()
-
-ggplot(table_1_2017, aes(x = month, y = test_results, color="clarity" )) +
-  geom_point() + theme_bw()
-
-ggplot(table_1_2017, aes(x = month, y = total_suppressed, color="clarity" )) +
-  geom_point() + theme_bw()
-
-# --------------------
-
-table_1_2018 <- uganda_vl[ year==2018,
-                           .(total_patients = sum(patients_received), samples = sum(samples_received), 
-                             test_results = sum(valid_results), total_suppressed = sum(suppressed),
-                             suppression_ratio=100*(sum(suppressed)/sum(valid_results))),
-                           by=month]
-
-# scatter of plot % suppressed over time
-ggplot(table_1_2018, aes(x = month, y = suppression_ratio )) +
-  geom_point() + theme_bw()
-
-
-
-# ----------------------------------------------
-
-uganda_vl[year==2014, sum(patients_received), by = month]
-uganda_vl[year==2015, sum(tb_status, na.rm=TRUE), by = month]
-
-
-# females, % suppressed over time
-#scatter plot
-ggplot(uganda_vl[sex==1], aes(x = month, y = sup_ratio, color = year )) +
-  geom_point() + theme_bw()
-
-ggplot(uganda_vl, aes)
-
-ggplot(uganda_vl[sex==1], aes(x = month, y = sup_ratio, color = )) +
-  geom_jitter() +
-  facet_grid(. ~ Species)
-
-
-# table : patients, samples, valid results, %suppressed, % not suppressed, % active TB
-
-tab1 <- uganda_vl[ ,
-                   .(total_patients=sum(patients_received), samples=sum(samples_received), valid_results=sum(valid_results), total_suppressed=sum(suppressed), active_tb=sum(tb_status, na.rm=TRUE), 
-                     suppression_ratio=100*(sum(suppressed)/sum(valid_results)), not_suppressed=100*(1-(sum(suppressed)/sum(valid_results))), 
-                     suppressed_w_tb=100*(sum(sup_tb, na.rm=TRUE)/sum(suppressed)), suppressed_no_tb=100*(sum(sup_no_tb, na.rm=TRUE)/sum(suppressed))),
-                   
-                   by=.(month, year)]
 
 # export table 1 as a .csv
 write.csv(tab1, "J:/Project/Evaluation/GF/outcome_measurement/uga/vl_dashboard/tab1.csv")
