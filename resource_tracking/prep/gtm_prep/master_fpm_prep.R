fefa560f7681401eb66a1fe48a761534206dddf7
raubsaurier <33269530+raubsaurier@users.noreply.github.com>
2018-03-23 13:39:32 -0700

    update gtm code to prep more files

--- resource_tracking/prep/gtm_prep/master_fpm_prep.R
+++ resource_tracking/prep/gtm_prep/master_fpm_prep.R
@@ -27,26 +27,30 @@ library(zoo)
 # ----------------------------------------------
 
 loc_id <- "gtm"
-dir <- 'J:/Project/Evaluation/GF/resource_tracking/gtm/gf/fpm/'
-file_list <- read.csv(paste0(dir, "fpm_budget_filelist.csv"))
+dir <- 'J:/Project/Evaluation/GF/resource_tracking/gtm/gf/'
+file_list <- read.csv(paste0(dir, "fpm/fpm_budget_filelist.csv"))
 
-for(i in 1:length(file_list$filename)){
+for(i in 1:length(file_list$file_name)){
   if(file_list$format[i]=="detailed"){ ## fpm detailed budgets 
-    tmpData <- prep_fpm_detailed_budget(dir, paste0(file_list$filename[i], file_list$extension[i]), as.character(file_list$sheet[i]),
+    tmpData <- prep_fpm_detailed_budget(dir, file_list$file_name[i], as.character(file_list$sheet[i]),
                                         ymd(file_list$start_date[i]), file_list$qtr_number[i], file_list$disease[i], file_list$period[i], 
                                         file_list$lang[i], file_list$grant_number[i])
   } else if (file_list$format[i]=="summary"){ ## only summary level data - no municipalities 
-    tmpData <- prep_fpm_summary_budget(dir, paste0(file_list$filename[i], file_list$extension[i]), as.character(file_list$sheet[i]),
+    tmpData <- prep_fpm_summary_budget(dir, file_list$file_name[i], as.character(file_list$sheet[i]),
                                        ymd(file_list$start_date[i]), file_list$qtr_number[i], file_list$disease[i], file_list$period[i], 
                                        file_list$grant_number[i], file_list$recipient[i])
     tmpData$loc_id <- "gtm"
   } else if (file_list$format[i]=="other"){ ## there's an older version of detailed fpm budgets
-    tmpData <- prep_other_detailed_budget(dir, paste0(file_list$filename[i], file_list$extension[i]), as.character(file_list$sheet[i]),
+    tmpData <- prep_other_detailed_budget(dir, file_list$file_name[i], as.character(file_list$sheet[i]),
                                         ymd(file_list$start_date[i]), file_list$qtr_number[i], file_list$disease[i], file_list$period[i], 
                                         file_list$lang[i], file_list$grant_number[i])
-    tmpData$loc_id <- "gtm"
+  } else if (file_list$format[i]=="pudr"){ 
+    tmpData <- prep_gtm_pudr(dir, file_list$file_name[i], as.character(file_list$sheet[i]),
+                                          ymd(file_list$start_date[i]), file_list$qtr_number[i], file_list$disease[i], file_list$period[i], 
+                                          file_list$grant_number[i], file_list$data_source[i])
+    tmpData$recipient <- loc_id ##change this when we get SR info
   }
-  
+  tmpData$loc_id <- loc_id
   tmpData$data_source <- file_list$data_source[i]
   
   if(i==1){
@@ -59,9 +63,8 @@ for(i in 1:length(file_list$filename)){
 }
 
 resource_database$budget <- as.numeric(resource_database$budget)
-
+resource_database$expenditure<- as.numeric(resource_database$expenditure)
 ## since we only have budget data, include exp and disbursed as 0:  
-resource_database$expenditure <- 0 
 resource_database$disbursement<- 0 
 resource_database$source <- "gf"
 
@@ -73,7 +76,7 @@ data_check1<- as.data.frame(resource_database[, sum(budget, na.rm = TRUE),by = c
 ##output dataset to the correct folder as a csv: 
 
 
-write.csv(resource_database, "J:/Project/Evaluation/GF/resource_tracking/gtm/prepped/prepped_fpm_budgets.csv", row.names = FALSE,
+write.csv(resource_database, "J:/Project/Evaluation/GF/resource_tracking/gtm/prepped/prepped_fpm_pudr.csv", row.names = FALSE,
           fileEncoding = "latin1")
 
 
