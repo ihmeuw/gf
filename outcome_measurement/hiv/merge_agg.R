5d5a44dcc4b4890dba3f7b40339a90cd78a13b52
Caitlin O'Brien-Carelli <ccarelli@ihme.washington.edu>
2018-03-26 12:33:09 -0700

    Fix merge code error

--- outcome_measurement/hiv/merge_agg.R
+++ outcome_measurement/hiv/merge_agg.R
@@ -66,89 +66,6 @@ for(f in files) {
 str(full_data)
 
 # ----------------------------------------------
-# upload the facilities data and check for disparate values
-
-# reset working directory to main folder
-dir <- setwd("J:/Project/Evaluation/GF/outcome_measurement/uga/vl_dashboard")
-
-facilities <- readRDS(paste0(dir,"/facilities.rds"))
-str(facilities)
-
-# list unique facility ids
-full_data [,facility_id, by=facility_id] # 2042 values
-facilities[, facility_id, by=facility_id] # 2012 values
-
-# identify mismatches
-full_data$facility_id[!full_data$facility_id %in% facilities$facility_id] 
-check <- full_data$facility_id[!full_data$facility_id %in% facilities$facility_id] 
-unique(check) # 91 facility ids are in the full data but not the facility names list
-
-facilities$facility_id[!facilities$facility_id %in% full_data$facility_id] # 61 names are on the list but not in the data
-
-# ---------------
-# check for missing districts 
-# 0 missing district ids
-full_data$district_id[!full_data$district_id %in% facilities$district_id] 
-facilities$district_id[!facilities$district_id %in% full_data$district_id]
-
-# ---------------
-
-
-# ----------------------------------------------
-# merge the facility and district names into the full data set
-
-# add table 1 to uganda_vl 
-# these values will repeat to match the number of values in the data table
-uvl_sex <- merge(full_data, 
-                 facilities,
-                 by='facility_id', all.x=TRUE)
-
-# handle missing names
-uvl_sex [is.na(facility_name), name:=paste0('Facility #',facility_id)]
-
-# ---------------
-
-#reset hub, district ids to y values; set x values as process districts and hubs
-uvl_sex <- uvl_sex[ ,
-             .(facility_id, process_id=district_id.x, process_hub=hub_id.x, samples_received,
-               patients_received, suppressed,  valid_results, rejected_samples,  dbs_samples,
-               total_results, year, month, sex, district_id=district_id.y, facility_name,     
-               hub_id=hub_id.y, district_name)]
-
-# rename sex
-uvl_sex[ , .(class(sex)) ]
-uvl_sex[sex=='m', sex:='Male']
-uvl_sex[sex=='f', sex:='Female']
-uvl_sex[sex=='x', sex:='Unknown']
-
-
-#save the final data as an RDS
-saveRDS(full_data, file= paste0(dir, "sex_data.rds") )
-
-# ----------------------------------------------
-
-
-
-# ----------------------------------------------
-# additional to code to identify merge mismatches and downloading errors
-
-# check for mismatch in district IDs
-uvl_sex[district_id.x!=district_id.y, .(district_id.x, district_id.y, district_name), by=district_name]
-
-# Gomba is under both 75 and 89; Rakai 134 snd 80
-
-uvl_sex[district_id.x!=district_id.y, .(district_id.x, district_id.y, district_name)]
-
-# Kabira HC III GOVT, Kalisizo Hospital, Nabigasa HC III, Mutukula HC III, Kasensero HC II, Kirumba  HC III
-uvl_sex[district_id.x==134, .(unique(facility_name)),by=.(district_name, district_id.x, district_id.y)]  
-uvl_sex[district_id.y==80, .(unique(facility_name)),by=.(district_name, district_id.x, district_id.y)]
-
-# Kitwe HC II
-uvl_sex[district_id.x==75, .(unique(facility_name)),by=.(district_name, district_id.x, district_id.y)]  
-uvl_sex[district_id.y==89, .(unique(facility_name)),by=.(district_name, district_id.x, district_id.y)]
-
-
-# ----------------------------------------------
 
 #  stats to check if the sex, tb data disaggregated data downloaded correctly 
 
@@ -173,4 +90,85 @@ full_data[sex=='x', .(total_sup=sum(samples_received)), by=.(month, year)]
 full_data[sex=='x', .(total_sup=sum(suppressed)), by=.(month, year)]
 
 # ----------------------------------------------
+
+# ----------------------------------------------
+# upload the facilities names, ids and check for disparate values
+
+# reset working directory to main folder
+setwd("J:/Project/Evaluation/GF/outcome_measurement/uga/vl_dashboard")
+dir <- "J:/Project/Evaluation/GF/outcome_measurement/uga/vl_dashboard"
+
+facilities <- readRDS(paste0(dir,"/facilities.rds"))
+str(facilities)
+
+# list unique facility ids
+full_data [,facility_id, by=facility_id] # 2042 values
+facilities[, facility_id, by=facility_id] # 2012 values
+
+# identify mismatches
+full_data$facility_id[!full_data$facility_id %in% facilities$facility_id] 
+check <- full_data$facility_id[!full_data$facility_id %in% facilities$facility_id] 
+unique(check) # 91 facility ids are in the full data but not the facility names list
+
+facilities$facility_id[!facilities$facility_id %in% full_data$facility_id] # 61 names are on the list but not in the data
+
+# ---------------
+# check for missing districts 
+# 0 missing district ids
+full_data$district_id[!full_data$district_id %in% facilities$district_id] 
+facilities$district_id[!facilities$district_id %in% full_data$district_id]
+
+# ---------------
+
+# ----------------------------------------------
+# merge the facility and district names into the full data set
+
+uvl_sex <- merge(full_data, 
+                 facilities,
+                 by='facility_id', all.x=TRUE)
+
+# handle missing names
+uvl_sex [is.na(facility_name), name:=paste0('Facility #',facility_id)]
+
+# ----------------------------------------------
+# additional to code to identify merge mismatches and downloading errors
+
+# check for mismatch in district IDs
+uvl_sex[district_id.x!=district_id.y, .(district_id.x, district_id.y, district_name), by=district_name]
+
+# Gomba is under both 75 and 89; Rakai 134 snd 80
+
+uvl_sex[district_id.x!=district_id.y, .(district_id.x, district_id.y, district_name)]
+
+# Kabira HC III GOVT, Kalisizo Hospital, Nabigasa HC III, Mutukula HC III, Kasensero HC II, Kirumba  HC III
+uvl_sex[district_id.x==134, .(unique(facility_name)),by=.(district_name, district_id.x, district_id.y)]  
+uvl_sex[district_id.y==80, .(unique(facility_name)),by=.(district_name, district_id.x, district_id.y)]
+
+# Kitwe HC II
+uvl_sex[district_id.x==75, .(unique(facility_name)),by=.(district_name, district_id.x, district_id.y)]  
+uvl_sex[district_id.y==89, .(unique(facility_name)),by=.(district_name, district_id.x, district_id.y)]
+
+
+
+# ---------------
+
+#reset hub, district ids to y values; set x values as process districts and hubs
+uvl_sex <- uvl_sex[ ,
+                    .(facility_id, process_id=district_id.x, process_hub=hub_id.x, samples_received,
+                      patients_received, suppressed,  valid_results, rejected_samples,  dbs_samples,
+                      total_results, year, month, sex, district_id=district_id.y, facility_name,     
+                      hub_id=hub_id.y, district_name)]
+
+# rename sex
+uvl_sex[ , .(class(sex)) ]
+uvl_sex[sex=='m', sex:='Male']
+uvl_sex[sex=='f', sex:='Female']
+uvl_sex[sex=='x', sex:='Unknown']
+
+
+#save the final data as an RDS
+saveRDS(uvl_sex, file= paste0(dir, "/sex_data.rds") )
+
+# ----------------------------------------------
+
 
