8a4cd34bd4b3494e93a61ed2ac957c68487bc407
Audrey Batzel <abatzel@ihme.washington.edu>
2018-04-06 17:06:48 -0700

    read in each sheet of the .xlsx, clean them, bind them together

--- outcome_measurement/malaria/gtm/prep_GTM_mal_data.R
+++ outcome_measurement/malaria/gtm/prep_GTM_mal_data.R
@@ -56,7 +56,7 @@
       "INFORME ANTIMALARICOS 2017"
     )
   
-  # sheets within each file to bind together (months)
+  # sheets within each file to bind together (months) for each year 
     sheetnames12 <- c("ENE12", "FEB12", "MAR2012", "ABR12", "MAY12", "JUL12", "AGO12", "SEPT12", "OCT12", "NOV12", "DIC12")
     sheetnames13 <- c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Octubre"," Noviembre")
     sheetnames14 <- c("ABR", "MAY", "JUN","JUL", "AGO", "SEP", "OCT", "NOV", "DIC")
@@ -64,31 +64,62 @@
     sheetnames16 <- c("ENE16", "FEB16", "MAR16", "ABR16", "MAY16", "JUN16")
     sheetnames17 <- c("Enero 2017", "Febrero 2017", "Abril 2017", "Mayo 2017", "Junio 2017") 
     
-    
-    # 2012 data   
+  # July 2012 - December 2012 column names (some order is reversed, names the same)
+    columnNames2 <- c("DAS_code", "DAS", "Cloroquina250_previousBalance", "Cloroquina250_topLevelEntries", "Cloroquina250_deliveredToUser", 
+                     "Cloroquina250_notDeliveredToUser", "Cloroquina250_realDemand", "Cloroquina250_readjustments", "Cloroquina250_nextMonthBalance", 
+                     "Cloroquina250_existenceInBodega", "Cloroquina250_avgMonthlyDemand", "Cloroquina250_monthsAvailable", "Primaquina15_previousBalance", 
+                     "Primaquina15_topLevelEntries", "Primaquina15_deliveredToUser", "Primaquina15_notDeliveredToUser", "Primaquina15_realDemand", 
+                     "Primaquina15_readjustments", "Primaquina15_nextMonthBalance", "Primaquina15_existenceInBodega", "Primaquina15_avgMonthlyDemand", 
+                     "Primaquina15_monthsAvailable", "Primaquina5_previousBalance", "Primaquina5_topLevelEntries", "Primaquina5_deliveredToUser", 
+                     "Primaquina5_notDeliveredToUser", "Primaquina5_realDemand", "Primaquina5_readjustments", "Primaquina5_nextMonthBalance", 
+                     "Primaquina5_existenceInBodega", "Primaquina5_avgMonthlyDemand", "Primaquina5_monthsAvailable")
+  
+  # January 2012 - May 2012 column names
+    columnNames1 <- c("DAS_code", "DAS", "Primaquina15_previousBalance", "Primaquina15_topLevelEntries", "Primaquina15_deliveredToUser", "Primaquina15_notDeliveredToUser",
+                      "Primaquina15_realDemand", "Primaquina15_readjustments", "Primaquina15_nextMonthBalance", "Primaquina15_existenceInBodega", "Primaquina15_avgMonthlyDemand", 
+                      "Primaquina15_monthsAvailable", "Primaquina5_previousBalance", "Primaquina5_topLevelEntries", "Primaquina5_deliveredToUser", "Primaquina5_notDeliveredToUser", 
+                      "Primaquina5_realDemand", "Primaquina5_readjustments", "Primaquina5_nextMonthBalance", "Primaquina5_existenceInBodega", "Primaquina5_avgMonthlyDemand", 
+                      "Primaquina5_monthsAvailable", "Cloroquina250_previousBalance", "Cloroquina250_topLevelEntries", "Cloroquina250_deliveredToUser", "Cloroquina250_notDeliveredToUser",
+                      "Cloroquina250_realDemand", "Cloroquina250_readjustments", "Cloroquina250_nextMonthBalance", "Cloroquina250_existenceInBodega", "Cloroquina250_avgMonthlyDemand", 
+                      "Cloroquina250_monthsAvailable" )
+
+  # 2012 data processing:
+    # this is really slow....
     i = 1
-    # read in file, looping through each sheet and binding them together   
-      for (s in sheetnames12){
-        currentDT <- data.table(read_excel("J:/Project/Evaluation/GF/outcome_measurement/gtm/MALARIA/Antimalaricos/INFORME ANTIMALARICOS 2012.xlsx",  sheet=s))
+    # loop through each sheet and clean & bind them together into a DT for each year
+      for (s in sheetnames12){  # for each of the sheets in 2012 data
         
-        # add a column to keep track of the sheetname/month within the dt
+        # read in current sheet
+          currentDT <- data.table(read_excel("J:/Project/Evaluation/GF/outcome_measurement/gtm/MALARIA/Antimalaricos/INFORME ANTIMALARICOS 2012.xlsx",  sheet=s))
+        
+        # drop extra column that is in the MAY12 data tacked on at the end
+          ifelse ((s == "MAY12"), currentDT <- currentDT[,-c(33)], currentDT <- currentDT)
+          
+        # set column names, different for Jan-May, and Jul-Dec (no Jun)
+          ifelse (i < 6, names(currentDT) <- columnNames1, names(currentDT) <- columnNames2)
+        
+        # add a column to keep track of the sheetname/month within the DT as we loop through each sheet with the doc
           currentDT$month <- s
+          
+        # and a column for the year - add this to a function later, hard-coded for now
+          currentDT$year <- "2012"
         
         # delete first two rows in sheets ENE12-MAY12, first three rows in JUL12-DIC12
+          # ifelse ( i < 6, (currentDT <- currentDT[-c(1:3),]), (currentDT <- currentDT[-c(1:4),]))
+          currentDT <- currentDT[-c(1:3),]
           
-        
-        
-        if (i==1) dt <- currentDT
-        if (i>1)  dt <- rbind(dt, currentDT, fill=TRUE)
-        i <- i + 1
-        
-        # print the sheet that processed - for us to see where it's working/goes wrong
+        # delete first two rows in sheets ENE12-MAY12, first three rows in JUL12-DIC12
+          currentDT <- currentDT[complete.cases(currentDT[ , 1]),]
+          
+          if (i==1) dt <- currentDT
+          if (i>1)  dt <- rbind(dt, currentDT, fill=TRUE)
+          i <- i + 1
+          
+        # print the sheet that processed - for us to see where it's working/goes wrong for now
           print(s)
       }
         
 # ----------------------------------------------
-# ---------------------------------------------- 
-# ----------------------------------------------
-        
+
         
         
