019142fd739d58df91bd43ca174b806e1776388c
Audrey Batzel <abatzel@ihme.washington.edu>
2018-04-10 16:06:19 -0700

    use linear regression to identify outliers

--- outcome_measurement/malaria/cod/COD_PNLP_missing_data_analysis.R
+++ outcome_measurement/malaria/cod/COD_PNLP_missing_data_analysis.R
@@ -20,7 +20,6 @@
     library(rlang)
     library(zoo)
     library(tidyr)
-    library(dplyr)
     library(gridExtra)
 # --------------------
 
@@ -37,6 +36,7 @@
       dt <- fread(paste0(dir,"/","COD_PNLP_Data_Indicators_Long.csv")) 
       dt_wide <- fread(paste0(dir,"/","COD_PNLP_Data_Indicators_Wide.csv")) 
       dt2 <- fread(paste0(dir, "/", "COD_PNLP_Data_Interventions_Long.csv"))
+      fullData <- fread(paste0(dir, "/", "Full Data for MI.csv"))
         
     # output files:
 
@@ -46,6 +46,21 @@
       dt2[, value := as.numeric(value)]
 # ----------------------------------------------
 
+
+# ----------------------------------------------
+# Use linear regression to identify outliers
+idVars = names(fullData)[1:5]
+regVars = names(fullData)[!names(fullData) %in% idVars]
+
+i = 1
+outcome = regVars[i]
+otherVars = regVars[-i]
+formula = paste(c(outcome, '~', paste(otherVars, collapse='+')), collapse='')
+formula = as.formula(formula)
+lmFit = lm()
+
+# ----------------------------------------------
+      
 
 # ----------------------------------------------
   # Outlier Analysis - Indicators
@@ -59,20 +74,33 @@
       `severeMalariaTreated` = "Cases of Severe Malaria Treated",
       `malariaDeaths` = "Number of Deaths from Malaria"
     )
-
+    
+  # Make a histogram for each indicator by supopulation and dps
     pdf("J:/Project/Evaluation/GF/outcome_measurement/cod/visualizations/PNLP_Data/Outlier Analysis Indicators.pdf", height=6, width=9)   
     for (i in indicators){
-    # Subset of data
-        dt_byIndicator <- dt[indicator == i,
-                             indicator,
-                             by= c('date', 'dps', 'health_zone', 'subpopulation', 'value')]
-        
-    # Make a histogram for each variable
-        # qplot(dt_byIndicator$value, geom="histogram")
-        
-        g <- ggplot(data=dt_byIndicator, aes(dt_byIndicator$value)) + geom_histogram(bins = 50, fill="cornflowerblue") 
-        g <- g + ggtitle(indicator_names[i]) + xlab("Value") + ylab("Count") + facet_grid(subpopulation ~ dps, scales="free")
-        print(g)
+      # Subset of data
+          dt_byIndicator <- dt[indicator == i,
+                               indicator,
+                               by= c('date', 'dps', 'health_zone', 'subpopulation', 'value')]
+     
+      # for geom_vline() - calculate the diff percentiles for the data
+          p95 <- dt_byIndicator[, quantile(value, .95, na.rm=T), by=c('dps', 'subpopulation')]
+          p99 <- dt_byIndicator[, quantile(value, .99, na.rm=T), by=c('dps', 'subpopulation')]
+          p995 <- dt_byIndicator[, quantile(value, .995, na.rm=T), by=c('dps', 'subpopulation')]
+          p999 <- dt_byIndicator[, quantile(value, .999, na.rm=T), by=c('dps', 'subpopulation')]
+          # p95 = dt_byIndicator[, quantile(value, .95, na.rm=T),by=c('dps')]
+  
+      g <- ggplot(dt_byIndicator, aes(value)) + 
+          geom_histogram(bins = 50, fill="cornflowerblue") + 
+          ggtitle(paste0("Outlier Analysis for ", indicator_names[i])) + xlab("Value") + ylab("Count") +
+          geom_vline(data=p95, aes(xintercept=V1, color="green"), linetype="dashed") + 
+          geom_vline(data=p99, aes(xintercept=V1, color="yellow"), linetype="dashed") + 
+          geom_vline(data=p995, aes(xintercept=V1, color="orange"), linetype="dashed") + 
+          geom_vline(data=p999, aes(xintercept=V1, color="red"), linetype="dashed") + 
+          scale_color_manual(name = "Percentile", labels = c("95%", "99%", "99.5%", "99.9%"), values = c("green", "yellow", "orange", "red")) +
+          facet_grid(subpopulation~dps, scales='free') + 
+          xlab("Value") + ylab("Count")
+      print(g)
     }
     dev.off()  
 # ----------------------------------------------
@@ -95,7 +123,7 @@
       `healthFacilities` = "Health Facilities Reporting",
       `reports` = "Number of Reports"
     )
-    
+  # Make a histogram for each intervention by subpop and dps
     pdf("J:/Project/Evaluation/GF/outcome_measurement/cod/visualizations/PNLP_Data/Outlier Analysis Interventions.pdf", height=6, width=9)   
     for (i in interventions){
       # Subset of data
@@ -103,10 +131,23 @@
                                intervention,
                                by= c('date', 'dps', 'health_zone', 'intervention_spec', 'value')]
           
-      # Make a histogram for each variable
-          g <- ggplot(data=dt_byIntervention, aes(dt_byIntervention$value)) + geom_histogram(bins = 50) 
-          g <- g + ggtitle(intervention_names[i]) + xlab("Value") + ylab("Count") + facet_grid(intervention_spec ~ dps, scales="free")
-          print(g)
+      # for geom_vline() - calculate the diff percentiles for the data
+          p95 <- dt_byIntervention[, quantile(value, .95, na.rm=T), by=c('dps', 'intervention_spec')]
+          p99 <- dt_byIntervention[, quantile(value, .99, na.rm=T), by=c('dps', 'intervention_spec')]
+          p995 <- dt_byIntervention[, quantile(value, .995, na.rm=T), by=c('dps', 'intervention_spec')]
+          p999 <- dt_byIntervention[, quantile(value, .999, na.rm=T), by=c('dps', 'intervention_spec')]
+      
+      g <- ggplot(dt_byIntervention, aes(value)) + 
+        geom_histogram(bins = 75, fill="cornflowerblue") + 
+        ggtitle(paste0("Outlier Analysis for ", intervention_names[i])) + xlab("Value") + ylab("Count") +
+        geom_vline(data=p95, aes(xintercept=V1, color="blue"), linetype="dashed") + 
+        geom_vline(data=p99, aes(xintercept=V1, color="yellow"), linetype="dashed") + 
+        geom_vline(data=p995, aes(xintercept=V1, color="orange"), linetype="dashed") + 
+        geom_vline(data=p999, aes(xintercept=V1, color="red"), linetype="dashed") + 
+        scale_color_manual(name = "Percentile", labels = c("95%", "99%", "99.5%", "99.9%"), values = c("green", "yellow", "orange", "red")) +
+        facet_grid(intervention_spec~dps, scales='free') + 
+        xlab("Value") + ylab("Count")
+      print(g)
     }
     dev.off()  
 # ---------------------------------------------- 
