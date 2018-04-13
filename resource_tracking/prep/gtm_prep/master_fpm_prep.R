36f632c3e26285c91165af741e19d88dfc0b90ad
raubsaurier <33269530+raubsaurier@users.noreply.github.com>
2018-04-13 16:35:20 -0700

    update the gtm pudr code to include SR information

--- resource_tracking/prep/gtm_prep/master_fpm_prep.R
+++ resource_tracking/prep/gtm_prep/master_fpm_prep.R
@@ -35,20 +35,23 @@ for(i in 1:length(file_list$file_name)){
     tmpData <- prep_fpm_detailed_budget(dir, file_list$file_name[i], as.character(file_list$sheet[i]),
                                         ymd(file_list$start_date[i]), file_list$qtr_number[i], file_list$disease[i], file_list$period[i], 
                                         file_list$lang[i], file_list$grant_number[i])
+    tmpData$disbursement<- 0 
   } else if (file_list$format[i]=="summary"){ ## only summary level data - no municipalities 
     tmpData <- prep_fpm_summary_budget(dir, file_list$file_name[i], as.character(file_list$sheet[i]),
                                        ymd(file_list$start_date[i]), file_list$qtr_number[i], file_list$disease[i], file_list$period[i], 
                                        file_list$grant_number[i], file_list$recipient[i])
     tmpData$loc_name <- "gtm"
+    tmpData$disbursement<- 0 
   } else if (file_list$format[i]=="other"){ ## there's an older version of detailed fpm budgets
     tmpData <- prep_other_detailed_budget(dir, file_list$file_name[i], as.character(file_list$sheet[i]),
                                         ymd(file_list$start_date[i]), file_list$qtr_number[i], file_list$disease[i], file_list$period[i], 
                                         file_list$lang[i], file_list$grant_number[i])
+    tmpData$disbursement<- 0 
   } else if (file_list$format[i]=="pudr"){ 
     tmpData <- prep_gtm_pudr(dir, file_list$file_name[i], as.character(file_list$sheet[i]),
                                           ymd(file_list$start_date[i]), file_list$qtr_number[i], file_list$disease[i], file_list$period[i], 
-                                          file_list$grant_number[i], file_list$data_source[i])
-    tmpData$recipient <- loc_name ##change this when we get SR info
+                                          file_list$grant_number[i], file_list$data_source[i], loc_name)
+   
   }
   tmpData$loc_name <- loc_name
   tmpData$data_source <- file_list$data_source[i]
@@ -67,8 +70,8 @@ resource_database$adm1 <- 128
 resource_database$adm2 <- resource_database$adm1
 resource_database$budget <- as.numeric(resource_database$budget)
 resource_database$expenditure<- as.numeric(resource_database$expenditure)
+resource_database$disbursement<- as.numeric(resource_database$disbursement)
 ## since we only have budget data, include exp and disbursed as 0:  
-resource_database$disbursement<- 0 
 resource_database$source <- "gf"
 
 # ----------------------------------------------
@@ -76,10 +79,55 @@ resource_database$source <- "gf"
 data_check1<- as.data.frame(resource_database[, sum(budget, na.rm = TRUE),by = c("grant_number", "disease")])
 
 # ----------------------------------------------
+##### Map to the GF Modules and Interventions #####
+##run the map_modules_and_interventions.R script first
+
+gtmData <- strip_chars(resource_database, unwanted_array, remove_chars)
+gtmData[is.na(module), module:=intervention]
+
+
+mapping_list <- load_mapping_list("J:/Project/Evaluation/GF/mapping/multi_country/intervention_categories/intervention_and_indicator_list.xlsx")
+
+## before we get it ready for mapping, copy over so we have the correct punctuation for final mapping: 
+final_mapping <- copy(mapping_list)
+final_mapping$disease <- NULL
+setnames(final_mapping, c("module", "intervention"), c("gf_module", "gf_intervention"))
+mapping_list$coefficient <- 1
+
+
+gf_mapping_list <- total_mapping_list("J:/Project/Evaluation/GF/mapping/multi_country/intervention_categories/intervention_and_indicator_list.xlsx",
+                                      mapping_list, unwanted_array, remove_chars)
+# ----------------------------------------------
+# USE THIS TO CHECK FOR ANY MODULE/INTERVENTION COMBOS IN THE DATA THAT AREN'T IN THE MAPPING
+# gf_concat <- paste0(gf_mapping_list$module, gf_mapping_list$intervention)
+# gtm_concat <- paste0(gtmData$module, gtmData$intervention)
+# unmapped_mods <- gtm_concat[!gtm_concat%in%gf_concat]
+
+
+
+gtm_init_mapping <- merge(gtmData, gf_mapping_list, by=c("module", "intervention", "disease"), all.x=TRUE,allow.cartesian = TRUE)
+
+##use this to check if any modules/interventions were dropped:
+# dropped_gf <- gtm_init_mapping[is.na(gtm_init_mapping$code)]
+
+mappedGtm <- merge(gtm_init_mapping, final_mapping, by="code")
+mappedGtm$budget <- mappedGtm$budget*mappedGtm$coefficient
+mappedGtm$expenditure <- mappedGtm$expenditure*mappedGtm$coefficient
+mappedGtm$disbursement <- mappedGtm$disbursement*mappedGtm$coefficient
+
+##sum to make sure that budget numbers aren't dropped:
+
+# data_check1 <- gtmData[, sum(budget, na.rm = TRUE),by = c( "module","intervention","disease")]
+# data_check2 <-mappedGtm[, sum(budget, na.rm = TRUE),by = c("module", "intervention","disease")]
+
+mappedGtm$year <- year(mappedGtm$start_date)
+
+# ----------------------------------------------
+
 ##output dataset to the correct folder as a csv: 
 
 
-write.csv(resource_database, "J:/Project/Evaluation/GF/resource_tracking/gtm/prepped/prepped_fpm_pudr.csv", row.names = FALSE,
+write.csv(mappedGtm, "J:/Project/Evaluation/GF/resource_tracking/gtm/prepped/prepped_fpm_pudr.csv", row.names = FALSE,
           fileEncoding = "latin1")
 
 
--- resource_tracking/prep/gtm_prep/prep_gtm_pudr.R
+++ resource_tracking/prep/gtm_prep/prep_gtm_pudr.R
@@ -12,7 +12,7 @@
 # ----------------------------------------------
 
 # start function
-prep_gtm_pudr = function(dir, inFile, sheet_name, year, qtr_num, disease, period, grant, source) {
+prep_gtm_pudr = function(dir, inFile, sheet_name, year, qtr_num, disease, period, grant, source, loc_name) {
   
   # Load/prep data
   gf_data <-data.table(read_excel(paste0(dir,inFile), sheet=sheet_name))
@@ -23,25 +23,38 @@ prep_gtm_pudr = function(dir, inFile, sheet_name, year, qtr_num, disease, period
     colnames(gf_data)[3] <- "budget"
     colnames(gf_data)[4] <- "expenditure"
     gf_data$sda_activity <- "All"
+    gf_data$disbursement <- 0 
+    gf_data$recipient <- loc_name ##change this when we get SR info
+    
     gf_data <- gf_data[c(grep("modul", tolower(gf_data$module)):(grep("implem", tolower(gf_data$module)))),]
     gf_data <- gf_data[-1, ,drop = FALSE]
+  } else if (sheet_name=="LFA_Annex-SR Financials"){
+      colnames(gf_data)[1] <- "recipient"
+      colnames(gf_data)[5] <- "budget"
+      colnames(gf_data)[6] <- "disbursement"
+      gf_data <- gf_data[c(grep("entity", tolower(gf_data$recipient)):(grep("total", tolower(gf_data$recipient)))),]
+      gf_data$sda_activity <- "all"
+      gf_data$module <- "all"
+      gf_data$intervention <- "all"
+      gf_data$expenditure <- 0
   } else {
-    colnames(gf_data)[2] <- "module"
-    colnames(gf_data)[3] <- "sda_activity"
-    colnames(gf_data)[4] <- "intervention"
-    colnames(gf_data)[5] <- "budget"
-    colnames(gf_data)[7] <- "expenditure"
-    gf_data <- gf_data[c(grep("objetivos", tolower(gf_data$sda_activity)):(grep("seleccio", tolower(gf_data$module)))),]
+      colnames(gf_data)[2] <- "module"
+      colnames(gf_data)[3] <- "sda_activity"
+      colnames(gf_data)[4] <- "intervention"
+      colnames(gf_data)[5] <- "budget"
+      colnames(gf_data)[7] <- "expenditure"
+      gf_data$disbursement <- 0 
+      gf_data$recipient <- loc_name
+      gf_data <- gf_data[c(grep("objetivos", tolower(gf_data$sda_activity)):(grep("seleccio", tolower(gf_data$module)))),]
   }
   
-  gf_data <- gf_data[, c("module","sda_activity", "intervention", "budget", "expenditure"),with=FALSE]
+  gf_data <- gf_data[, c("recipient","module","sda_activity", "intervention", "budget", "expenditure", "disbursement"),with=FALSE]
   budget_dataset <- gf_data[-1, drop = FALSE]
   budget_dataset <- budget_dataset[-nrow(budget_dataset) ,drop = FALSE]
   
   budget_dataset <- na.omit(budget_dataset , cols=1, invert=FALSE)
   budget_dataset <- budget_dataset[!grepl("total", tolower(budget_dataset$module)),]
 
-  
   budget_dataset$start_date <- year
   budget_dataset$period <- period
   budget_dataset$disease <- disease
