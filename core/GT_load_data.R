0b6d0e95216284414a33ddf1d5bc2e23ca34838c
Guillermo <lgar89@gmail.com>
2018-04-18 08:55:39 -0600

    hospital and deaths data are loaded by calling functions now.

--- core/GT_load_data.R
+++ core/GT_load_data.R
@@ -8,6 +8,7 @@
 library(data.table)
 library(reshape2)
 library(stringr)
+library(rgdal)
 library(ggplot2)
 library(stringdist)
 library(haven)
@@ -21,23 +22,29 @@ codePath = "PCE/gf/"
 
 # Deaths from 2009 to 2015
 # TODO: add 2016 and 2017.
-defsData = c()
-for (year in seq(2009, 2016, 1)) {
-  defsData[[year]] = data.table(read_sav(paste0(extDataPath, "GTVitales y Censo/Defunciones ", year, ".sav")))
-  defsData[[year]]$CaudefPRE = str_sub(defsData[[year]]$Caudef, 1, 3) 
-  print(year)
+loadDeathsData <- function() {
+    defsData = c()
+    for (year in seq(2009, 2016, 1)) {
+      defsData[[year]] = data.table(read_sav(paste0(extDataPath, "GTVitales y Censo/Defunciones ", year, ".sav")))
+      defsData[[year]]$CaudefPRE = str_sub(defsData[[year]]$Caudef, 1, 3) 
+      print(year)
+    }
+    defsData
 }
-
 # ----Load data--------------------------------------------
 
 # Private hospital internal services from 2009 to 2015
 # TODO: add 2016 and 2017.
-privHospIData = c()
-for (year in seq(2009, 2016, 1)) {
-  privHospIData[[year]] = data.table(read_sav(paste0(extDataPath, "ServHospitalariosPrivados/ServHosptlPrivInternos-", year, ".sav")))
-  privHospIData[[year]]$CAUFINPRE = str_sub(privHospIData[[year]]$CAUFIN, 1, 3) 
-  print(year)
+loadPrivateHospitalAdmsData <- function() {
+    privHospIData = c()
+    for (year in seq(2009, 2016, 1)) {
+      privHospIData[[year]] = data.table(read_sav(paste0(extDataPath, "ServHospitalariosPrivados/ServHosptlPrivInternos-", year, ".sav")))
+      privHospIData[[year]]$CAUFINPRE = str_sub(privHospIData[[year]]$CAUFIN, 1, 3) 
+      print(year)
+    }
+    privHospIData
 }
+
 
 # Municipalities and departments GIS geometries
 gtmMunisIGN = readOGR(paste0(dataPath, "Covariates and Other Data/GIS/GT-IGN-cartografia_basica-Division politica Administrativa (Municipios).geojson"))
