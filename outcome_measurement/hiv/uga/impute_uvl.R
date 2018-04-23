83bf79dfd6ec16581a7c4ddf64f72ac2d079618a
IHME\ccarelli <ccarelli@uw.edu>
2018-04-23 14:31:12 -0700

    Imputed UVl update name

--- outcome_measurement/hiv/uga/impute_uvl.R
+++ outcome_measurement/hiv/uga/impute_uvl.R
@@ -1,25 +1,8 @@
 # ----------------------------------------------
 # Caitlin O'Brien-Carelli
 #
-# 4/19/2018
-#
-# Use the Amelia package to impute missing values
-# ----------------------------------------------
-
-# --------------------
-# Set up R
-rm(list=ls())
-library(data.table)
-library(jsonlite)
-library(httr)
-library(stringr) # to help extract meta data from file names
-library(ggplot2)
-
-# ----------------------------------------------
-# Caitlin O'Brien-Carelli
-#
-#4/18/2018
-# Descriptive statistics and maps for the Uganda Viral Load Dashboard
+# 4/23/2018
+# Multiple imputation for the Uganda Viral Load Dashboard
 # ----------------------------------------------
 # Set up R
 
@@ -32,8 +15,8 @@ library(rgdal)
 library(tibble)
 library(dplyr)
 library(RColorBrewer)
-
-# -------------------------
+library(Amelia)
+library(MASS)
 
 
 # set input/output directory
@@ -43,41 +26,79 @@ dir <- 'J:/Project/Evaluation/GF/outcome_measurement/uga/vl_dashboard'
 # upload the data with month, year, sex
 uganda_vl <- readRDS(paste0(dir, "/sex_data.rds"))
 
-# drop out the current month
+# start from scale up and continue to febrary 2018
 uganda_vl <- uganda_vl[!(month==3 & year==2018)]
-
-# view the data set and variable names
-class(uganda_vl) # check that it is a data table
-str(uganda_vl)
-
-# -------------------------
-# only working with 2017 for now
- 
-uganda_vl <- uganda_vl[year==2017]
-
-uganda_vl[,length(unique(facility_id))] #1959 facilities reported in 2017
-
-# -------------------------
-
-facilities <- uganda_vl[,.(facility_id=unique(facility_id), facility_name=unique(facility_name))]
-str(facilities)
-  
-
-monthly <- uganda_vl[, .(total_facilities=(length(unique(facility_id)))), by=month] 
-
-ggplot (monthly, aes(x=factor(month), y=total_facilities)) + 
-  geom_point() + 
-  theme_bw()
+uganda_vl <- uganda_vl[year==2016 | year==2017 | year==2018]
 
 
-uganda_vl <- merge(uganda_vl, monthly, all.x=TRUE)
-str(uganda_vl)
-
-# -------------------------
-
-uganda_vl[(month==11 & patients_received==1), .(unique(facility_id), patients_received), by=facility_id]
+# ----------------------------------------------
+# prep the data for imputation 
 
 
+# --------------
+# create a list of the facility ids - change to include names
+f_ids <- unique(uganda_vl$facility_id)
+length(f_ids)
+
+sexes <- unique(uganda_vl$sex)
+dates <- seq(from=min(uganda_vl$date), to=max(uganda_vl$date), by='month')
+
+expanded_data <- data.table(expand.grid(f_ids, dates, sexes))
+setnames(expanded_data, c('facility_id', 'date','sex'))
+
+# --------------------
+
+# merge in the blank rows for facility_id (by date, sex)
+uganda_vl <- merge(uganda_vl, expanded_data, by=c('facility_id', 'date','sex'), all=TRUE)
+
+# --------------------
+
+# add facility names
+facilities <- data.table(facility_id=uganda_vl$facility_id, facility_name=uganda_vl$facility_name)
+facilities <- facilities[is.na(facility_name)==FALSE]
+facilities <- facilities[ , .(facility_id, facility_name), by=.(facility_id, facility_name)]
+facilities <- facilities[ ,.(facility_id, facility_name)]
+
+uganda_vl <- merge(uganda_vl, facilities, by='facility_id', all.x=TRUE)
+
+# make facility_name.y the facility name and drop out x (contains missing values)
+uganda_vl[,facility_name.x:=NULL]
+setnames(uganda_vl, "facility_name.y", "facility_name")
+
+# --------------------
+# add districts
+# districts <- data.table(district_id=uganda_vl$district_id, dist_name=uganda_vl$dist_name)
+# districts <- districts[is.na(dist_name)==FALSE]
+# districts <- districts[ , .(district_id, dist_name), by=.(district_id, dist_name)]
+# districts <- districts[ ,.(district_id, dist_name)]
+# 
+# districts[(!district_id %in% uganda_vl$district_id), district_id]
+# uganda_vl[(!district_id %in% districts$district_id) & !is.na(district_id), district_id]
+# 
+# # glitchy fix later
+# uganda_vl <- merge(uganda_vl, districts, by='district_id', all.x=TRUE)
+# 
+# uganda_vl[is.na(facility_name.y), facility_id] # all ids matched with facility names
+# 
+# setnames(uganda_vl, "facility_name.y", "facility_name")
+
+
+# ----------------------------------------------
+# amelia  test
+uvl <- uganda_vl[ ,.(facility_id, facility_name, district_id, sex, date,
+                    patients_received, samples_received,  dbs_samples, total_results,
+                    rejected_samples, valid_results, ratio=(suppressed/valid_results))]             
+
+
+#Completed_data<-amelia(data,m=3,ts="year", cs="country", p2s=0, ords="polity", noms="signed", idvars=c("year","country"))
+
+# should I include sex bc i want it in the moel
+idVars <- c("facility_id", "facility_name", "district_id", "sex", "date")
+
+#do I need ts?
+
+imputed_data <- amelia(uvl, m=2, idvars=idVars)
+View(imputed_data$imp1)
 
 
 
@@ -85,3 +106,6 @@ uganda_vl[(month==11 & patients_received==1), .(unique(facility_id), patients_re
 
 
 
+
+
+lm(logit(ratio)~log(samples received), data=dt)
