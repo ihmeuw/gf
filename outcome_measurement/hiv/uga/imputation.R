dac605c7ad244c3dc6d2dbe65c94fb48a802977d
IHME\davidp6 <david6@uw.edu>
2018-04-23 14:04:32 -0700

    Add modifications to get imputation running for UVL data

--- outcome_measurement/hiv/uga/imputation.R
+++ outcome_measurement/hiv/uga/imputation.R
@@ -37,33 +37,43 @@ uganda_vl <- uganda_vl[year==2016 | year==2017 | year==2018]
 
 # --------------
 # create a list of the facility ids - change to include names
+
+# store list of unique faciltiies, sexes and dates
 f_ids <- unique(uganda_vl$facility_id)
 length(f_ids)
-
 sexes <- unique(uganda_vl$sex)
 dates <- seq(from=min(uganda_vl$date), to=max(uganda_vl$date), by='month')
 
+# make a "fully rectangularized" dataset with all months for each facility-sex
 expanded_data <- data.table(expand.grid(f_ids, dates, sexes))
 setnames(expanded_data, c('facility_id', 'date','sex'))
-
 # --------------------
+
 
 # merge in the blank rows for facility_id (by date, sex)
 uganda_vl <- merge(uganda_vl, expanded_data, by=c('facility_id', 'date','sex'), all=TRUE)
 
+
+# collapse to facility-sex-date level to ensure unique identifiers
+sumVars = c("patients_received", "samples_received", "dbs_samples", "total_results", "rejected_samples", "valid_results", "suppressed")
+uganda_vl <- uganda_vl[, lapply(.SD, sum, na.rm=TRUE), by=c('facility_id','sex','date'), .SDcols=sumVars]
+
+# # test unique identifiers
+test = nrow(uganda_vl[duplicated(uganda_vl[,c('facility_id','sex','date'),with=F])])
+if (test>0) stop('Facility-sex-hub-date does not uniquely identify rows! This is necessary for Amelia to do cs/ts operations!') 
 # --------------------
 
-# add facility names
-facilities <- data.table(facility_id=uganda_vl$facility_id, facility_name=uganda_vl$facility_name)
-facilities <- facilities[is.na(facility_name)==FALSE]
-facilities <- facilities[ , .(facility_id, facility_name), by=.(facility_id, facility_name)]
-facilities <- facilities[ ,.(facility_id, facility_name)]
+# # add facility names
+# facilities <- data.table(facility_id=uganda_vl$facility_id, facility_name=uganda_vl$facility_name)
+# facilities <- facilities[is.na(facility_name)==FALSE]
+# facilities <- facilities[ , .(facility_id, facility_name), by=.(facility_id, facility_name)]
+# facilities <- facilities[ ,.(facility_id, facility_name)]
 
-uganda_vl <- merge(uganda_vl, facilities, by='facility_id', all.x=TRUE)
+# uganda_vl <- merge(uganda_vl, facilities, by='facility_id', all.x=TRUE)
 
-# make facility_name.y the facility name and drop out x (contains missing values)
-uganda_vl[,facility_name.x:=NULL]
-setnames(uganda_vl, "facility_name.y", "facility_name")
+# # make facility_name.y the facility name and drop out x (contains missing values)
+# uganda_vl[,facility_name.x:=NULL]
+# setnames(uganda_vl, "facility_name.y", "facility_name")
 
 # --------------------
 # add districts
@@ -85,27 +95,37 @@ setnames(uganda_vl, "facility_name.y", "facility_name")
 
 # ----------------------------------------------
 # amelia  test
-uvl <- uganda_vl[ ,.(facility_id, facility_name, district_id, sex, date,
+# uvl <- uganda_vl[ ,.(facility_id, facility_name, district_id, sex, date,
+                    # patients_received, samples_received,  dbs_samples, total_results,
+                    # rejected_samples, valid_results, ratio=(suppressed/valid_results))]             
+
+uvl <- uganda_vl[ ,.(facility_id, sex, date,
                     patients_received, samples_received,  dbs_samples, total_results,
                     rejected_samples, valid_results, ratio=(suppressed/valid_results))]             
-
 
 #Completed_data<-amelia(data,m=3,ts="year", cs="country", p2s=0, ords="polity", noms="signed", idvars=c("year","country"))
 
 # should I include sex bc i want it in the moel
-idVars <- c("facility_id", "facility_name", "district_id", "sex", "date")
+# idVars <- c("facility_id", "facility_name", "district_id", "sex", "date")
 
-#do I need ts?
+# make cs variable
+uvl[, cs_variable:=paste0(facility_id, sex)]
+uvl$facility_id <- NULL
+uvl$sex <- NULL
 
-imputed_data <- amelia(uvl, m=2, idvars=idVars)
-View(imputed_data$imp1)
+# run imputation
+imputed_data <- amelia(uvl, m=2, cs='cs_variable', ts='date', lags='ratio')
+# View(imputed_data$imp1)
+
+# graph one test case to see how it looks
+cstmp = sample(unique(uvl$cs_variable),1)
+merged = merge(uvl, imputed_data[[1]]$imp1, by=c('cs_variable','date'), all=T)
+ggplot(merged[cs_variable==cstmp], aes(y=ratio.y, x=date)) + 
+	geom_point(color='red') + 
+	geom_point(aes(y=ratio.x), color='black') + 
+	labs(title=paste('Facility-sex:', cstmp))
 
 
 
 
-
-
-
-
-
-lm(logit(ratio)~log(samples received), data=dt)
+# lm(logit(ratio)~log(samples received), data=dt)
