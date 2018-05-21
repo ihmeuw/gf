e1190c9b1388fdc20ac86f0f0d6eb8d2ed6617d5
raubsaurier <33269530+raubsaurier@users.noreply.github.com>
2018-05-21 10:56:31 -0700

    add some formatting cleanup and comment out optional calculations in the code

--- outcome_measurement/malaria/gtm/master_gtm_prep.R
+++ outcome_measurement/malaria/gtm/master_gtm_prep.R
@@ -1,54 +1,53 @@
 # ----------------------------------------------
-  # Audrey Batzel
-  #
-  # 4/5/18
-  # Master prep file for GTM Malaria supply chain data 
+# Audrey Batzel
+# 4/5/18
+# Master prep file for GTM Malaria supply chain data 
 # ----------------------------------------------
   ###### Set up R / install packages  ###### 
 # ----------------------------------------------
-    rm(list=ls())
-    library(data.table)
-    library(reshape2)
-    library(stringr)
-    library(RColorBrewer)
-    library(ggplot2)
-    library(lubridate)
-    library(readxl)
-    library(stats)
-    library(rlang)
-    library(zoo)
-    library(tidyr)
-    library(dplyr)
-
-
+rm(list=ls())
+library(data.table)
+library(reshape2)
+library(stringr)
+library(lubridate)
+library(readxl)
+library(stats)
+library(rlang)
+library(zoo)
+library(tidyr)
 # ----------------------------------------------
 ###### Call directories and load the prep data  ###### 
 # ----------------------------------------------
-  # file path where the files are stored
-  local_dir <- "J:/Project/Evaluation/GF/outcome_measurement/gtm/"
+# file path where the files are stored
+local_dir <- "J:/Project/Evaluation/GF/outcome_measurement/gtm/"
+
+antimalarials <- "J:/Project/Evaluation/GF/outcome_measurement/gtm/MALARIA/Antimalaricos/"
   
-  antimalarials <- "J:/Project/Evaluation/GF/outcome_measurement/gtm/MALARIA/Antimalaricos/"
-  
-  # ----------------------------------------------
+# ----------------------------------------------
   ######For loop that appends each data file to our databese  ###### 
 # ----------------------------------------------
-  file_list <- data.table(read.csv(paste0(antimalarials, "prep_file_list.csv")))
-  file_list$start_date <- ymd(file_list$start_date)
-  file_list$file_name <- as.character(file_list$file_name)
-  for(i in 1:length(file_list$file_name)){
-    tmpData <- prep_malaria_data(antimalarials, file_list$file_name[i], file_list$sheet[i]
-                                 , ymd(file_list$start_date[i]), file_list$period[i])
-    if(i==1){
-      antimal_database <- tmpData
-    } else {
-      antimal_database <- rbind(tmpData, antimal_database)
-    }
-    print(i)
-  }
-  
+file_list <- data.table(read.csv(paste0(antimalarials, "prep_file_list.csv")))
+file_list$start_date <- ymd(file_list$start_date)
+file_list$file_name <- as.character(file_list$file_name)
+for(i in 1:length(file_list$file_name)){
+  tmpData <- prep_malaria_data(antimalarials, file_list$file_name[i], file_list$sheet[i]
+                             , ymd(file_list$start_date[i]), file_list$period[i])
+if(i==1){
+  antimal_database <- tmpData
+} else {
+  antimal_database <- rbind(tmpData, antimal_database)
+}
+  print(i)
+}
+
+
 antimal_database$amount <- as.numeric(antimal_database$amount)
 
+# ----------------------------------------------
+###### Add more variables that track indicators: 
+# ----------------------------------------------
 
+##function to create "drug type" 
 get_antimalarial_types <- function(antimal){
   x <- "Antimoniato 10 ml"
   if(grepl("Cloroquina", antimal)){
@@ -63,15 +62,17 @@ get_antimalarial_types <- function(antimal){
   return(x)
 }
 
-
-
 antimal_database$drug_type <- mapply(get_antimalarial_types, antimal_database$antimalarial_input)
 
+##get rid of special characters in the dataset (necessary if we want to map these to the shapefiles)
 antimal_database$department <- gsub("\\*", "",antimal_database$department) 
 antimal_database$department <- trimws(antimal_database$department, "r")
 antimal_database[department=="Guate Nor Occidente"]$department <- "Guatemala Nor Occidente"
 antimal_database[department=="Peten Sur occidental"]$department <- "Guatemala Nor Occidente"
 
+# ----------------------------------------------
+##export as CSV 
+# ----------------------------------------------
 write.csv(antimal_database, paste0(local_dir, "prepped_data/", "antimalarial_prepped_data.csv"))
 
   
--- outcome_measurement/malaria/gtm/prep_malaria_data.R
+++ outcome_measurement/malaria/gtm/prep_malaria_data.R
@@ -1,7 +1,6 @@
 
 # ----------------------------------------------
 # Irena Chen 
-#
 # 4/5/18
 # Function that cleans the GTM malaria supply chain excel book and returns a data frame
 
@@ -14,8 +13,10 @@ prep_malaria_data <- function(dir, inFile, sheet_name,start_date, period){
 
   ##drop columns that contain only NAs: 
   sc_data <-Filter(function(x)!all(is.na(x)), sc_data)
-  
+  # ----------------------------------------------
   ## set column names according to the supply data they contain: 
+  # ----------------------------------------------
+  
   if(ncol(sc_data)>32&year(start_date)==2012){
     col_names <- c("dept_code", "department", "Cloroquina250_previousBalance", "Cloroquina250_topLevelEntries", "Cloroquina250_deliveredToUser", 
                    "Cloroquina250_notDeliveredToUser", "Cloroquina250_realDemand", "Cloroquina250_readjustments", "Cloroquina250_nextMonthBalance", 
@@ -66,35 +67,43 @@ prep_malaria_data <- function(dir, inFile, sheet_name,start_date, period){
   }
    
    colnames(sc_data) <- col_names
-   sc_data$Cloroquina250_sumDelivered <- as.numeric(sc_data$Cloroquina250_notDeliveredToUser)
-   +as.numeric(sc_data$Cloroquina250_deliveredToUser)
-   sc_data$Primaquina15_sumDelivered <- as.numeric(sc_data$Primaquina15_notDeliveredToUser)
-                                                   +as.numeric(sc_data$Primaquina15_deliveredToUser)
-   sc_data$Primaquina5_sumDelivered <- as.numeric(sc_data$Primaquina5_notDeliveredToUser)
-                                                  +as.numeric(sc_data$Primaquina5_deliveredToUser)
-   if(!is.null(sc_data$Antimoniato10_realDemand)){
-     sc_data$Antimoniato10_sumDelivered <- as.numeric(sc_data$Antimoniato10_notDeliveredToUser)
-     +as.numeric(sc_data$Antimoniato10_deliveredToUser)
-   }
-   sc_data$Cloroquina250_sumExistence <- as.numeric(sc_data$Cloroquina250_previousBalance)
-   +as.numeric(sc_data$Cloroquina250_deliveredToUser)
-   sc_data$Primaquina15_sumExistence <- as.numeric(sc_data$Primaquina15_previousBalance)
-   +as.numeric(sc_data$Primaquina15_deliveredToUser)
-   sc_data$Primaquina5_sumExistence <- as.numeric(sc_data$Primaquina5_previousBalance)
-   +as.numeric(sc_data$Primaquina5_deliveredToUser)
-   if(!is.null(sc_data$Antimoniato10_realDemand)){
-     sc_data$Antimoniato10_sumExistence <- as.numeric(sc_data$Antimoniato10_previousBalance)
-     +as.numeric(sc_data$Antimoniato10_deliveredToUser)
-   }
+  
+   # --------------------------------------------
+   ###  Optional calculations that you can include if you want ### 
+   # --------------------------------------------
+   # 
+   #  sc_data$Cloroquina250_sumDelivered <- as.numeric(sc_data$Cloroquina250_notDeliveredToUser)
+   # +as.numeric(sc_data$Cloroquina250_deliveredToUser)
+   # sc_data$Primaquina15_sumDelivered <- as.numeric(sc_data$Primaquina15_notDeliveredToUser)
+   #                                                 +as.numeric(sc_data$Primaquina15_deliveredToUser)
+   # sc_data$Primaquina5_sumDelivered <- as.numeric(sc_data$Primaquina5_notDeliveredToUser)
+   #                                                +as.numeric(sc_data$Primaquina5_deliveredToUser)
+   # if(!is.null(sc_data$Antimoniato10_realDemand)){
+   #   sc_data$Antimoniato10_sumDelivered <- as.numeric(sc_data$Antimoniato10_notDeliveredToUser)
+   #   +as.numeric(sc_data$Antimoniato10_deliveredToUser)
+   # }
+   # sc_data$Cloroquina250_sumExistence <- as.numeric(sc_data$Cloroquina250_previousBalance)
+   # +as.numeric(sc_data$Cloroquina250_deliveredToUser)
+   # sc_data$Primaquina15_sumExistence <- as.numeric(sc_data$Primaquina15_previousBalance)
+   # +as.numeric(sc_data$Primaquina15_deliveredToUser)
+   # sc_data$Primaquina5_sumExistence <- as.numeric(sc_data$Primaquina5_previousBalance)
+   # +as.numeric(sc_data$Primaquina5_deliveredToUser)
+   # if(!is.null(sc_data$Antimoniato10_realDemand)){
+   #   sc_data$Antimoniato10_sumExistence <- as.numeric(sc_data$Antimoniato10_previousBalance)
+   #   +as.numeric(sc_data$Antimoniato10_deliveredToUser)
+   # }
    
    
-   
-   
+   # --------------------------------------------
+   ### Reshape the data so that all of the variables are in one column ("input") ### 
+   # --------------------------------------------
+  
    sc_data <- sc_data[-c(1:3),] ##the first three rows have no use to us 
    sc_data<- melt(sc_data, id.vars = c("dept_code", "department"), variable.name = "antimalarial_input", value.name = "amount")
    
    sc_data <- na.omit(sc_data, cols=1, invert=FALSE)
    
+   ##add some useful tracking variables: 
    sc_data$start_date <- start_date
    sc_data$period <- period
    
