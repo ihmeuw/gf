57b1c371408aa0d77c52db01cee681b8227f3177
Audrey Batzel <abatzel@ihme.washington.edu>
2018-06-14 13:11:18 -0700

    add comments and clean up R script

--- outcome_measurement/malaria/cod/prep_COD_Malaria_data_function.R
+++ outcome_measurement/malaria/cod/prep_COD_Malaria_data_function.R
@@ -34,9 +34,8 @@
 # data directory
   # file path where the files are stored
   dir <- "J:/Project/Evaluation/GF/outcome_measurement/cod/National_Malaria_Program/"
-  dir_prepped <-"J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/PNLP"
     
-  # csv of file names for importing
+  # csv of file names for importing data
   PNLP_files <- read.csv(paste0(dir, "PNLP_file_names.csv"), fileEncoding = "latin1")
   
   # prep_data() function
@@ -44,22 +43,14 @@
   source(prep_data)
 
 # output files 
-  # (output to prepped_data folder within cod folder)
-  
-  # file path: J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/", fileName , ".csv"))
-  
-  # file names: 
-    # COD_PNLP_Data_Indicators_long - prepped data.table object, appended data from 2014-2016
-    # COD_PNLP_Data_Indicators_wide
-    # COD_PNLP_Data_Interventions_long - prepped data.table object, appended data from 2014-2016
-    # COD_PNLP_Data_Interventions_wide
+  dir_prepped <-"J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/PNLP"
 # ----------------------------------------------
   
   
 # ----------------------------------------------
-# Load data - to visualize and work through cleaning to make sure function will work
-  # example
-  # cod_mdata_KIN16 <- data.table(read_excel(paste0(dir,"/", PNLP_files$File.Names.[1], '.xls'), sheet= "KIN"))
+# Load data - to visualize what the raw data looks like in R, and to work through cleaning process to make sure function will work
+  # example from older version of data files:
+    # cod_mdata_KIN16 <- data.table(read_excel(paste0(dir,"/", PNLP_files$File.Names.[1], '.xls'), sheet= "KIN"))
 
   # new data example:
     #dt <- data.table(read_excel(paste0(dir, "/", PNLP_files$File.Names[9], ".xls"), sheet= 'KIN'))
@@ -70,12 +61,16 @@
 # Use a loop to run prep_data() on each of the three data sheets for the three years
   # for which we have the data.
   
-  # variables needed:
+  # set-up variables needed:
   i <- 1
-  index <- 4
-  
+  index <- 4  # index is set to 4 because we will not include the first three files in the PNLP_files df
+              # these are older versions of the 2014-2016 data.
+
+# loop through each file in the df PNLP_files
  for(index in 4:11) {
    
+   # this if statement just accounts for a different file type used in one of the excel sheets, and then
+   # sets the sheetnames to be the sheetnames in each file
    if (index==11){
      sheetnames <- excel_sheets(paste0(dir, PNLP_files$File.Names[index], ".xlsx"))
      sheetnames = sheetnames[!sheetnames %in% 'INPUTRDC']
@@ -84,6 +79,7 @@
      sheetnames = sheetnames[!sheetnames %in% 'INPUTRDC']
    }
 
+   # loop through each sheet in each file, to read the data into RStudio, again accounting for different file types in one of the sheets.
     for(s in sheetnames) {
 
       if (index==11){
@@ -92,38 +88,43 @@
           dt <- data.table(read_excel(paste0(dir, PNLP_files$File.Names[index], ".xls"), sheet= s))
       }
       
+      # this line of code is what actually runs the function to prep the current excel sheet of data
         currentSheet <- prep_data(dt, s, index)
      
-      # to show where it is breaking if there is an error
+      # the following two lines can be commented out, but they show where the code is breaking if there is a problem
+      # and also the number of rows after the data has been prepped so that this can be checked against the total number
+      # of observations that we would expect. 
         print(s)
         print(nrow(currentSheet))
         
       # to figure out which rows are missing from the prepped data that shouldn't be
-        if (s=="NK"|s=="MN"){
-          healthzone<-dt[["X__1"]]
-        } else {
-          healthzone<-dt[["X__2"]]
-        }
-        
-        healthzone = unique(healthzone)
-        healthzone = tolower(healthzone)
-        healthzoneprep <- currentSheet[["health_zone"]]
-        healthzoneprep <- unique(healthzoneprep)
-        missing_hz <- healthzone[!healthzone %in% healthzoneprep]
-        print(missing_hz)
-
-        for (h in healthzoneprep){
-          if ( nrow(currentSheet[health_zone==h, ]) != 12){
-            print( h )
-            print( nrow(currentSheet[health_zone==h,]) )
-          }
-        }
-        
+      # ----------------------------------------------
+        # if (s=="NK"|s=="MN"){
+        #   healthzone<-dt[["X__1"]]
+        # } else {
+        #   healthzone<-dt[["X__2"]]
+        # }
+        # 
+        # healthzone = unique(healthzone)
+        # healthzone = tolower(healthzone)
+        # healthzoneprep <- currentSheet[["health_zone"]]
+        # healthzoneprep <- unique(healthzoneprep)
+        # missing_hz <- healthzone[!healthzone %in% healthzoneprep]
+        # print(missing_hz)
+        # 
+        # for (h in healthzoneprep){
+        #   if ( nrow(currentSheet[health_zone==h, ]) != 12){
+        #     print( h )
+        #     print( nrow(currentSheet[health_zone==h,]) )
+        #   }
+        # }
+        #
         # to compare missing health zones between two years
         # hz_missing_2011 <- hzNK2014[!hzNK2014 %in% hzNK2011]
         # print(hz_missing_2011)
         # hz_missing_2014 <- hzNK2011[!hzNK2011 %in% hzNK2014]
         # print(hz_missing_2014)
+        # ----------------------------------------------
         
       # need if statement to distinguish first sheet, and then
       # add to the first sheet with subsequent ones with rbind()
@@ -131,8 +132,6 @@
       if (i>1) fullData <- rbind(fullData, currentSheet, fill=TRUE)
       i <- i+1
     }
-   print("Checking to see if date is NA anywhere...")
-   print(fullData[is.na(date), c(1:8)])
   }
 # ----------------------------------------------
   
