733ea7e2ec476377a483086b49e56fcbe3cd95b0
Audrey Batzel <abatzel@uw.edu>
2018-06-27 18:46:15 -0700

    aggregate and calculate mean by DPS for graphing by DPS later

--- outcome_measurement/malaria/cod/run_amelia.R
+++ outcome_measurement/malaria/cod/run_amelia.R
@@ -6,25 +6,26 @@
   # 6/15/18
   # Modified for COD PNLP data 2010-2017
   
-    setwd('C:/local/gf/')
-
   # THE FOLLOWING R SCRIPT WAS RUN ON THE CLUSTER AT IHME
 # ----------------------------------------------
 
 
 # --------------------
   # Set up R / install packages
-    rm(list=ls())
-    library(data.table)
-    library(stringr)
-    library(reshape2)
-    library(ggplot2)
-    library(lubridate)
-    library(readxl)
-    library(stats)
-    library(Rcpp)
-    library(Amelia)
-    library(parallel)
+  tol = commandArgs()[4]
+  run_name = commandArgs()[5]
+  tol = as.numeric(gsub('\r', '', tol))
+  run_name = gsub('\r', '', run_name)
+  print(tol)
+  # print(run_name)
+  library(data.table)
+  library(stringr)
+  library(reshape2)
+  library(ggplot2)
+  library(stats)
+  library(Rcpp)
+  library(Amelia)
+  library(parallel)
 # --------------------  
 
 
@@ -64,7 +65,7 @@
     all_vars <- c(colnames(dt))
     id_vars <- c("province", "dps", "health_zone", "date", "id")
     indicators <- all_vars[!all_vars %in% id_vars] 
-    indicators <- indicators[!indicators %in% c("healthFacilities_numReported")]
+    indicators <- indicators[!indicators %in% c("healthFacilities_total", "healthFacilities_numReportedWithinDeadline")]
     
     # remove healthFacilities_numReported from the data.table
     dt <- dt[,c(id_vars, indicators), with=F] 
@@ -120,6 +121,22 @@
   # set up for parallel
     parallelMethod = ifelse(as.logical(Sys.info()['sysname']=='Windows'), 'snow', 'multicore')
     ncores = detectCores()
+    
+  # set up priors 
+    dtOrig[]
+    # index column names as numbers
+    means = copy(dtOrig)
+    setnames(means, as.character(1:length(names(means))))
+    # compute means by health zone-indicator
+    valueVars = as.character(which(names(dtOrig) %in% indicators))
+    for(v in valueVars) means[, (v):=mean(get(v),na.rm=TRUE), by='3']
+    # drop unneedd variables
+    means = means[, valueVars, with=FALSE]
+    # index rows
+    means[, row_idx:=seq_len(.N)]
+    # melt to get to the right shape for Amelia priors
+    means = melt(means, id.vars='row_idx')
+
   
   # run the imputation - no polytime
     # ts variable: the date
@@ -128,13 +145,13 @@
     # lags/leads: indicators no la
     # intercs = FALSE by default, try with = TRUE
     
-    id_vars_for_amelia <- id_vars[!id_vars %in% c("health_zone", "date")]
+    id_vars_for_amelia <- id_vars[!id_vars %in% c("health_zone", "date", "dps", "province")]
+    id_vars_for_amelia <- c(id_vars_for_amelia, "healthFacilities_max", "healthFacilities_totalOrig", "healthFacilities_numReported")
     
-    measured_vars <- all_vars[!all_vars %in% id_vars]
-    measured_vars <- measured_vars[!measured_vars %in% c("healthFacilities_numReported")]
+    measured_vars <- all_vars[!all_vars %in% id_vars_for_amelia]
     
     amelia.results <- amelia(dtLog, m=50, cs= "health_zone", ts="date", idvars= id_vars_for_amelia, tolerance= 0.001, lags = measured_vars,
-                             leads = measured_vars, parallel= parallelMethod, ncpus= 40)
+                             leads = measured_vars, noms= c("dps", "province"), parallel= parallelMethod, ncpus= 50)
 
 # ---------------------------------------------- 
   
@@ -167,7 +184,7 @@
 # ---------------------------------------------- 
   # export imputed data to have a saved version (do this as an RDS so it is faster/smaller file)
   # write.csv(dtExp, paste0(dir, output))
-  saveRDS(dtExp, paste0(dir, "imputedData.rds"))
+    saveRDS(dtExp, paste0(dir, "imputedData", run_name, ".rds"))
 # ---------------------------------------------- 
   
 # ----------------------------------------------     
@@ -182,15 +199,16 @@
     
   # reshape imputed data long
     imputedDataLong <- melt(dtExp, id.vars=c(imputed_id_vars))
-  
+    imputedDataLong <- imputedDataLong[, c("indicator", "subpopulation") := tstrsplit(variable, "_", fixed=TRUE)]
+
+# ----------------------------------------------
+  # Calculate by health_zone 
   # compute upper middle and lower for the imputed points for the error bars in the graphs
     graphData <- imputedDataLong[, .(mean=mean(value), 
                                    lower=quantile(value, .05), 
-                                   upper=quantile(value, .95)), by=c(id_vars,"variable")]
+                                   upper=quantile(value, .95)), by=c(id_vars,"indicator", "subpopulation")]
   
   # split subpopulations out from indicators
-    graphData <- graphData[, c("indicator", "subpopulation") := tstrsplit(variable, "_", fixed=TRUE)]
-    
     missMatrixMelt <- melt(dtOrig, id.vars=id_vars)
     missMatrixMelt[, isMissing:=is.na(value)]
     
@@ -202,15 +220,72 @@
     
   # export graphDataComplete
     # write.csv(graphDataComplete, "J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/Imputed Data.csv")
-    saveRDS(graphDataComplete, paste0(dir, "condensed_imputedData_forGraphing.rds"))
-  
+    saveRDS(graphDataComplete, paste0(dir, "imputedData_forGraphing", run_name, ".rds"))
+    
+# ----------------------------------------------
     
     
+# ----------------------------------------------
+  # Aggregate and calculate mean by DPS to graph by DPS
     
+    # aggregate all indicator/intervention data by dps, within each imputation
+      aggData  <- imputedDataLong[, .(aggValue = sum(value)), by=c( "date", "province", "dps", "indicator", "subpopulation", "imputation_number" )]
     
+    # then compute the mean, upper and lower across all imputations for each unique dps/date
+      aggData <- aggData[, .(mean=mean(aggValue), 
+                             lower=quantile(aggValue, .05), 
+                             upper=quantile(aggValue, .95)), by=c("date", "province", "dps", "indicator", "subpopulation")]
+      
+    # set upper and lower values to NA where the value was not imputed (where mean==lower and mean==upper)
+      aggData <- aggData[mean==lower, lower := NA]
+      aggData <- aggData[mean==upper, upper := NA]
+      
+    # export data
+      # write.csv(graphDataComplete, "J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/Imputed Data.csv")
+      saveRDS(aggData, paste0(dir, "imputedData_byDPS_forGraphing", run_name, ".rds"))
+# ----------------------------------------------
+      
+      
+# ----------------------------------------------
+  # Aggregate and calculate mean by old province
+      
+    # aggregate all indicator/intervention data by dps, within each imputation
+      aggDataOldProvince  <- imputedDataLong[, .(aggValue = sum(value)), by=c( "date", "province", "indicator", "subpopulation", "imputation_number" )]
+      
+    # then compute the mean, upper and lower across all imputations for each unique dps/date
+      aggDataOldProvince <- aggDataOldProvince[, .(mean=mean(aggValue), 
+                             lower=quantile(aggValue, .05), 
+                             upper=quantile(aggValue, .95)), by=c("date", "province", "indicator", "subpopulation")]
+      
+    # set upper and lower values to NA where the value was not imputed (where mean==lower and mean==upper)
+      aggDataOldProvince <- aggDataOldProvince[mean==lower, lower := NA]
+      aggDataOldProvince <- aggDataOldProvince[mean==upper, upper := NA]
+      
+    # export data
+      # write.csv(graphDataComplete, "J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/Imputed Data.csv")
+      saveRDS(aggDataOldProvince, paste0(dir, "imputedData_byOldProvince_forGraphing", run_name, ".rds"))
+# ----------------------------------------------
+
+
+# ----------------------------------------------    
+  # Aggregate and calculate at the country level to graph national values
     
+    # aggregate all indicator/intervention data by dps, within each imputation
+      fullCountryData  <- imputedDataLong[, .(aggValue = sum(value)), by=c( "date", "indicator", "subpopulation", "imputation_number" )]
     
-    
+    # then compute the mean, upper and lower across all imputations for each unique dps/date
+      fullCountryData <- fullCountryData[, .(mean=mean(aggValue), 
+                             lower=quantile(aggValue, .05), 
+                             upper=quantile(aggValue, .95)), by=c("date", "indicator", "subpopulation")]
+      
+    # set upper and lower values to NA where the value was not imputed (where mean==lower and mean==upper)
+      fullCountryData <- fullCountryData[mean==lower, lower := NA]
+      fullCountryData <- fullCountryData[mean==upper, upper := NA]
+     
+    # export data
+      # write.csv(graphDataComplete, "J:/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/Imputed Data.csv")
+      saveRDS(fullCountryData, paste0(dir, "imputedData_countryLevel_forGraphing", run_name, ".rds"))
+# ----------------------------------------------          
     
     
     
