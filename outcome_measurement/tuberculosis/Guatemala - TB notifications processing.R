8e24b27bd91b19be4fad381562d8e338008f8b54
Guillermo <lgar89@gmail.com>
2018-07-17 13:20:59 -0600

    Exploring TB notifs data. A few new graphs

--- outcome_measurement/tuberculosis/Guatemala - TB notifications processing.R
+++ outcome_measurement/tuberculosis/Guatemala - TB notifications processing.R
@@ -26,21 +26,36 @@ source(paste0(codePath, "core/GT_helper_functions.R"), encoding = "UTF-8")
 tbnots = read.csv("PCE/Outcome Measurement Data/TUBERCULOSIS/GTM - TB notifications 2012-2017.csv")
 
 tbnots = data.table(tbnots)
-ggplot(data = tbnots[,.(Count = .N),
+
+# Overview of enter condition
+ggplot(data = tbnots[CONDICIONINGRESO %in% c("nuevo", "fracaso", "recaida", "abandono recuperado", "abandono recuperada"),
+                     .(Count = .N),
                         by=.(YearMonth = floor(YearMonth/100)*100 + floor(( YearMonth%% 100 - 1)/3)*3 + 1 , CONDICIONINGRESO)][,
                      .(Count, CONDICIONINGRESO, YearMonth =factor(YearMonth))]) + 
     geom_col(aes(YearMonth, Count, fill = CONDICIONINGRESO))  + theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 8)) + 
     labs(x="Quarter", title="TB Notifications time series\nGrouped by condition of entrance (including exits)")
-
 # Confirming our quarter generator works well:
 # tbnots[,.N,by = .(YearMonth = floor(YearMonth/100)*100 + floor(( YearMonth%% 100 - 1)/3)*3 + 1, YearMonth) ]
 
+
+# NAs are kids, and most probably they are new cases. It turns out these are prophylactic cases that receive isoniazide. Mostly children.
 ggplot(data = tbnots[is.na(CONDICIONINGRESO), .(Count = .N), by=.(AgeGroup = ceiling(EDAD/5)*5)][order(AgeGroup), 
                    .(AgeGroup = factor(AgeGroup), Count)]) +
     geom_col(aes(AgeGroup, Count))
 
-# NAs are kids, and most probably they are new cases.
+# Prophylactic cases
+ggplot(data = tbnots[CONTACTOS == "quimio", .(AgeGroup = ceiling(EDAD/5)*5), by= .(YEAR)]) +
+    geom_violin(aes(YEAR, AgeGroup), fill="blue")  + labs(title="Age distribution of contacts under treatment by year")
+ggplot(data = tbnots[CONTACTOS == "quimio", .(Count = .N), by= .(YEAR)]) +
+    geom_col(aes(YEAR, Count), fill="blue")  + labs(title="Contacts under treatment by year")
 
+# New cases
+ggplot(data = tbnots[CONDICIONINGRESO == "nuevo", .(AgeGroup = ceiling(EDAD/5)*5), by= .(YEAR) ]) +
+    geom_violin(aes(factor(YEAR), AgeGroup), fill="blue")  + labs(title="Age distribution of new cases by year")
+ggplot(data = tbnots[CONDICIONINGRESO == "nuevo", .(Count=.N), by= .(YEAR) ]) +
+    geom_col(aes(factor(YEAR), Count), fill="blue")  + labs(title="New cases by year")
+
+# Incidence time series:
 yearly = tbnots[CONDICIONINGRESO %in% c(NA, "nuevo"),.(Count = .N),
        by=.(Year = floor(YearMonth/100))]
 
@@ -48,7 +63,7 @@ yearly$Pop = sapply(2012:2017, function (i)     sum(GTMuniPopulation(dt.munisGT$
                                     rep(i, nrow(dt.munisGT))), na.rm = T))
 
 yearly[,Incidence := 100000*Count/Pop]
-ggplot(data=yearly) + geom_line(aes(Year, Incidence)) + ylim(0,50) + labs(title = "Guatemala TB incidence rate per 100,000 people\nby year")
+ggplot(data=yearly) + geom_line(aes(Year, Incidence)) + ylim(0,40) + labs(title = "Guatemala TB incidence rate per 100,000 people\nby year")
 
 # Map of tb incidence by DAS for 2017.
 mapdata = tbnots[CONDICIONINGRESO %in% c(NA, "nuevo") & floor(YearMonth/100) == 2017,.(counts = .N),
