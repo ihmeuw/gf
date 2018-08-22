fdc9a12de7c7f855f9df419b53be9d8ad7f12830
Naomi Provost <ninip@ihme.washington.edu>
2018-08-22 15:52:35 -0700

    compare_resources_casing adding vFm

--- outcome_measurement/tuberculosis/gtm/compare_resources_cases.r
+++ outcome_measurement/tuberculosis/gtm/compare_resources_cases.r
@@ -17,20 +17,27 @@ library(ggplot2)
 # ---------------------------------------------------
 # Directories
 
-# root directory - change this
-dir = 'C:/Users/davidp6/Google Drive/Work/IHME Work/GF/Workshops/gtm_aug2018/'
+# # root directory - change this
+# dir = 'C:/Users/davidp6/Google Drive/Work/IHME Work/GF/Workshops/gtm_aug2018/'
+# 
+# # resource tracking data
+# rtFile = paste0(dir, 'Day 1/total_resource_tracking_data_tb.csv')
+# 
+# # drug distribution data
+# distrFile = paste0(dir, 'Day 2/GTM-TB-distribution-2013-2018.csv')
+# 
+# # cohort data
+# cohortFile = paste0(dir, 'Day 2/GTM - Tx cohort data 2012-2016.csv')
+# 
+# # output file
+# outFile = paste0(dir, 'Day 2/Resource Comparison.pdf')
 
-# resource tracking data
-rtFile = paste0(dir, 'Day 1/total_resource_tracking_data_tb.csv')
+outcome_dir = "J:/Project/Evaluation/GF/outcome_measurement/gtm/"
+rtFile = "J:/Project/Evaluation/GF/resource_tracking/multi_country/mapping/total_resource_tracking_data.csv"
+distrFile = paste0(outcome_dir, "prepped_data/GTM-TB-distribution-2013-2018_with_Unit_Cost.csv")
+cohortFile = paste0(outcome_dir, "prepped_data/GTM - Tx cohort data 2012-2016.csv")
+outFile = paste0(outcome_dir, "Resource Comparison.pdf")
 
-# drug distribution data
-distrFile = paste0(dir, 'Day 2/GTM-TB-distribution-2013-2018.csv')
-
-# cohort data
-cohortFile = paste0(dir, 'Day 2/GTM - Tx cohort data 2012-2016.csv')
-
-# output file
-outFile = paste0(dir, 'Day 2/Resource Comparison.pdf')
 # ---------------------------------------------------
 
 
@@ -45,16 +52,16 @@ rtData = rtData[disease=='tb']
 
 # subset to GHE/external from sicoin and GF from budgets
 rtData = rtData[country=='Guatemala' & 
-		((data_source=='sicoin' & financing_source %in% c('ghe','donacions')) | 
-		(data_source%in%c('fgh','fpm') & financing_source=='gf')) ]
+                  ((data_source=='sicoin' & financing_source %in% c('ghe','other_dah')) | 
+                     (data_source%in%c('fgh','fpm') & financing_source=='gf')) ]
 
-
-# convert to usd
-rates = c(7.77226,7.82188,7.84517,7.72041,7.63987,7.58393,7.33155,7.38807)
-conv = data.table(year=seq(2011,2018), rate=rates)
-rtData = merge(rtData, conv, by='year',all.x=TRUE)
-rtData[data_source=='sicoin', budget:=budget/rate]
-rtData[data_source=='sicoin', disbursement:=disbursement/rate]
+# 
+# # convert to usd
+# rates = c(7.77226,7.82188,7.84517,7.72041,7.63987,7.58393,7.33155,7.38807)
+# conv = data.table(year=seq(2011,2018), rate=rates)
+# rtData = merge(rtData, conv, by='year',all.x=TRUE)
+# rtData[data_source=='sicoin', budget:=budget/rate]
+# rtData[data_source=='sicoin', disbursement:=disbursement/rate]
 
 # aggregate by year
 rtData = rtData[, .(budget=sum(budget), disbursement=sum(disbursement)), by=c('financing_source','year')]
@@ -66,13 +73,13 @@ rtAgg = rtData[, .(budget=sum(budget), disbursement=sum(disbursement)), by='year
 
 # ---------------------------------------------------
 # Load/prep distribution data
-
 # load
 distrData = fread(distrFile)
+setnames(distrData, old = "Unit Cost (USD)", new = "unitCost")
 
 # aggregate
 distrAgg = distrData[Medicine=='RIFAMPICINA', 
-		.(rifampicin=sum(Amount, na.rm=TRUE)), by='Year']
+                     .(rifampicin=sum(Amount * unitCost, na.rm=TRUE)), by=c("Year")]
 # ---------------------------------------------------
 
 
@@ -109,7 +116,7 @@ cohortAggNoMDR = merge(cohortAgg, cohortAggMDR, by='year')
 cohortAggNoMDR[, cured:=cured-second_line_begun]
 cohortAggNoMDR[, total:=total-second_line_begun]
 cohortAggNoMDR$second_line_begun = NULL
-setnames(cohortAggNoMDR, c('cured','total'), c('cured_first_line','total_first_line'))
+setnames(cohortAggNoMDR, old = c('cured','total'), new = c('cured_first_line','total_first_line'))
 # ---------------------------------------------------
 
 
