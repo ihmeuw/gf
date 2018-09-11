4bb555a6be85b24ea25e0aa3934a2856449a32dd
Naomi Provost <ninip@ihme.washington.edu>
2018-09-11 09:46:53 -0700

    adding mapping elements

--- outcome_measurement/hiv/gtm/prep_hiv_data_SIGSA.R
+++ outcome_measurement/hiv/gtm/prep_hiv_data_SIGSA.R
@@ -17,6 +17,7 @@ library(raster)
 library(rgeos)
 library(ggplot2)
 library(maptools)
+library(rcolorbrewer)
 
 # ----------------------------------------------
 #----------------------------------
@@ -78,6 +79,7 @@ total_data = merge(total_data, preg_eng, by = "pregnancy_stage")
 total_data$completed_hiv_screening_test = gsub("Si", "Yes", total_data$completed_hiv_screening_test)
 total_data$completed_hiv_screening_test = gsub("Reactivo", "Yes", total_data$completed_hiv_screening_test)
 total_data$completed_hiv_screening_test = gsub("-", "No", total_data$completed_hiv_screening_test)
+total_data$hiv_screening_result = gsub("-", "Inconclusive", total_data$hiv_screening_result)
 
 # Write csv to folderpath
 write.csv(total_data, paste0(prep_dir, "hiv_sigsa_data_prepped.csv"), row.names = FALSE)
@@ -86,17 +88,32 @@ write.csv(total_data, paste0(prep_dir, "hiv_sigsa_data_prepped.csv"), row.names 
 #Dropping 2014 for analysis becuase data does not seem accurate
 mapping_data = total_data[year != "2014"]
 
-#for the maps
+# Prep data for map
 shapeData = shapefile(paste0(mapping_dir,'GTM_adm1.shp'))
 coordinates = as.data.table(fortify(shapeData, region='NAME_1'))
 coordinates$id <- toupper(coordinates$id)
 
 mapping_data$hospital_department = sub("GUATEMALA", "GUATEMALA", mapping_data$hospital_department)
-
+mapping_data[grepl("GUATEMALA", hospital_department), hospital_department := "GUATEMALA"]
+mapping_data[grepl("PETÉN", hospital_department), hospital_department := "PETÉN"]
+mapping_data[grepl("IXCÁN", hospital_department), hospital_department := "QUICHÉ"]
+mapping_data[grepl("QUETZALTENANGO", hospital_department), hospital_department := "QUEZALTENANGO"]
 
 mapping_data$completed_hiv_screening_test = ifelse(mapping_data$completed_hiv_screening_test == "Yes", 1, 0)
 mapping_data$hiv_screening_result = ifelse(mapping_data$hiv_screening_result == "Reactivo", 1, 0)
 
+
+mapping_data[, test_by_department := sum(completed_hiv_screening_test), by = c("year", "hospital_department")]
+mapping_data[, test_by_department_positive := sum(hiv_screening_result), by = c("year", "hospital_department")]
+
+data_to_map = unique(mapping_data[,c("hospital_department", 
+                                     "test_by_department",
+                                     "test_by_department_positive",
+                                     "year")])
+graphData <- merge(coordinates, data_to_map, by.x='id', by.y='hospital_department', all=TRUE, allow.cartesian=TRUE)
+graphData$group = as.character(graphData$group)
+
+           
 # Graph desired things
 mapping_data[, test_by_date := sum(completed_hiv_screening_test), by = "date"]
 mapping_data[, test_by_date_postive := sum(hiv_screening_result), by = "date"]
@@ -111,7 +128,7 @@ mapping_data[, test_by_reason := sum(completed_hiv_screening_test), by = c( "dat
 mapping_data[, test_by_reason_positive := sum(hiv_screening_result), by = c( "date", "reason_for_visit_eng")]
 
 mapping_data_sub = unique(mapping_data[,c("date", "reason_for_visit_eng", "sexual_orientation", "risk_condition_eng",  "test_by_date", "test_by_date_postive", "test_by_orientation", "test_by_orientation_positive",
-                                      "test_by_risk", "test_by_risk_positive", "test_by_reason", "test_by_reason_positive")])
+                                          "test_by_risk", "test_by_risk_positive", "test_by_reason", "test_by_reason_positive")])
 
 #### Make graphs of data ####
 p0 = ggplot(unique(mapping_data_sub), aes(y=test_by_date, x=date)) + 
@@ -137,11 +154,10 @@ p8 = ggplot(unique(mapping_data_sub), aes(y=test_by_date_postive / test_by_date 
   labs(title='Percenatage (%) of HIV Tests over Time', 
        y='Percent (%) positive HIV test', x='') + 
   theme_bw()
-  
+
 p2 = ggplot(unique(mapping_data_sub[sexual_orientation != "Heterosexual" & sexual_orientation != "-" ]), aes(y=test_by_orientation, x=date, colour=sexual_orientation)) + 
   geom_line(size=1) +
   geom_point(size=1, color='grey45') + 
-  #geom_text(hjust=1, vjust=0) + 
   labs(title='Completed HIV test by at-risk Sexual Orientation over Time', 
        y='Amount of completed HIV test', x='') + 
   theme_bw()
@@ -149,7 +165,6 @@ p2 = ggplot(unique(mapping_data_sub[sexual_orientation != "Heterosexual" & sexua
 p3 = ggplot(unique(mapping_data_sub[sexual_orientation != "Heterosexual" & sexual_orientation != "-" ]), aes(y=test_by_orientation_positive, x=date, colour=sexual_orientation)) + 
   geom_line(size=1) +
   geom_point(size=1, color='grey45') + 
-  #geom_text(hjust=1, vjust=0) + 
   labs(title='Positive HIV test by at-risk Sexual Orientation over Time', 
        y='Amount of positive HIV test', x='') + 
   theme_bw()
@@ -157,17 +172,13 @@ p3 = ggplot(unique(mapping_data_sub[sexual_orientation != "Heterosexual" & sexua
 p9 = ggplot(unique(mapping_data_sub[sexual_orientation != "Heterosexual" & sexual_orientation != "-" ]), aes(y=test_by_orientation_positive / test_by_orientation * 100, x=date, colour=sexual_orientation)) + 
   geom_line(size=1) +
   geom_point(size=1, color='grey45') + 
-  #geom_text(hjust=1, vjust=0) + 
   labs(title='Percentage of Postive HIV test by at-risk Sexual Orientation over Time', 
        y='Percent (%) positive HIV test', x='') + 
   theme_bw()
 
-
-
 p4 = ggplot(unique(mapping_data_sub), aes(y=test_by_risk, x=date, colour=risk_condition_eng)) + 
   geom_line(size=1) +
   geom_point(size=1, color='grey45') + 
-  #geom_text(hjust=1, vjust=0) + 
   labs(title='Completed HIV test by Risk Group', 
        y='Amount of completed HIV test', x='') + 
   theme_bw()
@@ -175,7 +186,6 @@ p4 = ggplot(unique(mapping_data_sub), aes(y=test_by_risk, x=date, colour=risk_co
 p5 = ggplot(unique(mapping_data_sub), aes(y=test_by_risk_positive, x=date, colour=risk_condition_eng)) + 
   geom_line(size=1) +
   geom_point(size=1, color='grey45') + 
-  #geom_text(hjust=1, vjust=0) + 
   labs(title='Positive HIV test by Risk Group', 
        y='Amount of positive HIV test', x='') + 
   theme_bw()
@@ -183,7 +193,6 @@ p5 = ggplot(unique(mapping_data_sub), aes(y=test_by_risk_positive, x=date, colou
 p10 = ggplot(unique(mapping_data_sub), aes(y=test_by_risk_positive / test_by_risk * 100, x=date, colour=risk_condition_eng)) + 
   geom_line(size=1) +
   geom_point(size=1, color='grey45') + 
-  #geom_text(hjust=1, vjust=0) + 
   labs(title='Percentage of Positive HIV test by Risk Group', 
        y='Percent (%) of positive HIV test', x='') + 
   theme_bw()
@@ -192,7 +201,6 @@ p10 = ggplot(unique(mapping_data_sub), aes(y=test_by_risk_positive / test_by_ris
 p6 = ggplot(unique(mapping_data_sub[reason_for_visit_eng != "Own Initiative" & reason_for_visit_eng != "Other"]), aes(y=test_by_reason, x=date, colour=reason_for_visit_eng)) + 
   geom_line(size=1) +
   geom_point(size=1, color='grey45') + 
-  #geom_text(hjust=1, vjust=0) + 
   labs(title='Completed HIV test by Visit Reason', 
        y='Amount of completed HIV test', x='') + 
   theme_bw()
@@ -200,7 +208,6 @@ p6 = ggplot(unique(mapping_data_sub[reason_for_visit_eng != "Own Initiative" & r
 p7 = ggplot(unique(mapping_data_sub[reason_for_visit_eng != "Own Initiative" & reason_for_visit_eng != "Other"]), aes(y=test_by_reason_positive, x=date, colour=reason_for_visit_eng)) + 
   geom_line(size=1) +
   geom_point(size=1, color='grey45') + 
-  #geom_text(hjust=1, vjust=0) + 
   labs(title='Positive HIV test by Visit Reason', 
        y='Amount of positive HIV test', x='') + 
   theme_bw()
@@ -208,12 +215,59 @@ p7 = ggplot(unique(mapping_data_sub[reason_for_visit_eng != "Own Initiative" & r
 p11 = ggplot(unique(mapping_data_sub[reason_for_visit_eng != "Own Initiative" & reason_for_visit_eng != "Other" & reason_for_visit_eng != "Blood donor"]), aes(y=test_by_reason_positive / test_by_reason * 100, x=date, colour=reason_for_visit_eng)) + 
   geom_line(size=1) +
   geom_point(size=1, color='grey45') + 
-  #geom_text(hjust=1, vjust=0) + 
   labs(title='Percentage of Postive HIV test by Visit Reason', 
        y='Percent (%) of positive HIV test', x='') + 
   theme_bw()
 
-outFile = "/homes/ninip/SIGSA_results.pdf"
+p12 = (ggplot() + geom_polygon(data=graphData[year == 2016], aes(x=long, y=lat, group=group, fill=as.numeric(test_by_department))) + 
+           coord_equal() + ##so the two shapefiles have the same proportions 
+           geom_path(data=graphData, aes(x=long, y=lat, group=group), color="black", size=0.2, alpha=0.2) +
+           scale_fill_continuous(trans = 'reverse') +
+           theme_void()+
+           theme(plot.title = element_text(size = 18), legend.title=element_text(size=16), legend.text=element_text(size=10), plot.caption=element_text(size=12))) +
+  labs(title= "2016 Completed HIV test by Department", fill=paste0('Amount of tests done'))
+
+p13 = (ggplot() + geom_polygon(data=graphData[year == 2016], aes(x=long, y=lat, group=group, fill=test_by_department_positive/ test_by_department * 100)) + 
+          coord_equal() + ##so the two shapefiles have the same proportions 
+          geom_path(data=graphData, aes(x=long, y=lat, group=group), color="black", size=0.2, alpha=0.2) +
+          scale_fill_continuous(trans = 'reverse') +
+          theme_void()+
+          theme(plot.title = element_text(size = 18), legend.title=element_text(size=16), legend.text=element_text(size=10), plot.caption=element_text(size=12))) +
+  labs(title= "2016 Percent(%) Positive HIV Test", fill=paste0('Percent (%) HIV+')) 
+
+p14 <- (ggplot() + geom_polygon(data=graphData[year == 2017], aes(x=long, y=lat, group=group, fill=test_by_department)) + 
+          coord_equal() + ##so the two shapefiles have the same proportions 
+          geom_path(data=graphData, aes(x=long, y=lat, group=group), color="black", size=0.2, alpha=0.2) +
+          scale_fill_continuous(trans = 'reverse') +
+          theme_void()+
+          theme(plot.title = element_text(size = 18), legend.title=element_text(size=16), legend.text=element_text(size=10), plot.caption=element_text(size=12))) +
+  labs(title= "2017 Completed HIV test by Department", fill=paste0('Amount of tests done')) 
+
+p15 <- (ggplot() + geom_polygon(data=graphData[year == 2017], aes(x=long, y=lat, group=group, fill=test_by_department_positive/ test_by_department * 100)) + 
+          coord_equal() + 
+          geom_path(data=graphData, aes(x=long, y=lat, group=group), color="black", size=0.2, alpha=0.2) +
+          scale_fill_continuous(trans = 'reverse') +
+          theme_void()+
+          theme(plot.title = element_text(size = 18), legend.title=element_text(size=16), legend.text=element_text(size=10), plot.caption=element_text(size=12))) +
+         labs(title= "2017 Percent(%) Positive HIV Test", fill=paste0('Percent (%) HIV+')) 
+p16 = (ggplot() + geom_polygon(data=graphData[year == 2015], aes(x=long, y=lat, group=group, fill=as.numeric(test_by_department))) + 
+         coord_equal() + ##so the two shapefiles have the same proportions 
+         geom_path(data=graphData, aes(x=long, y=lat, group=group), color="black", size=0.2, alpha=0.2) +
+         scale_fill_continuous(trans = 'reverse') +
+         theme_void()+
+         theme(plot.title = element_text(size = 18), legend.title=element_text(size=16), legend.text=element_text(size=10), plot.caption=element_text(size=12))) +
+  labs(title= "2015 Completed HIV test by Department", fill=paste0('Amount of tests done'))
+
+p17 = (ggplot() + geom_polygon(data=graphData[year == 2015], aes(x=long, y=lat, group=group, fill=test_by_department_positive/ test_by_department * 100)) + 
+         coord_equal() + ##so the two shapefiles have the same proportions 
+         geom_path(data=graphData, aes(x=long, y=lat, group=group), color="black", size=0.2, alpha=0.2) +
+         scale_fill_continuous(trans = 'reverse') +
+         theme_void()+
+         theme(plot.title = element_text(size = 18), legend.title=element_text(size=16), legend.text=element_text(size=10), plot.caption=element_text(size=12))) +
+  labs(title= "2015 Percent(%) Positive HIV Test", fill=paste0('Percent (%) HIV+')) 
+
+
+#outFile = "/homes/ninip/SIGSA_results.pdf"
 pdf(outFile, height=5.5, width=7)
 p0
 p8
@@ -223,5 +277,11 @@ p4
 p10
 p6
 p11
+p12
+p13
+p14
+p15
+p16
+p17
 dev.off()
 
