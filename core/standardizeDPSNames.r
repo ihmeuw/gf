3a7709041e5a66fd0b51b5670ffa7905e1df0b23
David Phillips <davidp6@ihme.washington.edu>
2018-09-20 13:58:48 -0700

    Add more text processing for diacritics etc to name standardization function

--- core/standardizeDPSNames.r
+++ core/standardizeDPSNames.r
@@ -25,9 +25,20 @@ standardizeDPSNames = function(nameVector=NULL) {
 	alternateNames = unique(alternateNames[, c('dps','dps_snis','dps_pnlp'), with=FALSE])
 	alternateNames = melt(alternateNames, id.vars='dps', value.name='alternate_name')
 	
+	# make sure standard names are also an option as an input
+	tmp = unique(alternateNames[, 'dps', with=FALSE])
+	tmp[, alternate_name:=dps]
+	alternateNames = rbind(alternateNames, tmp, fill=TRUE)
+	
 	# clean up alternate names
 	alternateNames[, alternate_name:=tolower(alternate_name)]
-		# ADD MORE NECESSARY THINGS HERE
+	alternateNames[, alternate_name:=iconv(alternate_name, to='ASCII//TRANSLIT')]
+	alternateNames[, alternate_name:=gsub('-', ' ', alternate_name)]
+	
+	# clean up input vector
+	nameVector = tolower(nameVector)
+	nameVector = iconv(nameVector, to='ASCII//TRANSLIT')
+	nameVector = gsub('-', ' ', nameVector)
 	
 	# convert input vector to standardized names
 	idx = match(nameVector, alternateNames$alternate_name)
@@ -35,7 +46,7 @@ standardizeDPSNames = function(nameVector=NULL) {
 	
 	# test that it worked
 	idx = which(is.na(cleanVector))
-	if (any(is.na(cleanVector))) warning(paste0('DPS \'', paste(nameVector[idx], collapse=', '), '\' not in alternate names list (', altNamesFile, ')'))
+	if (any(is.na(cleanVector))) warning(paste0('DPS \'', paste(unique(nameVector[idx]), collapse=', '), '\' not in alternate names list (', altNamesFile, ')'))
 	if (length(nameVector)!=length(cleanVector)) stop('Something went wrong. The input vector is a different length than the output vector!') 
 	
 	# return cleaned names
