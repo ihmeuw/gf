564a89261ff58d73dc7d60f3ab2044a31895ad03
Caitlin O'Brien-Carelli <ccarelli@ihme.washington.edu>
2018-09-25 15:49:50 -0700

    Add screen output

--- outcome_measurement/all/cod/quantregScript.r
+++ outcome_measurement/all/cod/quantregScript.r
@@ -2,6 +2,9 @@
 e = commandArgs()[3]
 o = commandArgs()[4]
 i = commandArgs()[5]
+print(e)
+print(o)
+print(i)
 
 # detect if operating on windows or on the cluster 
 library(data.table)
@@ -19,14 +22,18 @@ vl[, element_id:=.GRP, by='variable']
 
 # subset the data
 subset = vl[element_id==e & org_unit_id==o] 
+head(subset)
 
 # run quantreg
 quantFit <- rq(value~date, data=subset, tau=0.5)
+summary(quantFit)
 
 # list the residuals and add them to the out file
 r <- resid(quantFit)
 subset[, fitted_value:=predict(quantFit)]
 subset[, resid:=r]
+head(subset)
 
 # save
+print(paste0('Saving: ', paste0('/ihme/scratch/users/ccarelli/quantreg_output', i)))
 saveRDS(subset, paste0('/ihme/scratch/users/ccarelli/quantreg_output', i))
