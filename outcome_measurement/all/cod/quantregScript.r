49c9783a585996e235e2fab6fc107def54cb2ea4
Caitlin O'Brien-Carelli <ccarelli@ihme.washington.edu>
2018-10-02 12:39:49 -0700

    Removed fixed effect on group

--- outcome_measurement/all/cod/quantregScript.r
+++ outcome_measurement/all/cod/quantregScript.r
@@ -33,12 +33,16 @@ nx = length(unique(subset$date))
 if(n>=3 & var!=0 & nx>=2) {  
   
   # add fixed effect on group if more than one group exists
-  form = 'value~date'
-  if (length(unique(subset$group))>1) form = paste0(form, '+factor(group)')
-  form = as.formula(form)
+  # form = 'value~date'
+  # if (length(unique(subset$group))>1) form = paste0(form, '+factor(group)')
+  # form = as.formula(form)
+  
+  # # run quantreg
+  # quantFit <- rq(form, data=subset, tau=0.5)
+  # summary(quantFit)
   
   # run quantreg
-  quantFit <- rq(form, data=subset, tau=0.5)
+  quantFit <- rq(value~date, data=subset, tau=0.5)
   summary(quantFit)
   
   # list the residuals and add them to the out file
