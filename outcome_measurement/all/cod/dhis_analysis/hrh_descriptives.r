1a85c09c33d82cede40a7783755cd833ae200de8
David Phillips <davidp6@uw.edu>
2018-10-15 16:00:27 -0700

    Update outcome_measurement/all/cod/dhis_analysis/hrh_descriptives.r

--- outcome_measurement/all/cod/dhis_analysis/hrh_descriptives.r
+++ outcome_measurement/all/cod/dhis_analysis/hrh_descriptives.r
@@ -11,6 +11,7 @@
 rm(list=ls())
 library(data.table)
 library(quantreg)
+library(stringr)
 library(ggplot2)
 library(scales)
 library(RColorBrewer)
@@ -43,7 +44,9 @@ inFile = paste0(dir, 'pre_prep/merged/base_services_drc_01_2015_09_2018.rds')
 # interim file to avoid reloading
 interimFile = paste0(dir, 'prepped/hrh_data.rds')
 
-# output file
+# output files
+outFileFacilities = paste0(dir, '../visualizations/snis_hrh_facilities.pdf')
+outFileOutliers = paste0(dir, '../visualizations/snis_hrh_outliers.pdf')
 outFile = paste0(dir, '../visualizations/snis_hrh.pdf')
 # ------------------------------------------------------------------------
 
@@ -76,9 +79,20 @@ if(reprep_data) {
 	data[element=='A 4.7 Nutritionnistes A2 /A1/ L2 Agents', cadre:='Nutritionist']
 
 	# collapse
-	data[, value:=as.numeric(value)]
+	data[, value:=as.numeric(as.character(value))]
 	byVars = c('date','org_unit','dps','health_zone','health_area','level','cadre')
 	data = data[, .(value=sum(value)), by=byVars]
+
+	# exclude August 2018
+	data = data[date<'2018-08-01']
+
+	# clean up province names
+	data[, dps:=str_sub(dps,4)]
+	data[, dps:=gsub(' Province', '', dps)]
+
+	# clean up level names
+	data[, level:=gsub('_', ' ', level)]
+	data[, level:=str_to_title(level)]
 
 	# save interim file
 	saveRDS(data, interimFile)
@@ -91,7 +105,7 @@ if (!reprep_data) data = readRDS(interimFile)
 # Remove outliers
 
 # set up
-idVars = c('date','org_unit','health_zone','cadre','dps','health_area','level',,'candidate')
+idVars = c('date','org_unit','health_zone','cadre','dps','health_area','level','candidate')
 outlierPlots = list()
 
 # loop over levels
@@ -100,45 +114,64 @@ for(l in unique(data$level)) {
 	print(l)
 	
 	# subset
-	tmp = data[level==l]
+	if (!is.na(l)) tmp = data[level==l]
+	if (is.na(l)) tmp = data[is.na(level)]
 	facs = unique(tmp$org_unit)
 	s = seq(length(facs))
 	
 	# run chunks of facilities
-	chunks = cut_interval(s, length=50, labels=F)
+	chunkSize = 50
+	chunks = cut_interval(s, length=chunkSize, labels=F)
 	for(c in unique(chunks)) { 
 		
 		# subset to chunk 
 		tmp2 = tmp[org_unit %in% facs[which(chunks==c)]]
 		
+		# remove facilities with <=2 months
+		tmp2[, n_months:=length(unique(date)), by=c('org_unit','cadre')]
+		tmp2 = tmp2[n_months>2]
+		
+		# skip if the whole chunk was <=2 months
+		if (nrow(tmp2)==0) next 
+		
+		# mean-center to avoid fixed effects
+		tmp2[, mean:=mean(value), by=c('org_unit','cadre')]
+		tmp2[, value_mc:=value-mean]
+		
 		# run regression
-		lmFit = lm(value ~ org_unit * cadre * date, data=tmp2)
+		lmFit = rq(value_mc ~ date, data=tmp2)
 		
 		# identify candidates
 		tmp2[, pred:=predict(lmFit)]
 		tmp2[, resid:=resid(lmFit)]
 		tmp2[, median:=median(resid)]
 		tmp2[, sd:=sd(resid)]
-		f = 3.25
+		f = 14
 		tmp2[, candidate:= resid>(median+(f*sd)) | resid<(median-(f*sd))]
 		candidateFacilities = unique(tmp2[candidate==TRUE]$org_unit)
-		if (i==1) candidateOutliers = tmp2[candidate==TRUE, idVars]
-		if (i>1) candidateOutliers = rbind(candidateOutliers, tmp2[candidate==TRUE, idVars])
+		if (length(candidateFacilities)==0) next
+		if (i==1) candidateOutliers = tmp2[candidate==TRUE, idVars, with=FALSE]
+		if (i>1) candidateOutliers = rbind(candidateOutliers, tmp2[candidate==TRUE, idVars, with=FALSE])
 		
 		# graph
-		outlierPlots[[i]] = ggplot(tmp2[org_unit %in% candidates], aes(y=value,x=date)) + 
+		outlierPlots[[i]] = ggplot(tmp2[org_unit %in% candidateFacilities], aes(y=value,x=date)) + 
 			geom_line(aes(color=cadre)) + 
 			geom_point(color='black') + 
 			geom_point(data=tmp2[candidate==TRUE], color='red') + 
-			geom_line(aes(y=pred, color=cadre), linetype='dashed') + 
+			geom_line(aes(y=pred+mean, color=cadre), linetype='dashed') + 
 			facet_wrap(~org_unit, scales='free') + 
+			labs(title=paste0('Detected Outliers among Facilities ', 
+					(c*chunkSize)-chunkSize, '-', c*chunkSize, ' (chunk ', c, ')'), 
+				subtitle=paste('Level:', l) , y='Number of Health Care Workers', x='', 
+				color='Cadre', caption='Dashed lines: linear fit\nRed points: candidate outliers') + 
 			theme_bw()
 		i=i+1
 	}
 }
 
 # remove
-data = merge(data, candidateOutliers, by=idVars)
+data = merge(data, candidateOutliers, by=idVars[idVars!='candidate'], all.x=TRUE)
+data[, original_value:=value]
 data[candidate==TRUE, value:=NA]
 # -------------------------------------------------------------------------
 
@@ -147,13 +180,17 @@ data[candidate==TRUE, value:=NA]
 # Aggregate
 
 # national level
-aggN = data[, .(value=sum(value)), by=c('date','cadre')]
-
+aggN = data[, .(value=sum(value, na.rm=TRUE), mean=mean(value,na.rm=TRUE), median=median(value,na.rm=TRUE),
+			q25=quantile(value, .25, na.rm=TRUE), q75=quantile(value, .75, na.rm=TRUE),
+			n_facilities=length(unique(org_unit))), by=c('date','cadre')]
+			
 # dps level
-aggD = data[, .(value=sum(value)), by=c('dps', 'date','cadre')]
+aggD = data[, .(value=sum(value, na.rm=TRUE), mean=mean(value,na.rm=TRUE), median=median(value,na.rm=TRUE), 
+			n_facilities=length(unique(org_unit))), by=c('dps', 'date','cadre')]
 
-# health facility type level
-aggT = data[, .(value=sum(value)), by=c('org_unit_type', 'date','cadre')]
+# health facility level
+aggL = data[, .(value=sum(value, na.rm=TRUE), mean=mean(value,na.rm=TRUE), median=median(value,na.rm=TRUE), 
+			n_facilities=length(unique(org_unit))), by=c('level', 'date','cadre')]
 # -------------------------------------------------------------------------
 
 
@@ -161,30 +198,154 @@ aggT = data[, .(value=sum(value)), by=c('org_unit_type', 'date','cadre')]
 # Set up to graph
 c = brewer.pal(12, 'Paired')
 colors = c('General Practitioner'=c[1], 'Nurse (A1, A2 or L2)'=c[4], 
-		'Lab Technician'=c[8], 'Midwife'=c[10], 'Nutritionist'=c[6])
+		'Lab Technician'=c[8], 'Midwife'=c[10], 'Nutritionist'=c[6], 
+		'Other Staff'=c[12])
+sizeRange1 = c(.5,3.5)
+sizeRange2 = c(.5,1.5)
 # ---------------------------------------------
 
 
 # ------------------------------------------------------
-# Graph
+# Graph counts
 
-ggplot(data, aes(y=value, x=factor(date))) + 
-	geom_violin() + 
-	theme_bw()
-
-ggplot(aggN, aes(y=value, x=date, color=cadre)) + 
+# national counts
+countPlots = list()
+i=1
+countPlots[[i]] = ggplot(aggN, aes(y=value/1000, x=date, color=cadre)) + 
 	geom_line() + 
-	geom_point() + 
+	geom_point(aes(size=n_facilities)) + 
+	scale_color_manual(values=colors) + 
+	scale_size_continuous(range=sizeRange1) + 
+	labs(title='National-Level Counts', y='Number of Health Care Workers (in Thousands)', 
+		x='', color='Cadre', size='Number of\nFacilities\nReporting') + 
 	theme_bw()
 
+# level counts
+i=i+1
+countPlots[[i]] = ggplot(aggL, aes(y=value, x=date, color=cadre)) + 
+	geom_line() + 
+	geom_point(aes(size=n_facilities)) + 
+	scale_color_manual(values=colors) + 
+	scale_size_continuous(range=sizeRange2) + 
+	facet_wrap(~level, scales='free_y') + 
+	labs(title='Facility Platform Counts', y='Number of Health Care Workers', 
+		x='', color='Cadre', size='Number of\nFacilities\nReporting') + 
+	theme_bw()
+
+# dps counts
+dpss = unique(aggD$dps)
+chunkSize=9
+chunks = cut_interval(seq(length(dpss)), length=chunkSize, labels=F)
+for(c in seq(max(chunks))) { 
+	i=i+1
+	countPlots[[i]] = ggplot(aggD[dps %in% dpss[chunks==c]], aes(y=value, x=date, color=cadre)) + 
+		geom_line() + 
+		geom_point(aes(size=n_facilities)) + 
+		scale_color_manual(values=colors) + 
+		scale_size_continuous(range=sizeRange2) + 
+		facet_wrap(~dps, scales='free_y') + 
+		labs(title=paste0('DPS Counts (DPS\'s ', (c*chunkSize)-chunkSize+1, '-', c*chunkSize, ')'), 
+			y='Number of Health Care Workers', 
+			x='', color='Cadre', size='Number of\nFacilities\nReporting') + 
+		theme_bw()
+}
+# ------------------------------------------------------
+
+
+# ------------------------------------------------------
+# Graph rates
+
+# national rates
+ratePlots = list()
+i=1
+ratePlots[[i]] = ggplot(aggN, aes(y=mean, x=date, color=cadre)) + 
+	geom_line() + 
+	geom_point(aes(size=n_facilities)) + 
+	scale_color_manual(values=colors) + 
+	scale_size_continuous(range=sizeRange1) + 
+	labs(title='National-Level Medians', y='Health Care Workers per Facility', 
+		x='', color='Cadre', size='Number of\nFacilities\nReporting') + 
+	theme_bw()
+
+# level rates
+i=i+1
+ratePlots[[i]] = ggplot(aggL, aes(y=mean, x=date, color=cadre)) + 
+	geom_line() + 
+	geom_point(aes(size=n_facilities)) + 
+	scale_color_manual(values=colors) + 
+	scale_size_continuous(range=sizeRange2) + 
+	facet_wrap(~level) + 
+	labs(title='Facility Platform Medians', y='Health Care Workers per Facility', 
+		x='', color='Cadre', size='Number of\nFacilities\nReporting') + 
+	theme_bw()
+
+# dps rates
+dpss = unique(aggD$dps)
+chunkSize=9
+chunks = cut_interval(seq(length(dpss)), length=chunkSize, labels=F)
+for(c in seq(max(chunks))) { 
+i=i+1
+ratePlots[[i]] = ggplot(aggD[dps %in% dpss[chunks==c]], aes(y=mean, x=date, color=cadre)) + 
+	geom_line() + 
+	geom_point(aes(size=n_facilities)) + 
+	scale_color_manual(values=colors) + 
+	scale_size_continuous(range=sizeRange2) + 
+	facet_wrap(~dps) + 
+	labs(title=paste0('DPS Medians (DPS\'s ', (c*chunkSize)-chunkSize+1, '-', c*chunkSize, ')'), 
+		y='Health Care Workers per Facility', 
+		x='', color='Cadre', size='Number of\nFacilities\nReporting') + 
+	theme_bw()
+}
+# ------------------------------------------------------
+
+
+# ------------------------------------------------------
+# Facility counts
+facPlots = list()
+i=1
+facs = unique(data$org_unit)
+chunkSize=9
+chunks = cut_interval(seq(length(facs)), length=chunkSize, labels=F)
+for(c in seq(max(chunks))) { 
+	facPlots[[i]] = ggplot(data[org_unit %in% facs[chunks==c]], aes(y=value, x=date, color=cadre)) + 
+		geom_line() + 
+		geom_point() + 
+		scale_color_manual(values=colors) + 
+		facet_wrap(~org_unit, scales='free_y') + 
+		labs(title=paste0('Facility Counts (Facilities ', (c*chunkSize)-chunkSize+1, '-', c*chunkSize, ')'), 
+			y='Number of Health Care Workers', x='', color='Cadre') + 
+		theme_bw()
+	i=i+1
+}
 # ------------------------------------------------------
 
 
 # --------------------------------------------------
 # Save
-pdf(outFile, height=5.5, width=9)
-for(i in seq(length(meanPlots))) print(meanPlots[[i]])
-for(i in seq(length(pctPlots))) print(pctPlots[[i]])
+
+# descriptives
+pdf(outFile, height=5.5, width=10)
+for(i in seq(length(ratePlots))) print(ratePlots[[i]])
+for(i in seq(length(countPlots))) print(countPlots[[i]])
+dev.off()
+
+# outlier plots
+pdf(outFileOutliers, height=5.5, width=9)
+for(i in seq(length(outlierPlots))) { 
+	print(outlierPlots[[i]])
+	cat(paste0('\r ',round(i/length(outlierPlots)*100),'%'))
+	flush.console()
+}
+dev.off()
+
+# facility plots (random sample of 100)
+set.seed(1)
+pdf(outFileFacilities, height=5.5, width=9)
+for(i in sample(seq(length(facPlots)), 100)) { 
+	print(facPlots[[i]])
+	cat(paste0('\r ',round(i/length(facPlots)*100),'%'))
+	flush.console()
+}
 dev.off()
 # --------------------------------------------------
 
