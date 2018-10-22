ef0024b5a7287e9b7872fa8661740cdb3ab99fac
Audrey Batzel <abatzel@uw.edu>
2018-10-22 16:54:55 -0700

    update descriptive analyses on pnlt case outcomes

--- outcome_measurement/tuberculosis/cod/descriptive_analysis_PNLT.R
+++ outcome_measurement/tuberculosis/cod/descriptive_analysis_PNLT.R
@@ -1,7 +1,7 @@
 # ----------------------------------------------
 # Audrey Batzel
-# 8/7/2018
-# Map visualizations of PNLT data
+# 9/2?/2018
+# First decriptive analysis of PNLT data
 # ----------------------------------------------
 ## Set up R / install packages 
 # ----------------------------------------------
@@ -31,7 +31,7 @@ pnlt_outcomes_18_tb_hiv <- "PNLT_case_outcomes_2018_TBHIV.csv"
 pnlt_case_screening_17 <- "PNLT_case_screening_2017.csv"
 
 # output files
-compare_dps_graphs <- "PNLT_case_outcomes_by_dps.pdf"
+compare_dps_graphs <- "PNLT case outcomes - incident cases - by dps.pdf"
 compare_dps_graphs_faceted <- "PNLT_case_outcomes_by_dps_(faceted).pdf"
 # ----------------------------------------------
 
@@ -46,18 +46,23 @@ dt17 <- as.data.table(dt17)
 incid_cases_patterns <- c("NC", "RECH")
 dt17[, incid_case := ifelse (grepl(paste(incid_cases_patterns, collapse="|"), dt17$TB_type), "yes", "no")]
 
-dt18 <- read.csv(paste0(data_dir, pnlt_data_case_outcomes18))
-dt18 <- as.data.table(dt18)
+# Can't match 2018 and 2017 up this way unfortunately
+# dt18 <- read.csv(paste0(data_dir, pnlt_data_case_outcomes18))
+# dt18 <- as.data.table(dt18)
+# 
+# dt18[, incid_case:= ifelse (grepl("CAS INCID", dt18$sheet), "yes", "no")]
+# 
+# dt <-  rbindlist(list(dt17, dt18), use.names=TRUE, fill= TRUE)
 
-dt18[, incid_case:= ifelse (grepl("CAS INCID", dt18$sheet), "yes", "no")]
-
-dt <-  rbindlist(list(dt17, dt18), use.names=TRUE, fill= TRUE)
-dt[, X:=NULL]
-
-dt_incid <- dt[incid_case == "yes", ]
+dt_incid <- dt17[incid_case == "yes", ]
+dt_incid[, X:=NULL]
 dt_incid <- dt_incid[, lapply(.SD, sum, na.rm=TRUE), by= c("dps", "data_year", "file_year", "quarter"), .SDcols= 2:10 ]
 
 dt <- copy(dt_incid)
+
+dt_natl <- dt_incid[, lapply(.SD, sum, na.rm=TRUE), by= c("data_year", "file_year", "quarter"), .SDcols= 5:13 ]
+dt <- copy(dt_natl)
+
 # ~~~~~~~~~~
 # dt_tbhiv <- read.csv(paste0(data_dir, pnlt_outcomes_18_tb_hiv))
 # 
@@ -112,16 +117,15 @@ dt <- copy(dt_incid)
 # ----------------------------------------------
 ## MOVE TO PREP CODE:
 # ----------------------------------------------
-# set quarters:
-dt$quarter <- gsub("T", "", dt$quarter)
-dt$quarter <- as.numeric(dt$quarter)
-
-# rename cols
-# setnames(dt, "TB_type", "tb_type")
-
 # change class of vars
 dt$tb_type <- as.character(dt$tb_type)
 dt$dps <- as.character(dt$dps)
+# rename cols
+# setnames(dt, "TB_type", "tb_type")
+
+# set quarters:
+dt$quarter <- gsub("T", "", dt$quarter)
+dt$quarter <- as.numeric(dt$quarter)
 
 # create date variable:
 dt[ file_year=="2017" & quarter=="1", date:= "2017-01-01"]
@@ -138,6 +142,7 @@ dt[, full_trt_course := healed + trt_complete]
 dt[, pct_comp_full_trt_course := full_trt_course/tot_cas_reg] #using this rather than over the # of cases evaluated 
 dt[, pct_died := died/tot_cas_reg]
 
+
 # # melt data
 # melted_dt <- melt.data.table(dt, id.vars=id_vars, variable.name = "outcome")
 # melted_dt$outcome <- as.character(melted_dt$outcome)
@@ -151,12 +156,12 @@ dps_group3 <- dps_names[15:21]
 dps_group4 <- dps_names[22:27]
 
 
-dps_group1 <- c("kwilu", "kongo-central-ouest", "haut???lomami", "sud???kivu", "tshuapa" )
+dps_group1 <- c("kwilu", "kongo-central-ouest", "haut-lomami", "sud-kivu", "tshuapa" )
 dps_group2 <- c("kinshasa", "maniema", "lomami", "tshopo")
-dps_group3 <- c("kongo-central-est", "kasai???central", "haut???katanga", "sud-ubangi", "nord-ubangi" )
-dps_group4 <- c("nord???kivu", "haut???uele", "mongala", "kwango" ) 
+dps_group3 <- c("kongo-central-est", "kasai-central", "haut-katanga", "sud-ubangi", "nord-ubangi" )
+dps_group4 <- c("nord-kivu", "haut-uele", "mongala", "kwango" ) 
 dps_group5 <- c("equateur", "ituri") 
-dps_group6 <- c("kasai", "kasai???oriental","sankuru", "bas???uele") 
+dps_group6 <- c("kasai", "kasai-oriental","sankuru", "bas-uele") 
 dps_group7 <- c("lualaba", "tanganyika", "mai-ndombe")
 
 
@@ -173,38 +178,64 @@ dt[dps %in% dps_group7, dps_group:=7]
 # ----------------------------------------------
 ## graphing function / loop through dps and tb_type and make graphs
 # ----------------------------------------------
-# make a graph for each DPS
-pdf(file=paste0(export_dir, "PNLT_percent_trt_comp_by_trend.pdf"), height = 9, width = 11)
-  
-for (d in 1:7){
-    g <- ggplot(dt[dps_group==d, ], aes(date, pct_comp_full_trt_course, color=dps, ymin=.6, ymax=1) ) + theme_bw() + 
-      geom_point() + 
-      geom_line() + xlab("Date") + ylab("Percent") +
-      ggtitle(paste0(d, ": Percent of registered TB cases that recorded a completed course of treatment")) +
-      scale_y_continuous(labels=function(x)x*100)
-    print(g)
-}
+# # Graphs by "trend"
+# pdf(file=paste0(export_dir, "cases_registered_over_time.pdf"), height = 9, width = 11)
+# for (d in 1:7){
+#     g <- ggplot(dt, aes(date, tot_cas_reg, color=dps) ) + theme_bw() + 
+#       geom_point() + 
+#       geom_line() + xlab("Date") + ylab("Percent") +
+#       ggtitle(paste0("Registered TB cases over time")) +
+#       scale_y_continuous(labels= scales:: comma)
+#     print(g)
+# }
+# dev.off()
+# ----------------------------------------------
 
-dev.off()
-
-
-pdf(file=paste0(export_dir, compare_dps_graphs), height = 9, width = 11)
-
-print(makeGraph("pct_comp_full_trt_course", "Percent of registered TB cases that recorded a completed course of treatment", .6, 1.0))
-print(makeGraph("pct_died", "Percent of deaths out of TB cases registered", 0, .1))
-
-dev.off()
-
-
+# ----------------------------------------------
+# Case outcomes for incident cases graphs
 makeGraph <- function(variable, title, min_y, max_y){
   g <- ggplot(dt, aes(date, get(variable), color=dps, ymin=min_y, ymax=max_y) ) + theme_bw() + 
     geom_point() + 
     geom_line() + xlab("Date") + ylab("Percent") +
-    ggtitle(title)+
+    ggtitle(paste0("2017: ", title)) +
     scale_y_continuous(labels=function(x)x*100)
 }
 
+makeGraphPercent <- function(variable, title, min_y, max_y){
+  g <- ggplot(dt, aes(date, get(variable), ymin=min_y, ymax=max_y) ) + theme_bw() + 
+    geom_point() + 
+    geom_line() + xlab("Date") + ylab("Percent") +
+    ggtitle(paste0("2017 DRC: ", title)) +
+    scale_y_continuous(labels=function(x)x*100)
+}
 
+makeGraphNumber <- function(variable,title){
+  g <- ggplot(dt, aes(date, get(variable)) ) + theme_bw() + 
+    geom_point() + 
+    geom_line() + xlab("Date") + ylab("Number") +
+    ggtitle(paste0("2017 DRC: ", title)) +
+    scale_y_continuous(labels = scales::comma)
+}
+
+pdf(file=paste0(export_dir, "case_outcomes_data/", compare_dps_graphs), height = 9, width = 11)
+print(makeGraph("pct_comp_full_trt_course", "Percent of registered incident TB cases that recorded a completed course of treatment", .6, 1.0))
+print(makeGraph("pct_died", "Percent of deaths out of incident TB cases registered", 0, .1))
+print(makeGraph("percent_cases_eval", "Percent of incident TB cases registered that were evaluated", 0, .1))
+dev.off()
+
+pdf(file=paste0(export_dir, "case_outcomes_data/PNLT case outcomes - incident cases - national.pdf"), height = 9, width = 11)
+print(makeGraphNumber("tot_cas_reg", "Number of registered incident TB cases"))
+print(makeGraphPercent("percent_cases_eval", "Percent of incident TB cases registered that were evaluated", 0, .1))
+print(makeGraphNumber("full_trt_course", "Number of registered incident TB cases that recorded a completed course \nof treatment"))
+print(makeGraphNumber("trt_failed", "Number of registered incident TB cases that recorded a treatment failure"))
+print(makeGraphPercent("pct_comp_full_trt_course", "Percent of registered incident TB cases that recorded a completed \ncourse of treatment", .6, 1.0))
+print(makeGraphNumber("died", "Number of registered incident TB cases that died"))
+print(makeGraphPercent("pct_died", "Percent of deaths out of incident TB cases registered", 0, .1))
+dev.off()
+# ----------------------------------------------
+
+# ----------------------------------------------
+# TB HIV cases graphs
 dps_to_remove <- dt[case_type=="tb_hiv_case" & is.na(pct_comp_full_trt_course), dps]
 dt$X <- NULL
 id_vars <- c(id_vars, "case_type", "date")
@@ -237,6 +268,10 @@ g <- ggplot(dt[case_type == "tb_hiv_case"], aes(date, pct_comp_full_trt_course, 
 #     facet_wrap(~ tb_type, scales="free_y")
 #   return(g)
 # }
+# ----------------------------------------------
+
+# ----------------------------------------------
+# Initial descriptive analysis graphs
 
 for (i in tb_type){
   pdf(paste0(export_dir, i, "_graph.pdf"), height = 9, width = 11)
