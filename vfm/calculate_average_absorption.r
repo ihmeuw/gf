f5d7146b4f29a722fce8c58c087de50aae67fcfe
Emily Linebarger <eklinebarg@gmail.com>
2018-11-14 07:42:01 -0800

    Adding a script to calculate average absorption.

--- vfm/calculate_average_absorption.r
+++ vfm/calculate_average_absorption.r
@@ -36,36 +36,11 @@ inFile = paste0(dir, 'mapping/total_resource_tracking_data.csv')
 allData = fread(inFile)
 grants <- c('UGA-C-TASO', 'UGA-H-MoFPED', 'UGA-M-MoFPED', 'UGA-M-TASO', 'UGA-T-MoFPED', 'COD-C-CORDAID', 'COD-M-SANRU', 'COD-H-MOH', 'COD-M-MOH', 
             'COD-T-MOH', 'GTM-T-MSPAS', 'GTM-H-HIVOS')
+data$data_source = ifelse(data$data_source == "fpm_final", "fpm", data$data_source)
 #-------------------------------------
 
-#---------------------------------------------
-# Report on historical absorption 
-
-# subset to GOS and PUDRs
-historical_exp = allData[data_source %in% c('gos','pudr')]
-
-# aggregate the PUDRs by grant but GOS overall
-historical_exp[data_source=='gos', grant_number:='all']
-historical_exp[data_source=='gos', grant_period:='all']
-
-# identify RSSH modules
-historical_exp[, rssh:=grepl('R',code)]
-
-# compute absorption by module
-byVars = c('country','grant_number','grant_period','abbrev_module','rssh','data_source')
-agg = historical_exp[, .(expenditure=sum(expenditure, na.rm=T), budget=sum(budget,na.rm=T)), by=byVars]
-agg[, absorption:=expenditure/budget]
-agg = agg[which(agg$grant_number %in% grants)]
-
-#Want to collapse on grant_number and abbrev_module to get average over time. 
-agg = summaryBy(absorption~grant_number+abbrev_module, FUN=c(mean), data = agg)
-agg$absorption.mean <- round(agg$absorption.mean, 2)
-
 #-----------------------------------
-
-
-#-----------------------------------
-#Report on absorption for 1st and 2nd quarters of 2018 cycle 
+# PART 1: Report on absorption for 1st and 2nd quarters of 2018 cycle 
 # subset to budgets
 data = allData[data_source=='fpm']
 
@@ -101,8 +76,6 @@ data[, observed_absorption:=expenditure/budget]
 # -----------------------------
 # Subset to only the grants from 2018 
 
-grants <- c('UGA-C-TASO', 'UGA-H-MoFPED', 'UGA-M-MoFPED', 'UGA-M-TASO', 'UGA-T-MoFPED', 'COD-C-CORDAID', 'COD-M-SANRU', 'COD-H-MOH', 'COD-M-MOH', 
-            'COD-T-MOH', 'GTM-T-MSPAS', 'GTM-H-HIVOS')
 # subset to only the 1st and 2nd quarter of the grants we want #ADD DRC PUDRS
 out = data[ which(data$grant_number %in% grants), ]
 out = out[which((out$quarter == 1 | out$quarter == 2) & out$year ==2018)]
@@ -131,7 +104,47 @@ cols <- names(out)[3:length(out)]
 out = out[,(cols) := round(.SD,1), .SDcols=cols]
 
 # -----------------------------
-#Optional to use- making dataframes for each grant so it's easier to view them. 
-cod_m_sanru <- out[, grepl('COD-M-SANRU', names(out))]
+# cod_m_sanru <-out[, grep("COD-M-SANRU", colnames(out))]
+# cod_m_sanru <- append(cod_m_sanru, 1:2)
+# print_data <- out[c=(cod_m_sanru)]
+
+cod_c_cordaid <- out[, c(1, 2, 3:6)]
+cod_h_moh <- out[, c(1, 2, 7:10)]
+cod_m_moh <- out[, c(1, 2, 11:14)]
+cod_m_sanru <- out[, c(1, 2, 15:18)]
+cod_t_moh <- out[, c(1, 2, 19:22)]
+uga_c_taso <- out[, c(1, 2, 23:26)]
+uga_h_mofped <- out[, c(1, 2, 27:30)]
+uga_m_mofped <- out[, c(1, 2, 31:34)]
+uga_m_taso<- out[, c(1, 2, 35:38)]
+uga_t_mofped<- out[, c(1, 2, 39:42)]
+
+
+
+#---------------------------------------------
+# PART 2: Report on historical absorption 
+
+# subset to GOS and PUDRs
+historical_exp = allData[data_source %in% c('gos')]
+
+# aggregate the PUDRs by grant but GOS overall
+historical_exp[data_source=='gos', grant_number:='all']
+historical_exp[data_source=='gos', grant_period:='all']
+
+# identify RSSH modules
+historical_exp[, rssh:=grepl('R',code)]
+
+# compute absorption by module
+byVars = c('country', 'disease', 'grant_period','abbrev_module')
+agg = historical_exp[, .(expenditure=sum(expenditure, na.rm=T), budget=sum(budget,na.rm=T)), by=byVars]
+agg[, absorption:=expenditure/budget]
+
+#Want to collapse on grant_number and abbrev_module to get average over time. 
+agg = summaryBy(absorption~country+disease+abbrev_module, FUN=c(mean), data = agg)
+agg$absorption.mean <- round(agg$absorption.mean, 2)
+
+write.csv(agg, "J:/Project/Evaluation/GF/vfm/outputs/historical_absorption_by_country_and_disease.csv", row.names = F)
+#-----------------------------------
+
 
 
