50bad636d92c4bf4712fa70e767b4fc2e8cfc3e1
Emily Linebarger <eklinebarg@gmail.com>
2018-12-10 11:18:49 -0800

    Removing some old files

--- results_chains/4_map_outputs.r
+++
@@ -1,7 +0,0 @@
-# ----------------------------------------------------
-# AUTHOR: Emily Linebarger 
-# PURPOSE: Map outputs as part of results chain. 
-#       
-# DATE: Last updated November 2018. 
-#-----------------------------------------------------
-
