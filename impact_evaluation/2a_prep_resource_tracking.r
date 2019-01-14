733f1921359deb987617dcf3b07ca1b434e275ac
Emily Linebarger <eklinebarg@gmail.com>
2019-01-14 15:50:01 -0800

    For impact evaluation code: 1. Adding in high-level code flow to master file, and 2. Adding in resource tracking prep (pretty minor work needed here)

--- impact_evaluation/2a_prep_resource_tracking.r
+++ impact_evaluation/2a_prep_resource_tracking.r
@@ -5,3 +5,19 @@
 # DATE: Last updated January 2019. 
 # ----------------------------------------------------------
 
+#------------------------------------
+# TO-DO: 
+# -Do we want to include any GHE data? 
+#------------------------------------
+
+#Read in final budget and expenditure data from RT database, and bind together. 
+final_budgets <- readRDS("J:/Project/Evaluation/GF/resource_tracking/multi_country/mapping/final_budgets.rds")
+#final_expenditures <- readRDS("J:/Project/Evaluation/GF/resource_tracking/multi_country/mapping/final_expenditures.rds")
+
+# budget_factors = is.factor(final_budgets)
+# 
+# for (col in colnames(final_expenditures)){
+#   levels(col) = unique(final_expenditures$col)
+# }
+
+resource_tracking <- final_budgets #Will want to add expenditures in here eventually. 
