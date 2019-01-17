312b7ae3216a9e5643cb1feb5f7650a3f35b0afc
Audrey Batzel <abatzel@uw.edu>
2019-01-16 17:52:06 -0800

    set up pilot dataset

--- impact_evaluation/3_merge_data.R
+++ impact_evaluation/3_merge_data.R
@@ -4,3 +4,9 @@
 # DATE: Last updated January 2019. 
 # ----------------------------------------------------------
 
+j = ifelse(Sys.info()[1]=='Windows', 'J:', '/home/j')
+dir = paste0(j, '/Project/Evaluation/GF/impact_evaluation/cod/prepped_data/')
+
+outputs_activities = paste0(dir, "outputs_activites_for_pilot.RDS")  # produced by code in 2b_prep_activities_outputs
+
+dt <- readRDS(outputs_activities)
