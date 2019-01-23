706fd14bcdcbdd3cf92d639b7835a32e0f45dd54
Audrey Batzel <abatzel@ihme.washington.edu>
2019-01-22 17:48:04 -0800

    function to make renaming variables in the data easier/faster

---
+++ outcome_measurement/malaria/cod/function_rename_variable.R
@@ -0,0 +1,22 @@
+rename_variable <- function(data_table, variable_to_rename){
+  vars <- unique(data_table[, get(variable_to_rename)])
+  
+  data_table$old_name = data_table[, get(variable_to_rename)] # save the old names in case the renaming messes up
+  
+  for (v in vars){
+    input <- readline(prompt= paste0("What do you want to rename ", v, "? Press ENTER to skip or type 'STOP' to end.  "))
+    input <- trimws(input)
+    
+    if(input == "") { 
+      next
+    } else if (input== "STOP"){
+      break
+    } else {
+      data_table[get(variable_to_rename) == v, eval(variable_to_rename) := input]
+    }
+  }
+  return(data_table)
+}
+
+dt <- copy(dt_pnlp)
+dt <- rename_variable(dt, "indicator")
