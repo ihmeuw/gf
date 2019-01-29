55840409aee223eb35b6e01e920ddee6783f0d75
David Phillips <davidp6@uw.edu>
2019-01-29 14:03:41 -0800

    Update wce dummy example to include back-casting

--- impact_evaluation/wce/wce_dummy_example.r
+++ impact_evaluation/wce/wce_dummy_example.r
@@ -17,7 +17,24 @@ library(ggplot2)
 
 # -------------------------------------------------------------------------------------
 # Make fake data
+
+# parameters
+syear = 2010
+eyear = 2017
+
+# generate date
 data = data.table(year = seq(from=2010, to=2017), nets = rnorm(mean=1000, sd=100, n=8))
+
+# extend first value back
+tmp = data[rep(which(data$year==syear),10)]
+tmp[, year:=seq(from=syear-nrow(tmp), to=syear-1)]
+data = rbind(tmp, data)
+
+# backcast
+lmFit = lm(nets~year, data)
+backcast = predict(lmFit, newdata=tmp)
+data[, nets_backcast:=nets]
+data[year<syear, nets_backcast:=backcast]
 # -------------------------------------------------------------------------------------
 
 
@@ -44,12 +61,22 @@ sigma5 = optim(par=.75, fn=weightFunction, delta=5)$par
 # create weighted sums with sigma3
 for(i in seq(nrow(data))) { 
 	# create weights
-	wvar = paste0('w',i)
-	data[1:i, (wvar):=weightFunction(sigma=sigma3, t=year)]
+	wvar3 = paste0('w3',i)
+	wvar5 = paste0('w5',i)
+	data[1:i, (wvar3):=weightFunction(sigma=sigma3, t=year)]
+	data[1:i, (wvar5):=weightFunction(sigma=sigma5, t=year)]
 	
 	# create weighted sum
-	tmp = cumsum(data[1:i]$nets*data[1:i][[wvar]])
-	data[i, weighted_cumulative:=tmp[i]]
+	tmp3 = cumsum(data[1:i]$nets*data[1:i][[wvar3]])
+	data[i, weighted_cumulative3:=tmp3[i]]
+	tmp5 = cumsum(data[1:i]$nets*data[1:i][[wvar5]])
+	data[i, weighted_cumulative5:=tmp5[i]]
+	
+	# create weighted sum using backcast rather than extended values
+	tmp3 = cumsum(data[1:i]$nets_backcast*data[1:i][[wvar3]])
+	data[i, weighted_cumulative3_backcast:=tmp3[i]]
+	tmp5 = cumsum(data[1:i]$nets_backcast*data[1:i][[wvar5]])
+	data[i, weighted_cumulative5_backcast:=tmp5[i]]
 }
 # ----------------------------------------------
 
@@ -57,13 +84,35 @@ for(i in seq(nrow(data))) {
 # ----------------------------------------------
 # Graph to check
 
+# set up to graph weights
+wvar3 = paste0('w3',nrow(data))
+wvar5 = paste0('w5',nrow(data))
+graphData1 = data[, c('year', wvar3, wvar5), with=FALSE]
+graphData1 = melt(graphData1, id.vars='year')
+graphData1[, t:=eyear-year]
+graphData1[, variable:=ifelse(variable==wvar3, '3-Year ITN Life', '5-Year ITN Life')]
+
 # graph weight curve
-ggplot(data, aes(y=w8,x=year)) + 
-	geom_line()
-	
-# graph cumulative sums
-ggplot(data, aes(y=nets, x=year)) + 
+ggplot(graphData1[year>=syear], aes(y=value, x=t, linetype=variable)) + 
 	geom_line() + 
-	geom_line(aes(y=weighted_cumulative), color='red') + 
-	geom_line(aes(y=simple_cumulative), color='green') 
+	labs(title='Weight Functions', y='Weight', x='Time Since Distribution of Net') + 
+	theme_bw()
+	
+# set up to graph cumulative sums
+colors=c('Quantity'='black', 'Weighted Cumulative Sum (3-year ITN life)'='red', 
+	'Weighted Cumulative Sum (5-year ITN life)'='purple', 
+	'Weighted Cumulative Sum (3-year ITN life) w/ Backcasting'='orange', 
+	'Weighted Cumulative Sum (5-year ITN life) w/ Backcasting'='blue', 
+	'Simple Cumulative Sum'='green')
+
+# graph cumulative sums
+ggplot(data[year>=syear], aes(y=nets, x=year)) + 
+	geom_line(aes(color='Quantity')) + 
+	geom_line(aes(y=weighted_cumulative3, color='Weighted Cumulative Sum (3-year ITN life)')) + 
+	geom_line(aes(y=weighted_cumulative5, color='Weighted Cumulative Sum (5-year ITN life)')) + 
+	geom_line(aes(y=weighted_cumulative3_backcast, color='Weighted Cumulative Sum (3-year ITN life) w/ Backcasting')) + 
+	geom_line(aes(y=weighted_cumulative5_backcast, color='Weighted Cumulative Sum (5-year ITN life) w/ Backcasting')) + 
+	geom_line(aes(y=simple_cumulative, color='Simple Cumulative Sum')) + 
+	scale_color_manual(values=colors) + 
+	theme_bw()
 # ----------------------------------------------
