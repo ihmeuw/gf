8fa89883e5148a5d9d3fa5b7680a1c28d62249ba
David Phillips <davidp6@ihme.washington.edu>
2019-02-04 20:46:34 -0800

    Add series of unrelated linear models for comparison

--- impact_evaluation/5b_run_analysis.R
+++ impact_evaluation/5b_run_analysis.R
@@ -21,6 +21,33 @@ source('./impact_evaluation/models/drc_malaria2.r')
 
 
 # -------------------------
+# Run series of unrelated linear models
+
+lmFit1 = lm(value_ITN_received ~ budget_M1_1_cumulative + budget_M1_2_cumulative + other_dah_M1_1_cumulative + other_dah_M1_2_cumulative, data)
+lmFit1 = lm(value_RDT_received ~ budget_M2_1_cumulative + budget_M2_3_cumulative + other_dah_M2_1_cumulative + other_dah_M2_3_cumulative, data)
+lmFit1 = lm(value_ACT_received ~ budget_M2_1_cumulative + budget_M2_3_cumulative + other_dah_M2_1_cumulative + other_dah_M2_3_cumulative, data)
+
+# linkage 2 regressions
+lmFit4 = lm(value_ITN_consumed ~ value_ITN_received, data)
+lmFit5 = lm(value_ACTs_CHWs ~ value_ACT_received, data)
+lmFit6 = lm(value_RDT_completed ~ value_RDT_received, data)
+lmFit7 = lm(value_SP ~ budget_M3_1_cumulative, data)
+lmFit8 = lm(value_severeMalariaTreated ~ budget_M2_6_cumulative + value_ACT_received, data)
+lmFit9 = lm(value_totalPatientsTreated ~ value_ACT_received, data)
+
+summary(lmFit1)
+summary(lmFit2)
+summary(lmFit3)
+summary(lmFit4)
+summary(lmFit5)
+summary(lmFit6)
+summary(lmFit7)
+summary(lmFit8)
+summary(lmFit9)
+# -------------------------
+
+
+# -------------------------
 # Run model
 semFit = bsem(model, data, adapt=500, burnin=1000, sample=1000)
 summary(semFit)
