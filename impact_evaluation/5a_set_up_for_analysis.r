ce9afa156ccf1cc92baa1e8c7a601e593f20f461
David Phillips <davidp6@ihme.washington.edu>
2019-02-04 19:43:52 -0800

    Add hotfixes for heywood cases to 5a

--- impact_evaluation/5a_set_up_for_analysis.r
+++ impact_evaluation/5a_set_up_for_analysis.r
@@ -22,8 +22,33 @@ for(v in rtVars) data[, (paste0(v,'_cumulative')):=cumsum(get(v))]
 # -----------------------------------------------------------------
 
 
+# -----------------------------------------------------------------------
+# Hotfixes for Heywood cases
+
+# drop zero-variance variables
+for(v in names(data)) if (sd(data[[v]],na.rm=T)==0) data[[v]] = NULL
+
+# extrapolate where necessary
+data = data[date>=2010 & date<=2018]
+for(v in names(data)) {
+	form = as.formula(paste0(v,'~date'))
+	lmFit = glm(form, data, family='poisson')
+	data[, tmp:=exp(predict(lmFit, newdata=data))]
+	# print(ggplot(data, aes_string(y=v,x='date')) + geom_point() + geom_line(aes(y=tmp)) + labs(title=v))
+	data[is.na(get(v)), (v):=tmp]
+}
+data$tmp = NULL
+
+# drop completeness variables (for now)
+for(v in names(data)[grepl('completeness', names(data))]) data[[v]]=NULL
+# -----------------------------------------------------------------------
+
+
 # ---------------------------------------------------------------------------------------
 # Run final tests
+
+# na omit
+data = na.omit(data)
 
 # test unique identifiers
 test = nrow(data)==nrow(unique(data[,'date', with=F]))
@@ -31,7 +56,7 @@ if (test==FALSE) stop(paste('Something is wrong. date does not uniquely identify
 # ---------------------------------------------------------------------------------------
 
 
-# --------------------
+# -------------------------
 # Save file
-saveRDS(outputFile5a)
-# --------------------
+saveRDS(data, outputFile5a)
+# -------------------------
