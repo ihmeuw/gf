ccad8f9530754a5fada0fc4acafa84e851c73603
David Phillips <davidp6@ihme.washington.edu>
2019-02-27 12:38:53 -0800

    Add two comments to suggest changes

--- resource_tracking/prep/global_fund_prep/1_master_file.r
+++ resource_tracking/prep/global_fund_prep/1_master_file.r
@@ -22,6 +22,7 @@ prep_files <- TRUE
 prep_gos <- FALSE
 
 include_stops = TRUE #Set to true if you would like scripts to stop when errors are found (specifically, module mapping)
+# this doesn't appear to have a function in scripts 2-6... is it just planned for later?
 verbose = FALSE #Set to true if you would like warning messages printed (helpful for debugging functions). Urgent messages will always be flagged regardless of this switch. 
 rerun_filelist <- TRUE  #Set to TRUE if you want to prep all files in the file list again. 
 limit_filelist <- FALSE #Set to TRUE if you want to only run files that will be saved in final budgets and expenditures. 
@@ -30,6 +31,8 @@ limit_filelist <- FALSE #Set to TRUE if you want to only run files that will be 
 # #Mark which grants are currently active to save in file - this should be updated every grant period! 
 # ---------------------------------------
 
+# this info is a good cadidate to save as a little csv somewhere... 
+# as a general rule of thumb, try not to embed anything that resembles data/metadata in the code
 current_gtm_grants <- c('GTM-H-HIVOS', 'GTM-H-INCAP', 'GTM-M-MSPAS', 'GTM-T-MSPAS')
 current_gtm_grant_period <- c('2018', '2019-2020', '2018-2020', '2016-2019')
 
