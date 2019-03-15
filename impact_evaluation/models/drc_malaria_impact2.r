d0b07e333cae224d764eb3d28d257847c88c498a
David Phillips <davidp6@ihme.washington.edu>
2019-03-15 15:04:06 -0700

    Update display

--- impact_evaluation/models/drc_malaria_impact2.r
+++ impact_evaluation/models/drc_malaria_impact2.r
@@ -1,33 +1,44 @@
 # model: drc_malaria_impact2.r
 # purpose: drc_malaria_impact1 with outputs, RDTs, CHWs and ANC
 
-# TO DO
-# Add arrow from RDT_completed to severeMalariaTreated, totalPatientsTreated and ACTs_CHWs
+# to do
+
 
 model = '
 
 	# linkage 1 regressions
-	lag_ITN_rate ~ 
-	lag_mildMalariaTreated_rate ~ 
-	lag_severeMalariaTreated_rate ~ 
-	ACTs_CHWs_rate ~ 
-	SP_rate ~ 
-	RDT_rate ~ 
+	ITN_rate ~ ITN
+	mildMalariaTreated_rate ~ mildMalariaTreated + RDT_rate
+	severeMalariaTreated_rate ~ severeMalariaTreated + RDT_rate
+	ACTs_CHWs_rate ~ SSCACT
+	SP_rate ~ SP
+	RDT_rate ~ RDT
 	
 	# linkage 2 regressions
-	newCasesMalariaMild_rate ~ lag_ITN_rate + lag_mildMalariaTreated_rate + date
-	newCasesMalariaSevere_rate ~ lag_ITN_rate + lag_severeMalariaTreated_rate + date
-	malariaDeaths_rate ~ newCasesMalariaMild_rate + newCasesMalariaSevere_rate + lag_mildMalariaTreated_rate + lag_severeMalariaTreated_rate + date
+	lead_newCasesMalariaMild_rate ~ ITN_rate + mildMalariaTreated_rate + ACTs_CHWs_rate + SP_rate + date
+	lead_newCasesMalariaSevere_rate ~ ITN_rate + severeMalariaTreated_rate + ACTs_CHWs_rate + SP_rate + date
+	lead_malariaDeaths_rate ~ lead_newCasesMalariaMild_rate + lead_newCasesMalariaSevere_rate + mildMalariaTreated_rate + severeMalariaTreated_rate + ACTs_CHWs_rate + SP_rate + date
 	
 	# latent variables
 	
 	# fixed variances
-	# value_RDT_received ~ 1*value_RDT_received
 	
 	# covariances
-	# exp_M1_1_cumulative ~~ other_dah_M1_1_cumulative
 	
 	# fixed covariances
-	# exp_M2_3_cumulative ~~ 0*exp_M3_1_cumulative
-	
+	ITN ~~ 0*mildMalariaTreated
+	ITN ~~ 0*severeMalariaTreated
+	ITN ~~ 0*SSCACT
+	ITN ~~ 0*SP
+	ITN ~~ 0*RDT
+	mildMalariaTreated ~~ 0*RDT
+	mildMalariaTreated ~~ 0*severeMalariaTreated
+	mildMalariaTreated ~~ 0*SSCACT
+	mildMalariaTreated ~~ 0*SP
+	severeMalariaTreated ~~ 0*RDT
+	severeMalariaTreated ~~ 0*SSCACT
+	severeMalariaTreated ~~ 0*SP
+	RDT ~~ 0*SSCACT
+	RDT ~~ 0*SP
+	RDT ~~ 0*SP
 '
