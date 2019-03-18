c393bbec4644cde9485229eb1c1bad85fc41b517
Caitlin O'Brien-Carelli <ccarelli@ihme.washington.edu>
2019-03-18 10:00:24 -0700

    Update final PNLS prep

--- outcome_measurement/all/cod/dhis/prep_dhis/pnls_function.R
+++ outcome_measurement/all/cod/dhis/prep_dhis/pnls_function.R
@@ -60,31 +60,7 @@ x[is.na(element_new2), element_new:=element_new1]
 x[ , c( 'last', 'element1','element_new1', 'element_new2', 'element_new3', 'element'):=NULL]
 setnames(x, 'element_new', 'element')
 
-#---------------------------------------
-# export the abbreviated elements for translation
-
-# to do this on the cluster, you must export as an RDS, then use local code to save
-elements = x[ ,.(element = unique(element)), by=.(element_id)]
-saveRDS(elements, paste0(dir, 'meta_data/translate/pnls_elements.rds'))
-
-#---------------------
-# xlsx files do not work well on the cluster
-# code to run offline
-# to_translate = readRDS(elements, paste0(dir, 'meta_data/translate/pnls_elements.rds'))
-# write.xlsx(paste0(paste0(dir,'meta_data/translate/pnls_elements_to_translate.xlsx' )))
-
-# translate using onlinedoctranslator.com and save as file path below
-#---------------------
-
-# import the translated elements
-new_elements = read.xlsx(paste0(paste0(dir,
-        'meta_data/translate/pnls_elements_translations.xlsx' )))
-
-# reset the variable name for the merge and merge on element id
-setnames(new_elements, 'element', 'element_eng')
-x = merge(x, new_elements, by='element_id', all.x=T )
-
-#---------------------------------------
+#------------------------
 return(x)
 
 }
