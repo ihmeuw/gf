0dae4f8452d450b60ceba5ef63c4729a88b9c17c
David Phillips <davidp6@ihme.washington.edu>
2019-03-22 10:58:47 -0700

    Switch back to normal priors

--- impact_evaluation/models/drc_malaria_impact3.r
+++ impact_evaluation/models/drc_malaria_impact3.r
@@ -14,9 +14,9 @@ model = '
 	RDT_rate ~ prior("dgamma(1,1)")*RDT
 	
 	# linkage 2 regressions
-	lead_newCasesMalariaMild_rate ~ prior("dunif(-1, 0)")*ITN_rate_cumul + prior("dunif(-1, 0)")*mildMalariaTreated_rate + prior("dunif(-1, 0)")*ACTs_CHWs_rate + prior("dunif(-1, 0)")*SP_rate + date
-	lead_newCasesMalariaSevere_rate ~ prior("dunif(-1, 0)")*ITN_rate_cumul + prior("dunif(-1, 0)")*severeMalariaTreated_rate + prior("dunif(-1, 0)")*ACTs_CHWs_rate + prior("dunif(-1, 0)")*SP_rate + date
-	lead_malariaDeaths_rate ~ lead_newCasesMalariaMild_rate + lead_newCasesMalariaSevere_rate + prior("dunif(-1, 0)")*mildMalariaTreated_rate + prior("dunif(-1, 0)")*severeMalariaTreated_rate + prior("dunif(-1, 0)")*ACTs_CHWs_rate + prior("dunif(-1, 0)")*SP_rate + date
+	lead_newCasesMalariaMild_rate ~ prior("dnorm(-1,.01)")*ITN_rate_cumul + prior("dnorm(-1,.01)")*mildMalariaTreated_rate + prior("dnorm(-1,.01)")*ACTs_CHWs_rate + prior("dnorm(-1,.01)")*SP_rate + date
+	lead_newCasesMalariaSevere_rate ~ prior("dnorm(-1,.01)")*ITN_rate_cumul + prior("dnorm(-1,.01)")*severeMalariaTreated_rate + prior("dnorm(-1,.01)")*ACTs_CHWs_rate + prior("dnorm(-1,.01)")*SP_rate + date
+	lead_malariaDeaths_rate ~ lead_newCasesMalariaMild_rate + lead_newCasesMalariaSevere_rate + prior("dnorm(-1,.01)")*mildMalariaTreated_rate + prior("dnorm(-1,.01)")*severeMalariaTreated_rate + prior("dnorm(-1,.01)")*ACTs_CHWs_rate + prior("dnorm(-1,.01)")*SP_rate + date
 	
 	# latent variables
 	
