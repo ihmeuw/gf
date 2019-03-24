154b5add5c132ae078fb57e2805b30acbe14e6f6
David Phillips <davidp6@ihme.washington.edu>
2019-03-24 11:07:06 -0700

    Update 3_merge to health zone level

--- impact_evaluation/3_merge_data.R
+++ impact_evaluation/3_merge_data.R
@@ -6,23 +6,62 @@
 # ----------------------------------------------------------
 
 
-#Specify the variables that you want to merge by here. 
-merge_vars = c('date')
-
 #Read in the previously saved files for resource tracking in 2b 
 resource_tracking <- readRDS(outputFile2a)
 
 #Read in the previously saved file for outputs/activities in 2b
 outputs_activities <- readRDS(outputFile2b_wide)
-    # merge outputs_activites file with drc_mal_map
-    # outputs_activities <- merge(drc_mal_map, outputs_activities, by = "indicator", all = TRUE, allow.cartesian = TRUE)
+outputs_activities = outputs_activities[!is.na(health_zone)]
+    # merge outputs_activities file with drc_mal_map
+    # outputs_activities <- merge(drc_mal_map, outputs_activities, by = 'indicator', all = TRUE, allow.cartesian = TRUE)
 
-#Merge resource tracking and outputs/activites data 
-merge_file <- merge(resource_tracking, outputs_activities, by = merge_vars, all.x = TRUE)
+# rectangularize before merge
+hzFrame = unique(outputs_activities[, c('dps','health_zone')])
+i=1
+for(d in unique(resource_tracking$date)) {
+	hzFrame[, date:=d]
+	if(i==1) frame = copy(hzFrame)
+	if(i>1) frame = rbind(frame, hzFrame)
+	i=i+1
+}
+resource_tracking_rect = merge(frame, resource_tracking, by='date', all.x=TRUE)
+outputs_activities_rect = merge(frame, outputs_activities, by=c('dps','health_zone','date'), all.x=TRUE)
+	
+#Merge rectangularized resource tracking and outputs/activites data 
+merge_file <- merge(resource_tracking_rect, outputs_activities_rect, by=c('dps','health_zone','date'))
 
-# merge_file$loc_name = "cod"
-# merge_file$disease = "malaria"
-# merge_file <- setorderv(merge_file, c("year", "quarter", "code", "indicator", "indicator_type"))
+# distribute inputs by health zone proportionally to activities
+inVars = c('other_dah_M1_1', 'other_dah_M1_2', 'other_dah_M2', 'other_dah_M2_3', 'exp_M1_1', 'exp_M1_2', 'exp_M2', 'exp_M2_1', 'exp_M2_3', 'exp_M2_6', 'exp_M3_1', 'ghe', 'oop')
+actVars = c('ITN_received', 'ITN_received', 'total_tx', 'total_tx', 'ITN_received', 'ITN_received', 'total_tx', 'total_tx', 'total_tx', 'total_tx', 'ACT_received', 'total', 'total')
+merge_file[, total_tx:=ACT_received+RDT_received]
+merge_file[, total:=ACT_received+RDT_received+ITN_received]
+for(i in seq_along(inVars)) {
+	v = inVars[i]
+	a = actVars[i]
+	
+	# disallow zeroes
+	min = min(merge_file[get(a)>0][[a]], na.rm=TRUE)
+	merge_file[, mean:=mean(get(a), na.rm=TRUE), by=health_zone]
+	
+	# distribute proportionally
+	merge_file[, tmp:=get(a)+min]
+	merge_file[is.na(tmp), tmp:=mean]
+	merge_file[, prop:=tmp/sum(tmp), by=date]
+	merge_file[, (v):=get(v)*prop]
+	
+	# test that it worked
+	orig = sum(resource_tracking[[v]], na.rm=TRUE)
+	new = sum(merge_file[[v]], na.rm=TRUE)
+	if (abs(orig-new)>.1) stop(i) 
+}	
+
+# clean up 
+merge_file = merge_file[, -c('mean','tmp','prop','total','total_tx')]
+
+
+# merge_file$loc_name = 'cod'
+# merge_file$disease = 'malaria'
+# merge_file <- setorderv(merge_file, c('year', 'quarter', 'code', 'indicator', 'indicator_type'))
 # merge_file <- merge_file[, .(year, quarter, code, module, intervention, indicator, indicator_type, value, completeness, budget, loc_name, disease, data_source)] #EKL are we missing some variables?
 # merge_file <- merge_file[, .(year, quarter, code, module, intervention, indicator, indicator_type, value, completeness, budget, loc_name, disease, other_dah, data_source)]
 
