9eda93c22850266df20896888896ea95d685a65e
David Phillips <davidp6@ihme.washington.edu>
2019-03-24 09:32:44 -0700

    Add some exploratory analyses

--- impact_evaluation/visualizations/model_diagnostics_impact.r
+++ impact_evaluation/visualizations/model_diagnostics_impact.r
@@ -26,7 +26,7 @@ nodeTable = nodeTable[variable %in% names(data)]
 # Graphs
 
 # health zone-level coefficients
-p = ggplot(summaries[lhs=='lead_malariaDeaths_rate' & rhs=='severeMalariaTreated_rate'], 
+p1 = ggplot(summaries[lhs=='lead_malariaDeaths_rate' & rhs=='severeMalariaTreated_rate'], 
 		aes(y=est.std, ymin=ci.lower, ymax=ci.upper, x=reorder(health_zone,est.std))) + 
 		geom_pointrange(alpha=.5) +
 		geom_hline(yintercept=0) + 
@@ -35,19 +35,27 @@ p = ggplot(summaries[lhs=='lead_malariaDeaths_rate' & rhs=='severeMalariaTreated
 		theme_bw() + 
 		theme(axis.text.x=element_blank())
 
+# matrix plot for a random health zone
+library(GGally)
+h = sample(unique(data$health_zone),1)
+mortVars = c("newCasesMalariaMild_rate", "newCasesMalariaSevere_rate", "mildMalariaTreated_rate", 
+	"severeMalariaTreated_rate", "ACTs_CHWs_rate", "SP_rate")
+tmp = data[health_zone==h, mortVars, with=FALSE]
+ggpairs(tmp)
+		
 # ----------------------------------------------
 
 
 # -----------------------------------
 # Save output
-pdf(outputFile6, height=6, width=9)
+# pdf(outputFile6, height=6, width=9)
 p1
 p2
-dev.off()
+# dev.off()
 
 # save a time-stamped version for reproducibility
-date_time = gsub('-|:| ', '_', Sys.time())
-outputFile6Archive = gsub('visualizations/', 'visualizations/archive/', outputFile6)
-outputFile6Archive = gsub('.pdf', paste0('_', date_time, '.pdf'), outputFile6Archive)
-file.copy(outputFile6, outputFile6Archive)
+# date_time = gsub('-|:| ', '_', Sys.time())
+# outputFile6Archive = gsub('visualizations/', 'visualizations/archive/', outputFile6)
+# outputFile6Archive = gsub('.pdf', paste0('_', date_time, '.pdf'), outputFile6Archive)
+# file.copy(outputFile6, outputFile6Archive)
 # -----------------------------------
