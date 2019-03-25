d6538a53dc03336719ab5e6daf8c0e85f1abef2b
David Phillips <davidp6@ihme.washington.edu>
2019-03-24 17:50:56 -0700

    Fix file path

--- impact_evaluation/5e_run_second_half_analysis.r
+++ impact_evaluation/5e_run_second_half_analysis.r
@@ -99,7 +99,7 @@ if (runAsQsub==TRUE) {
 	system(paste0('qsub -cwd -N ie_job_array -t 1:', T, 
 		' -l fthread=1 -l m_mem_free=1G -q all.q -P ihme_general -e ', 
 		clustertmpDireo, ' -o ', clustertmpDireo, 
-		' ./core/r_shell_blavaan.sh ./impact_evaluation/5e_run_second_half_analysis_single_hz.r'))
+		' ./core/r_shell_blavaan.sh ./impact_evaluation/5f_run_second_half_analysis_single_hz.r'))
 	# wait for jobs to finish (2 files per job)
 	while(length(list.files(clustertmpDir2))<(T*2)) { 
 		Sys.sleep(5)
