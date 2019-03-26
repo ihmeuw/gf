3c728bff19180868edd3cba0e2e54c46b5597d0c
Audrey Batzel <abatzel@ihme.washington.edu>
2019-03-26 12:54:59 -0700

    function to create copies of output files in archive folder

---
+++ impact_evaluation/_common/archive_function.R
@@ -0,0 +1,16 @@
+# save a time-stamped version of output files in archive folder, for reproducibility of impact evaluation results
+
+
+archive <- function( object, outputFile ){
+  
+  if( class(object)[1] != "data.table" & class(object)[1] != "data.frame" ) stop ("Object is not a data table or data frame, you might not want to save it as an RDS")
+  
+  date_time = gsub('-|:| ', '_', Sys.time())
+  
+  outputFileArchive = gsub('prepped_data/', 'prepped_data/archive/', outputFile)
+  outputFileArchive = gsub('.rds', (paste0('_', date_time, '.rds')), outputFileArchive, ignore.case = TRUE)
+  
+  file.copy(outputFile, outputFileArchive)
+  
+  return(paste0("File copied from ", outputFile, " to ", outputFileArchive))
+}
