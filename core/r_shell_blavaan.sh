989a2b7c12a70d0acd1a151a70c991c43bfcc729
David Phillips <davidp6@ihme.washington.edu>
2019-04-29 12:03:08 -0700

    Update shell script to better image

--- core/r_shell_blavaan.sh
+++ core/r_shell_blavaan.sh
@@ -1,2 +1,2 @@
 #$ -S /bin/sh
-singularity exec /share/singularity-images/health_fin/forecasting/best.img Rscript <$1 --no-save $@
+/share/singularity-images/health_fin/forecasting/best_new.img Rscript <$1 --no-save $@
