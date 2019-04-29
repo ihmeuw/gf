fb9f065cad0f903979f2be589a2e9edba89cadf1
David Phillips <davidp6@ihme.washington.edu>
2019-04-29 12:09:14 -0700

    Update r_shell_blavaan.sh

--- core/r_shell_blavaan.sh
+++ core/r_shell_blavaan.sh
@@ -1,2 +1,2 @@
 #$ -S /bin/sh
-/share/singularity-images/health_fin/forecasting/best_new.img Rscript <$1 --no-save $@
+singularity exec /share/singularity-images/health_fin/forecasting/best_new.img Rscript <$1 --no-save $@
