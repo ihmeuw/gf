68c73e620b78ded847a2d36b2185848384de619b
David Phillips <davidp6@ihme.washington.edu>
2019-04-30 13:14:37 -0700

    Update set_up_r.r

--- impact_evaluation/drc/set_up_r.r
+++ impact_evaluation/drc/set_up_r.r
@@ -159,7 +159,7 @@ outputFile3b = paste0(ieDir, 'outcomes_impact_corrected.RDS')
 outputFile3bGraphs = paste0(ieDir, '../visualizations/outcomes_impact_correction_results.pdf')
 
 # output file from 4a_set_up_for_analysis.r
-outputFile4a = paste0(ieDir, 'inputs_outputs_pre_model.rdata')
+outputFile4a = paste0(ieDir, 'first_half_pre_model.rdata')
 if (Sys.info()[1]!='Windows') { 
 	outputFile4a_scratch = paste0(clustertmpDir1, 'first_half_data_pre_model.rdata')
 }
@@ -171,7 +171,7 @@ if (Sys.info()[1]!='Windows') {
 }
 
 # output file from 4c and 4d_explore_data.r (graphs)
-outputFile4c = paste0(ieDir, '../visualizations/inputs_outputs_exploratory_graphs.pdf')
+outputFile4c = paste0(ieDir, '../visualizations/first_half_exploratory_graphs.pdf')
 outputFile4d = paste0(ieDir, '../visualizations/second_half_exploratory_graphs.pdf')
 
 # output file from 5a_run_first_half_analysis.R
