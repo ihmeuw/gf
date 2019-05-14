112aaa421eb0b837d307affbdb037757dbe689a1
Emily Linebarger <eklinebarg@gmail.com>
2019-05-14 10:00:14 -0700

    Continuing to format master file to run automatically on Guatemala data

--- impact_evaluation/gtm/1_master_file.r
+++ impact_evaluation/gtm/1_master_file.r
@@ -47,35 +47,35 @@ setDT(indicatorMap)
 # Prep resource tracking data  
 # ---------------------------------------
 if(rerun_inputs == TRUE){
-  source('./impact_evaluation/drc/2a_prep_resource_tracking.r')
+  source('./impact_evaluation/gtm/2a_prep_resource_tracking.r')
 }
 
 # ---------------------------------------
 # Prep activities and outputs data 
 # ---------------------------------------
 if(rerun_outputs == TRUE){
-  source('./impact_evaluation/drc/2b_prep_activities_outputs.R')
+  source('./impact_evaluation/gtm/2b_prep_activities_outputs.R')
 }
 
 # ---------------------------------------
 # Prep outputs, outcomes and impact data 
 # ---------------------------------------
 if(rerun_outcomes == TRUE){
-  source('./impact_evaluation/drc/2c_prep_outcomes_impact.r')
+  source('./impact_evaluation/gtm/2c_prep_outcomes_impact.r')
 }
 
 # ---------------------------------------
 # Merge datasets together 
 # ---------------------------------------
 if (rerun_merge==TRUE) { 
-	source('./impact_evaluation/drc/3_merge_data.R')
+	source('./impact_evaluation/gtm/3_merge_data.R')
 }
 
 # ---------------------------------------
 # Correct rates to model estimates
 # ---------------------------------------
 if (rerun_adjust==TRUE) { 
-	source('./impact_evaluation/drc/3b_correct_to_models.R')
+	source('./impact_evaluation/gtm/3b_correct_to_models.R')
 }
 
 # ---------------------------------------
@@ -89,28 +89,28 @@ if (rerun_adjust==TRUE) {
 # 4a and 4b will be very slow if not on IHME's cluster
 # ---------------------------------------
 if (rerun_explore==TRUE) { 
-	source('./impact_evaluation/drc/4a_set_up_for_first_half_analysis.r')
-	source('./impact_evaluation/drc/4b_set_up_for_second_half_analysis.r')
-	source('./impact_evaluation/drc/4c_explore_first_half_data.r')
-	source('./impact_evaluation/drc/4d_explore_second_half_data.r')
+	source('./impact_evaluation/gtm/4a_set_up_for_first_half_analysis.r')
+	source('./impact_evaluation/gtm/4b_set_up_for_second_half_analysis.r')
+	source('./impact_evaluation/gtm/4c_explore_first_half_data.r')
+	source('./impact_evaluation/gtm/4d_explore_second_half_data.r')
 }
 
 # ---------------------------------------
 # Run models 
 # ---------------------------------------
 if (rerun_models==TRUE) { 
-	source('./impact_evaluation/drc/5a_run_first_half_analysis.r')
-	source('./impact_evaluation/drc/5b_run_second_half_analysis.r')
+	source('./impact_evaluation/gtm/5a_run_first_half_analysis.r')
+	source('./impact_evaluation/gtm/5b_run_second_half_analysis.r')
 }
 
 # ---------------------------------------
 # Run analyses based on model
 # ---------------------------------------
 if (rerun_analysis==TRUE) { 
-	source('./impact_evaluation/drc/6a_display_sem_results.r')
-	source('./impact_evaluation/drc/6b_efficiency_effectiveness.r')
-	source('./impact_evaluation/drc/6c_impact_analysis.r')
-	source('./impact_evaluation/drc/6d_effect_sizes_by_hz.r')
+	source('./impact_evaluation/gtm/6a_display_sem_results.r')
+	source('./impact_evaluation/gtm/6b_efficiency_effectiveness.r')
+	source('./impact_evaluation/gtm/6c_impact_analysis.r')
+	source('./impact_evaluation/gtm/6d_effect_sizes_by_hz.r')
 }
 
 print(paste('Master script completed. Outputs saved here:', ieDir))
