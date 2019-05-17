099aeba88ca55e2fdbd6dd8fa86a28c7c215a056
Emily Linebarger <eklinebarg@gmail.com>
2019-05-17 14:09:30 -0700

    Editing shell script, and trying to get R to run automatically from it.

--- reports_2019/gdoc2rmd.sh
+++ reports_2019/gdoc2rmd.sh
@@ -1,7 +1,11 @@
 
-#!/bin/bash
-echo "This statement will print out to the screen". 
-echo "Now we are going to list files." 
-ls
-echo "Next we are going to print the directory we are in:" 
-pwd
+echo "Starting gdoc2rmd..."
+
+cd("C:/Users/elineb/Documents/gf/reports_2019")
+echo "Converting plain text to r markdown using java processor"
+java gdoc2rmd
+
+echo "R markdown output created - 'gdoc2rmd_out.rmd'"
+C:/Users/elineb/AppData/Local/CONTIN~1/ANACON~1/envs/rstudio/lib/R/bin/x64/R.exe do.call(source('run_r.R'))
+
+echo "Output PDF created." 
---
+++ reports_2019/run_r.R
@@ -0,0 +1,4 @@
+library('rmarkdown')
+rmarkdown::render('gdoc2rmd_out.rmd')
+quit()
+n
