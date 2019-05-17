4a157329fc62f0345b62d56c029497d849bdeb57
Emily Linebarger <eklinebarg@gmail.com>
2019-05-17 15:26:54 -0700

    Trying to get figure counter working.

--- reports_2019/gdoc2rmd.java
+++ reports_2019/gdoc2rmd.java
@@ -1,14 +1,6 @@
 // Prompts the user for a file name to convert from Google Doc report formatting 
 // to R Markdown formatting. 
 
-//Rules: 
-// "Header1" = #
-// "Header2" = ## 
-// "Header3" = ### 
-// "Header4" = ####
-// "Header5" = #####
-// "Header6" = ######
-
 import java.io.*;
 import java.util.*;
 
@@ -16,8 +8,8 @@ public class gdoc2rmd {
    
     public static void main(String[] args) throws IOException {   
         //Build up a dictionary to convert certain character strings into R markdown code. 
-        HashMap<String, String> dict = makeDictionary();     
-        Set<String> allKeys = dict.keySet(); 
+        HashMap<String, String> dict = makeDictionary();    
+        HashMap<String, Integer> figureCounter = makeCounter(dict);   
         
         Scanner console = new Scanner(System.in);
         System.out.print("What is the name of the plain text file you'd like to convert? ");
@@ -29,29 +21,46 @@ public class gdoc2rmd {
         Scanner fileScan = new Scanner(new File(inFile));
         PrintStream output = new PrintStream(new File(outputFile)); 
         System.setOut(output); 
-        processFile(fileScan);            
+        processFile(fileScan, dict, figureCounter);            
         output.close(); 
     }
     
+    //Build up a dictionary of keywords to convert to formatted 
+    // R-markdown text. 
     public static HashMap<String, String> makeDictionary(){
       HashMap<String, String> dict = new HashMap<String, String>(); 
       
       //Build up dictionary - add new conversions here. 
+      //Start every counter at 0. 
       dict.put("Chapter", "##"); 
       return(dict);
     }
     
+    //Build up a counter to auto-number figures and chapters from 
+    // the given dictionary. 
+    public static HashMap<String, Integer> makeCounter(HashMap<String, String> dict){
+      HashMap<String, Integer> counter = new HashMap<String, Integer>(); 
+      
+      //For all the keys that have been added to the dictionary above, 
+      // start the count at 0. 
+      for (String key:dict.keySet()){
+         counter.put(key, 0); 
+      } 
+      return(counter);
+    }
+    
     //Accepts a Scanner over a file, and formats it in 
     // the markup language we've selected. 
-    public static void processFile(Scanner fileScan){
+    public static void processFile(Scanner fileScan, HashMap<String, String> dict, HashMap<String, Integer> counter){
       writeYamlHeader(); 
       while(fileScan.hasNextLine()){
          System.out.println(); 
          String line = fileScan.nextLine(); 
          String lineArray[] = line.split(" ", 2); 
          String key = lineArray[0]; 
+         Set<String> allKeys = dict.keySet(); 
    	   if	(findHeaderKeys(key, allKeys)){
-   			System.out.print(convertHeaderKeys(key));	//Find the key and convert it if needed. 
+   			System.out.print(convertHeaderKeys(key, allKeys, dict, counter));	//Find the key and convert it if needed. 
             if (lineArray.length > 1){
                System.out.print(lineArray[1]); 
             }
@@ -71,13 +80,22 @@ public class gdoc2rmd {
       return(false); 
     }
     
-    public static String convertHeaderKeys(String key){
+    //Convert a given header to its corresponding R markdown code. 
+    public static String convertHeaderKeys(String key, Set<String> allKeys, HashMap<String, String> dict, HashMap<String, Integer> counter){
       if (allKeys.contains(key)){
          return(dict.get(key) + " "); 
       }
-      return(key + " "); 
+      return(key + " " + incrementCount(key, counter) + " "); 
     }
     
+    //Increment the count for a given header. 
+    public static int incrementCount(String key, HashMap<String, Integer> counter){
+      int count = counter.get(key);
+      counter.put(key, count++); 
+      return(count); 
+    }
+    
+    //Write a standard YAML header. 
     public static void writeYamlHeader(){
       System.out.println("---"); 
       System.out.println("title: \"Untitled\""); 
@@ -88,6 +106,5 @@ public class gdoc2rmd {
       System.out.println("```{r setup, include=FALSE}"); 
       System.out.println("```"); 
     }
-    //Emily do you want a function here to convert headers? 
 
 }
