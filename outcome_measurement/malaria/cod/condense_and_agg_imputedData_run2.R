e83c977728dd7c7109553882e457c0aa85c73ced
Audrey Batzel <abatzel@ihme.washington.edu>
2019-06-13 18:24:56 -0700

    Update condense_and_agg_imputedData_run2.R

--- outcome_measurement/malaria/cod/condense_and_agg_imputedData_run2.R
+++ outcome_measurement/malaria/cod/condense_and_agg_imputedData_run2.R
@@ -5,8 +5,8 @@
 # TO DO : healthFacilitiesProportion variable is NA for a lot
 # ----------------------------------------------     
 
-
 # --------------------  
+rm(list=ls())
 # Set up R / install packages
 library(data.table)
 library(stringr)
@@ -14,81 +14,74 @@ library(reshape2)
 library(ggplot2)
 # --------------------  
 
-
 # ----------------------------------------------
 # Overview - Files and Directories
+
+run_name = "run_0_01_aggVars_lagsLeads"
 
 # data directory
 # when run on Unix, data directory needs to be set to /home/j (to run on the cluster), so set this here:
 j = ifelse(Sys.info()[1]=='Windows', 'J:', '/home/j')
-dir = paste0(j, '/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/PNLP/')
+dir = paste0(j, '/Project/Evaluation/GF/outcome_measurement/cod/prepped_data/PNLP/post_imputation/')
 
 # input file:
-imputed_data <- "imputedData_run2.rds" # this is the version we want to use... lower tolerance level
+imputed_data = paste0("PNLP_imputedData_", run_name, ".rds")
 
 # output files:
-imputed_data_long <- "imputedData_run2_long.rds"
-imputed_data_long_corrected <- "imputedData_run2_long_corrected.rds"
-condensed_imputed_data_dps <- "imputedData_run2_condensed_dps.rds"
-condensed_imputed_data_country <- "imputedData_run2_condensed_country.rds"
-condensed_imputed_data_country2 <- "imputedData_run2_condensed_country_byYear.rds"
-condensed_imputed_data_hz <- "imputedData_run2_condensed_hz.rds"
+imputed_data_long = paste0("imputedData_", run_name, "_long.rds")
+imputed_data_long_corrected = paste0("imputedData_", run_name, "_long_corrected.rds")
+
+condensed_imputed_data_dps = paste0("imputedData_", run_name, "_condensed_dps.rds")
+condensed_imputed_data_country = paste0("imputedData_", run_name, "_condensed_country.rds")
+condensed_imputed_data_country2 = paste0("imputedData_", run_name, "_condensed_country_byYear.rds")
+condensed_imputed_data_hz = paste0("imputedData_", run_name, "_condensed_hz.rds")
 # ----------------------------------------------
-  
   
 # ---------------------------------------------- 
 ################### NOTE: can skip down to section that says "START FROM HERE"...
 # ---------------------------------------------- 
 # Load imputed data
 dt <- readRDS(paste0(dir, imputed_data))
-# Set up var vectors for manipulation
-all_vars <- c(colnames(dt))
-id_vars <- c("province", "dps", "health_zone", "date")
-indicators <- all_vars[!all_vars %in% id_vars] 
-indicators <- indicators[!indicators %in% c("V1")]
-indicators <- indicators[!indicators %in% c("imputation_number")]
-# ----------------------------------------------
 
+# Set up var vectors for manipulation
+all_vars = c(colnames(dt))
+id_vars = c("id", "dps", "health_zone", "date", "donor", "operational_support_partner", "population", "imputation_number", "combine")
+inds = all_vars[!all_vars %in% id_vars] 
+# ----------------------------------------------
 
 # ----------------------------------------------    
 # Set up a data table for graphing:
-imputed_id_vars <- c('province', 'dps', 'health_zone', 'date', 'id', 'imputation_number')
-
 # Reshape imputed data long
-imputedDataLong <- melt(dt, id.vars=c(imputed_id_vars))
-imputedDataLong$id <- NULL
-imputedDataLongSplit <- imputedDataLong[, c("indicator", "subpopulation") := tstrsplit(variable, "_", fixed=TRUE)]
+dt_long = melt.data.table(dt, id.vars = id_vars, variable.factor = FALSE)
 
 # Save data in this format (since it takes a while to run), so it can be used later on if the code breaks somewhere before all versions of the dt are produced
-saveRDS(imputedDataLongSplit, paste0(dir, imputed_data_long))
+saveRDS(dt_long, paste0(dir, imputed_data_long))
 # ----------------------------------------------
 
-
-# ----------------------------------------------
-# original data to merge to fix zeroes in the data
-# read in data table prepped by prep_for_MI.R
-input <- "final_data_for_impuation.csv"
-dtOrig <- fread(paste0(dir, input)) 
-dtOrig <- dtOrig[, V1:=NULL]
-
-for (i in indicators) {
-  dtOrig$i <- as.double(dtOrig$i)
-}
-
-missMatrixMelt <- melt(dtOrig, id.vars=c(id_vars, "id"))
-missMatrixMelt[, isMissing:=is.na(value)]
-missMatrixMelt$date <- as.Date(missMatrixMelt$date)
-
-corrected_data <- merge(imputedDataLongSplit, missMatrixMelt, all=T, by= c(id_vars, "variable"))
-
-# when the original value was 0, set the imputed values to be zero:
-setnames(corrected_data, "value.x", "imp_value")
-setnames(corrected_data, "value.y", "orig_value")
-corrected_data[orig_value==0, imp_value:=0]
-
-saveRDS(corrected_data, paste0( dir, imputed_data_long_corrected ))
-# ----------------------------------------------
-
+# # ----------------------------------------------
+# # original data to merge to fix zeroes in the data
+# # read in data table prepped by prep_for_MI.R
+# input <- "final_data_for_impuation.csv"
+# dtOrig <- fread(paste0(dir, input)) 
+# dtOrig <- dtOrig[, V1:=NULL]
+# 
+# for (i in indicators) {
+#   dtOrig$i <- as.double(dtOrig$i)
+# }
+# 
+# missMatrixMelt <- melt(dtOrig, id.vars=c(id_vars, "id"))
+# missMatrixMelt[, isMissing:=is.na(value)]
+# missMatrixMelt$date <- as.Date(missMatrixMelt$date)
+# 
+# corrected_data <- merge(imputedDataLongSplit, missMatrixMelt, all=T, by= c(id_vars, "variable"))
+# 
+# # when the original value was 0, set the imputed values to be zero:
+# setnames(corrected_data, "value.x", "imp_value")
+# setnames(corrected_data, "value.y", "orig_value")
+# corrected_data[orig_value==0, imp_value:=0]
+# 
+# saveRDS(corrected_data, paste0( dir, imputed_data_long_corrected ))
+# # ----------------------------------------------
 
 # ----------------------------------------------
 ################## START FROM HERE.... #################################
@@ -103,10 +96,9 @@ dt2 <- dt[variable != "healthFacilitiesProportion"]
 dt3 <- dt2[variable != "i"]
 # ----------------------------------------------
 
-
-# # ----------------------------------------------
-# # Calculate mean, upper, and lower across impuations by HEALTH ZONE
-# # ----------------------------------------------
+# ----------------------------------------------
+# Calculate mean, upper, and lower across impuations by HEALTH ZONE
+# ----------------------------------------------
 # compute upper middle and lower for the imputed points for the error bars in the graphs
 graphData <- dt3[, .(mean=mean(imp_value),
                     lower=quantile(imp_value, .05),
@@ -118,12 +110,11 @@ graphData <- graphData[mean==upper, upper:= NA ]
 
 # export graphDataComplete
 saveRDS(graphData, paste0(dir, condensed_imputed_data_hz))
-# # ----------------------------------------------
+# ----------------------------------------------
 
-
-# # ----------------------------------------------
-# # Aggregate first and then calculate mean and variance across imputations by DPS
-# # ----------------------------------------------
+# ----------------------------------------------
+# Aggregate first and then calculate mean and variance across imputations by DPS
+# ----------------------------------------------
 # aggregate all indicator/intervention data by dps, within each imputation
 aggData  <- dt3[, .(aggValue = sum(imp_value)), by=c( "date", "province", "dps", "indicator", "subpopulation", "imputation_number" )]
 
@@ -140,11 +131,10 @@ aggData <- aggData[mean==upper, upper := NA]
 saveRDS(aggData, paste0(dir, condensed_imputed_data_dps))
 # ----------------------------------------------
 
-
-# # ----------------------------------------------    
-# # Aggregate first and then calculate mean and variance across imputations by COUNTRY
-# # 8/29/18 update: changed country level confidence interval
-# # ----------------------------------------------
+# ----------------------------------------------
+# Aggregate first and then calculate mean and variance across imputations by COUNTRY
+# 8/29/18 update: changed country level confidence interval
+# ----------------------------------------------
 # aggregate all indicator/intervention data by dps, within each imputation
 # get rid of dps that is under category 0...
 dt3 <- dt3[ dps != "0", ]
