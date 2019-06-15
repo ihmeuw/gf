c68a708451ba5242b3f2411aab5a4856c06cb214
Caitlin O'Brien-Carelli <ccarelli@ihme.washington.edu>
2019-06-15 16:05:12 -0700

    Integrate functions directly into prep code

--- outcome_measurement/all/cod/dhis/prep_dhis/merge_dhis.R
+++ outcome_measurement/all/cod/dhis/prep_dhis/merge_dhis.R
@@ -16,6 +16,11 @@ library(dplyr)
 library(stringr) 
 library(openxlsx)
 library(lubridate)
+library(dplyr)
+
+# increase memory of RStudio session
+memory.limit(size = 20000)
+
 # --------------------
 # merge on the cluster
 # files take a long time to load - merge in a cluster IDE
@@ -30,10 +35,13 @@ library(lubridate)
 j = ifelse(Sys.info()[1]=='Windows', 'J:', '/home/j')
 
 # set the directory for input and output
-dir = paste0(j, '/Project/Evaluation/GF/outcome_measurement/cod/dhis_data/')
+# dir = paste0(j, '/Project/Evaluation/GF/outcome_measurement/cod/dhis_data/')
+
+# local directory 
+dir = 'C:/Users/ccarelli/Documents/dhis_data/'
 
 # source the merge and prep functions from the J Drive
-source(paste0(dir, 'code/merge_functions.R'))
+# source(paste0(dir, 'code/merge_functions.R'))
 
 #---------------------------------
 
@@ -68,14 +76,31 @@ fix_diacritics = function(x) {
   return(chartr(replace_me, replace_with, x)) }
 
 #---------------------------------
-# # set the working directory and read in the files
-setwd(paste0(dir, 'pre_prep/', folder, '/intermediate_data/'))
+# set the working directory and read in the files
+# setwd(paste0(dir, 'pre_prep/', folder, '/intermediate_data/'))
+
+# local working directory 
+setwd('C:/Users/ccarelli/Documents/dhis_data/pre_prep/pnls/intermediate_data')
 
 # list the files in the working directory
 files = list.files('./', recursive=TRUE)
 
+#---------------------------------
+# merge in categories piecewise as the category merge myseteriously breaks the code 
+
+# load the categories 
+categories = data.table(readRDS(paste0(dir, 'meta_data/data_elements_categories.rds')))
+categories[ , url_list:=NULL]
+categories[ , ID:=as.character(ID)]
+categories[ , displayName:=as.character(displayName)]
+
+setnames(categories,'displayName', 'category_name')
+setnames(categories,'ID', 'category')
+
+#---------------------------------
 # read in the files
 i = 1
+
 for(f in files[1:15]) {
   
   #load the RDs file
@@ -121,6 +146,7 @@ for(f in files[1:15]) {
   print(paste("Index:", i))
   
   # save the completed rbind for the first half of the data 
+  if (i==15) dt1 = merge(dt1, categories, by='category', all.x=T)
   if (i==15) saveRDS(dt1, paste0(dir, 'pre_prep/', folder, '/', folder, '_first_half.rds'))
   i = i+1
 }
@@ -172,17 +198,15 @@ for(f in files[16:length(files)]) {
   print(paste("Second round index:", i))
   
   # save the completed rbind for the second half of the data 
-  if (i==length(files)-15) saveRDS(dt2, paste0(dir, 'pre_prep/', folder, '/', folder, '_second_half.rds'))
+  # merge in the categories
+  if (i==length(files)-15) dt2 = merge(dt2, categories, by='category', all.x=T)
+  # if (i==length(files)-15) saveRDS(dt2, paste0(dir, 'pre_prep/', folder, '/', folder, '_second_half.rds'))
   i = i+1
   
 }
 
 #---------------------------------
 # perform distinct functions on half the data each time 
-
-# merge in the meta data 
-dt1 = merge_meta_data(dt1)
-dt2 = merge_meta_data(dt2)
 
 # bind the two sets together
 dt = data.table(rbind(dt1, dt2))
@@ -210,22 +234,181 @@ max_date = gsub('-', '_', max_date)
 
 # save the raw data before the merge 
 saveRDS(dt, paste0(dir, 'pre_prep/', folder, '/', folder, min_date, '_', max_date, 'full.rds'))
+print("File saved!")
+
+#----------------------------------------------------------------
+
+#----------------------------------------------------------------
+# read in the interim file you saved
+
+# file_name1 = paste0(dir, 'pre_prep/', folder, '/', folder, min_date, '_', max_date, 'full.rds')
+# dt = readRDS(file_name1)
 
 #---------------------------------
 # collapse across the file names 
+test_rows = nrow(dt)
 
 byVars = names(dt)[names(dt)!='file' & names(dt)!='download_number' & names(dt)!='value']
 dt = dt[ ,.(value=sum(value)), by=byVars]
+
+# unit test of unique identifiers
+if (test_rows == nrow(dt)) { print("There is no overlap between downloads.")
+} else {print("Houston, we have a problem.")}
 
 #---------------------------------
 # merge in the meta data 
 # includes english translations to be formatted later
 
-dt = merge_meta_data(dt)
-#---------------------------------
-# run the prep function to prepare some variables for use
+# read in master facilities and variable names
+facilities = data.table(readRDS(paste0(dir, 'meta_data/master_facilities.rds')))
+data_elements = data.table(readRDS(paste0(dir, 'meta_data/updated_data_elements.rds')))
 
-dt = prep_dhis_geography(dt)
+#---------------------------------
+# format health facility information, including geographic location 
+
+# replace dps with the name only
+facilities$dps1 = unlist(lapply(strsplit(facilities$dps, " "), "[", 2))
+facilities$dps2 = unlist(lapply(strsplit(facilities$dps, " "), "[", 3))
+facilities[dps2!='Province', dps:=paste(dps1, dps2)]
+facilities[dps2=='Province', dps:=dps1]
+facilities[ , c('dps1', 'dps2'):=NULL]
+
+# replace health zone with the name only
+facilities$health_zone1 = unlist(lapply(strsplit(facilities$health_zone, " "), "[", 2))
+facilities$health_zone2 = unlist(lapply(strsplit(facilities$health_zone, " "), "[", 3))
+facilities$health_zone3 = unlist(lapply(strsplit(facilities$health_zone, " "), "[", 4))
+facilities[health_zone3 != 'Zone' & health_zone2 != 'Zone', health_zone:=paste(health_zone1, health_zone2, health_zone3) ]
+facilities[health_zone3=='Zone', health_zone:=paste(health_zone1, health_zone2)]
+facilities[health_zone2=='Zone', health_zone:=health_zone1]
+facilities[ , c('health_zone1', 'health_zone2', 'health_zone3'):=NULL]
+
+#-------------------
+# change the names of the ID variables in elements and categories to match for the merge
+setnames(dt, 'org_unit_ID', 'org_unit_id')
+setnames(dt, 'data_element_ID', 'data_element_id')
+
+#--------------------------------------------------------
+# merge in the meta data 
+
+# merge in the facilities meta data 
+dt = merge(dt, facilities, by='org_unit_id', all.x=T)
+
+# merge in the data elements
+# some data elements contain duplicate ids - set if statements for these sets
+if (folder=='pnls') dt[ , data_set_id:='wIMw0dzITTs']
+if (folder=='base') dt[ , data_set_id:='pMbC0FJPkcm']
+dt[ , data_set_id:=as.character(data_set_id)]
+
+# subset down the list of elements to the data set being processed
+sub_id = dt[ , unique(data_set_id)]
+data_elements = data_elements[datasets_ID==sub_id]
+
+# drop unecessary variables
+data_elements[ , c('datasets_url', 'data_element_url'):=NULL]
+
+#-------------------------------
+# format variable names 
+
+if (folder=='pnls') {
+
+# create a variable that contains only the last word
+data_elements[ , data_element_name:=as.character(data_element_name)] # confirm element is a character variable
+data_elements[ , last:=word(data_element_name, -1)]
+data_elements[ , last:=tolower(last)]
+
+# drop anything containing soutien as the last word
+# these elements are duplicates or totals of other variables
+data_elements[grep('soutien', last), drop:=TRUE]
+
+# drop out elements that end in 'sex' or 'age'
+# these variables are stratified only by sex or age, while others include both sex/age
+data_elements[grep('sex', last), drop:=TRUE]
+data_elements[grep('age', last), drop:=TRUE]
+data_elements[is.na(drop), drop:=FALSE]
+
+#---------------------------------------
+# create a data set variable based on element codes
+data_elements[ , set:=(unlist(lapply(strsplit(data_element_name, "-"), '[', 2)))]
+
+# translate groupings
+data_elements[set=='CDV', set:='VCT']
+data_elements[set=='IST', set:='STI']
+data_elements[set=='PTME', set:='PMTCT']
+
+#---------------------------------------
+# create an element that is easier to grep
+data_elements[ , element1:=tolower(data_element_name)]
+
+# run the function to eliminate diacritical marks
+data_elements[ , element1:=fix_diacritics(element1)]
+
+#---------------------------------------
+# create new variable names without 'PNLS' or set code
+
+# drop the pnls and set codes from the variable names
+# in 'co-infected' elements, there are three hyphens
+data_elements[ , element_new1:=(unlist(lapply(strsplit(data_element_name, "-"), '[', 3)))]
+data_elements[ , element_new2:=(unlist(lapply(strsplit(data_element_name, "-"), '[', 4)))]
+data_elements[ , element_new3:=(unlist(lapply(strsplit(data_element_name, "-"), '[', 5)))]
+
+data_elements[!is.na(element_new2) & !is.na(element_new3), element_new:=paste0(element_new1, "-", element_new2, "-", element_new3)]
+data_elements[!is.na(element_new2) & is.na(element_new3), element_new:=paste0(element_new1, "-", element_new2)]
+data_elements[is.na(element_new2), element_new:=element_new1]
+
+# drop out the variables used to create new elements
+data_elements[ , c('last', 'element1','element_new1', 'element_new2', 'element_new3', 'data_element_name'):=NULL]
+setnames(data_elements, 'element_new', 'element')
+
+}
+
+#--------------------------------------------
+# change name only for other data sets; already changes in pnls
+if (folder!='pnls') setnames(data_elements, 'data_element_name', 'element')
+
+#---------------------------------------
+# merge in the variable names 
+data_elements[, datasets_ID:=NULL]
+dt = merge(dt, data_elements, by='data_element_id', all.x=T)
+
+# fix variable types 
+dt[ , element:=as.character(element)] # check name is accurate
+dt[ , data_element_id:=as.character(data_element_id)]
+dt[ , datasets_name:=as.character(datasets_name)]
+
+# drop unecessary variables to simplify
+dt[ , c('category', 'opening_date', 'data_set_id'):=NULL] 
+setnames(dt, 'category_name', 'category')
+
+print("Metadata merged; beginning to add translations.")
+
+#-------------------
+# merge in the english translations
+translations = data.table(readRDS(paste0(dir, 'meta_data/data_elements.rds')))
+translations = translations[ ,.(data_set_id, data_element_id=element_id, element_eng)]
+translations = translations[data_set_id==sub_id]
+translations[ ,data_set_id:=NULL]
+dt = merge(dt, translations, by='data_element_id', all.x=T)
+
+#--------------------------------------
+# change the names to intuitive naming conventions
+
+# create vectors of old and new variable names
+old_names = names(dt)
+if (folder=='pnls') { new_names = c("element_id", "org_unit_id", "last_update", "date", "category",
+              "value", "coordinates", "org_unit", "country", "dps", "health_zone", 
+              "health_area", "org_unit_type", "facility_level", "data_set", 
+              "drop", "pnls_set", "element", "element_eng")
+          } else {new_names = c("element_id", "org_unit_id", "last_update", "date", "category",
+                    "value", "coordinates", "org_unit", "country", "dps", "health_zone", 
+                    "health_area", "org_unit_type", "facility_level", "data_set",
+                    "element", "element_eng")}
+
+# check the vectors are the same length
+if (length(old_names)!= length(new_names)) print("Something's up. :(")
+
+# reset the names
+setnames(dt, old_names, new_names)
+
 #--------------------------------------
 # save the merged rds file 
 
@@ -236,17 +419,41 @@ max = dt[ , max(date)]
 max = gsub('-', '_', max)
 
 # save a merged rds file 
-saveRDS(dt, paste0(dir, 'pre_prep/merged/', folder,'_full_', min, '_', max, '.rds' ))
+saveRDS(dt, paste0(dir, 'pre_prep/merged/', folder,'_full_', min, '_', max, '_LOCAL.rds' ))
 
-#--------------------------------------
-# save a subsetted version of pnls with only relevant variables
-# variables in pnls are repeated based on stratification 
-# drop duplicates and save
+#------------------------------------------
+
+#-----------------------------------------------------------
+# read in the full data set you saved
+
+# dt = readRDS(paste0(dir, 'pre_prep/merged/', folder,'_full_', min, '_', max, '.rds' ))
+
+# local copy of the full data set 
+# dt = readRDS("C:/Users/ccarelli/Documents/dhis_data/pre_prep/merged/pnls_full_2017_01_01_2019_04_01.rds")
+
+
+if (folder=='pnls') dt = dt[drop==FALSE]
+if (folder=='pnls') dt[ ,element_eng:=]
+
+  #--------------------------------------
+  # save a subsetted version of pnls with only relevant variables
+  # variables in pnls are repeated based on stratification 
+  # drop duplicates and save
 
 if (folder=='pnls') {
-  dt = pnls_subset(dt)
-  saveRDS(dt, paste0(dir, 'pre_prep/merged/', folder,'_subset_', min, '_', max, '.rds' ))
+  
+  for (s in unique(dt$set)) {
+    x = dt[set==s]
+    
+    
+  }
+  
+  
 }
+  
+  
+saveRDS(dt, paste0(dir, 'pre_prep/merged/', folder,'_subset_', min, '_', max, '.rds' ))
+
 
 #---------------------------------------
 
--- outcome_measurement/hiv/gtm/sigsa_prep.R
+++ outcome_measurement/hiv/gtm/sigsa_prep.R
@@ -3,7 +3,7 @@
 # 6/5/2019
 #
 # Format SIGSA HIV testing data
-# Rewrite later as a function - many overlapping processes
+# To combine with SIGPRO
 # ----------------------------------------------
 
 #-----------------------------------------------
@@ -15,8 +15,9 @@ library(data.table)
 library(ggplot2)
 library(Hmisc)
 library(stringr)
-library(readxl)
+library(xlsx)
 library(XLConnect)
+library(tools)
 #---------------------------------------
 # Set up directories 
 #----------------------------------------
@@ -34,25 +35,25 @@ out_dir = paste0(dir, 'prepped/sigpro/')
 #-----------------------------------------
 # load in the file 
 
-dt = data.table(read.xlsx(paste0(dir,'sigsa/Patient Level Data/Solicitud 0593-2018 SIGSA SIDA 1.2 años 2014 al 2017.xlsx'), sheet=1))
-
+dt = data.table(read.csv(paste0(dir,'sigsa/Patient Level Data/Solicitud 0593-2018 Sigsa 6 mensual producción TB y VIH 2014 al 2017_csv.csv'), skip=3,
+                         stringsAsFactors = FALSE))
 #-----------------------------------------
 # format the column names and drop unecessary variables 
 
 # create a list of variable names
-varNames = as.character(dt[1, ])
-varAlt = as.character(dt[2, ])
+varNames = as.character(names(dt))
+varAlt = as.character(dt[1])
 names = data.table(cbind(varNames, varAlt))
-names[is.na(varNames), varNames:=varAlt]
+names[grep("^X", varNames), varNames:=varAlt]
 names = names$varNames
 
 # replace with the variable names
 setnames(dt, names)
 
 # drop rows 1 and 2 (include variable names)
-dt = dt[-(1:2)]
+dt = dt[-1]
 
-# drop the last three rows - just summary information
+# drop the last three rows - just summary information/often blank
 dt = head(dt, -3)
 
 #subset to the hiv-related variables
@@ -63,19 +64,19 @@ dt = dt[ ,c(1:9, 12:19, 21:30, 38)]
 
 new_names = c("year", "month", "health_area", "health_district", "health_service",
               "service_type", "muni", "date", "cui", "birthday_day",
-              "birthday_month", "birthday_year", "nationality", "sex", "residence_dept",
+              "birthday_month", "birthday_year", "nationality", "gender", "residence_dept",
               "residence_muni", "sexual_orientation", "linguistic_community",
               "risk_condition", "reason_for_orientation", 
               "pregnancy_post_partum", "pre_test", "test_completed", "hiv", "result",
-              "confirmatory_test", "result", "referral")
+              "confirmatory_test", "confirmatory_result", "referral")
 
 setnames(dt, new_names)
 
 #-----------------------------------------
-# excel date origin is 1899
+# format dates 
 
 # format activity date
-dt[ , date:=as.Date(as.numeric(date), origin='1899-12-30')]
+dt[ , date:=as.Date(date, format='%m/%d/%Y')]
 
 # format birthdate
 dt[as.numeric(birthday_day) < 10, birthday_day:=paste0('0', birthday_day)]
@@ -90,13 +91,50 @@ dt[ ,c('year', 'month', 'birthday_day',
        'birthday_year', 'birthday_month', 'dob'):=NULL]
 
 #-----------------------------------------
-# format depatments
+# format the data 
+
+# replace missing data 
+vars = names(dt)[names(dt)!='date' & names(dt)!='age']
+for (v in vars) dt[get(v)=="-", (v):=NA]
+
+# replace si and no values with logicals
+dt[!is.na(pre_test), pre_test1:=(pre_test=='Si')]
+dt[!is.na(test_completed), test_completed1:=(test_completed=='Si')]
+dt[!is.na(hiv), hiv1:=(hiv=='Si')]
+dt[!is.na(result), result1:=(result=='Si')]
+dt[!is.na(confirmatory_test), confirmatory_test1:=(confirmatory_test=='Si')]
+dt[!is.na(confirmatory_result), confirmatory_result1:=(confirmatory_result=='Si')]
+
+# delete the character variables in favor of the logicals
+dt[ ,c("pre_test", "test_completed", "hiv", "result",
+      "confirmatory_test", "confirmatory_result"):=NULL]
+
+# use the new variables
+setnames(dt, c("pre_test1", "test_completed1", "hiv1", "result1",
+               "confirmatory_test1", "confirmatory_result1"),
+              c("pre_test", "test_completed", "hiv", "result",
+               "confirmatory_test", "confirmatory_result"))
+
+#-----------------------------------------
+# format gender
+
+dt[gender=="Femenino", gender:="Female"]
+dt[gender=="Masculino", gender:="Male"]
+#-----------------------------------------
+# format the department and municipalities
+
+names = c(names(dt)[1:2], names(dt)[12:length(dt)])
 
 
-# format nationalities
+dt = dt[ ,lapply(.SD, tolower), by=names, .SDcols=c(3:5, 10:11)]
+# dt = dt[ ,lapply(.SD, toTitleCase), by=names, .SDcols=c(1:3, 10:11)]
 
+#-----------------------------------------
+# save a prepped version 
 
+saveRDS(dt, paste0(dir, 'prepped/sigsa/prepped_sigsa_data.rds'))
 
+#-----------------------------------------
 
 
 
