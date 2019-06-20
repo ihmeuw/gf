3c61720d138748c26b1c0640b70f771e082d6eaf
David Phillips <davidp6@uw.edu>
2019-06-20 15:49:00 -0700

    Update singularity exec in shell scripts

--- core/r_shell.sh
+++ core/r_shell.sh
@@ -1,2 +1,2 @@
 #$ -S /bin/sh
-singularity exec /share/singularity-images/health_fin/forecasting/best_new.img R <$1 --no-save $@
+singularity exec --bind /tmp:/tmp /share/singularity-images/health_fin/forecasting/best_new.img R <$1 --no-save $@
