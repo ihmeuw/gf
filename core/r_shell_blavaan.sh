0cc091ac16ac27699d1f1deefa975206b6c1a404
David Phillips <davidp6@uw.edu>
2019-07-29 11:25:00 -0700

    Update blavaan shell script to use Rscript instead of R to make trailingOnly work

--- core/r_shell_blavaan.sh
+++ core/r_shell_blavaan.sh
@@ -1,2 +1,2 @@
 #$ -S /bin/sh
-singularity exec --bind /tmp:/tmp /share/singularity-images/health_fin/forecasting/best_new.img R <$1 --no-save $@
+singularity exec --bind /tmp:/tmp /share/singularity-images/health_fin/forecasting/best_new.img Rscript <$1 --no-save $@
