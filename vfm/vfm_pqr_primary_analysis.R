a55744f4a65106fc07a253a5c45e25d395983bb7
Audrey Batzel <abatzel@uw.edu>
2019-07-31 12:54:42 -0700

    do initial figures and checks

--- vfm/vfm_pqr_primary_analysis.R
+++ vfm/vfm_pqr_primary_analysis.R
@@ -12,6 +12,7 @@ library(data.table)
 library(ggplot2)
 library(RColorBrewer)
 library(zoo)
+library(scales)
 # --------------------
 
 # ----------------------------------------------
@@ -26,6 +27,10 @@ inFile = paste0(dir, 'unit_cost_data/prepped_data/prepped_full_pqr.rds')
 
 # output files
 compare_ref_prices = paste0(dir, 'visualizations/compare_ref_price_unit_cost.pdf')
+compare_ref_prices_with_size = paste0(dir, 'visualizations/compare_ref_price_unit_cost_with_size_by_volume.pdf')
+compare_ref_prices_volume_on_x = paste0(dir, 'visualizations/compare_ref_price_unit_cost_with_volume_on_x.pdf')
+scatterplot_unit_cost_vs_intl_ref_price = paste0(dir, 'visualizations/scatterplot_unit_cost_vs_intl_ref_price.pdf')
+hist_ref_price_by_product_category = paste0(dir, 'visualizations/hist_diff_unit_cost_ref_price_by_product_category.pdf')
 # outFile = paste0(dir, 'visualizations/.pdf')
 # ----------------------------------------------
 
@@ -44,28 +49,76 @@ data[, product_pack_usd := NULL]
 # ----------------------------------------------
 # graphs comparing unit costs and reference prices
 # ----------------------------------------------
-# one per commodity, y=unit cost, x=date and a horizontal line for the reference price, color by countries
-commodity_missing_irp = c()
-for ( p in unique(data$product_name_en) ){
-  if(all(is.na(data[product_name_en == p, po_international_reference_price]))) commodity_missing_irp = c(commodity_missing_irp,p)
-}
 
-pdf(compare_ref_prices, height = 9, width = 11)
+# commodity_missing_irp = c()
+# for ( p in unique(data$product_name_en) ){
+#   if(all(is.na(data[product_name_en == p, po_international_reference_price]))) commodity_missing_irp = c(commodity_missing_irp,p)
+# }
+
+# one per commodity, y=unit cost, x=date and a horizontal line for the reference price, color by countries
+# pdf(compare_ref_prices, height = 9, width = 11)
+pdf(compare_ref_prices_with_size, height = 9, width = 11)
 for ( p in unique(data$product_name_en) ){
-  
-  if(all(is.na(data[product_name_en == p, po_international_reference_price]))) commodity_missing_irp = c(commodity_missing_irp,p)
-  
-  print(ggplot(data[product_name_en == p, ], aes(x=purchase_order_date, y=unit_cost_usd, color=country_name)) +
-    geom_point(size = 5) +
+  print(ggplot(data[product_name_en == p, ], aes(x=purchase_order_date, y=unit_cost_usd, color=country_name, size = nb_tests_units)) +
+    geom_point() +
     geom_line(aes(x=purchase_order_date, y=po_international_reference_price), color = 'darkgrey', size = 1.5 ) +
     geom_point(aes(x=purchase_order_date, y=po_international_reference_price), color = 'darkgrey', size = 3 ) +
     theme_bw() +
-    theme(text = element_text(size=18), legend.position = 'bottom') +
+    theme(text = element_text(size=14)) +
     facet_wrap( ~description, scales = "free") +
-    labs(title = paste0("Comparison of unit costs and international reference prices for \n", p, " by type and country"),
-          x='Date', y='Unit Cost (USD)', color="Country Name"))
+    scale_size_continuous(labels = comma) +
+    labs(title = paste0("Comparison of unit costs and international reference prices for ", p), 
+         subtitle = '(Gray points represent the international reference price over time)',
+         x='Date', y='Unit Cost (USD)', color="Country Name", size = 'Volume Purchased'))
 }
 dev.off()
+
+pdf(compare_ref_prices_volume_on_x, height = 9, width = 11)
+for ( p in unique(data$product_name_en) ){
+  print(ggplot(data[product_name_en == p, ], aes(x=nb_tests_units, y=unit_cost_usd, color=country_name)) +
+          geom_point(size = 4.5) +
+          geom_line(aes(x=nb_tests_units, y=po_international_reference_price), color = 'darkgrey', size = 1.5 ) +
+          geom_point(aes(x=nb_tests_units, y=po_international_reference_price), color = 'darkgrey', size = 3 ) +
+          theme_bw() +
+          theme(text = element_text(size=14)) +
+          facet_wrap( ~description, scales = "free") +
+          scale_x_continuous(labels = comma) +
+          theme(legend.position = 'bottom') + 
+          labs(title = paste0("Comparison of unit costs and international reference prices for ", p), 
+               subtitle = '(Gray points represent the international reference price over time)',
+               x='Units Purchased', y='Unit Cost (USD)', color="Country Name"))
+}
+dev.off()
+
+pdf(scatterplot_unit_cost_vs_intl_ref_price, height = 9, width = 11)
+for ( p in unique(data$product_name_en) ){
+    print(ggplot(data[product_name_en == p, ], aes(x=po_international_reference_price, y=unit_cost_usd, color=country_name, size=nb_tests_units)) +
+            geom_point() + geom_abline() + 
+            theme_bw() +
+            theme(text = element_text(size=14)) +
+            facet_wrap( ~description) +
+            scale_size_continuous(labels = comma) +
+            labs(title = paste0("Comparison of unit costs and international reference prices for ", p), 
+                 x='International Reference Price', y='Unit Cost (USD)', color="Country Name", size = 'Volume Purchased'))
+}
+dev.off()
+
+pdf(hist_ref_price_by_product_category, height = 9, width = 11)
+for ( p in unique(data$product_category) ){
+  print(ggplot(data[product_category == p, ], aes(x=unit_cost_usd - po_international_reference_price)) + geom_histogram() + facet_grid(iso3codecountry~product_category))
+}
+dev.off()
+# ----------------------------------------------
+
+# ----------------------------------------------
+# data checks David asked for:
+# ----------------------------------------------
+# 4.	Are we sure about the ITN costs on page 10? I believe the budget says 1$ and the program plans for $2.50, but the graphs are between 3.50 and 5.
+check = data[product_category == 'bednet_irs', ]
+check[pack_cost_usd != unit_cost_usd]
+check[, .(min = min(unit_cost_usd), max = max(unit_cost_usd)), by = 'product_name_en']
+# 6.	How often are there two different reference prices for the same commodity at the same time?
+check = data[, .(num_of_irps = length(unique(po_international_reference_price))), by = .(product_name_en, description, purchase_order_date)]
 # ----------------------------------------------
 
 # ----------------------------------------------
