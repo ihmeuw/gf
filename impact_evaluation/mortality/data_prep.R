5ec0b6b28a9d47e1e5d5b66558bf6ef35ec70d85
Francisco Rios Casas <frc2@uw.edu>
2019-08-12 11:24:49 -0700

    Updates to drivers of mortality analysis

--- impact_evaluation/mortality/data_prep.R
+++ impact_evaluation/mortality/data_prep.R
@@ -2,9 +2,10 @@
 # data prep for Drivers of mortality analysis plan
 # August 8, 2019
 
-# Data step that is pending: the TB data is disaggregated by HIV status, so that would need to be combined to get an overall value
+# Limitation 1 :TB data is disaggregated by HIV status, so that would need to be combined to get an overall value
 
 # set up
+source('./impact_evaluation/drc/set_up_r.r')
 library(data.table)
 library(ggplot2)
 library(GGally)
@@ -17,6 +18,19 @@ TB = TB[cause_name=="Drug-susceptible tuberculosis"]
 TB = TB[metric_name=="Rate"]
 TB = TB[age_name=="Age-standardized"]
 
+# keep columns of interest
+TB = TB[,.(measure_name, cause_name, location_name, year,val)]
+
+# reshape the data to be wide
+data = dcast(TB, cause_name + location_name + year ~ measure_name, value.var = "val")
+
 #  compute MI ration
-TB = TB[]
-TB$mi_ratio <- T
+
+data$mi_ratio <- data$Deaths/data$Incidence
+data
+
+# plot pairwise correlations for each country
+ggpairs(data[location_name=="Senegal",c('Deaths', 'Incidence', 'mi_ratio'), with=F])
+ggpairs(data[location_name=="Democratic Republic of the Congo",c('Deaths', 'Incidence', 'mi_ratio'), with=F])
+ggpairs(data[location_name=="Guatemala",c('Deaths', 'Incidence', 'mi_ratio'), with=F])
+ggpairs(data[location_name=="Uganda",c('Deaths', 'Incidence', 'mi_ratio'), with=F])
