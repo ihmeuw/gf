e73a23f4ec40d889f36d9ad0e46f5b032abae816
Emily Linebarger <eklinebarg@gmail.com>
2019-08-13 09:34:22 -0700

    Running simple linear regressions for each relationship to check VIF and eigenvalues. - Commented out everything in model that had a VIF by linear regression greater than 10.

--- impact_evaluation/gtm/models/gtm_tb_first_half2.R
+++ impact_evaluation/gtm/models/gtm_tb_first_half2.R
@@ -17,23 +17,23 @@ model = '
 
   # Linkage 1 = relationships between inputs and activities
 
-    Isoniazid_Distributed_act_cumulative ~ exp_T1_2_cumulative + ghe_T1_2_cumulative + other_dah_T1_2_cumulative + exp_R1_ALL_cumulative + date #Test commenting this out to see if it resolves linear dependence issue. EL 7/30/2019
-    Total_Drugs_Distributed_act_cumulative ~ exp_T1_2_cumulative + exp_R1_ALL_cumulative + other_dah_T1_2_cumulative + ghe_T1_2_cumulative + date
-    Number_of_Cases_Screened_for_MDR_act_cumulative ~ exp_T3_ALL + other_dah_T3_1_cumulative + date
-#   PLHIV_Screened_for_TB_act_cumulative ~ exp_T2_ALL_cumulative + date
-#   TB_Patients_Tested_for_HIV_act_cumulative ~ exp_T2_ALL_cumulative + date
-# 	
-# 	# Linkage 2 = relationships between activities and outputs or inputs and outputs
-#   
-#   Cases_Notified_out_cumulative ~ exp_T1_1_cumulative + ghe_T1_1_cumulative + other_dah_T1_1_cumulative + date #Can add outreach and training and microscopy tests if data come through
-#   HIV_TB_Cases_Notified_out_cumulative ~ PLHIV_Screened_for_TB_act_cumulative + TB_Patients_Tested_for_HIV_act_cumulative + date
-#   MDR_Cases_Notified_out_cumulative ~ Number_of_Cases_Screened_for_MDR_act_cumulative + date
-#   Cases_Started_on_Treatment_out_cumulative ~ exp_T1_1_cumulative + ghe_T1_1_cumulative + other_dah_T1_1_cumulative + Isoniazid_Distributed_act_cumulative + date #Can add outreach if data come through
-#   MDR_Cases_Started_Treatment_out_cumulative ~ exp_T3_ALL + other_dah_T3_2_cumulative + Number_of_Cases_Screened_for_MDR_act_cumulative + date
-#   Additional_Cases_Detected_via_ACF_out_cumulative ~ exp_T1_5_cumulative + date #Change this to Outreach_teams_formed when that is populated
-#   PLHIV_started_on_IPT_out_cumulative ~ PLHIV_Screened_for_TB_act_cumulative + Isoniazid_Distributed_act_cumulative + date
-#   Cases_Notified_in_Prisons_out_cumulative ~ exp_T1_6_cumulative + date
-#   
+  #   Isoniazid_Distributed_act_cumulative ~ exp_T1_2_cumulative + ghe_T1_2_cumulative + other_dah_T1_2_cumulative + exp_R1_ALL_cumulative + date #Test commenting this out to see if it resolves linear dependence issue. EL 7/30/2019
+  #   Total_Drugs_Distributed_act_cumulative ~ exp_T1_2_cumulative + exp_R1_ALL_cumulative + other_dah_T1_2_cumulative + ghe_T1_2_cumulative + date
+      Number_of_Cases_Screened_for_MDR_act_cumulative ~ exp_T3_ALL + other_dah_T3_1_cumulative + date
+      PLHIV_Screened_for_TB_act_cumulative ~ exp_T2_ALL_cumulative + date
+      TB_Patients_Tested_for_HIV_act_cumulative ~ exp_T2_ALL_cumulative + date
+  # 
+  # 	# Linkage 2 = relationships between activities and outputs or inputs and outputs
+  # 
+  #   Cases_Notified_out_cumulative ~ exp_T1_1_cumulative + ghe_T1_1_cumulative + other_dah_T1_1_cumulative + date #Can add outreach and training and microscopy tests if data come through
+  #   HIV_TB_Cases_Notified_out_cumulative ~ PLHIV_Screened_for_TB_act_cumulative + TB_Patients_Tested_for_HIV_act_cumulative + date
+      MDR_Cases_Notified_out_cumulative ~ Number_of_Cases_Screened_for_MDR_act_cumulative + date
+  #   Cases_Started_on_Treatment_out_cumulative ~ exp_T1_1_cumulative + ghe_T1_1_cumulative + other_dah_T1_1_cumulative + Isoniazid_Distributed_act_cumulative + date #Can add outreach if data come through
+      MDR_Cases_Started_Treatment_out_cumulative ~ exp_T3_ALL + other_dah_T3_2_cumulative + Number_of_Cases_Screened_for_MDR_act_cumulative + date
+      Additional_Cases_Detected_via_ACF_out_cumulative ~ exp_T1_5_cumulative + date #Change this to Outreach_teams_formed when that is populated
+  #   PLHIV_started_on_IPT_out_cumulative ~ PLHIV_Screened_for_TB_act_cumulative + Isoniazid_Distributed_act_cumulative + date
+      Cases_Notified_in_Prisons_out_cumulative ~ exp_T1_6_cumulative + date
+
   #These are all leftover examples from the malaria model that I have not updated yet for TB---------------------------------------------------------------------
 	# latent variables
 	
