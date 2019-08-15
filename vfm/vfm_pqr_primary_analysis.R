3a11ac76a973c394b6dd56053f8d942b9b37b5f4
Audrey Batzel <abatzel@uw.edu>
2019-08-15 16:36:53 -0700

    organize code and comment better; generate codebook of commodities; use codebook to subset data to most common commodities

--- vfm/vfm_pqr_primary_analysis.R
+++ vfm/vfm_pqr_primary_analysis.R
@@ -30,6 +30,7 @@ j = ifelse(Sys.info()[1]=='Windows','J:','/home/j')
 # input file
 dir = paste0(j, '/Project/Evaluation/GF/vfm/')
 inFile = paste0(dir, 'unit_cost_data/prepped_data/prepped_full_pqr.rds')
+outFile_codebook = paste0(dir, 'unit_cost_data/prepped_data/commodity_codebook.csv')
 
 # output files for figures
 # for primary analyses
@@ -60,68 +61,33 @@ data = data[iso3codecountry %in% c('COD', 'GTM', 'SEN', 'UGA')]
 # ----------------------------------------------
 
 # ----------------------------------------------
-# IRP checks - make histograms of IRP by commodity, and time series to show if there are duplicates by date/commodity but with different IRPs. 
-# ----------------------------------------------
-data[, purchase_order_year := as.numeric(year(purchase_order_date))]
-data[, purchase_order_month := as.numeric(month(purchase_order_date))]
-check_irp = data[, unique(po_international_reference_price), by = .(product_name_en, purchase_order_year, purchase_order_month)]
-check_irp[, order_date := as.yearmon(paste(check_irp$purchase_order_year, check_irp$purchase_order_month), "%Y%m")]
-check_irp[, order_date := as.Date(order_date)]
-
-check_irp[, more_than_one_irp := FALSE]
-dups = check_irp[duplicated(check_irp[, .(product_name_en, order_date)]),]
-for( row in 1:nrow(dups) ){
-  product = dups[row, product_name_en]
-  date = dups[row, order_date]
-  check_irp[product_name_en == product & order_date == date, more_than_one_irp := TRUE]
-  if ( check_irp[product_name_en == product & order_date == date & !is.na(V1), .N] <= 1 ) {
-    check_irp[product_name_en == product & order_date == date, more_than_one_irp := FALSE]
-  }
-}
-check_irp[, more_than_one_irp:=as.factor(more_than_one_irp)]
-colors = c('cadetblue3', 'coral2')
-names(colors) = levels(check_irp$more_than_one_irp)
-
-pdf(irp_value_check, height = 9, width = 11)
-for( x in unique(data$product_name_en) ){
-  g1 = ggplot( check_irp[ product_name_en == x, ], aes(x = order_date, y = V1, color = more_than_one_irp) ) +
-        geom_point(size = 3) + theme_bw() + theme(text = element_text(size = 16)) + 
-        labs( title = paste0("Time series of international reference prices for ", x), x= 'Order date (year, month)', y = 'International reference price (USD per unit)') +
-        scale_color_manual(name = 'more_than_one_irp', values = colors) + ylim(0, NA)
-  g2 = ggplot( data[ product_name_en == x, ], aes(x = po_international_reference_price) ) + geom_histogram() + 
-        theme_bw() + theme(text = element_text(size = 16)) + 
-        labs( title = paste0("International reference prices for ", x), x= 'International reference price (USD per unit)')
-  grid.arrange(g1, g2, nrow = 2)
-}
-dev.off()
-
-# unique(data[po_international_reference_price < (0.05*unit_cost_usd), .(po_international_reference_price, unit_cost_usd)])
-# 
-# tot = data[, .(total_rows = .N), by = .(product_name_en, product_category)]
-# na = data[is.na(po_international_reference_price)|po_international_reference_price == 0|(po_international_reference_price > (0.05*unit_cost_usd)), 
-#           .(irp_wrong_or_missing = .N), 
-#           by = .(product_name_en, product_category)]
-# 
-# check = merge(tot, na,by = c('product_name_en', 'product_category'), all = TRUE)
-# check = check[ !product_category %in% c('bednet', 'diagnostic_test_tb', 'diagnostic_test_other', 'irs')]
-# sum(check$irp_wrong_or_missing, na.rm = TRUE) / sum(check$total_rows) # = 14.7% NA outside of the above categories
-# check[total_rows == irp_wrong_or_missing, all_irp_wrong_or_missing := TRUE]
-# ----------------------------------------------
-
-# ----------------------------------------------
 # Narrow down to the most common products by country / disease to about 10 per country / disease
 # Try it using purchasing volume, overall spend, and total number of orders
 # ----------------------------------------------
 # David said we don't care about specific brand of bednet, so make it so those will sum over product_name here:
 dt = data[product_category == 'bednet', product_name_en := 'bednet' ]
 
-# make a separate 
+# make a separate "product category" for first and second line TB drugs:
+first_line = c("Rifampicin" , "Pyrazinamide", "Ethambutol+Isoniazid+Pyrazinamide+Rifampicin (RHZE", "Isoniazid", 
+               "Ethambutol+Isoniazid+Rifampicin - FDC", "Isoniazid+Pyrazinamide+Rifampicin - FDC", "Isoniazid+Rifampicin - FDC", 
+               "Ethambutol+Isoniazid - FDC", "Streptomycin", "Ethambutol", "PAS Sodium")
+second_line = c("Ofloxacin", "Levofloxacin", "Moxifloxacin", "Cycloserine", "Protionamide", "Amikacin", "Ethionamide", "Kanamycin",
+                "Capreomycin", "Linezolid", "Bedaquiline", "Meropenem", "Clofazimine", "Amoxicillin+Clavulanate - FDC")
+other = c("Water for injection")
 
-# make a code book of all commodities, whether or not will use them in analysis, and whether or not they have a ref price
-codebook = unique(dt[, .(product_name_en, product_category, po_international_reference_price)])
-codebook = codebook[, has_irp := !all(is.na(po_international_reference_price)), by = .(product_name_en, product_category)]
-codebook = unique(codebook[,.(product_name_en, product_category, has_irp)])
-                  
+dt = dt[ product_name_en %in% first_line, sub_product_category := "first_line"]
+dt = dt[ product_name_en %in% second_line, sub_product_category := "second_line"]
+dt = dt[ product_name_en %in% other, sub_product_category := "other"]
+
+dt[ product_category == 'anti_tb_medicine', product_category := paste(product_category, sub_product_category, sep = "_") ]
+
+# # make a code book of all commodities, whether or not will use them in analysis, and whether or not they have a ref price
+# codebook = unique(dt[, .(product_name_en, product_category, po_international_reference_price)])
+# codebook = codebook[, has_irp := !all(is.na(po_international_reference_price)), by = .(product_name_en, product_category)]
+# codebook = unique(codebook[,.(product_name_en, product_category, has_irp)])
+# codebook[, included_in_analysis := TRUE]
+# write.csv(codebook, outFile_codebook)
+
 # Calculate purchasing volume, overall spend, and number of orders from 2015 on
 # sum by country, product_category and product_name
 dt = dt[purchase_order_date >= '2015-01-01',.(purchasing_volume = sum(total_units_in_order), 
@@ -133,34 +99,34 @@ dt[is.na(product_disease), product_disease := 'other']
 dt[, product_disease := as.factor(product_disease)]
 dt[, overall_spend_in_millions := overall_spend/1000000]
 
-# Subset dt to the top 10 commodities per country/disease:
-subset = dt[product_disease != 'other']
-# subset_by_vol = setorder(subset, -purchasing_volume)[, head(.SD, 5L), keyby =c('iso3codecountry', 'product_disease')]
-# subset_by_orders = setorder(subset, -total_number_of_orders)[, head(.SD, 5L), keyby =c('iso3codecountry', 'product_disease')]
-# subset_by_spend = setorder(subset, -overall_spend)[, head(.SD, 5L), keyby =c('iso3codecountry', 'product_disease')]
-subset_by_vol = setorderv(subset, c('iso3codecountry', 'product_disease', 'purchasing_volume'), c(1, 1, -1))[, head(product_name_en, 7L), c('iso3codecountry', 'product_category')]
-subset_by_spend = setorderv(subset, c('iso3codecountry', 'product_disease', 'overall_spend'), c(1, 1, -1))[, head(product_name_en, 7L), c('iso3codecountry', 'product_category')]
-# subset_by_orders = setorderv(subset, c('iso3codecountry', 'product_disease', 'total_number_of_orders'), c(1, 1, -1))[, head(product_name_en, 7L), c('iso3codecountry', 'product_category')]
-
-setnames(subset_by_vol, 'V1', 'product_name_en')
-setnames(subset_by_spend, 'V1', 'product_name_en')
-# setnames(subset_by_orders, 'V1', 'product_name_en')
-
-subset_by_vol[, by_vol_purchased := TRUE] 
-subset_by_spend[, by_tot_spend := TRUE] 
-# subset_by_orders[, by_num_orders := TRUE] 
-
-# subset = merge(subset_by_vol, subset_by_orders, all = TRUE, by = c('iso3codecountry', 'product_disease', 'product_name_en'))
-subset = merge(subset_by_vol, subset_by_spend, all = TRUE, by = c('iso3codecountry', 'product_category', 'product_name_en'))
-subset = merge(subset, data, by = c('iso3codecountry', 'product_disease', 'product_name_en'), all.x = TRUE)
-
-dt = dt[!product_category %in% c('irs', 'diagnostic_test_other')]
-dt[, product_category := as.factor(product_category)]
-# Make a color palette so colors are the same for disease across different plots (even if some 'levels' are removed - for ex, SEN does not have 'other')
-colors = brewer.pal(8, 'Set2')
-names(colors) = levels(dt$product_category)
-fillScale = scale_fill_manual(name = 'product_category', values = colors)
-
+# # Subset dt to the top 10 commodities per country/disease:
+# subset = dt[product_disease != 'other']
+# # subset_by_vol = setorder(subset, -purchasing_volume)[, head(.SD, 5L), keyby =c('iso3codecountry', 'product_disease')]
+# # subset_by_orders = setorder(subset, -total_number_of_orders)[, head(.SD, 5L), keyby =c('iso3codecountry', 'product_disease')]
+# # subset_by_spend = setorder(subset, -overall_spend)[, head(.SD, 5L), keyby =c('iso3codecountry', 'product_disease')]
+# subset_by_vol = setorderv(subset, c('iso3codecountry', 'product_disease', 'purchasing_volume'), c(1, 1, -1))[, head(product_name_en, 7L), c('iso3codecountry', 'product_category')]
+# subset_by_spend = setorderv(subset, c('iso3codecountry', 'product_disease', 'overall_spend'), c(1, 1, -1))[, head(product_name_en, 7L), c('iso3codecountry', 'product_category')]
+# # subset_by_orders = setorderv(subset, c('iso3codecountry', 'product_disease', 'total_number_of_orders'), c(1, 1, -1))[, head(product_name_en, 7L), c('iso3codecountry', 'product_category')]
+# 
+# setnames(subset_by_vol, 'V1', 'product_name_en')
+# setnames(subset_by_spend, 'V1', 'product_name_en')
+# # setnames(subset_by_orders, 'V1', 'product_name_en')
+# 
+# subset_by_vol[, by_vol_purchased := TRUE] 
+# subset_by_spend[, by_tot_spend := TRUE] 
+# # subset_by_orders[, by_num_orders := TRUE] 
+# 
+# # subset = merge(subset_by_vol, subset_by_orders, all = TRUE, by = c('iso3codecountry', 'product_disease', 'product_name_en'))
+# subset = merge(subset_by_vol, subset_by_spend, all = TRUE, by = c('iso3codecountry', 'product_category', 'product_name_en'))
+# subset = merge(subset, data, by = c('iso3codecountry', 'product_disease', 'product_name_en'), all.x = TRUE)
+# 
+# dt = dt[!product_category %in% c('irs', 'diagnostic_test_other')]
+# dt[, product_category := as.factor(product_category)]
+# # Make a color palette so colors are the same for disease across different plots (even if some 'levels' are removed - for ex, SEN does not have 'other')
+# colors = brewer.pal(8, 'Set2')
+# names(colors) = levels(dt$product_category)
+# fillScale = scale_fill_manual(name = 'product_category', values = colors)
+# 
 # # Treemap (one per country) showing purchasing volume by disease and product with overall spend as the gradient
 # pdf(treemap_purchasing_volume_by_disease_country, height = 10, width = 15)
 # for ( x in unique(dt$iso3codecountry) ){
@@ -218,8 +184,13 @@ fillScale = scale_fill_manual(name = 'product_category', values = colors)
 # }
 # dev.off()
 
-dt[, iso3codecountry := as.factor(iso3codecountry)]
+# REMAKE TREEMAPS - so there is a page for each product category and the first subgroup is by country. 
+# This can show us within a given product category, which specific products are most common within and among countries 
+# Note: This was orgiinally done before TB drug commodities were reclassified into first- and second-line drugs. 
+
 # Make a color palette so colors are the same for disease across different plots (even if some 'levels' are removed - for ex, SEN does not have 'other')
+dt[, iso3codecountry := as.factor(iso3codecountry)]
+
 colors = brewer.pal(4, 'Set2')
 names(colors) = levels(dt$iso3codecountry)
 fillScale = scale_fill_manual(name = 'iso3codecountry', values = colors)
@@ -280,6 +251,52 @@ dev.off()
 # ----------------------------------------------
 
 # ----------------------------------------------
+# subset data to only "most common" commodities 
+# ----------------------------------------------
+subset_with_codebook = read.csv(outFile_codebook) # make manual changes to "included_in_analysis" column based on treemaps and then read that back in
+
+products_to_include_in_analysis = subset_with_codebook[ included_in_anlaysis == TRUE, product_name_en ]
+subset = data[ product_name_en %in% products_to_include_in_analysis ]
+# ----------------------------------------------
+
+# ----------------------------------------------
+# IRP checks - make histograms of IRP by commodity, and time series to show if there are duplicates by date/commodity but with different IRPs. 
+# ----------------------------------------------
+data[, purchase_order_year := as.numeric(year(purchase_order_date))]
+data[, purchase_order_month := as.numeric(month(purchase_order_date))]
+check_irp = data[, unique(po_international_reference_price), by = .(product_name_en, purchase_order_year, purchase_order_month)]
+check_irp[, order_date := as.yearmon(paste(check_irp$purchase_order_year, check_irp$purchase_order_month), "%Y%m")]
+check_irp[, order_date := as.Date(order_date)]
+
+check_irp[, more_than_one_irp := FALSE]
+dups = check_irp[duplicated(check_irp[, .(product_name_en, order_date)]),]
+for( row in 1:nrow(dups) ){
+  product = dups[row, product_name_en]
+  date = dups[row, order_date]
+  check_irp[product_name_en == product & order_date == date, more_than_one_irp := TRUE]
+  if ( check_irp[product_name_en == product & order_date == date & !is.na(V1), .N] <= 1 ) {
+    check_irp[product_name_en == product & order_date == date, more_than_one_irp := FALSE]
+  }
+}
+check_irp[, more_than_one_irp:=as.factor(more_than_one_irp)]
+colors = c('cadetblue3', 'coral2')
+names(colors) = levels(check_irp$more_than_one_irp)
+
+pdf(irp_value_check, height = 9, width = 11)
+for( x in unique(data$product_name_en) ){
+  g1 = ggplot( check_irp[ product_name_en == x, ], aes(x = order_date, y = V1, color = more_than_one_irp) ) +
+    geom_point(size = 3) + theme_bw() + theme(text = element_text(size = 16)) + 
+    labs( title = paste0("Time series of international reference prices for ", x), x= 'Order date (year, month)', y = 'International reference price (USD per unit)') +
+    scale_color_manual(name = 'more_than_one_irp', values = colors) + ylim(0, NA)
+  g2 = ggplot( data[ product_name_en == x, ], aes(x = po_international_reference_price) ) + geom_histogram() + 
+    theme_bw() + theme(text = element_text(size = 16)) + 
+    labs( title = paste0("International reference prices for ", x), x= 'International reference price (USD per unit)')
+  grid.arrange(g1, g2, nrow = 2)
+}
+dev.off()
+# ----------------------------------------------
+
+# ----------------------------------------------
 # Graphs comparing unit costs and international reference prices
 # ----------------------------------------------
 pdf(compare_ref_prices_with_size, height = 9, width = 11)
@@ -330,22 +347,24 @@ dev.off()
 # ----------------------------------------------
 
 # ----------------------------------------------
-# Histogram of international reference price by product category and country
-# ----------------------------------------------
-pdf(hist_ref_price_by_product_category, height = 9, width = 11)
-for ( p in unique(data$product_category) ){
-  print(ggplot(data[product_category == p, ], aes(x=unit_cost_usd - po_international_reference_price)) + geom_histogram() + facet_grid(iso3codecountry~product_category))
-}
-dev.off()
-# ----------------------------------------------
-
-# ----------------------------------------------
-# Data checks David asked for:
-# ----------------------------------------------
-# 4.	Are we sure about the ITN costs on page 10? I believe the budget says 1$ and the program plans for $2.50, but the graphs are between 3.50 and 5.
-check = data[product_category == 'bednet_irs', ]
-check[pack_cost_usd != unit_cost_usd]
-check[, .(min = min(unit_cost_usd), max = max(unit_cost_usd)), by = 'product_name_en']
-# 6.	How often are there two different reference prices for the same commodity at the same time?
-check = data[, .(num_of_irps = length(unique(po_international_reference_price))), by = .(product_name_en, description, purchase_order_date)]
-# ----------------------------------------------
+# Other/Off-hand analyses quickly
+# # ----------------------------------------------
+# # Histogram of international reference price by product category and country
+# # ----------------------------------------------
+# pdf(hist_ref_price_by_product_category, height = 9, width = 11)
+# for ( p in unique(data$product_category) ){
+#   print(ggplot(data[product_category == p, ], aes(x=unit_cost_usd - po_international_reference_price)) + geom_histogram() + facet_grid(iso3codecountry~product_category))
+# }
+# dev.off()
+# # ----------------------------------------------
+# 
+# # ----------------------------------------------
+# # Data checks David asked for:
+# # ----------------------------------------------
+# # 4.	Are we sure about the ITN costs on page 10? I believe the budget says 1$ and the program plans for $2.50, but the graphs are between 3.50 and 5.
+# check = data[product_category == 'bednet_irs', ]
+# check[pack_cost_usd != unit_cost_usd]
+# check[, .(min = min(unit_cost_usd), max = max(unit_cost_usd)), by = 'product_name_en']
+# # 6.	How often are there two different reference prices for the same commodity at the same time?
+# check = data[, .(num_of_irps = length(unique(po_international_reference_price))), by = .(product_name_en, description, purchase_order_date)]
+# # ----------------------------------------------
