076659f67f93dafe264168772f5df93aa5ad34bd
Emily Linebarger <eklinebarg@gmail.com>
2019-08-15 09:31:10 -0700

    Setting up model to run just GLM for gtm_tb_first_half3 as a test, with 2nd jitter scheme

--- impact_evaluation/gtm/set_up_r.r
+++ impact_evaluation/gtm/set_up_r.r
@@ -38,10 +38,13 @@ library(splitstackshape)
 # ------------------
 
 #----------------------------------
+#Set global variables - pulling to top so easier to read EL 8/15/19
 # Current model versions
 
-modelVersion1 = 'gtm_tb_first_half4'
+modelVersion1 = 'gtm_tb_first_half3'
 modelVersion2 = 'gtm_tb_sec_half2'
+
+START_YEAR = 1990 #If available, what's the earliest year you should model data from? 
 
 #-----------------------------------
 
