c48f5deb07c871c262beff96bd446909d8c2df55
Emily Linebarger <eklinebarg@gmail.com>
2019-08-20 16:38:51 -0700

    Making second half visualizations more like first half.

--- impact_evaluation/gtm/4d_explore_second_half_data.r
+++ impact_evaluation/gtm/4d_explore_second_half_data.r
@@ -15,8 +15,7 @@ library(GGally)
 load(outputFile4b)
 
 # load model object
-modelVersion = 'gtm_tb_sec_half2'
-source(paste0('./impact_evaluation/gtm/models/', modelVersion, '.r'))
+source(paste0('./impact_evaluation/gtm/models/', modelVersion2, '.r'))
 
 # load "node table" for convenient labels
 nodeTable = fread(nodeTableFile2)
@@ -26,6 +25,21 @@ n = 6
 dpts = sample(data$department, n)
 sample = data[department %in% dpts]
 sample_untr = untransformed[department %in% dpts]
+
+#Grab financial variables only, and activity/output variables only to compare separately
+outp_vars = names(data)[grep("_outp", names(data))]
+outc_vars = names(data)[grep("_outc", names(data))]
+
+outp_vars = c('department', 'date', outp_vars)
+outc_vars = c('department', 'date', outc_vars)
+
+outp_data = data[, outp_vars, with=FALSE]
+outc_data = data[, outc_vars, with=FALSE]
+
+outp_data = melt(outp_data, id.vars=c('department', 'date'))
+outc_data = melt(outc_data, id.vars = c('department', 'date'))
+# --------------------------------------------------------------------
+
 # --------------------------------------------------------------------
 
 
@@ -54,10 +68,14 @@ long[is.na(label), label:=variable]
 # ----------------------------------------------
 
 #----------------------------------------------------
-# Limit graph data to 2012, the year the activities/outputs data starts 
-sample = sample[date>=2012]
-sample_untr = sample_untr[date>=2012]
-long = long[date>=2012]
+# Limit graph data to START_YEAR, the year the activities/outputs data starts 
+# and limit variables to only ones currently used by model 
+sample = sample[date>=START_YEAR, c(modelVars, 'department', 'date'), with=F]
+sample_untr = sample_untr[date>=START_YEAR, c(modelVars, 'department', 'date'), with=F]
+long = long[date>=START_YEAR & variable%in%modelVars]
+outc_data = outc_data[variable%in%modelVars]
+outp_data = outp_data[variable%in%modelVars]
+data = data[, c(modelVars, 'department', 'date'), with=F]
 
 # -------------------------------------------------------------------
 # Make histograms
@@ -122,23 +140,72 @@ corPlots = lapply(seq(length(varGroups)), function(g) {
 })
 # ----------------------------------------------
 
+#----------------------------------------------
+# make department time-series plots 
+outp_plot = function(d){
+  ggplot(outp_data[date>=2010 & department==d], aes(y=value, x=date)) + 
+    geom_line() + 
+    facet_wrap(~variable, scales='free') + 
+    labs(title=paste('Time series of all output vars for department ', d), y='Value', x='Date') + 
+    theme_bw()
+}
+
+outc_plot = function(d){
+  ggplot(outc_data[date>=2010 & department==d], aes(y=value, x=date)) + 
+    geom_line() + 
+    facet_wrap(~variable, scales='free') + 
+    labs(title=paste('Time series of all outcome vars for department ', d), y='Value', x='Date') + 
+    theme_bw()
+}
 
 # --------------------------------
 # Save file
 print(paste('Saving:', outputFile4d)) 
 pdf(outputFile4d, height=5.5, width=9)
+for(d in unique(outp_data$department)){
+  print(outp_plot(d))
+}
+for(d in unique(outc_data$department)){
+  print(outc_plot(d))
+}
 for(i in seq(length(tsPlots))) { 
   print(tsPlots[[i]])
 }
 for(i in seq(length(corPlots))) {
   print(corPlots[[i]])
 }
-for(i in seq(length(histograms))) { 
-  print(histograms[[i]])
-  print(histograms_untr[[i]])
-}
+# for(i in seq(length(histograms))) { 
+#   print(histograms[[i]])
+#   print(histograms_untr[[i]])
+# }
 dev.off()
 
 # save a time-stamped version for reproducibility
 archive(outputFile4d)
 # --------------------------------
+
+
+#Full time series of all municipalities for each column. 
+# Once normal, and once log-transformed. 
+cols = names(data)[!names(data)%in%c('department', 'date', 'year', 'min')]
+cols = cols[!grepl('total', cols)]
+pdf(paste0(visIeDir, 'log_transform_second_half.pdf'), height=5.5, width=9)
+for (c in cols){
+  print(c)
+  #Normal graph 
+  hist1 = hist(data[, get(c)], main=paste0(c, " Untransformed"))
+  
+  #Log transformed
+  p = quantile(data[get(c)!=0], .01, na.rm=T)
+  if (is.na(p) | is.infinite(p)){ p=0}
+  data[, paste0(c, "_log"):=log(get(c)+p)]
+  data[is.na(get(paste0(c, "_log"))) | is.infinite(get(paste0(c, "_log"))), (paste0(c, "_log")):=0] #Adding a step to turn NAs into 0. 
+  hist2 = hist(data[, get(paste0(c, "_log"))], main=paste0(c, " Log-transformed"))
+  
+  #Graph 
+  par(mfrow=c(1,2)) 
+  print(hist1)
+  print(hist2)
+}
+dev.off()
+
