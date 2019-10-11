01d886560d6a386a8ed2c5b4027d0b94ff113b70
Audrey Batzel <abatzel@uw.edu>
2019-10-11 16:55:25 -0700

    add the extracted malaria estimates; combine tb and malaria into one data set

--- impact_evaluation/mortality/1_data_prep.R
+++ impact_evaluation/mortality/1_data_prep.R
@@ -9,29 +9,33 @@ library(ggplot2)
 j = ifelse(Sys.info()[1]=='Windows','J:','/home/j')
 dir = paste0(j, "/Project/Evaluation/GF/impact_evaluation/mortality/")
 
-inFile = paste0(dir, "gbd_estimates_tb_pce_countries.csv")
-outFile = paste0(dir, "prepped_data/tb_pce_data.RDS")
+set_disease = 'malaria' # change to 'tb' to do TB
+
+inFile = ifelse(set_disease == 'tb', paste0(dir, "gbd_estimates_tb_pce_countries.csv"), paste0(dir, "gbd_estimates_malaria_pce_countries.csv"))
+outFile =  ifelse(set_disease == 'tb', paste0(dir, "prepped_data/tb_pce_data.RDS"), paste0(dir, "prepped_data/malaria_pce_data.RDS"))
 
 # read data
-TB = fread(inFile)
+dt = fread(inFile)
 
 # subset data to include:
-if ( length(unique(TB$metric_name))!= 1 & unique(TB$metric_name) != "Rate" ) TB = TB[metric_name=="Rate"]
-if ( length(unique(TB$age_group_name))!= 1 & unique(TB$age_group_name) != "Age-standardized" ) TB = TB[age_group_name=="Age-standardized"]
+if ( length(unique(dt$metric_name))!= 1 & unique(dt$metric_name) != "Rate" ) dt = dt[metric_name=="Rate"]
+if ( length(unique(dt$age_group_name))!= 1 & unique(dt$age_group_name) != "Age-standardized" ) dt = dt[age_group_name=="Age-standardized"]
 
 # keep columns of interest
-TB = TB[,.(year_id, location_name, cause_name, measure_name, val)]
-names(TB) = c('year', 'country', 'disease', 'measure', 'val')
+dt = dt[,.(year_id, location_name, cause_name, measure_name, val)]
+names(dt) = c('year', 'country', 'disease', 'measure', 'val')
 
 # sum across TB types
-TB = TB[, .(val = sum(val)), by = .(year, country, measure)]
-TB[, disease := "tb_all_types"]
+if (set_disease == 'tb'){
+  dt = dt[, .(val = sum(val)), by = .(year, country, measure)]
+  dt[, disease := "tb_all_types"]
+}
 
 # reshape the data to be wide
-data = dcast(TB,year + country + disease ~ measure, value.var = "val")
+data = dcast(dt, year + country + disease ~ measure, value.var = "val")
 
-# remove 2018 because right now 2018 not avail
-data = data[year != 2018]
+# remove 2018 because right now 2018 not avail (it's just NA)
+data = data[!is.na(Deaths)]
 
 #  compute MI ration
 data[, mi_ratio := Deaths/Incidence]
@@ -40,3 +44,19 @@ data[, mi_ratio := Deaths/Incidence]
 saveRDS(data, outFile)
 
 
+# combine both data sources after prep:
+dt1 =  readRDS(paste0(dir, "prepped_data/tb_pce_data.RDS"))
+dt2 =  readRDS(paste0(dir, "prepped_data/malaria_pce_data.RDS"))
+dt = rbindlist(list(dt1, dt2), use.names = TRUE)
+
+# change country names to standard format
+dt[grepl(country, pattern = 'guatemala', ignore.case = TRUE), country := 'gtm']
+dt[grepl(country, pattern = 'congo', ignore.case = TRUE), country := 'cod']
+dt[grepl(country, pattern = 'senegal', ignore.case = TRUE), country := 'sen']
+dt[grepl(country, pattern = 'uganda', ignore.case = TRUE), country := 'uga']
+
+# change disease names
+dt[disease == 'tb_all_types', disease := 'tb']
+dt[, disease:=tolower(disease)]
+
+saveRDS(dt, paste0(dir, 'prepped_data/tb_malaria_pce_countries_data.RDS'))
