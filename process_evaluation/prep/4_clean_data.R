16916dd37eed90296f53344b21deb3b6ec75ec35
Francisco Rios Casas <frc2@uw.edu>
2019-10-17 13:02:32 -0700

    Sen TB Model and PUDR updates

--- process_evaluation/prep/4_clean_data.R
+++ process_evaluation/prep/4_clean_data.R
@@ -1,8 +1,8 @@
 # --------------------------------------
 # AUTHOR: Francisco Rios Casas
 # PURPOSE: script that cleans PUDR Indicators Performance Framework Data for analyses
-# DATE: Created 11Sept2019. 
-# NOTE: this assumes that Guatemala-TB data is kept in a different file than the other indicators for the rest of the countries which are combined
+# DATE: Created 11Sept2019. Updated 14Oct2019.
+# NOTE: this version assumes that all country data is stored in same file
 # --------------------------------------
 
 # set up
@@ -11,27 +11,60 @@ library(data.table)
 # read in COD, SEN, UGA data
 DT <- readRDS("J:/Project/Evaluation/GF/process_evaluation/pudr_indicator_extraction/prepped_data/all_prepped_data.rds")
 
-# read in GTM data
-DT2 <- readRDS("J:/Project/Evaluation/GF/process_evaluation/pudr_indicator_extraction/prepped_data/gtm_1A.rds")
-
-# for COD, SEN, UGA code----------------------------------------
+# for COD, SEN, UGA, GTM code----------------------------------------
 # clean typos in the formatting (missing ampersands, missing colons, and missing Indicator Codes)
-DT$indicator[which(DT$indicator=='M&amp;E-2: Proportion de rapports reçus des formations sanitaires  par rapport aux rapports attendus attendus au cours de la période de rapportage')] <- "M&E-2: Proportion de rapports reçus des formations sanitaires  par rapport aux rapports attendus attendus au cours de la période de rapportage"
-DT$indicator[which(DT$indicator=="M&amp;E-1: Pourcentage d'entités déclarantes présentant leurs rapports dans les délais selon les directives nationales")] <- "M&E-1: Pourcentage d'entités déclarantes présentant leurs rapports dans les délais selon les directives nationales"
+DT$indicator[which(DT$indicator=='M&amp;E-2: Proportion de rapports reÃ§us des formations sanitaires  par rapport aux rapports attendus attendus au cours de la pÃ©riode de rapportage')] <- "M&E-2: Proportion de rapports reÃ§us des formations sanitaires  par rapport aux rapports attendus attendus au cours de la pÃ©riode de rapportage"
+DT$indicator[which(DT$indicator=="M&amp;E-1: Pourcentage d'entitÃ©s dÃ©clarantes prÃ©sentant leurs rapports dans les dÃ©lais selon les directives nationales")] <- "M&E-1: Pourcentage d'entitÃ©s dÃ©clarantes prÃ©sentant leurs rapports dans les dÃ©lais selon les directives nationales"
 DT$indicator[which(DT$indicator=='M&amp;E-1: Percentage of HMIS or other routine reporting units submitting timely reports according to national guidelines')] <- "M&E-1: Percentage of HMIS or other routine reporting units submitting timely reports according to national guidelines"
 DT$indicator[which(DT$indicator=='M&amp;E-2: Proportion of facility reports received over the reports expected during the reporting period')] <- "ME-2: Proportion of facility reports received over the reports expected during the reporting period"
-DT$indicator[which(DT$indicator=='Nombre et pourcentage de structures de santé (Centres de Santé intégrés) qui rapporte une rupture de stock des tests de diagnostic rapide (TDR) malaria')] <- "RDT SO: Nombre et pourcentage de structures de santé (Centres de Santé intégrés) qui rapporte une rupture de stock des tests de diagnostic rapide (TDR) malaria"
+DT$indicator[which(DT$indicator=='Nombre et pourcentage de structures de santÃ© (Centres de SantÃ© intÃ©grÃ©s) qui rapporte une rupture de stock des tests de diagnostic rapide (TDR) malaria')] <- "RDT SO: Nombre et pourcentage de structures de santÃ© (Centres de SantÃ© intÃ©grÃ©s) qui rapporte une rupture de stock des tests de diagnostic rapide (TDR) malaria"
 DT$indicator[which(DT$indicator=='Number and percentage of health facilities (Integrated health facilities) reporting stock out of malaria rapid diagnostic test (RDT)')] <- "RDT SO: Number and percentage of health facilities (Integrated health facilities) reporting stock out of malaria rapid diagnostic test (RDT)"
 DT$indicator[which(DT$indicator=='GP other-2 Nombre de SVS ayant recu le kit PEP dans les 72h')] <- "GP other-2: Nombre de SVS ayant recu le kit PEP dans les 72h"
+DT$indicator[which(DT$indicator=="M&amp;E-2: ProporciÃ³n de informes de centros de salud recibidos entre el total de informes previstos durante el perÃ­odo de reporte")] <- "M&E-2: ProporciÃ³n de informes de centros de salud recibidos entre el total de informes previstos durante el perÃ­odo de reporte"
 
-# remove percent signs
+# remove rows which are not necessary
+DT <- DT[indicator!="[Impact Indicator Name]"]
+DT <- DT[indicator!="[Outcome Indicator Name]"]
+DT <- DT[indicator!="0"]
+
+# clean special characters, blanks, and NAs
+DT$baseline_value[which(DT$baseline_value=="N/A")] <- NA
+DT$baseline_value[which(DT$baseline_value=="ND")] <- NA
+DT$baseline_value <- gsub("/100,000","",DT$baseline_value)
+DT$baseline_value[which(DT$baseline_value=="16,121,172")] <- "16121172"
+DT$baseline_value <- gsub(",",".",DT$baseline_value)
 DT$baseline_value <- gsub("%","",DT$baseline_value)
+DT$baseline_value <- gsub("â€°","",DT$baseline_value)
+DT$baseline_value <- gsub("???","",DT$baseline_value)
+
+# pr 
+DT$pr_result_n[which(DT$pr_result_n=="ND")]<- NA
+DT$pr_result_d[which(DT$pr_result_d=="ND")]<- NA
+
+DT$`pr_result_%`[which(DT$`pr_result_%`=="ND")]<- NA
+DT$pr_result_achievement_ratio[which(DT$pr_result_achievement_ratio==" ")] <- NA
+
+# lfa
+DT$lfa_result_n[which(DT$lfa_result_n=="ND")]<- NA
+DT$lfa_result_d[which(DT$lfa_result_d=="ND")]<- NA
+
+DT$`lfa_result_%`[which(DT$`lfa_result_%`==" ")] <- NA
+DT$`lfa_result_%` <- gsub(",",".",DT$`lfa_result_%`)
+DT$`lfa_result_%` <- gsub("%","",DT$`lfa_result_%`)
+
+# gf
+DT$gf_result_n[which(DT$gf_result_n=="qu ")] <- NA
+DT$gf_result_n[which(DT$gf_result_n=="i")] <- NA
+
+DT$gf_result_achievement_ratio[which(DT$gf_result_achievement_ratio==" ")] <- NA
 
 # fix variables' "class"
 DT$baseline_value <- as.numeric(DT$baseline_value)
 
 DT$lfa_result_achievement_ratio <- as.numeric(DT$lfa_result_achievement_ratio)
-
+DT$pr_result_achievement_ratio <- as.numeric(DT$pr_result_achievement_ratio)
+DT$gf_result_achievement_ratio <- as.numeric(DT$gf_result_achievement_ratio)
+                                             
 DT$target_n <- as.numeric(DT$target_n)
 DT$target_d <- as.numeric(DT$target_d)
 DT$`target_%` <- as.numeric(DT$`target_%`)
@@ -48,57 +81,49 @@ DT$gf_result_n <- as.numeric(DT$gf_result_n)
 DT$gf_result_d <- as.numeric(DT$gf_result_d)
 DT$`gf_result_%` <- as.numeric(DT$`gf_result_%`)
 
-# fix date variables
-# might be done in other stage?
+# gather any_achievement_ratio
+DT$any_achievement_ratio <- NA
+DT$any_achievement_ratio <-ifelse(is.na(DT$gf_result_achievement_ratio),
+                                  ifelse(is.na(DT$lfa_result_achievement_ratio), 
+                                         ifelse(is.na(DT$pr_result_achievement_ratio), NA, 
+                                                DT$pr_result_achievement_ratio), 
+                                         DT$lfa_result_achievement_ratio),
+                                  DT$gf_result_achievement_ratio)
+
+# verify achievement_ratios
+DT$ihme_target_n <- NA
+DT$ihme_target_n <- ifelse(is.na(DT$`target_%`), DT$target_n, DT$`target_%`)
+
+DT$ihme_result_n <- NA
+DT$ihme_result_n <- ifelse(is.na(DT$`lfa_result_%`), DT$lfa_result_n, DT$`lfa_result_%`)
+
+# calculate ihme_results_achievement_ratio
+DT$ihme_result_achievement_ratio <-NA
+DT$ihme_target_n <- as.numeric(DT$ihme_target_n)
+DT$ihme_result_n <- as.numeric(DT$ihme_result_n)
+DT$ihme_result_achievement_ratio <- DT$ihme_result_n/DT$ihme_target_n
 
 # create variable with indicator code (makes cross--country comparisons easier) can merge short description using this code later
-DT = DT[, c("indicator_code", "indicator_description", "indicator_misc") := tstrsplit(indicator, ": ", fixed=TRUE)]
-DT = DT[,c("indicator_description", "indicator_misc"):=NULL]
+DT = DT[, c("indicator_code", "indicator_description", "indicator_misc", "indicator_misc2") := tstrsplit(indicator, ": ", fixed=TRUE)]
+DT = DT[,c("indicator_description", "indicator_misc", "indicator_misc2"):=NULL]
 
+# read and merge codebook to standardize names
+codebook <- fread("C:/Users/frc2/Documents/gf/special_assessments/synthesis/2019_multicountry_analyses/indicators_codebook.csv")
+DT1 <- merge(DT, codebook, by="indicator_code", all.x = TRUE)
 
-# for GTM - TB --------------------------------
-# remove rows that are not necessary
-DT2 <- DT2[indicator!="[Impact Indicator Name]"]
-DT2 <- DT2[indicator!="[Outcome Indicator Name]"]
+# clean and standardize data source names across PUDRs
+# create spreadsheet of data source names to standardize
+#DTtemp <- data.table(unique(DT1$baseline_source))
+#DTtemp <- rbind(DTtemp, data.table(unique(DT1$pr_result_source)))
+#DTtemp <- rbind(DTtemp, data.table(unique(DT1$lfa_result_source)))
+#write.csv(DTtemp, "C:/Users/frc2/Documents/gf/special_assessments/synthesis/2019_multicountry_analyses/data_source_codebook_original.csv")
 
-# remove duplicated column titled "pr_result_verification_method" 
-DT2 <- DT2[,unique(names(DT)),with=FALSE]
+# read and merge codebook to standardize data source names
+# pending
 
-# remove unnecessary rows in the Guatemala data which are mostly blank
-DT2 <- DT2[indicator!="[Impact Indicator Name]"]
-DT2 <- DT2[indicator!="[Outcome Indicator Name]"]
-
-# remove percent signs
-DT2$baseline_value <- gsub("%","",DT2$baseline_value)
-
-# fix variables' "class"
-DT2$baseline_value <- as.numeric(DT2$baseline_value)
-
-DT2$lfa_result_achievement_ratio <- as.numeric(DT2$lfa_result_achievement_ratio)
-
-DT2$target_n <- as.numeric(DT2$target_n)
-DT2$target_d <- as.numeric(DT2$target_d)
-DT2$`target_%` <- as.numeric(DT2$`target_%`)
-
-DT2$pr_result_n <- as.numeric(DT2$pr_result_n)
-DT2$pr_result_d <- as.numeric(DT2$pr_result_d)
-DT2$`pr_result_%` <- as.numeric(DT2$`pr_result_%`)
-
-DT2$lfa_result_n <- as.numeric(DT2$lfa_result_n)
-DT2$lfa_result_d <- as.numeric(DT2$lfa_result_d)
-DT2$`lfa_result_%` <- as.numeric(DT2$`lfa_result_%`)
-
-DT2$gf_result_n <- as.numeric(DT2$gf_result_n)
-DT2$gf_result_d <- as.numeric(DT2$gf_result_d)
-DT2$`gf_result_%` <- as.numeric(DT2$`gf_result_%`)
-
-# fix date variables
-# might be done in other stage?
-
-# create variable with indicator code (makes cross--country comparisons easier) can merge short description using this code later
-DT2 = DT2[, c("indicator_code", "indicator_description", "indicator_misc") := tstrsplit(indicator, ": ", fixed=TRUE)]
-DT2 = DT2[,c("indicator_description", "indicator_misc"):=NULL]
+# reverse country codes
 
 #------------------------------------------------------
 # SAVE FINAL DATA
-# --------------------------------------
+# -----------------------------------------------------
+saveRDS(DT1, "C:/Users/frc2/Documents/Data/cleaned_pfi.RDS")
