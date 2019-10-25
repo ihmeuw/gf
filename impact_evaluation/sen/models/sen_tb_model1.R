ed7dd1abdc4b1746ac8de6e8ec8b88ec171294d5
Francisco Rios Casas <frc2@uw.edu>
2019-10-25 16:35:42 -0700

    changes to model to add new variable

--- impact_evaluation/sen/models/sen_tb_model1.R
+++ impact_evaluation/sen/models/sen_tb_model1.R
@@ -18,8 +18,9 @@ model = '
     com_enf_ref_cumulative ~ com_cause_cumulative + com_vad_touss_cumulative
     com_nom_touss_cumulative ~ com_cause_cumulative  + com_vad_touss_cumulative
     tb_cas_id_cumulative ~ com_mobsoc_cumulative + com_cause_cumulative + com_vad_touss_cumulative + com_radio_cumulative + perf_lab + lag_other_dah_T1_1_cumulative + lag_exp_T1_1_cumulative + com_nom_touss_cumulative
-    tot_genexpert_cumulative ~ com_nom_touss_cumulative + lag_other_dah_T1_1_cumulative + lag_exp_T1_1_cumulative + lag_other_dah_T3_cumulative + lag_exp_T3_cumulative + perf_lab
-    dx_count_cumulative ~ lag_exp_T3_cumulative + lag_other_dah_T3_cumulative + tot_genexpert_cumulative
+    tot_genexpert_cumulative ~ com_nom_touss_cumulative + lag_other_dah_T1_1_cumulative + lag_exp_T1_1_cumulative + patients_prop_genexpert_cumulative + perf_lab
+    dx_count_cumulative ~ patients_prop_genexpert_cumulative + tot_genexpert_cumulative
+    patients_prop_genexpert_cumulative ~ lag_other_dah_T3_cumulative + lag_exp_T3_cumulative
     
     # linkage 3 regressions
     
