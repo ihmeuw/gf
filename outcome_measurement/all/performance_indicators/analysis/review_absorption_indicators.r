f04653c46a5bac9515ec4570ebda2d17069adfce
Francisco Rios Casas <frc2@uw.edu>
2019-11-06 09:17:00 -0800

    slight changes to visualizations

--- outcome_measurement/all/performance_indicators/analysis/review_absorption_indicators.r
+++ outcome_measurement/all/performance_indicators/analysis/review_absorption_indicators.r
@@ -31,16 +31,25 @@ dt[disease=="malaria", disease:="Malaria"]
 dt[loc_name=="cod", loc_name:="DRC"]
 dt[, loc_name:=toupper(loc_name)]
 
+
 #-----------------------------------
 # PLOTS 
 #-----------------------------------
-pdf("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/visualizations/absorption_indicators_comparison.pdf", width=10, height=8)
+pdf("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/visualizations/absorption_indicators_comparison3.pdf", width=10, height=8)
 #First, run a scatter plot of achivement ratio vs. absorption percentage. 
-ggplot(dt[start_date>="2018-01-01"], aes(x=ihme_result_achievement_ratio, y=absorption, color=disease)) + 
+ggplot(dt[start_date>="2018-01-01"], aes(x=absorption, y=ihme_result_achievement_ratio)) + 
   geom_point() + 
   theme_bw(base_size=18) + 
   facet_wrap(~start_date) + 
-  labs(title="Absorption vs. Achievement Ratio", x="Achievement Ratio", y="Absorption (%)", color="")
+  labs(title="Absorption vs. Achievement Ratio", x="Absorption (%)", y="Achievement Ratio", color="")+
+  geom_smooth(method = "lm")
+
+ggplot(dt[start_date>="2018-01-01"], aes(x=absorption, y=ihme_result_achievement_ratio, color=disease)) + 
+  geom_point() + 
+  theme_bw(base_size=18) + 
+  facet_wrap(~start_date) + 
+  labs(title="Absorption vs. Achievement Ratio", x="Absorption (%)", y="Achievement Ratio", color="")+
+  geom_smooth(method = "lm", se=FALSE)
 
 #Now, look at how targets have changed over time for the same indicator code. 
 dt[, grant_code:=paste0(grant, grant_period, indicator_code)]
@@ -57,11 +66,11 @@ for (c in c('DRC', 'SEN', 'UGA')){
   for (d in unique(dt$disease)) {
     subset = dt[start_date>="2018-01-01" & disease==d & loc_name==c]
     if (nrow(subset)>0){
-      p = ggplot(subset, aes(x=ihme_result_achievement_ratio, y=absorption)) + 
+      p = ggplot(subset, aes(x=absorption, y=ihme_result_achievement_ratio)) + 
         geom_point() + 
         theme_bw(base_size=18) + 
         facet_wrap(~start_date) + 
-        labs(title=paste0("Absorption vs. Achievement Ratio for ", c, " ", d), x="Achievement Ratio", y="Absorption (%)", color="")
+        labs(title=paste0("Absorption vs. Achievement Ratio for ", c, " ", d), x="Absorption (%)", y="Achievement Ratio", color="")
       print(p) 
     } 
   } 
