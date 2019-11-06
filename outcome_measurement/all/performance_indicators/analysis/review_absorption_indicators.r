aebcae9f42c01172c20a113f7f3f8ed6c1fae513
Emily Linebarger <eklinebarg@gmail.com>
2019-11-05 16:41:11 -0800

    Adding new scripts to aggregate absorption/indicator data and review it, and expanding on activity classifications from Synthesis presentation.

---
+++ outcome_measurement/all/performance_indicators/analysis/review_absorption_indicators.r
@@ -0,0 +1,70 @@
+# ------------------------------------------------------
+# AUTHOR: Emily Linebarger 
+# PURPOSE: Review absorption/indicator data 
+# DATE: Last updated November 2019 
+# ------------------------------------------------------
+
+library(data.table) 
+library(ggplot2) 
+library(scales) 
+
+dt = readRDS("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/cleaned_data/absorption_indicators_combined.rds")
+
+#----------------------------------------------------
+# DATA QUALITY CORRECTIONS 
+#----------------------------------------------------
+#Where absorption is bigger than 200%, cap it. 
+dt[absorption>200, absorption:=200]
+#Where absorption is below 0, replace with 0. 
+dt[absorption<0, absorption:=0]
+# Where achievement ratio is above 2, cap at 2. 
+dt[ihme_result_achievement_ratio>2, ihme_result_achievement_ratio:=2]
+
+dt = dt[!is.na(ihme_result_achievement_ratio) & !is.na(absorption)]
+
+#Fix disease labeling 
+dt[disease=="hiv", disease:="HIV"]
+dt[disease=="tb", disease:="TB"]
+dt[disease=="malaria", disease:="Malaria"]
+
+#Fix country labeling 
+dt[loc_name=="cod", loc_name:="DRC"]
+dt[, loc_name:=toupper(loc_name)]
+
+#-----------------------------------
+# PLOTS 
+#-----------------------------------
+pdf("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/visualizations/absorption_indicators_comparison.pdf", width=10, height=8)
+#First, run a scatter plot of achivement ratio vs. absorption percentage. 
+ggplot(dt[start_date>="2018-01-01"], aes(x=ihme_result_achievement_ratio, y=absorption, color=disease)) + 
+  geom_point() + 
+  theme_bw(base_size=18) + 
+  facet_wrap(~start_date) + 
+  labs(title="Absorption vs. Achievement Ratio", x="Achievement Ratio", y="Absorption (%)", color="")
+
+#Now, look at how targets have changed over time for the same indicator code. 
+dt[, grant_code:=paste0(grant, grant_period, indicator_code)]
+ggplot(dt[start_date>="2018-01-01" & ihme_target_n<100], aes(x=start_date, y=ihme_target_n, group=grant_code, color=grant_code)) + 
+  geom_point() + 
+  geom_line() + 
+  theme_bw(base_size=18) + 
+  theme(legend.position = "none") + 
+  labs(title="Change in targets over time", x="Date", y="Target", caption="*Each color represents a given grant and indicator code")
+#Setting target less than 100 based on the median, which is 89. 
+
+#Now, run for each disease, with panels representing country. 
+for (c in c('DRC', 'SEN', 'UGA')){
+  for (d in unique(dt$disease)) {
+    subset = dt[start_date>="2018-01-01" & disease==d & loc_name==c]
+    if (nrow(subset)>0){
+      p = ggplot(subset, aes(x=ihme_result_achievement_ratio, y=absorption)) + 
+        geom_point() + 
+        theme_bw(base_size=18) + 
+        facet_wrap(~start_date) + 
+        labs(title=paste0("Absorption vs. Achievement Ratio for ", c, " ", d), x="Achievement Ratio", y="Absorption (%)", color="")
+      print(p) 
+    } 
+  } 
+} 
+
+dev.off() 
---
+++ outcome_measurement/all/performance_indicators/prep/5_aggregate_interventions_indicators.r
@@ -0,0 +1,39 @@
+# ------------------------------------------------------
+# AUTHOR: Emily Linebarger 
+# PURPOSE: Combine prepped absorption and performance indicator data 
+# DATE: Last updated November 2019 
+# ------------------------------------------------------
+
+# Read in prepped absorption data 
+absorption = readRDS(paste0(box, "tableau_data/absorption.rds"))
+
+# Read in prepped indicator data 
+indicators = readRDS(paste0(dir, 'pudr_indicator_extraction/cleaned_data/kpi_data_for_analyses.rds'))
+
+#Read in map 
+map = data.table(read_excel(paste0(dir, "Indicators to Interventions Map.xlsx")))
+map = map[weight==1] #Limit map for now
+
+#Subset to the rows you need- just want to map code onto performance indicators data. 
+map = unique(map[, .(indicator_code, code)])
+#-----------------------------------------------
+# Merge data together - first merge map onto indicators. 
+unique(map[!indicator_code%in%indicators$indicator_code, .(indicator_code)]) #Make sure all the codes in the map exist in the data (this would be a typo otherwise) 
+indicators = merge(indicators, map, by='indicator_code') #Only want instances that exist in both, so don't specify an 'all' condition.
+indicators = indicators[, .(loc_name, grant, grant_period, start_date_programmatic, disease, indicator_code, indicator, baseline_value, 
+                            ihme_target_n, ihme_result_n, ihme_result_achievement_ratio, code)]
+setnames(indicators, 'start_date_programmatic', 'start_date')
+
+#Then, limit absorption to only the codes that are kept after this merge, and merge on as well. 
+#Want to do this merge by grant, grant period, semester, start date, module, and intervention. 
+absorption = absorption[code%in%indicators$code, .(budget=sum(budget, na.rm=T), expenditure=sum(expenditure, na.rm=T)), 
+                        by=c('loc_name' ,'grant', 'grant_period', 'start_date', 'disease', 'code', 'gf_module', 'gf_intervention')]
+absorption[, absorption:=round((expenditure/budget)*100, 1)]
+dt = merge(indicators, absorption, by=c('loc_name', 'grant', 'grant_period', 'start_date', 'disease', 'code'), all=T)
+
+#One known issue here is that the start date/end date of the financial data don't always correspond with the start/
+#end date of the programmatic data for the same PUDR. 
+
+#Save data, and archive a version. 
+saveRDS(dt, paste0(dir, "pudr_indicator_extraction/cleaned_data/absorption_indicators_combined.rds"))
+saveRDS(dt, paste0(dir, "pudr_indicator_extraction/cleaned_data/archive/absorption_indicators_combined_", Sys.Date(), ".rds"))
