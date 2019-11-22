3d3aa2456e0677bc12842c03ed39d43cfabe63fc
Emily Linebarger <eklinebarg@gmail.com>
2019-11-22 12:13:35 -0800

    New number plugs and graphs for reports

--- resource_tracking/analysis/deliverables/_UGA 2019 report analyses/report_graphs.r
+++ resource_tracking/analysis/deliverables/_UGA 2019 report analyses/report_graphs.r
@@ -8,6 +8,7 @@ library(gridExtra)
 source("C:/Users/elineb/Documents/gf/resource_tracking/analysis/graphing_functions.r")
 save_loc = "J:/Project/Evaluation/GF/resource_tracking/visualizations/deliverables/_UGA 2019 annual report/"
 
+# Malaria
 mofped_cumulative = get_cumulative_absorption(byVars = c('abbrev_mod'), grantSubset="UGA-M-MoFPED", currency="USD")
 p1 = budget_exp_bar(mofped_cumulative, trimAbsorption=TRUE, yScaleMax=200, angleText=TRUE, 
                     altTitle="Absorption by module\nfor UGA-M-MoFPED", altSubtitle="January 2018-June 2019")
@@ -28,3 +29,5 @@ p2 = budget_exp_bar(taso_cumulative, trimAbsorption=TRUE, yScaleMax=200, angleTe
                     altTitle="Absorption by module\nfor UGA-C-TASO", altSubtitle="January 2018-June 2019")
 
 ggsave(paste0(save_loc, "hiv_absorption.png"), grid.arrange(p1, p2, ncol=2, nrow=1), height=8, width=15)
+
+# TB 
--- resource_tracking/analysis/deliverables/_UGA 2019 report analyses/report_number_plug.r
+++ resource_tracking/analysis/deliverables/_UGA 2019 report analyses/report_number_plug.r
@@ -1,6 +1,7 @@
 # Emily Linebarger 
 # Calculate numbers to plug into UGA report 
 library(data.table)
+library(scales) 
 
 budgets = readRDS("C:/Users/elineb/Box Sync/Global Fund Files/UGA/prepped_data/final_budgets.rds")
 revisions = readRDS("C:/Users/elineb/Box Sync/Global Fund Files/UGA/prepped_data/budget_revisions.rds")
@@ -8,6 +9,9 @@ revisions = readRDS("C:/Users/elineb/Box Sync/Global Fund Files/UGA/prepped_data
 all_files = readRDS("C:/Users/elineb/Box Sync/Global Fund Files/UGA/prepped_data/budget_pudr_iterations.rds")
 absorption = readRDS("C:/Users/elineb/Box Sync/Global Fund Files/UGA/prepped_data/absorption_uga.rds")
 
+#-------------
+# HIV
+#-------------
 #How much of the UGA-H-MoFPED grant went to procuring ARVs? 
 mofped_hiv= all_files[file_name=="UGA-H-MoFPED_IL2_SB2_Optimization_7March19.xlsx"]
 stopifnot(length(unique(mofped_hiv$file_name))==1)
@@ -26,6 +30,12 @@ c_taso = all_files[file_name=="UGA-C-TASO_IL1_Catalytic Funding_DetailedBudget_I
 taso1 = c_taso[, .(budget=sum(budget, na.rm=T)), by='gf_module']
 taso1[, total:=sum(budget)]
 taso1[, mod_percent:=round((budget/total)*100, 1)]
+
+# How much of the H-MoFPED budget is devoted to each module? 
+h_mofped = all_files[file_name=="UGA-H-MoFPED_IL2_SB2_Optimization_7March19.xlsx"]
+h_mofped = h_mofped[, .(budget=sum(budget, na.rm=TRUE)), by='gf_module']
+h_mofped[, total:=sum(budget)]
+h_mofped[, mod_percent:=round((budget/total)*100, 1)]
 
 # Calculate overall grant-level absorption percentage, using cumulative 18-month data. 
 mofped_absorption = absorption[grant=="UGA-H-MoFPED" & grant_period=="2018-2020" & semester=="Semester 3", 
@@ -48,3 +58,27 @@ c_taso_absorption = absorption[grant=="UGA-C-TASO" & grant_period=="2018-2020" &
                                .(budget=sum(cumulative_budget, na.rm=T), expenditure=sum(cumulative_expenditure, na.rm=T)), 
                                by='gf_module']
 c_taso_absorption[, absorption:=round((expenditure/budget)*100, 1)]
+
+#-------------
+# TB
+#-------------
+# t_mofped = all_files[file_name=="Budget_UGA-T-MoFPED_DB_ IMPP4_IL1_May2019.xlsx"] # If you want to look at most recent revision
+t_mofped = budgets[grant=="UGA-T-MoFPED" & grant_period=="2018-2020"]
+t_mofped = t_mofped[, .(budget=sum(budget, na.rm=TRUE)), by='gf_module']
+t_mofped[, total:=sum(budget)]
+t_mofped[, mod_percent:=round((budget/total)*100, 1)]
+
+c_taso_tb = all_files[file_name=="UGA-C-TASO_IL1_Catalytic Funding_DetailedBudget_IMPP2_26April18.xlsx" & (disease=="tb"|gf_module=="TB/HIV")]
+c_taso_tb = c_taso_tb[, .(budget=sum(budget, na.rm=TRUE)), by='gf_module']
+c_taso_tb[, total:=sum(budget)]
+c_taso_tb[, mod_percent:=round((budget/total)*100, 1)]
+#-------------
+# MALARIA
+#-------------
+
+#Review cumulative absorption by module 
+cumulative_absorption = absorption[grant_period=="2018-2020" & semester=="Semester 3", .(c_budget=sum(cumulative_budget, na.rm=T), c_expenditure=sum(cumulative_expenditure, na.rm=T)), 
+                                   by=c('grant', 'gf_module')]
+cumulative_absorption[, c_absorption:=round((c_expenditure/c_budget)*100, 1)]
+cumulative_absorption[, c_expenditure:=dollar(c_expenditure)]
+cumulative_absorption[, c_budget:=dollar(c_budget)]
