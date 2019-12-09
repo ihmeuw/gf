ac6020642aceb334f31d3ec87028c1a0221b89e1
Francisco Rios Casas <frc2@uw.edu>
2019-12-09 10:45:03 -0800

    Graphics for reports

--- outcome_measurement/all/performance_indicators/analysis/sen_report_graphs_coverage.R
+++ outcome_measurement/all/performance_indicators/analysis/sen_report_graphs_coverage.R
@@ -1,5 +1,5 @@
 # make visualizations on each of the grant performance indicators
-# for DRC
+# for Sen
 # while creating a summary visual 
 # OCt 28, 2019
 # Francisco
@@ -15,18 +15,15 @@ library(scales)
 data <- readRDS("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/data_for_analysis/kpi_data_for_analyses2.RDS")
 
 # set parameters of data to be analyzed
-country = "cod"
+country = "sen"
 main_indicators = c('coverage_indicators_main')
 
 # subset as appropriate
 DT <- data
 DT = DT[loc_name==country & pudr_sheet %in% main_indicators]
 
-# change to numeric
-DT$lfa_result_achievement_ratio <- as.numeric(DT$lfa_result_achievement_ratio)
-
 # flip coding on reverse indicators
-# DT$achievement_ratio_final <- ifelse(DT$reverse_indicator_final=="yes", 1/DT$lfa_result_achievement_ratio, DT$lfa_result_achievement_ratio)
+DT$achievement_ratio_final <- ifelse(DT$reverse_indicator_final=="yes", 1/DT$ihme_result_achievement_ratio, DT$ihme_result_achievement_ratio)
 
 # recode semester period for each variable
 DT$semester <- NA
@@ -35,17 +32,14 @@ DT$semester[which(DT$start_date_programmatic=="2018-07-01" & DT$end_date_program
 DT$semester[which(DT$start_date_programmatic=="2019-01-01" & DT$end_date_programmatic=="2019-06-30")] <- 3
 
 # subset data
-dt_subset <- DT[,.(grant, indicator_code, semester, lfa_result_achievement_ratio)]
+dt_subset <- DT[,.(grant, indicator_code, semester, ihme_result_achievement_ratio, achievement_ratio_final)]
 
 # calculate average achievement by grant semester
-dt_subset <- dt_subset[, .(avg_ach_ratio= mean(lfa_result_achievement_ratio, na.rm = TRUE)),
+dt_subset <- dt_subset[, .(avg_ach_ratio= mean(achievement_ratio_final, na.rm = TRUE)),
                        by=c("grant", "semester")]
 
 # reshape wide
 table1 <- dcast(dt_subset, grant ~ semester)
-
-outputFile = "J:\\Project\\Evaluation\\GF\\outcome_measurement\\multi_country\\performance_indicators\\pudr_indicator_extraction\\visualizations\\cod_report_table_coverage_indic.csv"
-write.csv(table1, outputFile)
 
 # make graph of dt_subset
 a <- ggplot(dt_subset, aes(y=avg_ach_ratio, x=semester, group=grant))+
@@ -54,9 +48,11 @@ a <- ggplot(dt_subset, aes(y=avg_ach_ratio, x=semester, group=grant))+
   theme_bw(base_size = 18)+
   labs(y="Average Achievement Ratio",
        x="Semester")+
-  scale_x_continuous(limits = c(1,3), breaks = c(1,2,3))+
-  scale_y_continuous(limits = c(.7, 1.2), breaks=c(0.7, 0.8, 0.9, 1.0, 1.1, 1.2))
+  scale_x_continuous(limits = c(1,3), breaks = c(1,2,3))
 
-pdf("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/visualizations/cod_report_achievement_cov_indic_LFA_verified.pdf", width = 9, height = 6)
+outputFile = "J:\\Project\\Evaluation\\GF\\outcome_measurement\\multi_country\\performance_indicators\\pudr_indicator_extraction\\visualizations\\sen_report_table_coverage_indic.csv"
+write.csv(table1, outputFile)
+
+pdf("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/visualizations/sen_report_achievement_cov_indic.pdf", width = 9, height = 6)
 a
 dev.off()
--- outcome_measurement/all/performance_indicators/analysis/uga_report_graphs_coverage.R
+++ outcome_measurement/all/performance_indicators/analysis/uga_report_graphs_coverage.R
@@ -1,6 +1,7 @@
 # make visualizations on each of the grant performance indicators
-# for DRC
+# for UGA
 # while creating a summary visual 
+# using lfa verified values
 # OCt 28, 2019
 # Francisco
 
@@ -15,18 +16,13 @@ library(scales)
 data <- readRDS("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/data_for_analysis/kpi_data_for_analyses2.RDS")
 
 # set parameters of data to be analyzed
-country = "cod"
+country = "uga"
 main_indicators = c('coverage_indicators_main')
 
 # subset as appropriate
 DT <- data
 DT = DT[loc_name==country & pudr_sheet %in% main_indicators]
 
-# change to numeric
-DT$lfa_result_achievement_ratio <- as.numeric(DT$lfa_result_achievement_ratio)
-
-# flip coding on reverse indicators
-# DT$achievement_ratio_final <- ifelse(DT$reverse_indicator_final=="yes", 1/DT$lfa_result_achievement_ratio, DT$lfa_result_achievement_ratio)
 
 # recode semester period for each variable
 DT$semester <- NA
@@ -34,8 +30,24 @@ DT$semester[which(DT$start_date_programmatic=="2018-01-01" & DT$end_date_program
 DT$semester[which(DT$start_date_programmatic=="2018-07-01" & DT$end_date_programmatic=="2018-12-31")] <- 2
 DT$semester[which(DT$start_date_programmatic=="2019-01-01" & DT$end_date_programmatic=="2019-06-30")] <- 3
 
+# flip coding on reverse indicators
+#DT$achievement_ratio_final <- ifelse(DT$reverse_indicator_final=="yes", 1/DT$ihme_result_achievement_ratio, DT$ihme_result_achievement_ratio)
+
+# for semester 2 and 3 or the UGA-M-TASO grant replace the achievement_ratio_final with lfa_achievement_ratio
+#DT$achievement_ratio_final[which(DT$grant=="UGA-M-TASO" & DT$semester==1)] <- NA
+#DT$achievement_ratio_final[which(DT$grant=="UGA-M-TASO" & DT$semester==1)] <- as.numeric(DT$lfa_result_achievement_ratio)
+
+#DT[grant=="UGA-M-TASO" & semester==1,.(achievement_ratio_final=)]
+
+#DT$achievement_ratio_final[which(DT$grant=="UGA-M-TASO" & DT$semester==2)] <- NA
+#DT$achievement_ratio_final[which(DT$grant=="UGA-M-TASO" & DT$semester==2)] <- DT$lfa_result_achievement_ratio
+
+# make sure str is of type numeric
+DT$lfa_result_achievement_ratio <- as.numeric(DT$lfa_result_achievement_ratio)
+
 # subset data
 dt_subset <- DT[,.(grant, indicator_code, semester, lfa_result_achievement_ratio)]
+
 
 # calculate average achievement by grant semester
 dt_subset <- dt_subset[, .(avg_ach_ratio= mean(lfa_result_achievement_ratio, na.rm = TRUE)),
@@ -44,9 +56,6 @@ dt_subset <- dt_subset[, .(avg_ach_ratio= mean(lfa_result_achievement_ratio, na.
 # reshape wide
 table1 <- dcast(dt_subset, grant ~ semester)
 
-outputFile = "J:\\Project\\Evaluation\\GF\\outcome_measurement\\multi_country\\performance_indicators\\pudr_indicator_extraction\\visualizations\\cod_report_table_coverage_indic.csv"
-write.csv(table1, outputFile)
-
 # make graph of dt_subset
 a <- ggplot(dt_subset, aes(y=avg_ach_ratio, x=semester, group=grant))+
   geom_line(aes(color=grant))+
@@ -54,9 +63,11 @@ a <- ggplot(dt_subset, aes(y=avg_ach_ratio, x=semester, group=grant))+
   theme_bw(base_size = 18)+
   labs(y="Average Achievement Ratio",
        x="Semester")+
-  scale_x_continuous(limits = c(1,3), breaks = c(1,2,3))+
-  scale_y_continuous(limits = c(.7, 1.2), breaks=c(0.7, 0.8, 0.9, 1.0, 1.1, 1.2))
+  scale_x_continuous(limits = c(1,3), breaks = c(1,2,3))
 
-pdf("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/visualizations/cod_report_achievement_cov_indic_LFA_verified.pdf", width = 9, height = 6)
+outputFile = "J:\\Project\\Evaluation\\GF\\outcome_measurement\\multi_country\\performance_indicators\\pudr_indicator_extraction\\visualizations\\uga_report_table_coverage_indic.csv"
+write.csv(table1, outputFile)
+
+pdf("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/visualizations/ugan_report_achievement_cov_indic.pdf", width = 9, height = 6)
 a
 dev.off()
