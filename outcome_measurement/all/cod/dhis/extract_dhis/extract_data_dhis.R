527373d47715d5cf2fa1da27107253a9b6be0730
Audrey Batzel <abatzel@uw.edu>
2019-12-10 16:47:24 -0800

    restructuring dhis code to be functions run by a master script

--- outcome_measurement/all/cod/dhis/extract_dhis/extract_data_dhis.R
+++ outcome_measurement/all/cod/dhis/extract_dhis/extract_data_dhis.R
@@ -1,29 +1,28 @@
-# DHIS2 Extraction for DRC - SNIS
-# Extracts Data from: https://www.snisrdc.com/dhis-web-commons/security/login.action
-# Sources dhis_extracting_functions.R on J Drive for dhisextractr package and extraction functions
-# Website re-reroutes from https://www.snisdrc.com to https://snisdrc.com
-
-#---------------------------
+#----------------------------------
 # Caitlin O'Brien-Carelli
-# 1/15/2018
+# January 2018-December 2019
 
 # Audrey Batzel
-# 2/22/19 - updated to work around problem of some facilities not downloading still even with changed pace
-#---------------------------
+# January 2019-current
+#----------------------------------
 # Extract single data sets by specifying the data set number
 # Data will be merged with the meta data to create complete data sets
 # You must have the source functions and meta data accessible to run 
-#---------------------------
 
-#---------------------------
+# DHIS2 extraction script for DRC - SNIS
+# Extracts Data from: https://www.snisrdc.com/dhis-web-commons/security/login.action
+# Sources dhis_extracting_functions.R on J Drive for dhisextractr package and extraction functions
+# Website re-reroutes from https://www.snisdrc.com to https://snisdrc.com
+#----------------------------------
+
+#----------------------------------
 # To run on the cluster:
 # Copy shell script into a qlogin session to open an IDE
 # request at least 10 slots for extractions 
 
 # sh /ihme/code/jpy_rstudio/jpy_rstudio_qsub_script.sh -i /ihme/singularity-images/rstudio/ihme_rstudio_3501.img -l m_mem_free=50G -l fthread=20 -P proj_pce -q all -t rstudio -l archive=TRUE -l h_rt=72:00:00
-#---------------------------
+#----------------------------------
 # Set up R
-rm(list=ls())
 library(data.table)
 library(jsonlite)
 library(httr)
@@ -33,47 +32,41 @@ library(RCurl)
 library(XML)
 library(plyr)
 library(openxlsx)
-#---------------------------
+#----------------------------------
 # Set the directory to download the data
 # detect if operating on windows or on the cluster 
 root = ifelse(Sys.info()[1]=='Windows', 'J:', '/home/j')
 
-#---------------------------
+#----------------------------------
 # define main directory
 
 dir = paste0(root, '/Project/Evaluation/GF/outcome_measurement/cod/dhis_data/')
 setwd(dir)
-#---------------------------
+
+#----------------------------------
 # source the necessary functions to download the data 
 # change the location of the sourced file to your home directory or source from J
-
+dirx = paste0(dir, "packages/")
 source(paste0(dir, 'code/dhis_extracting_functions.R')) 
 
 # check to make sure the package loaded by viewing a help file
 ?extract_all_data
 
-#---------------------------
-# Input the start year, end year, and output directory
-
+#----------------------------------
+# Notes on parameters:
+#----------------------------------
 # select the start year and end year for the download
-start_year = '2017'
-end_year = '2018'
-start_month = '01'
-end_month = '01' # start month is inclusive, end month is exclusive
+# start month is inclusive, end month is exclusive
+# update_year =  the update year should be before the data begins - default is 2009
 
-# change the update year to before the data begins
-update_year = '2009'
+# set = identify the data set(s) you want to download by number (list below)
 
-#identify the data set(s) you want to download by number (list below)
-set = 30
+# set_name = change set_name to the name of the data set you are downloading 
+# it will determine which folder the data is saved within
 
-# change set_name to the name of the data set you are downloading 
-# set_name will change the file names for saving the data
-set_name = 'pnls'
-
-#---------------------------
+#----------------------------------
 # available data sets by number: 
-
+#----------------------------------
 # 1: pMbC0FJPkcm A- Services de Base 
 # 2: maDtHIFrSHx B- Services Secondaires 
 # 3: pePpSnKtAh3 C1- SIGL1 
@@ -106,100 +99,106 @@ set_name = 'pnls'
 # 30: wIMw0dzITTs PNLS- Canevas Unique FOSA 
 # 31: jcaxCOQLeQC PNLT -  Rapport Trimestriel Tuberculose 2019 
 # 32: mV0r6yDCZy3 Population 
-                            
-
-#---------------------------
-
 #----------------------------------
-# set the country, base_url, username, and password 
-country = 'drc'
-base_url = 'https://snisrdc.com'
-userID = 'Bethany_Huntley'
-password = 'Snisrdcongo1'
-
-#------------------------------------------------
-# import the meta data for the download
-# only organizational units and data sets are necessary 
-# other meta data is used for the merge 
-
-data_sets = readRDS(paste0(dir, 'meta_data/data_sets.rds'))
-org_units = readRDS(paste0(dir, 'meta_data/org_units.rds'))
 
 #-----------------------------------------------
-
+# extract the data and save 
 #-----------------------------------------------
-# extract the data and save! 
-
-#------------------------
-# extract the data set and export as a RDS
-# click 'plots' tab to watch download progress
-# extract_all_data is a function in the dhisextractr package
-
-# loop through months to extract data
-start_date = paste0(start_year, '-', start_month, "-01")
-start_date = as.Date(start_date)
-
-end_date = paste0(end_year, '-', end_month, "-01")
-end_date = as.Date(end_date)
-
-dates = seq(start_date,end_date, by = 'month')
-
-# NOTE: make sure there is an "intermediate_data" folder within the data set folder to save the data too.  I have set older versions to be "intermediate_data_archive"
-# and then created a new "intermediate_data" folder, otherwise it will still work but will overwrite old files (maybe this is okay because we have the combined file from intermediate ones still?)
-# TO DO: someday will make this all happen through the code...
-# EXTRACTION LOOP-----------------
-for (i in 1:((length(dates))-1) ){
+run_extraction_tool = function(start_year = '2018', end_year = '2019', 
+                               start_month = '01', end_month = '12', 
+                               update_year = '2009', 
+                               set = '1', set_name = 'base') {
   
-  start_current_loop = dates[i]
-  end_current_loop = dates[i+1]
+  #----------------------------------
+  # set the country, base_url, username, and password 
+  country = 'drc'
+  base_url = 'https://snisrdc.com'
+  userID = 'Bethany_Huntley'
+  password = 'Snisrdcongo1'
   
-  # extract month of data
-  extracted_data = extract_all_data(base_url = base_url, 
-                                    data_sets = data_sets[set, ],
-                                    org_units = org_units, 
-                                    deb_period = start_current_loop,
-                                    end_period = end_current_loop,
-                                    userID = userID, 
-                                    password = password,
-                                    pace = 20,
-                                    update_date = paste0(update_year, '-01-01'))
+  #----------------------------------
+  # import the meta data for the download
+  # only organizational units and data sets are necessary 
+  # other meta data is used for the merge 
   
-  save_month_start = month(start_current_loop)
-  save_year_start = year(start_current_loop)
-  save_month_end = month(end_current_loop)
-  save_year_end = year(end_current_loop)
+  data_sets = readRDS(paste0(dir, 'meta_data/data_sets.rds'))
+  org_units = readRDS(paste0(dir, 'meta_data/org_units.rds'))
   
-  # save intermediate data - 1st iteration
-  extracted_data$download_number = 1
-  saveRDS(extracted_data, paste0(dir, '1_initial_download/', set_name, '/intermediate_data/', set_name,'_0', save_month_start, '_', 
-                                 save_year_start, '_0', save_month_end, '_', save_year_end, '_first_download.rds'))
+  #----------------------------------
   
+  #----------------------------------
+  # extract the data set and export as a RDS
+  # click 'plots' tab to watch download progress
+  # extract_all_data is a function in the dhisextractr package
   
-  # extract month again on subset of org_units
-  extracted_fac = unique(extracted_data$org_unit_ID) %>% as.character
-  org_units = org_units[!org_unit_ID %in% extracted_fac]
+  # loop through months to extract data
+  start_date = paste0(start_year, '-', start_month, "-01")
+  start_date = as.Date(start_date)
   
-  extracted_data2 = extract_all_data(base_url = base_url, 
-                                     data_sets = data_sets[set, ],
-                                     org_units = org_units, 
-                                     deb_period = start_current_loop,
-                                     end_period = end_current_loop,
-                                     userID = userID, 
-                                     password = password,
-                                     pace = 10,
-                                     update_date = paste0(update_year, '-01-01'))
+  end_date = paste0(end_year, '-', end_month, "-01")
+  end_date = as.Date(end_date)
   
-  # save intermediate data - 2nd download
-  if (nrow(extracted_data2)==0){ 
-    print(paste0("skipping iteration ", i, " second download (second download is empty)"))
-    next
-  } else {
-    extracted_data2$download_number = 2
-    saveRDS(extracted_data2,  paste0(dir, '1_initial_download/', set_name, '/intermediate_data/', set_name,'_0', save_month_start, '_', 
-                                     save_year_start, '_0', save_month_end, '_', save_year_end, '_second_download.rds'))
+  dates = seq(start_date,end_date, by = 'month')
+  
+  # NOTE: make sure there is an "intermediate_data" folder within the data set folder to save the data too.  I have set older versions to be "intermediate_data_archive"
+  # and then created a new "intermediate_data" folder, otherwise it will still work but will overwrite old files (maybe this is okay because we have the combined file from intermediate ones still?)
+  # TO DO: someday will make this all happen through the code...
+  #----------------------------------
+  # EXTRACTION LOOP------------------
+  #----------------------------------
+  for (i in 1:((length(dates))-1) ){
+    
+    start_current_loop = dates[i]
+    end_current_loop = dates[i+1]
+    
+    # extract month of data
+    extracted_data = extract_all_data(base_url = base_url, 
+                                      data_sets = data_sets[set, ],
+                                      org_units = org_units, 
+                                      deb_period = start_current_loop,
+                                      end_period = end_current_loop,
+                                      userID = userID, 
+                                      password = password,
+                                      pace = 20,
+                                      update_date = paste0(update_year, '-01-01'))
+    
+    save_month_start = month(start_current_loop)
+    save_year_start = year(start_current_loop)
+    save_month_end = month(end_current_loop)
+    save_year_end = year(end_current_loop)
+    
+    # save intermediate data - 1st iteration
+    extracted_data$download_number = 1
+    saveRDS(extracted_data, paste0(dir, '1_initial_download/', set_name, '/intermediate_data/', set_name,'_0', save_month_start, '_', 
+                                   save_year_start, '_0', save_month_end, '_', save_year_end, '_first_download.rds'))
+    
+    
+    # extract month again on subset of org_units
+    extracted_fac = unique(extracted_data$org_unit_ID) %>% as.character
+    org_units = org_units[!org_unit_ID %in% extracted_fac]
+    
+    extracted_data2 = extract_all_data(base_url = base_url, 
+                                       data_sets = data_sets[set, ],
+                                       org_units = org_units, 
+                                       deb_period = start_current_loop,
+                                       end_period = end_current_loop,
+                                       userID = userID, 
+                                       password = password,
+                                       pace = 10,
+                                       update_date = paste0(update_year, '-01-01'))
+    
+    # save intermediate data - 2nd download
+    if (nrow(extracted_data2)==0){ 
+      print(paste0("skipping iteration ", i, " second download (the re-download is empty)"))
+      next
+    } else {
+      extracted_data2$download_number = 2
+      saveRDS(extracted_data2,  paste0(dir, '1_initial_download/', set_name, '/intermediate_data/', set_name,'_0', save_month_start, '_', 
+                                       save_year_start, '_0', save_month_end, '_', save_year_end, '_second_download.rds'))
+    }
+    print(paste0("Loop ", i, " of ", (length(dates)-1), " complete" ))
   }
-  print(paste0("Loop ", i, " of ", (length(dates)-1), " complete" ))
 }
-#------------------------
+#-----------------------------------------------
 
 
---
+++ outcome_measurement/all/cod/dhis/master_script_dhis2_prep.R
@@ -0,0 +1,65 @@
+# Audrey Batzel
+# December 12, 2019
+
+# Master script to (hopefully) organize the DHIS2 prep process
+# Run on the cluster
+#---------------------------------------------
+
+#---------------------------------------------
+# Files and directories
+#---------------------------------------------
+library(data.table)
+library(lubridate)
+
+# clone the ihme gf repo to your H drive
+user = Sys.info()[['user']]
+setwd(paste0('/homes/', user))
+code_dir = ('./outcome_measurement/all/cod/dhis/')
+#---------------------------------------------
+
+#---------------------------------------------
+# Set switches
+#---------------------------------------------
+# Note: If you run step 1 - the actual download - it should be done on the cluster because it takes forever
+# Note: Step X - outlier detection - must also be run on the cluster
+rerun_metadata_extraction = FALSE
+
+step1_extract_data = TRUE
+step2_prep_data = TRUE
+step3_remove_outliers = TRUE
+
+# get current date for versioning 
+current_date = Sys.Date()
+current_month = month(current_date)
+current_year = year(current_date)
+#---------------------------------------------
+
+#---------------------------------------------
+# Step 1 - extract data using the DHIS2 download tool
+#---------------------------------------------
+if (step1_extract_data) { 
+  # Step 1a - download data
+  source(paste0(code_dir, 'extract_dhis/extract_data_dhis.R'))
+  
+  run_extraction_tool(start_year = '2018', end_year = '2019', start_month = '01', end_month = '12', 
+                      set = '1', set_name = 'base')
+
+  # Step 1b - combine intermediate data files
+  source(paste0(code_dir, 'extract_dhis/aggregate_extracted_data_dhis.R'))
+}
+#---------------------------------------------
+
+#---------------------------------------------
+# Step 2 - prep data
+#---------------------------------------------
+if (step2_prep_data) { }
+#---------------------------------------------
+
+#---------------------------------------------
+# Step 3 - outlier removal
+#---------------------------------------------
+if (step3_remove_outliers) { }
+#---------------------------------------------
+
+
+
