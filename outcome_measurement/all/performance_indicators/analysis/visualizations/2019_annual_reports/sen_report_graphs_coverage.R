20e9b6457baf74e6976c31f231efcd87dcf0562d
Francisco Rios Casas <frc2@uw.edu>
2020-02-10 16:01:00 -0800

    Synthesis & AR figure updates

--- outcome_measurement/all/performance_indicators/analysis/visualizations/2019_annual_reports/sen_report_graphs_coverage.R
+++ outcome_measurement/all/performance_indicators/analysis/visualizations/2019_annual_reports/sen_report_graphs_coverage.R
@@ -1,4 +1,4 @@
-# make visualizations on each of the grant performance indicators
+ # make visualizations on each of the grant performance indicators
 # for Sen
 # while creating a summary visual 
 # OCt 28, 2019
@@ -10,9 +10,10 @@ library(knitr)
 library(dplyr)
 library(kableExtra)
 library(scales)
+library(colormap)
 
 # load data
-data <- readRDS("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/data_for_analysis/kpi_data_for_analyses2.RDS")
+data <- readRDS("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/prepped_data/cleaned_pfi.RDS")
 
 # set parameters of data to be analyzed
 country = "sen"
@@ -41,18 +42,25 @@ dt_subset <- dt_subset[, .(avg_ach_ratio= mean(achievement_ratio_final, na.rm = 
 # reshape wide
 table1 <- dcast(dt_subset, grant ~ semester)
 
+# visualization settings
+# set colors
+colors = c('SEN-H-CNLS'=colormap()[1], 'SEN-H-ANCS'=colormap()[71] , 'SEN-M-PNLP'=colormap()[30], 'SEN-Z-MOH'=colormap()[61])
+
 # make graph of dt_subset
 a <- ggplot(dt_subset, aes(y=avg_ach_ratio, x=semester, group=grant))+
-  geom_line(aes(color=grant))+
-  geom_point(aes(color=grant))+
+  geom_line(size=1.5, aes(color=grant))+
   theme_bw(base_size = 18)+
   labs(y="Average Achievement Ratio",
        x="Semester")+
-  scale_x_continuous(limits = c(1,3), breaks = c(1,2,3))
+  scale_x_continuous(limits = c(1,3), breaks = c(1,2,3)) +
+  scale_y_continuous(limits = c(0,1.25), breaks = c(0.25, 0.5, 0.75, 1.00, 1.25))+
+  theme(panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank(), 
+        plot.title=element_text(size=11), axis.title.y=element_text(size=11), plot.caption=element_text(size=8))+
+  scale_color_manual('', values=colors)
 
 outputFile = "J:\\Project\\Evaluation\\GF\\outcome_measurement\\multi_country\\performance_indicators\\pudr_indicator_extraction\\visualizations\\sen_report_table_coverage_indic.csv"
 write.csv(table1, outputFile)
 
-pdf("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/visualizations/sen_report_achievement_cov_indic.pdf", width = 9, height = 6)
+pdf("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/analysis/visualizations/2019_annual_reports/sen_report_achievement_cov_indic.pdf", width = 9, height = 6)
 a
 dev.off()
--- outcome_measurement/all/performance_indicators/analysis/visualizations/2019_annual_reports/uga_report_table.R
+++ outcome_measurement/all/performance_indicators/analysis/visualizations/2019_annual_reports/uga_report_table.R
@@ -12,11 +12,11 @@ library(kableExtra)
 library(scales)
 
 # load data
-data <- readRDS("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/data_for_analysis/kpi_data_for_analyses2.RDS")
+data <- readRDS("J:/Project/Evaluation/GF/outcome_measurement/multi_country/performance_indicators/pudr_indicator_extraction/prepped_data/cleaned_pfi.RDS")
 
 # set parameters of data to be analyzed
 country = "uga"
-main_indicators = c('impact_outcome_indicators_main', 'coverage_indicators_main')
+main_indicators = c('coverage_indicators_main') # updated to only include coverage indicators
 
 # subset as appropriate
 DT <- data
@@ -27,20 +27,27 @@ DT$target_met[which(DT$target_met=="yes")] <-1
 DT$target_met[which(DT$target_met=="no")] <- 0
 DT$target_met <- as.numeric(DT$target_met)
 
-# table1 <- DT[,.(percent_met=sum(target_met, na.rm=TRUE)/length(indicator_code), by=c('grant'))]
-
 table2 <-DT[,list(indicators=length(indicator_code), indicators_reported=sum(!is.na(ihme_result_achievement_ratio)), total_met=sum(target_met, na.rm = TRUE)), by=c('grant', 'start_date_programmatic', 'end_date_programmatic')]
+
+
+# tablle3a <- 
+  
+# table 3 subsets the data only to columns of interest
 table3 <-table2[,.(grant, start_date_programmatic, end_date_programmatic, indicators, indicators_reported, total_met, percent_met_all=total_met/indicators, percent_met_reported=total_met/indicators_reported)]
 
-# add new variable for the PUDR
+# add new variable indicating which semester the PUDR corresponds to
 table3$reporting_period <- NA
 table3$reporting_period[which(table3$start_date_programmatic=="2019-01-01" & table3$end_date_programmatic=="2019-06-30")] <- 3
 table3$reporting_period[which(table3$start_date_programmatic=="2018-07-01" & table3$end_date_programmatic=="2018-12-31")] <- 2
 table3$reporting_period[which(table3$start_date_programmatic=="2018-01-01" & table3$end_date_programmatic=="2018-06-30")] <- 1
 
+# table 4 subsets only the variables: grant, reporting period, and how percent targets were met
 table4 <- table3[,.(grant, reporting_period, percent_met_reported)]
 
+# table 5 melts the data long (so each row represents a grant and semester)
 table5 <- melt(table4, id.vars = c('grant', 'reporting_period'))
+
+# table 6 casts the data wide (so each colunn represents a semester)
 table6 <- dcast(table5, grant ~ reporting_period)
 
 
@@ -48,5 +55,5 @@ table6 <- dcast(table5, grant ~ reporting_period)
 cols <- names(table6)[2:4]
 table6[,(cols) := round(.SD,2), .SDcols=cols]
          
-outputFile = "J:\\Project\\Evaluation\\GF\\outcome_measurement\\multi_country\\performance_indicators\\pudr_indicator_extraction\\visualizations\\uga_report_table.csv"
+outputFile = "J:\\Project\\Evaluation\\GF\\outcome_measurement\\multi_country\\performance_indicators\\pudr_indicator_extraction\\analysis\\visualizations\\2019_annual_reports\\uga_report_table_27Dec2019.csv"
 write.csv(table6, outputFile)
