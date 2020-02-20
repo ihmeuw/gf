08687f79b7fa085073feee81cb79513ac3b620d7
Emily Linebarger <eklinebarg@gmail.com>
2020-02-20 11:26:55 -0800

    Set up master script to read user name dynamically

--- resource_tracking/prep/1_master_file.r
+++ resource_tracking/prep/1_master_file.r
@@ -15,10 +15,11 @@ rm(list=ls())
 # ----------------------------------------------
 # STEP 1: SET UP R
 # ----------------------------------------------
+user = Sys.info()[7]
 if (Sys.info()[1]=='Windows'){
-  setwd("C:/Users/elineb/Documents/gf/") #Change to the root of your repository
+  setwd(paste0("C:/Users/", user, "/Documents/gf/")) #Change to the root of your repository
 } else {
-  setwd("/ihme/homes/elineb/gf/")
+  setwd("/ihme/homes/", user, "/gf/")
 }
 source("./resource_tracking/prep/_common/set_up_r.R", encoding="UTF-8")
 source("./resource_tracking/prep/_common/load_master_list.r", encoding="UTF-8")
@@ -36,7 +37,7 @@ prep_commitments = FALSE
 
 #Processing options 
 include_stops = TRUE #Set to true if you would like scripts to stop when errors are found (specifically, module mapping) Recommended to always leave as TRUE. 
-verbose = FALSE #Set to true if you would like warning messages printed (helpful for debugging functions). Urgent messages will always be flagged regardless of this switch. 
+verbose = TRUE #Set to true if you would like warning messages printed (helpful for debugging functions). Urgent messages will always be flagged regardless of this switch. 
 rerun_filelist = TRUE  #Set to TRUE if you want to prep all files in the file list again. 
 limit_filelist = TRUE #Set to TRUE if you want to only run files that will be saved in final budgets and expenditures. 
 only_new_files = FALSE # Set to true if, when you re-run file list, you only want to process files that are additional. TRUE is the default. 
