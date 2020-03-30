97b48dfc7dee0d06c3235f616c44754b51de44b6
Emily Linebarger <eklinebarg@gmail.com>
2020-03-30 13:49:48 -0700

    Updating 2f unit testing code to work with new data structure

--- resource_tracking/prep/2f_gf_verify_outputs.R
+++ resource_tracking/prep/2f_gf_verify_outputs.R
@@ -1,195 +1,166 @@
 # ----------------------------------------------------------------------
 # AUTHOR: Emily Linebarger, based on code by Naomi Provost. 
-# PURPOSE: Verify budget/expenditure totals for Global Fund budgets and pudrs, 
-#           and SICOIN data in Guatemala. 
+# PURPOSE: Verify budget/expenditure totals for Global Fund budgets and pudrs
 #         
-# DATE: Last updated December 2018. 
+# DATE: Last updated March 2020 
 # -----------------------------------------------------------------------
 
-#-------------------------
-#To do: 
-# - incorporate SICOIN checks again
-#--------------------------
-
+#------------------------------------
+# Set up file paths and globals
+#------------------------------------
 base_dir = paste0(box, "tableau_data/")
 unit_test_dir = paste0(paste0(j, "/Project/Evaluation/GF/resource_tracking/_gf_files_gos/unit_testing/"))
 
-# -----------------------
-# Country-level tests 
-# -----------------------
-loc_names = c('cod', 'gtm', 'uga', 'sen')
-countries = c('DRC', 'Guatemala', 'Uganda', 'Senegal')
+#--------------------------------
+# Read in data 
+#--------------------------------
+# We are currently testing most recent budgets, approved budgets from grant-making, most recent semester of absorption, and cumulative absorption. 
+# We may want to revisit this and make sure all files released to partners (all budget revisions, performance indicators) are being tested. EL 3/30/20
 
-all_failed_budgets = data.table() 
-all_failed_expenditures = data.table() 
-all_failed_absorption = data.table()
+# Tableau data 
+# budgets
+most_recent_budgets = fread(paste0(base_dir, "most_recent_budgets.csv"))
+approved_budgets = fread(paste0(base_dir, "approved_budgets.csv"))
+budgets = rbind(most_recent_budgets, approved_budgets)
+budgets = budgets[, .(budget=round(sum(budget, na.rm=T))), by=c('file_name', 'start_date')]
+budgets[, start_date:=as.Date(start_date, fmt="%Y-%m-%d")]
 
-all_untested_budgets = data.table() 
-all_untested_expenditures = data.table() 
-all_untested_absorption = data.table() 
-for (i in 1:length(loc_names)){
-  loc_name = loc_names[i]
-  country = countries[i]
-  print(paste0("TESTING ", country ))
-  print("...")
-  
-  budgets = readRDS(paste0(base_dir, "final_budgets_", loc_name, ".rds"))
-  budgets = budgets[data_source=="budget"] #Drop GOS from these tests. 
-  expenditures = readRDS(paste0(base_dir, "final_expenditures_", loc_name, ".rds"))
-  expenditures = expenditures[data_source=="pudr"]
-  absorption = readRDS(paste0(box, toupper(loc_name), "/prepped_data/absorption_", loc_name, ".rds"))
-  
-  budget_tests = read.xlsx(paste0(unit_test_dir, loc_name, "_tests.xlsx"), sheet="budget", detectDates=T)
-  expenditure_tests = read.xlsx(paste0(unit_test_dir, loc_name, "_tests.xlsx"), sheet="expenditure", detectDates=T)
-  absorption_tests = read.xlsx(paste0(unit_test_dir, loc_name, "_tests.xlsx"), sheet="absorption", detectDates=T)
-  
-  #----------------------------
-  # BUDGET
-  #---------------------------- 
-  budgets1 = budgets[, .(budget=sum(budget, na.rm=T)), by=c('file_name', 'start_date')] #Collapse budget file. 
-  budgets1 = merge(budgets1, budget_tests, by=c('file_name', 'start_date'), all=T)
-  budgets1[, correct_bug_sum:=round(correct_bug_sum)]
-  budgets1[, budget:=round(budget)]
-  
-  #Check to make sure all files are being tested. 
-  untested_files = unique(budgets1[is.na(correct_bug_sum), file_name])
-  tested_files = unique(budgets1[!is.na(correct_bug_sum), file_name])
-  untested_files = untested_files[!untested_files%in%tested_files]
-  if (length(untested_files)!=0){
-    print(paste0("Some budget files are not being tested for ", country, "."))
-    all_untested_budgets = rbind(all_untested_budgets, untested_files, fill=T)
-  }
-  
-  failed_budgets = budgets1[correct_bug_sum!=budget]
-  failed_budgets[, loc:=loc_name]
-  all_failed_budgets = rbind(all_failed_budgets, failed_budgets, fill=T)
- 
-  #----------------------------
-  # EXPENDITURE
-  #----------------------------
-  expenditures1 = expenditures[, .(expenditure=sum(expenditure, na.rm=T)), by=c('grant', 'grant_period', 'start_date')] #Collapse expenditure file. 
-  expenditures1 = merge(expenditures1, expenditure_tests, by=c('grant', 'grant_period', 'start_date'), all=T)
-  expenditures1[, correct_exp:=round(correct_exp)]
-  expenditures1[, expenditure:=round(expenditure)]
-  
-  #Check to make sure all files are being tested. 
-  untested_grants = unique(expenditures1[is.na(correct_exp), .(grant, grant_period, start_date)])
-  untested_grants[, concat:=paste0(grant, "_", grant_period, "_", start_date)]
-  expenditures1[!is.na(correct_exp), concat:=paste0(grant, "_", grant_period, "_", start_date)]
-  untested_grants = untested_grants[!concat%in%expenditures1$concat]
-  if (nrow(untested_grants)!=0){
-    print(paste0("Some expenditure numbers are not being tested for ", country, "."))
-    all_untested_expenditures = rbind(all_untested_expenditures, untested_grants, fill=T)
-  }
-  
-  failed_expenditures = expenditures1[correct_exp!=expenditure]
-  failed_expenditures[, loc:=loc_name]
-  all_failed_expenditures = rbind(all_failed_expenditures, failed_expenditures, fill=T) 
-  
-  #----------------------------
-  # ABSORPTION
-  #----------------------------
-  absorption1 = absorption[, .(budget=sum(budget, na.rm=T), expenditure=sum(expenditure, na.rm=T)), by=c('grant', 'grant_period', 'semester')] #Collapse absorption file. 
-  absorption1[, absorption:=(expenditure/budget)*100]
-  absorption1 = merge(absorption1, absorption_tests, by=c('grant', 'grant_period', 'semester'), all=T)
-  for (var in c('budget', 'expenditure', 'correct_budget', 'correct_expenditure')){
-    absorption1[, (var):=round(get(var))]
-  }
-  for (var in c('absorption', 'correct_absorption')){
-    absorption1[, (var):=round(get(var), 2)]
-  }
-  
-  #Check to make sure all files are being tested. 
-  untested_grants = unique(absorption1[is.na(correct_budget), .(grant, grant_period, semester)])
-  untested_grants[, concat:=paste0(grant, "_", grant_period, "_", semester)]
-  absorption1[!is.na(correct_budget), concat:=paste0(grant, "_", grant_period, "_", semester)]
-  untested_grants = untested_grants[!concat%in%absorption1$concat]
-  if (nrow(untested_grants)!=0){
-    print(paste0("Some absorption numbers are not being tested for ", country, "."))
-    all_untested_absorption = rbind(all_untested_absorption, untested_grants, fill=T)
-  }
-  
-  failed_absorption = absorption1[absorption!=correct_absorption | round(expenditure)!=round(correct_expenditure)]
-  failed_absorption[, loc:=loc_name]
-  all_failed_absorption = rbind(all_failed_absorption, failed_absorption, fill=T)
-  
+# Most recent absorption 
+most_recent_absorption = fread(paste0(base_dir, "most_recent_absorption.csv"))
+most_recent_absorption = most_recent_absorption[, .(budget=round(sum(budget, na.rm=T)), expenditure=round(sum(expenditure, na.rm=T))), by=c('grant', 'grant_period', 'start_date', 'end_date')]
+most_recent_absorption[, absorption:=round((expenditure/budget)*100)]
+most_recent_absorption[, start_date:=as.Date(start_date, fmt="%Y-%m-%d")]
+most_recent_absorption[, end_date:=as.Date(end_date, fmt="%Y-%m-%d")]
+
+# Cumulative absorption 
+cumulative_absorption = fread(paste0(base_dir, "cumulative_absorption.csv"))
+cumulative_absorption = cumulative_absorption[, .(cumulative_budget=round(sum(cumulative_budget, na.rm=T)), 
+                                                  cumulative_expenditure=round(sum(cumulative_expenditure, na.rm=T))), 
+                                              by=c('grant', 'grant_period', 'start_date', 'end_date')]
+cumulative_absorption[, cumulative_absorption:=round((cumulative_expenditure/cumulative_budget))]
+cumulative_absorption[, start_date:=as.Date(start_date, fmt="%Y-%m-%d")]
+cumulative_absorption[, end_date:=as.Date(end_date, fmt="%Y-%m-%d")]
+
+# Unit tests
+# Budget
+cod_budget_tests = data.table(read_xlsx(paste0(unit_test_dir, "cod_tests.xlsx"), sheet="budget"))
+gtm_budget_tests = data.table(read_xlsx(paste0(unit_test_dir, "gtm_tests.xlsx"), sheet="budget"))
+uga_budget_tests = data.table(read_xlsx(paste0(unit_test_dir, "uga_tests.xlsx"), sheet="budget"))
+sen_budget_tests = data.table(read_xlsx(paste0(unit_test_dir, "sen_tests.xlsx"), sheet="budget"))
+
+budget_tests = rbindlist(list(cod_budget_tests, gtm_budget_tests, uga_budget_tests, sen_budget_tests))
+budget_tests[, start_date:=as.Date(start_date)]
+budget_tests[, correct_bug_sum:=round(correct_bug_sum)]
+
+# Most recent absorption 
+cod_absorption_tests = data.table(read_xlsx(paste0(unit_test_dir, "cod_tests.xlsx"), sheet="absorption"))
+gtm_absorption_tests = data.table(read_xlsx(paste0(unit_test_dir, "gtm_tests.xlsx"), sheet="absorption"))
+uga_absorption_tests = data.table(read_xlsx(paste0(unit_test_dir, "uga_tests.xlsx"), sheet="absorption"))
+sen_absorption_tests = data.table(read_xlsx(paste0(unit_test_dir, "sen_tests.xlsx"), sheet="absorption"))
+
+absorption_tests = rbindlist(list(cod_absorption_tests, gtm_absorption_tests, uga_absorption_tests, sen_absorption_tests), fill=T)
+absorption_tests[, start_date:=as.Date(start_date)]
+absorption_tests[, end_date:=as.Date(end_date)]
+absorption_tests[, correct_budget:=round(correct_budget)]
+absorption_tests[, correct_expenditure:=round(correct_expenditure)]
+absorption_tests[, correct_absorption:=round(correct_absorption)]
+
+# Cumulative absorption 
+cod_cumulative_absorption_tests = data.table(read_xlsx(paste0(unit_test_dir, "cod_tests.xlsx"), sheet="cumulative_absorption"))
+gtm_cumulative_absorption_tests = data.table(read_xlsx(paste0(unit_test_dir, "gtm_tests.xlsx"), sheet="cumulative_absorption"))
+uga_cumulative_absorption_tests = data.table(read_xlsx(paste0(unit_test_dir, "uga_tests.xlsx"), sheet="cumulative_absorption"))
+sen_cumulative_absorption_tests = data.table(read_xlsx(paste0(unit_test_dir, "sen_tests.xlsx"), sheet="cumulative_absorption"))
+
+cumulative_absorption_tests = rbindlist(list(cod_cumulative_absorption_tests, gtm_cumulative_absorption_tests, uga_cumulative_absorption_tests, sen_cumulative_absorption_tests), fill=T)
+cumulative_absorption_tests[, start_date:=as.Date(start_date)]
+cumulative_absorption_tests[, end_date:=as.Date(end_date)]
+cumulative_absorption_tests[, correct_cumulative_budget:=round(correct_cumulative_budget)]
+cumulative_absorption_tests[, correct_cumulative_expenditure:=round(correct_cumulative_expenditure)]
+cumulative_absorption_tests[, correct_cumulative_absorption:=round(correct_cumulative_absorption)]
+
+#----------------------------
+# TEST BUDGETS 
+#---------------------------- 
+print("Testing budgets...")
+merged_budgets = merge(budgets, budget_tests, by=c('file_name', 'start_date'), all.x=T)
+
+#Check to make sure all files are being tested. 
+all_untested_budgets = unique(merged_budgets[is.na(correct_bug_sum), file_name])
+all_tested_budgets = unique(merged_budgets[!is.na(correct_bug_sum), file_name])
+all_untested_budgets = all_untested_budgets[!all_untested_budgets%in%all_tested_budgets]
+if (length(all_untested_budgets)!=0){
+  print("ERROR: There are untested budgets in final data. Review 'all_untested_budgets'.")
 }
 
-# ------------------
-# RSSH tests
-# ------------------
-# print("...")
-# rssh_tests <- fread(paste0(dir, "_gf_files_gos/rssh_tests.csv"))
-# rssh_by_rt_code <- all_budgets[substring(code, 1, 1) == 'R']
-# rssh_by_rt_code = rssh_by_rt_code[, .(rt_code_rssh = round(sum(budget, na.rm = TRUE))), by = c('file_name')]
-# rssh_by_rt_code[is.na(rt_code_rssh), rt_code_rssh:=0]
-# 
-# check_rssh <- merge(rssh_tests, rssh_by_rt_code, by = c('file_name'), all.y = TRUE)
-# unwritten_rssh_tests = check_rssh[is.na(correct_rssh)]
-# 
-# #unmerged_tests = anti_join(rssh_tests, rssh_by_rt_code, by='file_name')
-# if(nrow(unwritten_rssh_tests)!=0){
-#   print("ERROR: Some files are don't have a unit test written for RSSH. Review 'unwritten rssh tests'. ")
-# }
-# check_rssh = check_rssh[!is.na(correct_rssh)]
-# 
-# failed_rssh_tests = check_rssh[correct_rssh!=rt_code_rssh]
+all_failed_budgets = merged_budgets[correct_bug_sum!=budget]
+if (length(all_failed_budgets)!=0){
+  print("ERROR: Budget unit tests failed. Review 'all_failed_budgets'.")
+}
 
-# ------------------
-# GOS Tests 
-# ------------------
+#----------------------------
+# TEST MOST RECENT ABSORPTION 
+#----------------------------
+print("Testing most recent absorption...")
+merged_absorption = merge(most_recent_absorption, absorption_tests, by=c('grant', 'grant_period', 'start_date', 'end_date'), all.x=T)
 
-# gos_tests = fread(paste0(dir, "_gf_files_gos/gos/gos_tests.csv"))
-# 
-# gos_rssh = gos_data[substring(code, 1, 1)=='R']
-# gos_rssh = gos_rssh[, .(gos_rssh = round(sum(budget, na.rm = TRUE))), by=c('grant')]
-# 
-# gos_data[is.na(budget), budget:=0]
-# gos_data[is.na(expenditure), expenditure:=0]
-# gos_data = gos_data[ , 
-#               lapply(.SD, sum) , 
-#               by = 'grant', 
-#               .SDcols = c("budget", "expenditure")]
-# gos_data <- unique(gos_data)
-# gos_data[, budget:=round(budget)]
-# gos_data[, expenditure:=round(expenditure)]
-# 
-# gos_data = merge(gos_data, gos_rssh, by='grant', all = TRUE)
-# gos_merge = merge(gos_data, gos_tests, by='grant', all.x = TRUE)
-# 
-# #Find failed tests and untested grants 
-# untested_gos = gos_merge[is.na(correct_bug_sum)]
-# if (nrow(untested_gos)!=0){
-#   print("ERROR: Some GOS grants don't have tests written for them. Review 'untested gos'.")
-# }
-# gos_merge = gos_merge[!is.na(correct_bug_sum)] #Only review failures for tests you've written and merged. 
-# failed_gos_rssh = gos_merge[gos_rssh!=correct_rssh]
-# failed_tests_gos = gos_merge[correct_bug_sum!=budget | correct_exp_sum != expenditure]
+#Check to make sure all files are being tested. 
+all_untested_absorption = unique(merged_absorption[is.na(correct_budget), .(grant, grant_period, start_date, end_date)])
+if (length(all_untested_absorption)!=0){
+  print("ERROR: There are untested absorption numbers. Review 'all_untested_absorption'.")
+}
 
+all_failed_absorption = merged_absorption[budget!=correct_budget | expenditure!=correct_expenditure | absorption!=correct_absorption]
+if (nrow(all_failed_absorption)!=0){
+  print("ERROR: Absorption unit tests failed. Review 'all_failed_absorption'.")
+}
+
+#----------------------------
+# TEST CUMULATIVE ABSORPTION 
+#----------------------------
+print("Testing cumulative absorption...")
+merged_cumul_absorption = merge(cumulative_absorption, cumulative_absorption_tests, by=c('grant', 'grant_period', 'start_date', 'end_date'), all.x=T)
+
+#Check to make sure all files are being tested. 
+all_untested_cumul_absorption = unique(merged_cumul_absorption[is.na(correct_cumulative_budget), .(grant, grant_period, start_date, end_date)])
+if (length(all_untested_cumul_absorption)!=0){
+  print("ERROR: There are untested absorption numbers. Review 'all_untested_cumul_absorption'.")
+}
+
+all_failed_cumul_absorption = merged_cumul_absorption[cumulative_budget!=correct_cumulative_budget |
+                                                        cumulative_expenditure!=correct_cumulative_expenditure | 
+                                                        cumulative_absorption!=correct_cumulative_absorption]
+if (nrow(all_failed_cumul_absorption)!=0){
+  print("ERROR: Absorption unit tests failed. Review 'all_failed_cumul_absorption'.")
+}
 # ------------------------------------
 # Print results and summary statistics
 # ------------------------------------
-{
 
-  #Country statistics 
-  print("...")
-  print("...")
-  print("UNIT TEST RESULTS") 
-  print(paste0("Total number of failed budget tests: ", nrow(all_failed_budgets)))
-  print(paste0("Total number of failed expenditure tests: ", nrow(all_failed_expenditures)))
-  print(paste0("Total number of failed absorption tests: ", nrow(all_failed_absorption)))
-  print("...")
-  
-  # Flag if any files did not merge 
-  if (nrow(all_untested_budgets) + nrow(all_untested_expenditures) + nrow(all_untested_absorption)>0){
-    print("SOME FINAL DATA IS NOT BEING TESTED; REVIEW UNTESTED FILES.")
-  }
-  
-  #Flag if any unit tests have failed. 
-  if (nrow(all_failed_budgets) + nrow(all_failed_expenditures) + nrow(all_failed_absorption)>0){
-    print("UNIT TESTS FAILED; REVIEW PREP CODE")
-  }
+#Summary statistics 
+print("...")
+print("...")
+print("UNIT TEST RESULTS") 
+print(paste0("Total number of failed budget tests: ", nrow(all_failed_budgets)))
+print(paste0("Total number of failed absorption tests: ", nrow(all_failed_absorption)+nrow(all_failed_cumul_absorption)))
+print("...")
 
+any_tests_missing = ((length(all_untested_budgets) + nrow(all_untested_absorption) + nrow(all_untested_cumul_absorption))>0)
+any_failures = ((nrow(all_failed_budgets) + nrow(all_failed_absorption) + nrow(all_failed_cumul_absorption))>0)
 
-
+# Flag if any files did not merge 
+if (any_tests_missing){
+  print("SOME FINAL DATA IS NOT BEING TESTED; REVIEW UNTESTED FILES.")
 }
+
+#Flag if any unit tests have failed. 
+if (any_failures){
+  print("UNIT TESTS FAILED; REVIEW PREP CODE")
+}
+
+if (!any_failures & !any_tests_missing) {
+  print("ALL UNIT TESTS BUILT AND PASSING.")
+}
+
+print("Step 2F: Verify outputs completed.")
+
+
