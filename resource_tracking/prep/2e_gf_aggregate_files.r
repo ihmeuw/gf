35ec1b13f3ddd891ac402f931eab8394aef8c669
Audrey Batzel <abatzel@uw.edu>
2020-06-11 16:36:35 -0700

    add activity level aggregate output

--- resource_tracking/prep/2e_gf_aggregate_files.r
+++ resource_tracking/prep/2e_gf_aggregate_files.r
@@ -62,13 +62,13 @@ write.csv(most_recent_budgets, paste0(final_write, "most_recent_budgets.csv"), r
 #---------------------------------------------
 # 3. ALL BUDGET REVISIONS
 #---------------------------------------------
-cod3 = fread(paste0(cod_prepped, list.files(cod_prepped, pattern="all_budget_revisions")))
+cod3 = fread(paste0(cod_prepped, list.files(cod_prepped, pattern="all_budget_revisions_cod")))
 cod3[, loc_name:="DRC"]
-gtm3 = fread(paste0(gtm_prepped, list.files(gtm_prepped, pattern="all_budget_revisions")))
+gtm3 = fread(paste0(gtm_prepped, list.files(gtm_prepped, pattern="all_budget_revisions_gtm")))
 gtm3[, loc_name:="Guatemala"]
-uga3 = fread(paste0(uga_prepped, list.files(uga_prepped, pattern="all_budget_revisions")))
+uga3 = fread(paste0(uga_prepped, list.files(uga_prepped, pattern="all_budget_revisions_uga")))
 uga3[, loc_name:="Uganda"]
-sen3 = fread(paste0(sen_prepped, list.files(sen_prepped, pattern="all_budget_revisions")))
+sen3 = fread(paste0(sen_prepped, list.files(sen_prepped, pattern="all_budget_revisions_sen")))
 sen3[, loc_name:="Senegal"]
 
 all_budget_revisions = rbindlist(list(cod3, gtm3, uga3, sen3), use.names = TRUE, fill = TRUE)
@@ -77,10 +77,26 @@ all_budget_revisions = rbindlist(list(cod3, gtm3, uga3, sen3), use.names = TRUE,
 # all_budget_revisions_long = all_budget_revisions[,.(grant, grant_period, gf_module, gf_intervention, disease, start_date, budget, budget_version, loc_name)] # subset to key variables
 # dcast(all_budget_revisions_long, grant + grant_period + gf_module + gf_intervention + disease + start_date + loc_name ~ budget_version, value.var = "budget" ) # cast wide
 
-
 all_budget_revisions = merge(all_budget_revisions, topic_areas, all.x = TRUE, by = c('loc_name', 'disease', 'gf_module', 'gf_intervention'))
 
 write.csv(all_budget_revisions, paste0(final_write, "all_budget_revisions.csv"), row.names=F)
+
+# -------------
+# activity level budget revisions
+cod3_act = fread(paste0(cod_prepped, list.files(cod_prepped, pattern="all_budget_revisions_act")))
+cod3_act[, loc_name:="DRC"]
+gtm3_act = fread(paste0(gtm_prepped, list.files(gtm_prepped, pattern="all_budget_revisions_act")))
+gtm3_act[, loc_name:="Guatemala"]
+uga3_act = fread(paste0(uga_prepped, list.files(uga_prepped, pattern="all_budget_revisions_act")))
+uga3_act[, loc_name:="Uganda"]
+sen3_act = fread(paste0(sen_prepped, list.files(sen_prepped, pattern="all_budget_revisions_act")))
+sen3_act[, loc_name:="Senegal"]
+
+all_budget_revisions_act = rbindlist(list(cod3_act, gtm3_act, uga3_act, sen3_act), use.names = TRUE, fill = TRUE)
+
+all_budget_revisions_act = merge(all_budget_revisions_act, topic_areas, all.x = TRUE, by = c('loc_name', 'disease', 'gf_module', 'gf_intervention'))
+
+write.csv(all_budget_revisions_act, paste0(final_write, "all_budget_revisions_activityLevel.csv"), row.names=F)
 #---------------------------------------------
 # 4. CURRENT ABSORPTION 
 #---------------------------------------------
