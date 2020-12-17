76ecf1ac05752893d477a6b10202fc46f938dcc6
Audrey Batzel <abatzel@ihme.washington.edu>
2020-12-17 11:00:27 -0800

    Update synthesis_ch3_heatmap_rssh.R

--- synthesis2020/synthesis_ch3_heatmap_rssh.R
+++ synthesis2020/synthesis_ch3_heatmap_rssh.R
@@ -64,28 +64,64 @@ dt[gf_module == 'Procurement and supply chain management systems', gf_module := 
 # calculate each module % of total
 dt[, rssh_total_by_country := sum(budget), by = c('loc_name')]
 dt[, module_percent_of_total_rssh := round((budget/rssh_total_by_country)*100, 2)]
-dt = dt[module_percent_of_total_rssh > 0, ]
+dt = dt[budget > 0]
+# -------------------------------------------------------------------
+# add in table of indicators for captions: 
+indicators = as.data.table(read_xlsx('J:/Project/Evaluation/GF/resource_tracking/visualizations2020/Synthesis/table in ch3 on rssh indicators.xlsx'))
 
-# order and limit to match what is in the synthesis report
-dt[, gf_module := factor(gf_module, levels = c('Health management information systems and M&E', 
-                                 'Health products management systems',
-                                 'Integrated service delivery and quality improvement',
-                                 'Community systems strengthening',
-                                 'Health sector governance and planning',
-                                 'Human resources for health, including community health workers',
-                                 'Laboratory systems',
-                                 'Financial management systems'))]
+# reshape
+indicators = melt.data.table(indicators, id.vars = c('Module', 'RSSH_indicator_NFM3'), variable.name = 'country', value.name = 'indicator_present' )
+indicators = indicators[indicator_present == 1, ]
 
+mod_country = unique(indicators[, .(Module, country)])
+mod_country[, indicators := '']
+
+# can probably make this into an lapply one-liner? 
+for(i in 1:nrow(mod_country)){
+  mod= mod_country[i, Module]
+  c= mod_country[i, country]
+  
+  list_of_indicators = indicators[Module == mod & country == c, unique(RSSH_indicator_NFM3)]
+  mod_country[Module == mod & country == c, indicators := paste(list_of_indicators, collapse = ',\n')]
+}
+
+# merge with dt of budgets
+mod_country[, country := as.character(country)]
+plot_dt = merge(dt, mod_country, by.x = c('loc_name', 'gf_module'), by.y = c('country', 'Module'), all = TRUE)
+plot_dt[is.na(indicators), indicators := '']
+
+# shorten column names - is there a better way to do this with data table?
+plot_dt[gf_module == 'Health management information systems and M&E', plot_module := 'HMIS and M&E']
+plot_dt[gf_module == 'Health products management systems', plot_module := 'Health products\nmanagement systems']
+plot_dt[gf_module == 'Integrated service delivery and quality improvement', plot_module := 'Int. service delivery\nand QE']
+plot_dt[gf_module == 'Community systems strengthening', plot_module := 'CSS']
+plot_dt[gf_module == 'Health sector governance and planning', plot_module := 'Health sector gov.\nand planning']
+plot_dt[gf_module == 'Human resources for health, including community health workers', plot_module := 'HRH, incl. CHWs']
+plot_dt[gf_module == 'Laboratory systems', plot_module := 'Laboratory systems']
+plot_dt[gf_module == 'Financial management systems', plot_module := 'Financial management\nsystems']
+
+# order to match what is in the synthesis report
+plot_dt[, plot_module := factor(plot_module, levels = c('HMIS and M&E', 
+                                                    'Health products\nmanagement systems',
+                                                    'Int. service delivery\nand QE',
+                                                    'CSS',
+                                                    'Health sector gov.\nand planning',
+                                                    'HRH, incl. CHWs',
+                                                    'Laboratory systems',
+                                                    'Financial management\nsystems'))]
+
+# -------------------------------------------------------------------
 # plot into a heatmap
-g = ggplot(dt, aes(loc_name, gf_module, fill= module_percent_of_total_rssh)) + geom_tile() + theme_bw() + 
-  scale_x_discrete(position = 'top') + labs( x = 'Country', y = "RSSH-related Module", fill = "% of Country's Total \nRSSH Spending") + 
+g = ggplot(plot_dt, aes(loc_name, plot_module, fill= module_percent_of_total_rssh)) + geom_tile() + theme_bw() + 
+  scale_x_discrete(position = 'top') + labs( x = '', y = "", fill = "% of Country's Total \nRSSH Spending") + 
   scale_fill_continuous(high = "#132B43", low = "#9fd4fc") +
-  scale_y_discrete(limits = rev(levels(dt$gf_module))) +
-  geom_text(aes(label= paste0(dt$module_percent_of_total_rssh, '%')), color = '#FFFFFF')
+  scale_y_discrete(limits = rev(levels(plot_dt$plot_module))) +
+  theme(axis.text=element_text(size=12), legend.text = element_text(size=11), legend.title = element_text(size = 13)) +
+  geom_text(aes(label= paste0(plot_dt$module_percent_of_total_rssh, '%', '\n', plot_dt$indicators)), size = 3.5, color = '#FFFFFF')
   # scale_fill_continuous(type = 'viridis') 
   # 56B1F7 - alternate/close to default color 
   
-pdf(outFile, height = 9, width = 14)
+pdf(outFile, height = 9, width = 12)
 print(g)
 dev.off()
 
